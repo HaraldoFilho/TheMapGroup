19d47e2571dd599d714c3fc0ff19e1dc0b3b1f3f
The Map Group <admin@the-map-group.pictures>
2020-07-30 08:22:53 -0300

    Updated Maps

---
+++ people/setup-user.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir /home/pi/github/the-map-group.github.io/people/$1
+cd /home/pi/github/the-map-group.github.io/people/$1
+ln -s ../../api_credentials.py .
+ln -s ../../mapbox_token .
+ln -s /home/pi/flickr_map/header.html .
+ln -P /home/pi/flickr_map/generate-map.py
+touch index.html
+ln -s index.html map.html
+echo "user = '$1'" > config.py
+echo "photoset_id = ''" >> config.py
+echo "photo_privacy = 1" >> config.py
+echo "geo_privacy = 1" >> config.py
+echo "dont_map_tag = 'DontMap'" >> config.py
+
