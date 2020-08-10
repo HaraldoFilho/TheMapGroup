46921ce4d91f2f66d77b2507d42c9d2a7f7bcc8b
The Map Group <admin@the-map-group.pictures>
2020-08-10 17:33:11 -0300

    Updated map for member 'haroldmoses'

--- people/mosesharold/locations.js
+++
@@ -1,5 +0,0 @@
-var locations = [
-    [[-61.283111, 15.535564], '<div style="max-height:410px;overflow:auto;"><a href="https://www.flickr.com/photos/mosesharold/5314011192" target="_blank"><img src="https://live.staticflickr.com/5289/5314011192_98c2e65f62_s.jpg"/></a> </div>', 1],
-    [[-61.389541, 15.292464], '<div style="max-height:410px;overflow:auto;"><a href="https://www.flickr.com/photos/mosesharold/5314020612" target="_blank"><img src="https://live.staticflickr.com/5166/5314020612_5ff0bb7599_s.jpg"/></a> </div>', 1],
-    [[-61.402416, 15.295776], '<div style="max-height:410px;overflow:auto;"><a href="https://www.flickr.com/photos/mosesharold/5313438413" target="_blank"><img src="https://live.staticflickr.com/5088/5313438413_7405de11b1_s.jpg"/></a> </div>', 1],
-    [[-61.864013, 14.328259], '<div style="max-height:410px;overflow:auto;"><a href="https://www.flickr.com/photos/mosesharold/5313452559" target="_blank"><img src="https://live.staticflickr.com/5162/5313452559_4b1c3ecc20_s.jpg"/></a> </div>', 1],
