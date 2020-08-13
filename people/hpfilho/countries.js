cd07bdb1acb6adafd311c8d2998dcc7ef108da9f
The Map Group <admin@the-map-group.pictures>
2020-08-12 22:01:25 -0300

    Updated map for member 'H. P. Filho'

--- people/hpfilho/countries.js
+++ people/hpfilho/countries.js
@@ -1,9 +1,9 @@
 var countries = [
-  ['BR', 176, 395],
-  ['US', 51, 132],
-  ['AR', 20, 31],
-  ['PY', 1, 1],
-  ['HR', 184, 243],
-  ['IT', 76, 125],
-  ['VA', 22, 31]
+  ['BR', 'Brazil', 176, 395],
+  ['US', 'United States of America', 51, 132],
+  ['AR', 'Argentina', 20, 31],
+  ['PY', 'Paraguay', 1, 1],
+  ['HR', 'Croatia', 184, 243],
+  ['IT', 'Italy', 76, 125],
+  ['VA', 'Vatican City', 22, 31]
 ]
