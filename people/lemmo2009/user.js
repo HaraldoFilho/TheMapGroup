6e642aeba9ec19e07ad40cf79019b8c8396653a5
The Map Group <admin@the-map-group.pictures>
2020-08-14 10:47:16 -0300

    Updated map for member 'Lemmo2009'

--- people/lemmo2009/user.js
+++ people/lemmo2009/user.js
@@ -1,6 +1,6 @@
 var user_info = {
   "id": "40749798@N07",
-  "name": "Lemmo2009",
+  "name": "Paul",
   "avatar": "https://live.staticflickr.com/5674/buddyicons/40749798@N07_r.jpg",
   "url": "https://www.flickr.com/photos/lemmo2009/",
   "markers": 4554,
