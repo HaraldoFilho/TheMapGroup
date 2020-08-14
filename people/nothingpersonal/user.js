6fd341fdf72d3a0242572c6c532448dfc99305c0
The Map Group <admin@the-map-group.pictures>
2020-08-14 14:01:49 -0300

    Updated 'nothingpersonal' icon

--- people/nothingpersonal/user.js
+++ people/nothingpersonal/user.js
@@ -1,7 +1,7 @@
 var user_info = {
   "id": "46721940@N00",
   "name": "Dmitry Djouce",
-  "avatar": "../../map.png",
+  "avatar": "../photographer.svg",
   "url": "https://www.flickr.com/photos/nothingpersonal/",
   "markers": 2308,
   "photos": 2641,
