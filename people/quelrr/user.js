74feafc20b01bef6897e498085917bd508e2d606
The Map Group <admin@the-map-group.pictures>
2020-08-14 11:05:02 -0300

    Updated map for member 'Quel.R.R'

--- people/quelrr/user.js
+++ people/quelrr/user.js
@@ -1,6 +1,6 @@
 var user_info = {
   "id": "132838898@N03",
-  "name": "Quel.R.R",
+  "name": "Raquel Rampasio",
   "avatar": "https://live.staticflickr.com/5674/buddyicons/132838898@N03_r.jpg",
   "url": "https://www.flickr.com/photos/quelrr/",
   "markers": 25,
