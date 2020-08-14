5290b3cb7b494eec225ec8c6c82e76175b0fb600
The Map Group <admin@the-map-group.pictures>
2020-08-14 11:08:00 -0300

    Updated map for member 'bobbyblack51'

--- people/57727182@N08/user.js
+++ people/57727182@N08/user.js
@@ -1,6 +1,6 @@
 var user_info = {
   "id": "57727182@N08",
-  "name": "bobbyblack51",
+  "name": "GEORGE",
   "avatar": "https://live.staticflickr.com/5674/buddyicons/57727182@N08_r.jpg",
   "url": "https://www.flickr.com/photos/57727182@N08/",
   "markers": 387,
