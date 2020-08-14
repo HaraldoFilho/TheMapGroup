6faccc32fa30a14f7df7075257c1742e72c2292f
The Map Group <admin@the-map-group.pictures>
2020-08-14 11:08:15 -0300

    Updated map for member 'Jason Frels'

--- people/jasonfrels/user.js
+++ people/jasonfrels/user.js
@@ -1,6 +1,6 @@
 var user_info = {
   "id": "76199006@N05",
-  "name": "Jason Frels",
+  "name": "Jason",
   "avatar": "https://live.staticflickr.com/5674/buddyicons/76199006@N05_r.jpg",
   "url": "https://www.flickr.com/photos/jasonfrels/",
   "markers": 46,
