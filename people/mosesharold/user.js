fc88f88d6db02ba8720d1ac25b4fa31dc2f23874
The Map Group <admin@the-map-group.pictures>
2020-08-14 10:45:33 -0300

    Updated map for member 'haroldmoses'

--- people/mosesharold/user.js
+++ people/mosesharold/user.js
@@ -1,6 +1,6 @@
 var user_info = {
   "id": "57729348@N02",
-  "name": "haroldmoses",
+  "name": "Harold  Moses",
   "avatar": "https://live.staticflickr.com/5674/buddyicons/57729348@N02_r.jpg",
   "url": "https://www.flickr.com/photos/mosesharold/",
   "markers": 2281,
