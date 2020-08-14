cfacf1b81d2edabcca0a3675547846be8b940d6e
The Map Group <admin@the-map-group.pictures>
2020-08-14 11:04:30 -0300

    Updated map for member 'Susie_D52'

--- people/susannemichelle/user.js
+++ people/susannemichelle/user.js
@@ -1,6 +1,6 @@
 var user_info = {
   "id": "21593175@N00",
-  "name": "Susie_D52",
+  "name": "Susie",
   "avatar": "https://live.staticflickr.com/5674/buddyicons/21593175@N00_r.jpg",
   "url": "https://www.flickr.com/photos/susannemichelle/",
   "markers": 149,
