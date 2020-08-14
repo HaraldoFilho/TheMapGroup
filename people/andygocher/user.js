a66da890a4e1f61b616d590d710914f761391044
The Map Group <admin@the-map-group.pictures>
2020-08-14 10:54:42 -0300

    Updated map for member 'Andy.Gocher'

--- people/andygocher/user.js
+++ people/andygocher/user.js
@@ -1,6 +1,6 @@
 var user_info = {
   "id": "110839836@N07",
-  "name": "Andy.Gocher",
+  "name": "Andy Gocher",
   "avatar": "https://live.staticflickr.com/5674/buddyicons/110839836@N07_r.jpg",
   "url": "https://www.flickr.com/photos/andygocher/",
   "markers": 561,
