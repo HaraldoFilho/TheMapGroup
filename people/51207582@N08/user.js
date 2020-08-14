85e70a0a714e186aa90e787723da05098b7c9389
The Map Group <admin@the-map-group.pictures>
2020-08-14 10:44:44 -0300

    Updated map for member 'GaryC4'

--- people/51207582@N08/user.js
+++ people/51207582@N08/user.js
@@ -1,6 +1,6 @@
 var user_info = {
   "id": "51207582@N08",
-  "name": "GaryC4",
+  "name": "Gary Cadman",
   "avatar": "https://live.staticflickr.com/5674/buddyicons/51207582@N08_r.jpg",
   "url": "https://www.flickr.com/photos/51207582@N08/",
   "markers": 2576,
