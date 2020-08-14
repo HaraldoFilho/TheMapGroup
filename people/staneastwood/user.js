5844038768b7e9c5c2a00bada5c97fdaa33590fe
The Map Group <admin@the-map-group.pictures>
2020-08-14 11:13:46 -0300

    Updated map for member '4.1 mil views - Thank you all.'

--- people/staneastwood/user.js
+++ people/staneastwood/user.js
@@ -1,6 +1,6 @@
 var user_info = {
   "id": "63713558@N00",
-  "name": "4.1 mil views - Thank you all.",
+  "name": "Stanley Eastwood",
   "avatar": "https://live.staticflickr.com/5674/buddyicons/63713558@N00_r.jpg",
   "url": "https://www.flickr.com/photos/staneastwood/",
   "markers": 4920,
