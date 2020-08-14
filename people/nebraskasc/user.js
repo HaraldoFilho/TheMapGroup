b2c8b41ce71718cea45e8c9f6d09e1525e832462
The Map Group <admin@the-map-group.pictures>
2020-08-14 12:21:48 -0300

    Updated map for member 'NebraskaSC Severe Weather Photography Videography'

--- people/nebraskasc/user.js
+++ people/nebraskasc/user.js
@@ -4,5 +4,5 @@ var user_info = {
   "avatar": "https://live.staticflickr.com/5674/buddyicons/14320768@N07_r.jpg",
   "url": "https://www.flickr.com/photos/nebraskasc/",
   "markers": 1911,
-  "photos": 17653,
+  "photos": 17654,
 }
