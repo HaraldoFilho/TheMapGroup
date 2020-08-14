457a47e63703013b98b42ad3eb42201ebc983338
The Map Group <admin@the-map-group.pictures>
2020-08-14 10:59:14 -0300

    Updated map for member 'ManxieB- prefer comments!'

--- people/barbaraelaw/user.js
+++ people/barbaraelaw/user.js
@@ -1,6 +1,6 @@
 var user_info = {
   "id": "25404639@N08",
-  "name": "ManxieB- prefer comments!",
+  "name": "Barbara Lawrence",
   "avatar": "https://live.staticflickr.com/5674/buddyicons/25404639@N08_r.jpg",
   "url": "https://www.flickr.com/photos/barbaraelaw/",
   "markers": 2349,
