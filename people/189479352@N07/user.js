bfb34d20632cb02eef62e3ec11607e6576f19a87
The Map Group <admin@the-map-group.pictures>
2020-08-14 11:15:24 -0300

    Updated map for member 'jklovelacephotography'

--- people/189479352@N07/user.js
+++ people/189479352@N07/user.js
@@ -1,6 +1,6 @@
 var user_info = {
   "id": "189479352@N07",
-  "name": "jklovelacephotography",
+  "name": "Jeffrey Lovelace",
   "avatar": "https://live.staticflickr.com/5674/buddyicons/189479352@N07_r.jpg",
   "url": "https://www.flickr.com/photos/189479352@N07/",
   "markers": 12,
