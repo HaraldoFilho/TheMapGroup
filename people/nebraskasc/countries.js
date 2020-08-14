7bc5a43ecac6bb6741637a67ded473e65c8a4317
The Map Group <admin@the-map-group.pictures>
2020-08-14 00:33:05 -0300

    Updated map for member 'NebraskaSC Severe Weather Photography Videography'

--- people/nebraskasc/countries.js
+++ people/nebraskasc/countries.js
@@ -1,3 +1,3 @@
 var countries = [
-  ['US', 'United States of America', 1907, 17336]
+  ['US', 'United States of America', 1908, 17337]
 ]
--- people/nebraskasc/user.js
+++ people/nebraskasc/user.js
@@ -3,6 +3,6 @@ var user_info = {
   "name": "NebraskaSC Severe Weather Phot",
   "avatar": "https://live.staticflickr.com/5674/buddyicons/14320768@N07_r.jpg",
   "url": "https://www.flickr.com/photos/nebraskasc/",
-  "markers": 1907,
-  "photos": 17336,
+  "markers": 1908,
+  "photos": 17650,
 }
