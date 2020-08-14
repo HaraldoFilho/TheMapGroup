64de14fcade1955a7fabb4735229b023f23a5abd
The Map Group <admin@the-map-group.pictures>
2020-08-14 11:02:17 -0300

    Updated map for member 'grundi1'

--- people/grundi1/user.js
+++ people/grundi1/user.js
@@ -1,6 +1,6 @@
 var user_info = {
   "id": "7182681@N07",
-  "name": "grundi1",
+  "name": "Christian Grundner",
   "avatar": "https://live.staticflickr.com/5674/buddyicons/7182681@N07_r.jpg",
   "url": "https://www.flickr.com/photos/grundi1/",
   "markers": 224,
