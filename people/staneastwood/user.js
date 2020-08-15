ec59ca157798204a58503c019524920530258a46
The Map Group <admin@the-map-group.pictures>
2020-08-15 13:52:42 -0300

    Updated map for member '4.1 mil views - Thank you all.'

--- people/staneastwood/user.js
+++ people/staneastwood/user.js
@@ -4,5 +4,5 @@ var user_info = {
   "avatar": "https://live.staticflickr.com/5674/buddyicons/63713558@N00_r.jpg",
   "url": "https://www.flickr.com/photos/staneastwood/",
   "markers": 4922,
-  "photos": 62610,
+  "photos": 62643,
 }
