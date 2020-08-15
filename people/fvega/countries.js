601070871ba056cd6eee1d2a1081cad8a22f8f12
The Map Group <admin@the-map-group.pictures>
2020-08-15 15:13:14 -0300

    Updated map for member 'Fernando Sa Rapita'

--- people/fvega/countries.js
+++ people/fvega/countries.js
@@ -1,5 +1,5 @@
 var countries = [
-  ['ES', 'Spain', 995, 1642],
+  ['ES', 'Spain', 996, 1643],
   ['DE', 'Germany', 25, 26],
   ['IS', 'Iceland', 449, 649],
   ['CH', 'Switzerland', 7, 7],
--- people/fvega/user.js
+++ people/fvega/user.js
@@ -3,6 +3,6 @@ var user_info = {
   "name": "Fernando Vega",
   "avatar": "https://live.staticflickr.com/5674/buddyicons/158292715@N07_r.jpg",
   "url": "https://www.flickr.com/photos/fvega/",
-  "markers": 2208,
-  "photos": 3180,
+  "markers": 2209,
+  "photos": 3181,
 }
