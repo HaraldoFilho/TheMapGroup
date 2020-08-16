a8994046497697608cc96e954f7390d4229c4c20
The Map Group <admin@the-map-group.pictures>
2020-08-16 15:02:59 -0300

    Updated map for member 'Joseph Hollick'

--- people/31155442@N03/countries.js
+++ people/31155442@N03/countries.js
@@ -1,6 +1,6 @@
 var countries = [
   ['GB', 'United Kingdom', 659, 742],
-  ['CA', 'Canada', 1367, 1912],
+  ['CA', 'Canada', 1368, 1913],
   ['GG', 'Guernsey', 3, 8],
   ['US', 'United States of America', 1065, 1290],
   ['BS', 'Bahamas', 19, 19],
