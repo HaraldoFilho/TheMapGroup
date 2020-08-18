73e9a1ef1534ba06dcd198d4019e2a7a135096be
The Map Group <admin@the-map-group.pictures>
2020-08-18 19:41:24 -0300

    Updated map for member 'Brit 70013 fan'

--- people/93936890@N02/user.js
+++
@@ -1,8 +0,0 @@
-var user_info = {
-  "id": "93936890@N02",
-  "name": "CLIFF BANCROFT",
-  "avatar": "https://live.staticflickr.com/5674/buddyicons/93936890@N02_r.jpg",
-  "url": "https://www.flickr.com/photos/93936890@N02/",
-  "markers": 621,
-  "photos": 916,
-}
