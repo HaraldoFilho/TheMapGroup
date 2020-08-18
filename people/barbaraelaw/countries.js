cce6d25b82b2bb5e757518b0c9466f6449901bcc
The Map Group <admin@the-map-group.pictures>
2020-08-18 18:51:32 -0300

    Updated map for member 'ManxieB- prefer comments!'

--- people/barbaraelaw/countries.js
+++ people/barbaraelaw/countries.js
@@ -1,6 +1,6 @@
 var countries = [
   ['TZ', 'Tanzania', 62, 335],
-  ['GB', 'United Kingdom', 1655, 2071],
+  ['GB', 'United Kingdom', 1654, 2071],
   ['IM', 'Isle of Man', 596, 682],
   ['IE', 'Ireland', 35, 53]
 ]
--- people/barbaraelaw/user.js
+++ people/barbaraelaw/user.js
@@ -4,5 +4,5 @@ var user_info = {
   "avatar": "https://live.staticflickr.com/5674/buddyicons/25404639@N08_r.jpg",
   "url": "https://www.flickr.com/photos/barbaraelaw/",
   "markers": 2352,
-  "photos": 3145,
+  "photos": 3146,
 }
