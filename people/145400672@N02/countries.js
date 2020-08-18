1783a3fce42205c99e4b929e4321a6e1f88c1d3e
The Map Group <admin@the-map-group.pictures>
2020-08-18 14:09:49 -0300

    Updated map for member 'Pit Spielmann'

--- people/145400672@N02/countries.js
+++ people/145400672@N02/countries.js
@@ -1,5 +1,5 @@
 var countries = [
-  ['NL', 'The Netherlands', 107, 180],
+  ['NL', 'The Netherlands', 105, 177],
   ['JP', 'Japan', 6, 20],
   ['BE', 'Belgium', 52, 76],
   ['FR', 'France', 86, 141],
