d621e068e8be1f7fc6c1ad93bda967f7ebce2cd9
The Map Group <admin@the-map-group.pictures>
2020-08-18 19:56:37 -0300

    Updated map for member 'Fernando Sa Rapita'

--- people/fvega/countries.js
+++ people/fvega/countries.js
@@ -1,9 +1,9 @@
 var countries = [
-  ['ES', 'Spain', 999, 1646],
+  ['ES', 'Spain', 1000, 1647],
   ['DE', 'Germany', 25, 26],
   ['IS', 'Iceland', 449, 649],
   ['CH', 'Switzerland', 7, 7],
   ['FI', 'Finland', 25, 25],
   ['NL', 'The Netherlands', 21, 22],
-  ['GB', 'United Kingdom', 685, 806]
+  ['GB', 'United Kingdom', 686, 809]
 ]
