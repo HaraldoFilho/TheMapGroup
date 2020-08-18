233e7dfdc751d058306e5ad16c01beb2b37a9aa2
The Map Group <admin@the-map-group.pictures>
2020-08-18 13:17:57 -0300

    Updated map for member 'I saw_that'

--- people/saw_that/countries.js
+++ people/saw_that/countries.js
@@ -1,7 +1,7 @@
 var countries = [
   ['US', 'United States of America', 75, 135],
   ['IT', 'Italy', 25, 27],
-  ['CA', 'Canada', 508, 1021],
+  ['CA', 'Canada', 508, 1022],
   ['GB', 'United Kingdom', 12, 15],
   ['FR', 'France', 8, 8],
   ['CD', 'Democratic Republic of the Congo', 1, 1],
