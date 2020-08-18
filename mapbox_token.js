1d7b76548ca04368a018793ddc3be26e096ec1bb
The Map Group <admin@the-map-group.pictures>
2020-08-17 23:19:19 -0300

    Corrected mapbox access token

--- mapbox_token.js
+++ mapbox_token.js
@@ -1,4 +1,4 @@
 // Include here the mapbox access token
 // Get one at: https://account.mapbox.com/access-tokens/create
 var mapbox_token =
-'pk.eyJ1IjoiaGFyYWxkby1maWxobyIsImEiOiJja2RxN3E4aXQyMDhlMnlsNXE4b3k4M2EzIn0.dc6ab7DDUcG_B373WIdsnw'
+'pk.eyJ1IjoiaGFyYWxkby1maWxobyIsImEiOiJja2Q0aGpzbm0wMWFuMnduMDFiZHhrdnNtIn0.UAiJqFUU6E02Kk7iwvO4Qw'
