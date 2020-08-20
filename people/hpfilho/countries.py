63dfcde54bba6b5eb6ca8ac88b1c67da6e852d68
The Map Group <admin@the-map-group.pictures>
2020-08-20 00:41:47 -0300

    Updated member 'hpfilho' with new files structure

---
+++ people/hpfilho/countries.py
@@ -0,0 +1,9 @@
+countries_dict = {
+  'BR': ['Brazil', 176, 395],
+  'IT': ['Italy', 76, 125],
+  'PY': ['Paraguay', 1, 1],
+  'VA': ['Vatican City', 22, 31],
+  'AR': ['Argentina', 20, 31],
+  'HR': ['Croatia', 184, 243],
+  'US': ['United States of America', 51, 132],
+}
