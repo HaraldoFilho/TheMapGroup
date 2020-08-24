d838f2b09a6006a4fa9db02ccd40ed47bf17043a
The Map Group <admin@the-map-group.pictures>
2020-08-24 13:35:43 -0300

    Updated map for member 'Elena Lebedeva (IG: ellebedeva_)'

--- people/ellebedeva/countries.py
+++ people/ellebedeva/countries.py
@@ -1,19 +1,19 @@
 countries_dict = {
   'FI': ['Finland', 14, 15],
-  'GR': ['Greece', 3, 3],
-  'CH': ['Switzerland', 12, 12],
-  'BY': ['Belarus', 1, 1],
-  'RU': ['Russia', 70, 74],
-  'ES': ['Spain', 35, 36],
+  'EG': ['Egypt', 2, 2],
   'AT': ['Austria', 2, 2],
+  'ES': ['Spain', 35, 36],
+  'RU': ['Russia', 70, 74],
+  'BY': ['Belarus', 1, 1],
+  'PT': ['Portugal', 7, 9],
   'LI': ['Liechtenstein', 1, 1],
   'MT': ['Malta', 7, 8],
-  'EG': ['Egypt', 2, 2],
+  'CZ': ['Czech Republic', 5, 5],
+  'MA': ['Morocco', 2, 3],
+  'GR': ['Greece', 3, 3],
   'HU': ['Hungary', 1, 1],
   'GB': ['United Kingdom', 3, 3],
-  'MA': ['Morocco', 2, 3],
-  'PT': ['Portugal', 7, 9],
   'KZ': ['Kazakhstan', 16, 16],
   'IT': ['Italy', 3, 3],
-  'CZ': ['Czech Republic', 5, 5]
+  'CH': ['Switzerland', 12, 12]
 }
--- people/ellebedeva/user.py
+++ people/ellebedeva/user.py
@@ -1,5 +1,6 @@
 user_info = {
   'id': '79292646@N05',
+  'alias': 'ellebedeva',
   'name': 'Elena Lebedeva (IG: ellebedeva',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/79292646@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/ellebedeva/',
