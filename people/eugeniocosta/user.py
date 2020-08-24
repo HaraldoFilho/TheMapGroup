68d6cadd0d0ae96dbacc69223b5f4d8c03e36505
The Map Group <admin@the-map-group.pictures>
2020-08-24 14:01:36 -0300

    Updated map for member 'Eugenio GV Costa'

--- people/eugeniocosta/user.py
+++ people/eugeniocosta/user.py
@@ -1,5 +1,6 @@
 user_info = {
   'id': '169904704@N04',
+  'alias': 'eugeniocosta',
   'name': 'Eugenio Costa',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/169904704@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/eugeniocosta/',
