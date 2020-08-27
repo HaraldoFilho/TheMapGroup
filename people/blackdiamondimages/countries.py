630a2a7de9463d378816583c35bae04bcae3b9be
The Map Group <admin@the-map-group.pictures>
2020-08-27 03:13:57 -0300

    Updated map for member 'Black Diamond Images'

--- people/blackdiamondimages/countries.py
+++ people/blackdiamondimages/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
   'NZ': ['New Zealand', 25, 49],
-  'AR': ['Argentina', 67, 216],
-  'CL': ['Chile', 122, 297],
   'PE': ['Peru', 84, 420],
-  'BO': ['Bolivia', 30, 95],
-  'CA': ['Canada', 499, 699],
+  'CL': ['Chile', 122, 297],
   'US': ['United States of America', 197, 246],
+  'BR': ['Brazil', 18, 18],
   'AU': ['Australia', 3876, 5121],
+  'AR': ['Argentina', 67, 216],
+  'BO': ['Bolivia', 30, 95],
   'SG': ['Singapore', 85, 524],
-  'UY': ['Uruguay', 1, 2],
-  'BR': ['Brazil', 18, 18]
+  'CA': ['Canada', 499, 699],
+  'UY': ['Uruguay', 1, 2]
 }
