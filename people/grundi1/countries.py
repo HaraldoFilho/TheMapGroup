9e5a0f949fe03cf3ba661b22ac192cc6170245b9
The Map Group <admin@the-map-group.pictures>
2020-08-27 02:47:05 -0300

    Updated map for member 'grundi1'

--- people/grundi1/countries.py
+++ people/grundi1/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'HR': ['Croatia', 13, 36],
   'IT': ['Italy', 34, 57],
-  'GR': ['Greece', 6, 8],
-  'SI': ['Slovenia', 1, 7],
+  'IE': ['Ireland', 38, 84],
   'AT': ['Austria', 132, 505],
-  'IE': ['Ireland', 38, 84]
+  'HR': ['Croatia', 13, 36],
+  'SI': ['Slovenia', 1, 7],
+  'GR': ['Greece', 6, 8]
 }
