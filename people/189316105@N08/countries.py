3a805a5789ee49a2e6441e9561989f0db9eb42e8
The Map Group <admin@the-map-group.pictures>
2020-08-29 02:12:30 -0300

    Updated map for member 'Miguel - AF'

---
+++ people/189316105@N08/countries.py
@@ -0,0 +1,3 @@
+countries_dict = {
+  'ES': ['Spain', 14, 80]
+}
---
+++ people/189316105@N08/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '189316105@N08',
+  'alias': '189316105@N08',
+  'name': 'Miguel Angel Flores',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/189316105@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/189316105@N08/',
+  'markers': 14,
+  'photos': 80
+}
