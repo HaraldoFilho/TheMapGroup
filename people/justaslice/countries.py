b498a9720976097a8dfa2e25e2537f5773e9eb11
The Map Group <admin@the-map-group.pictures>
2020-08-30 02:14:16 -0300

    Updated map for member '█ Slices of Light █▀ ▀ ▀'

--- people/justaslice/countries.py
+++ people/justaslice/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'SG': ['Singapore', 6, 6],
-  'US': ['United States of America', 365, 658],
-  'GB': ['United Kingdom', 120, 163],
-  'PT': ['Portugal', 19, 33],
-  'BE': ['Belgium', 1, 1],
-  'MY': ['Malaysia', 83, 130],
+  'VA': ['Vatican City', 102, 405],
   'CN': ['South China Sea Islands (disputed)', 217, 320],
-  'EG': ['Egypt', 2, 2],
+  'BE': ['Belgium', 1, 1],
   'ES': ['Spain', 1, 1],
   'FR': ['France', 95, 171],
+  'GB': ['United Kingdom', 125, 168],
+  'EG': ['Egypt', 2, 2],
+  'MY': ['Malaysia', 83, 130],
+  'SG': ['Singapore', 6, 6],
+  'US': ['United States of America', 365, 658],
   'IT': ['Italy', 278, 557],
-  'VA': ['Vatican City', 102, 405]
+  'PT': ['Portugal', 19, 33]
 }
