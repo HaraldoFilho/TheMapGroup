e799efec50104726c03100a1e718a622bbbfdd05
The Map Group <admin@the-map-group.pictures>
2020-09-01 02:29:08 -0300

    Updated map for member 'karen_ds1101'

---
+++ people/karen_ds1101/countries.py
@@ -0,0 +1,16 @@
+countries_dict = {
+  'NZ': ['New Zealand', 10, 10],
+  'IT': ['Italy', 5, 6],
+  'BN': ['Brunei Darussalam', 2, 3],
+  'IE': ['Ireland', 3, 3],
+  'GB': ['United Kingdom', 4, 4],
+  'PE': ['Peru', 11, 16],
+  'MY': ['Malaysia', 8, 11],
+  'JP': ['Japan', 10, 10],
+  'CL': ['Chile', 5, 9],
+  'AU': ['Australia', 730, 854],
+  'CN': ['South China Sea Islands (disputed)', 79, 125],
+  'US': ['United States of America', 11, 19],
+  'DE': ['Germany', 1, 1],
+  'SG': ['Singapore', 66, 78]
+}
---
+++ people/karen_ds1101/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '51073972@N02',
+  'alias': 'karen_ds1101',
+  'name': 'Karen',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/51073972@N02_r.jpg',
+  'url': 'https://www.flickr.com/photos/karen_ds1101/',
+  'markers': 945,
+  'photos': 1149
+}
