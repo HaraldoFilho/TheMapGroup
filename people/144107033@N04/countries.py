1955c70a828d917f79ff27dd9359a5921dabe560
The Map Group <admin@the-map-group.pictures>
2020-09-04 03:58:03 -0300

    Updated map for member 'sebastiánaguilar'

---
+++ people/144107033@N04/countries.py
@@ -0,0 +1,5 @@
+countries_dict = {
+  'FR': ['France', 83, 119],
+  'ES': ['Spain', 9602, 12314],
+  'PT': ['Portugal', 441, 527]
+}
---
+++ people/144107033@N04/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '144107033@N04',
+  'alias': '144107033@N04',
+  'name': 'Sebastián Aguilar',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/144107033@N04_r.jpg',
+  'url': 'https://www.flickr.com/photos/144107033@N04/',
+  'markers': 10126,
+  'photos': 12960
+}
