06c1a4fb2d5b934237fbb34bf1bff3ac0f48b3f0
The Map Group <admin@the-map-group.pictures>
2020-09-04 02:29:37 -0300

    Updated map for member 'letang.gilles'

---
+++ people/132920569@N02/countries.py
@@ -0,0 +1,3 @@
+countries_dict = {
+  'FR': ['France', 167, 185]
+}
---
+++ people/132920569@N02/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '132920569@N02',
+  'alias': '132920569@N02',
+  'name': 'Gilles Letang',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/132920569@N02_r.jpg',
+  'url': 'https://www.flickr.com/photos/132920569@N02/',
+  'markers': 167,
+  'photos': 185
+}
