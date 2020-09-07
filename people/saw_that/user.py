e5a79c3c203f6b8b44ba5991c0b42d3171114cbf
The Map Group <admin@the-map-group.pictures>
2020-09-07 02:18:56 -0300

    Updated map for member 'I saw_that'

--- people/saw_that/user.py
+++ people/saw_that/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'I saw_that',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/88196436@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/saw_that/',
-  'markers': 715,
-  'photos': 1358
+  'markers': 717,
+  'photos': 1361
 }
