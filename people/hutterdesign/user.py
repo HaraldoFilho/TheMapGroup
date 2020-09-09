8cd32d890b1388e844cbaf42e70125fe40a431b6
The Map Group <admin@the-map-group.pictures>
2020-09-09 02:26:28 -0300

    Updated map for member 'panoround hutter'

---
+++ people/hutterdesign/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '35555507@N03',
+  'alias': 'hutterdesign',
+  'name': 'Frederic Hutter',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/35555507@N03_r.jpg',
+  'url': 'https://www.flickr.com/photos/hutterdesign/',
+  'markers': 138,
+  'photos': 165
+}
