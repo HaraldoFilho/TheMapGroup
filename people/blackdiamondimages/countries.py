c01657765f7dbb1740178691b96cc1c68b540cc5
The Map Group <admin@the-map-group.pictures>
2020-09-10 10:03:39 -0300

    Updated map for member 'Black Diamond Images'

--- people/blackdiamondimages/countries.py
+++ people/blackdiamondimages/countries.py
@@ -1,26 +1,3 @@
 countries_dict = {
-  'NZ': ['New Zealand', 136, 164],
-  'CH': ['Switzerland', 2, 2],
-  'BO': ['Bolivia', 30, 95],
-  'GR': ['Greece', 1, 2],
-  'ME': ['Montenegro', 1, 1],
-  'NL': ['The Netherlands', 1, 1],
-  'IT': ['Italy', 5, 5],
-  'TL': ['East Timor', 4, 4],
-  'AR': ['Argentina', 69, 220],
-  'GB': ['United Kingdom', 1, 1],
-  'CA': ['Canada', 524, 724],
-  'US': ['United States of America', 350, 405],
-  'CL': ['Chile', 123, 299],
-  'JP': ['Japan', 45, 45],
-  'AL': ['Albania', 2, 2],
-  'UY': ['Uruguay', 1, 2],
-  'PE': ['Peru', 84, 420],
-  'AU': ['Australia', 6269, 8206],
-  'SG': ['Singapore', 90, 532],
-  'HR': ['Croatia', 1, 1],
-  'DE': ['Germany', 50, 83],
-  'FR': ['France', 1, 1],
-  'AT': ['Austria', 1, 1],
-  'BR': ['Brazil', 18, 18]
+  'AU': ['Australia', 6, 6]
 }
