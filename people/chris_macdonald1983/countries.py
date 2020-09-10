db369d62eb3c2cc1333be8ceda44b6a833f97ca4
The Map Group <admin@the-map-group.pictures>
2020-09-09 22:33:48 -0300

    Updated map for member 'CJphoto83'

--- people/chris_macdonald1983/countries.py
+++ people/chris_macdonald1983/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'US': ['United States of America', 6, 6],
   'CN': ['South China Sea Islands (disputed)', 13, 24],
-  'SG': ['Singapore', 13, 23]
+  'SG': ['Singapore', 13, 23],
+  'US': ['United States of America', 6, 6]
 }
