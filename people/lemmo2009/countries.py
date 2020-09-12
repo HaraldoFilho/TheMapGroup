5befcd75cbaf4e12a4f76354b43e7908773b8fd6
The Map Group <admin@the-map-group.pictures>
2020-09-12 02:09:35 -0300

    Updated map for member 'Lemmo2009'

--- people/lemmo2009/countries.py
+++ people/lemmo2009/countries.py
@@ -1,3 +1,4 @@
 countries_dict = {
+  'AR': ['Argentina', 2, 2],
   'GB': ['United Kingdom', 2, 2]
 }
--- people/lemmo2009/user.py
+++ people/lemmo2009/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Paul',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/40749798@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/lemmo2009/',
-  'markers': 2,
-  'photos': 2
+  'markers': 4,
+  'photos': 4
 }
