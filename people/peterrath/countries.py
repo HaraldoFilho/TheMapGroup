0da19b2379f4c5a19a614718cdaa819199248a31
The Map Group <admin@the-map-group.pictures>
2020-09-12 23:56:09 -0300

    Updated map for member 'peter.rath'

--- people/peterrath/countries.py
+++ people/peterrath/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'NO': ['Norway', 6, 9],
-  'DE': ['Germany', 222, 312],
-  'PL': ['Poland', 1, 4],
-  'BS': ['Bahamas', 1, 2],
-  'PT': ['Portugal', 2, 8],
-  'GR': ['Greece', 16, 60],
-  'TR': ['Turkey', 29, 107],
-  'GB': ['United Kingdom', 1, 3],
-  'ES': ['Spain', 79, 103],
   'EG': ['Egypt', 24, 83],
+  'BS': ['Bahamas', 1, 2],
+  'GR': ['Greece', 16, 60],
+  'ES': ['Spain', 79, 103],
+  'PL': ['Poland', 1, 4],
+  'DK': ['Denmark', 1, 1],
   'NL': ['The Netherlands', 2, 10],
+  'GB': ['United Kingdom', 1, 3],
   'AT': ['Austria', 6, 7],
-  'DK': ['Denmark', 1, 1]
+  'NO': ['Norway', 6, 9],
+  'DE': ['Germany', 223, 313],
+  'TR': ['Turkey', 29, 107],
+  'PT': ['Portugal', 2, 8]
 }
--- people/peterrath/user.py
+++ people/peterrath/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'peter.rath',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/189226334@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/peterrath/',
-  'markers': 390,
-  'photos': 709
+  'markers': 391,
+  'photos': 710
 }
