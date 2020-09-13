74385162c28e588a545e7861fc0d75303b0b1503
The Map Group <admin@the-map-group.pictures>
2020-09-13 13:43:43 -0300

    Updated map for member 'Northern Tony'

--- people/22572253@N08/countries.py
+++ people/22572253@N08/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'IT': ['Italy', 4, 4],
-  'GB': ['United Kingdom', 65, 65],
-  'US': ['United States of America', 18, 18],
   'VA': ['Vatican City', 1, 1],
-  'CA': ['Canada', 2, 2]
+  'IT': ['Italy', 4, 4],
+  'CA': ['Canada', 2, 2],
+  'US': ['United States of America', 18, 18],
+  'GB': ['United Kingdom', 66, 66]
 }
--- people/22572253@N08/user.py
+++ people/22572253@N08/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Northern Tony',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/22572253@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/22572253@N08/',
-  'markers': 90,
-  'photos': 90
+  'markers': 91,
+  'photos': 91
 }
