3dac0a77d2e9e84d382ad627ea89349697b3d3bf
The Map Group <admin@the-map-group.pictures>
2020-09-12 23:59:55 -0300

    Updated map for member 'Frank Lammel'

--- people/frank-lammel/countries.py
+++ people/frank-lammel/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
+  'IT': ['Italy', 24, 24],
   'GR': ['Greece', 8, 8],
-  'ES': ['Spain', 8, 12],
-  'FR': ['France', 6, 6],
   'DE': ['Germany', 78, 97],
   'SM': ['San Marino', 1, 1],
-  'IT': ['Italy', 24, 24]
+  'ES': ['Spain', 8, 12],
+  'FR': ['France', 6, 6]
 }
