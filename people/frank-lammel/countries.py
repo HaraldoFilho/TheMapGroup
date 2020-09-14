c66b6505a8ee3219382568f41a4e350174b93282
The Map Group <admin@the-map-group.pictures>
2020-09-14 15:24:56 -0300

    Updated repositories

--- people/frank-lammel/countries.py
+++
@@ -1,8 +0,0 @@
-countries_dict = {
-  'IT': ['Italy', 24, 24],
-  'GR': ['Greece', 8, 8],
-  'DE': ['Germany', 78, 97],
-  'SM': ['San Marino', 1, 1],
-  'ES': ['Spain', 8, 12],
-  'FR': ['France', 6, 6]
-}
--- people/frank-lammel/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '8861554@N02',
-  'alias': 'frank-lammel',
-  'name': 'Frank Lammel',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/8861554@N02_r.jpg',
-  'url': 'https://www.flickr.com/photos/frank-lammel/',
-  'markers': 125,
-  'photos': 148
-}
