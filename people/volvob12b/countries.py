35594de82c4a6b4103141f166c072658d46e4f36
The Map Group <admin@the-map-group.pictures>
2020-09-15 02:19:55 -0300

    Updated map for member 'Bernard Spragg'

--- people/volvob12b/countries.py
+++ people/volvob12b/countries.py
@@ -1,2 +1,3 @@
 countries_dict = {
+  'NZ': ['New Zealand', 2768, 3796]
 }
--- people/volvob12b/user.py
+++ people/volvob12b/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Bernard Spragg. NZ',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/88123769@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/volvob12b/',
-  'markers': 4190,
-  'photos': 5785
+  'markers': 4191,
+  'photos': 5786
 }
