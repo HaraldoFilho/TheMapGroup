72a7311b1677c6e3d1431d2e65b1fb6244da8f5b
The Map Group <admin@the-map-group.pictures>
2020-09-16 20:20:54 -0300

    Updated map for member 'Michael Leek Photography'

---
+++ people/113339808@N08/countries.py
@@ -0,0 +1,15 @@
+countries_dict = {
+  'FO': ['Faroe Islands', 1, 1],
+  'LA': ['Laos', 39, 43],
+  'TH': ['Thailand', 2, 2],
+  'VN': ['Vietnam', 68, 93],
+  'GB': ['United Kingdom', 1091, 1770],
+  'FR': ['France', 49, 66],
+  'IT': ['Italy', 30, 33],
+  'IE': ['Ireland', 10, 10],
+  'US': ['United States of America', 66, 76],
+  'CH': ['Switzerland', 1, 1],
+  'PL': ['Poland', 4, 5],
+  'CA': ['Canada', 2, 2],
+  'IS': ['Iceland', 7, 7]
+}
---
+++ people/113339808@N08/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '113339808@N08',
+  'alias': '113339808@N08',
+  'name': 'Michael Leek',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/113339808@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/113339808@N08/',
+  'markers': 1373,
+  'photos': 2114
+}
