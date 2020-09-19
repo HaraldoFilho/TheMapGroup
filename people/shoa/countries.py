b4b5f3a00356e3c8bb0b64cb297138592162bccd
The Map Group <admin@the-map-group.pictures>
2020-09-19 02:28:08 -0300

    Updated map for member 'Joe Dyndale'

---
+++ people/shoa/countries.py
@@ -0,0 +1,6 @@
+countries_dict = {
+  'DK': ['Denmark', 8, 8],
+  'IS': ['Iceland', 4, 4],
+  'US': ['United States of America', 2, 2],
+  'CH': ['Switzerland', 1, 1]
+}
---
+++ people/shoa/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '63488298@N00',
+  'alias': 'shoa',
+  'name': 'Joe Dyndale',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/63488298@N00_r.jpg',
+  'url': 'https://www.flickr.com/photos/shoa/',
+  'markers': 15,
+  'photos': 15
+}
