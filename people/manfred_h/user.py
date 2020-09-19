44d9bce6bed577da1d827428997b2f86a086d580
The Map Group <admin@the-map-group.pictures>
2020-09-19 12:20:36 -0300

    Updated map for member 'Manfred_H.'

--- people/manfred_h/user.py
+++ people/manfred_h/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/165713115@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/manfred_h/',
   'markers': 483,
-  'photos': 759
+  'photos': 760
 }
