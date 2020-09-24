6a3795dfd50d3c2914b28a20194d3670e2bf8db1
The Map Group <admin@the-map-group.pictures>
2020-09-24 02:12:17 -0300

    Updated map for member 'I saw_that'

--- people/saw_that/countries.py
+++ people/saw_that/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'CA': ['Canada', 524, 1046],
+  'FR': ['France', 8, 8],
+  'CU': ['Cuba', 77, 139],
+  'US': ['United States of America', 75, 135],
   'IT': ['Italy', 25, 27],
   'GB': ['United Kingdom', 12, 15],
-  'US': ['United States of America', 75, 135],
   'DE': ['Germany', 1, 2],
-  'CD': ['Democratic Republic of the Congo', 1, 1],
-  'CU': ['Cuba', 77, 139],
   'ET': ['Ethiopia', 1, 1],
-  'FR': ['France', 8, 8]
+  'CA': ['Canada', 525, 1047],
+  'CD': ['Democratic Republic of the Congo', 1, 1]
 }
--- people/saw_that/user.py
+++ people/saw_that/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'I saw_that',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/88196436@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/saw_that/',
-  'markers': 724,
-  'photos': 1374
+  'markers': 725,
+  'photos': 1375
 }
