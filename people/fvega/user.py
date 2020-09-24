ea53ae916814edd4f8c80e3c826f81c467674ea7
The Map Group <admin@the-map-group.pictures>
2020-09-24 02:20:04 -0300

    Updated map for member 'Fernando Sa Rapita'

--- people/fvega/user.py
+++ people/fvega/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Fernando Vega',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/158292715@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/fvega/',
-  'markers': 2256,
-  'photos': 3248
+  'markers': 2257,
+  'photos': 3249
 }
