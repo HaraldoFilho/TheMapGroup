bf9cc38b4e6290bce9cd42356f5ca56ff2c168a8
The Map Group <admin@the-map-group.pictures>
2020-09-30 02:13:27 -0300

    Updated map for member 'I saw_that'

--- people/saw_that/countries.py
+++ people/saw_that/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'CA': ['Canada', 526, 1048],
-  'US': ['United States of America', 75, 135],
+  'CA': ['Canada', 526, 1049],
   'GB': ['United Kingdom', 12, 15],
-  'CD': ['Democratic Republic of the Congo', 1, 1],
-  'FR': ['France', 8, 8],
-  'IT': ['Italy', 25, 27],
-  'DE': ['Germany', 1, 2],
   'CU': ['Cuba', 77, 139],
-  'ET': ['Ethiopia', 1, 1]
+  'IT': ['Italy', 25, 27],
+  'US': ['United States of America', 75, 135],
+  'CD': ['Democratic Republic of the Congo', 1, 1],
+  'ET': ['Ethiopia', 1, 1],
+  'DE': ['Germany', 1, 2],
+  'FR': ['France', 8, 8]
 }
