89e9a635020362b371be0558817c29dc09f4ee8b
The Map Group <admin@the-map-group.pictures>
2020-10-02 03:04:40 -0300

    Updated map for member 'isteeves'

---
+++ people/189420050@N03/countries.py
@@ -0,0 +1,5 @@
+countries_dict = {
+  'GB': ['United Kingdom', 3, 3],
+  'CA': ['Canada', 15, 19],
+  'HU': ['Hungary', 15, 17]
+}
---
+++ people/189420050@N03/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '189420050@N03',
+  'alias': '189420050@N03',
+  'name': 'Irene Steeves',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/189420050@N03_r.jpg',
+  'url': 'https://www.flickr.com/photos/189420050@N03/',
+  'markers': 33,
+  'photos': 39
+}
