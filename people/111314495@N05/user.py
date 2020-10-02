0947cd91e4994dbdf0d5516f9c6befdee16d9cee
The Map Group <admin@the-map-group.pictures>
2020-10-02 03:03:25 -0300

    Updated map for member 'xd_travel'

---
+++ people/111314495@N05/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '111314495@N05',
+  'alias': '111314495@N05',
+  'name': 'Mike X-d',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/111314495@N05_r.jpg',
+  'url': 'https://www.flickr.com/photos/111314495@N05/',
+  'markers': 2758,
+  'photos': 5280
+}
