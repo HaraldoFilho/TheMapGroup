26a20bb70740c5b0225cea84c0f4cbd4769da705
The Map Group <admin@the-map-group.pictures>
2020-10-03 02:25:54 -0300

    Updated map for member 'paco zerpa'

--- people/pacozerpa/countries.py
+++ people/pacozerpa/countries.py
@@ -1,4 +1,4 @@
 countries_dict = {
   'CZ': ['Czech Republic', 14, 14],
-  'ES': ['Spain', 191, 253]
+  'ES': ['Spain', 192, 254]
 }
--- people/pacozerpa/user.py
+++ people/pacozerpa/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'paco zerpa',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/129959166@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/pacozerpa/',
-  'markers': 205,
-  'photos': 267
+  'markers': 206,
+  'photos': 268
 }
