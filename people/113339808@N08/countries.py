1811d70c2d5c01528cf967e1aed74914289ffd97
The Map Group <admin@the-map-group.pictures>
2020-10-05 02:38:04 -0300

    Updated map for member 'Michael Leek Photography'

--- people/113339808@N08/countries.py
+++ people/113339808@N08/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'CA': ['Canada', 2, 2],
-  'IS': ['Iceland', 7, 7],
-  'CH': ['Switzerland', 1, 1],
-  'FO': ['Faroe Islands', 1, 1],
-  'TH': ['Thailand', 2, 2],
-  'IE': ['Ireland', 10, 10],
-  'FR': ['France', 49, 66],
-  'LA': ['Laos', 39, 43],
-  'PL': ['Poland', 4, 5],
   'IT': ['Italy', 30, 33],
-  'GB': ['United Kingdom', 1094, 1777],
+  'TH': ['Thailand', 2, 2],
+  'CH': ['Switzerland', 1, 1],
+  'FR': ['France', 49, 66],
   'US': ['United States of America', 66, 76],
-  'VN': ['Vietnam', 68, 93]
+  'LA': ['Laos', 39, 43],
+  'FO': ['Faroe Islands', 1, 1],
+  'CA': ['Canada', 2, 2],
+  'IE': ['Ireland', 10, 10],
+  'VN': ['Vietnam', 68, 93],
+  'PL': ['Poland', 4, 5],
+  'GB': ['United Kingdom', 1097, 1782],
+  'IS': ['Iceland', 7, 7]
 }
