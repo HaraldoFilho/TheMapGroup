cfb60c663330b3133327904ac7503794ceff6d81
The Map Group <admin@the-map-group.pictures>
2020-10-05 02:40:46 -0300

    Updated map for member 'ByronDelgado'

---
+++ people/delgadobyron/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '28214158@N04',
+  'alias': 'delgadobyron',
+  'name': 'ByronDelgado',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/28214158@N04_r.jpg',
+  'url': 'https://www.flickr.com/photos/delgadobyron/',
+  'markers': 86,
+  'photos': 287
+}
