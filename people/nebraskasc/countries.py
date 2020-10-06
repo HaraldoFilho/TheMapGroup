8284559ed16759ffdd80997a9d4686c3193886b1
The Map Group <admin@the-map-group.pictures>
2020-10-06 02:03:20 -0300

    Updated map for member 'NebraskaSC Severe Weather Photography Videography'

--- people/nebraskasc/countries.py
+++ people/nebraskasc/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'US': ['United States of America', 1919, 17512]
+  'US': ['United States of America', 1919, 17520]
 }
--- people/nebraskasc/user.py
+++ people/nebraskasc/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/14320768@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/nebraskasc/',
   'markers': 1919,
-  'photos': 17512
+  'photos': 17520
 }
