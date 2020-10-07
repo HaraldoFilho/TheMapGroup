312896d13da6e1790db635d8944f5eb81fcb7fb8
The Map Group <admin@the-map-group.pictures>
2020-10-07 03:27:02 -0300

    Removed member '113339808@N08'

--- people/113339808@N08/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '113339808@N08',
-  'alias': '113339808@N08',
-  'name': 'Michael Leek',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/113339808@N08_r.jpg',
-  'url': 'https://www.flickr.com/photos/113339808@N08/',
-  'markers': 1385,
-  'photos': 2136
-}
