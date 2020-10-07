65159287c0883cdd6ac7efd3a2f7389a7f6a17c1
The Map Group <admin@the-map-group.pictures>
2020-10-07 02:05:47 -0300

    Updated map for member 'Christopher Wallace'

--- people/chrispricephotography/countries.py
+++ people/chrispricephotography/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
+  'US': ['United States of America', 456, 641],
   'CA': ['Canada', 90, 158],
-  'US': ['United States of America', 455, 640],
-  'AU': ['Australia', 11, 11],
-  'NZ': ['New Zealand', 9, 9],
   'CH': ['Switzerland', 4, 7],
-  'RU': ['Russia', 1, 1],
   'GB': ['United Kingdom', 26, 28],
-  'PA': ['Panama', 1, 2]
+  'PA': ['Panama', 1, 2],
+  'RU': ['Russia', 1, 1],
+  'AU': ['Australia', 11, 11],
+  'NZ': ['New Zealand', 9, 9]
 }
--- people/chrispricephotography/user.py
+++ people/chrispricephotography/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Chris Price',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/chrispricephotography/',
-  'markers': 597,
-  'photos': 856
+  'markers': 598,
+  'photos': 857
 }
