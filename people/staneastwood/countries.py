5be1f26c5e8a04af58f1526e0eac313d36ddd0ab
The Map Group <admin@the-map-group.pictures>
2020-10-09 02:21:38 -0300

    Updated map for member '4.3 mil views - Thank you all.'

--- people/staneastwood/countries.py
+++ people/staneastwood/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'CA': ['Canada', 357, 1134],
-  'ES': ['Spain', 67, 665],
-  'FR': ['France', 9, 295],
-  'IM': ['Isle of Man', 322, 567],
-  'IE': ['Ireland', 20, 187],
-  'PT': ['Portugal', 42, 357],
-  'NO': ['Norway', 17, 234],
-  'GI': ['Gibraltar', 1, 94],
-  'DK': ['Denmark', 1, 59],
-  'IS': ['Iceland', 31, 213],
   'GL': ['Greenland', 7, 288],
+  'IE': ['Ireland', 20, 187],
+  'NO': ['Norway', 25, 327],
   'US': ['United States of America', 169, 580],
-  'GB': ['United Kingdom', 3984, 24259]
+  'ES': ['Spain', 67, 665],
+  'IM': ['Isle of Man', 322, 567],
+  'GI': ['Gibraltar', 1, 94],
+  'GB': ['United Kingdom', 3984, 24259],
+  'IS': ['Iceland', 31, 213],
+  'DK': ['Denmark', 1, 59],
+  'FR': ['France', 9, 295],
+  'CA': ['Canada', 357, 1134],
+  'PT': ['Portugal', 42, 357]
 }
