566660dd3462747167388a39f096581b2545ff1b
The Map Group <admin@the-map-group.pictures>
2020-10-12 02:37:16 -0300

    Updated map for member 'HereIsTom'

---
+++ people/hereistom/countries.py
@@ -0,0 +1,14 @@
+countries_dict = {
+  'TH': ['Thailand', 48, 56],
+  'GR': ['Greece', 1, 1],
+  'TR': ['Turkey', 63, 63],
+  'IT': ['Italy', 1, 1],
+  'NL': ['The Netherlands', 1194, 1293],
+  'BE': ['Belgium', 3, 3],
+  'HR': ['Croatia', 1, 1],
+  'SI': ['Slovenia', 1, 1],
+  'ES': ['Spain', 48, 48],
+  'BR': ['Brazil', 2, 2],
+  'PT': ['Portugal', 95, 96],
+  'DE': ['Germany', 2, 2]
+}
---
+++ people/hereistom/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '47035764@N03',
+  'alias': 'hereistom',
+  'name': 'Tom Jutte',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/47035764@N03_r.jpg',
+  'url': 'https://www.flickr.com/photos/hereistom/',
+  'markers': 1459,
+  'photos': 1567
+}
