1eae19164a8127e4a58682885f8197c325b71503
The Map Group <admin@the-map-group.pictures>
2020-10-15 02:17:40 -0300

    Updated map for member 'Joseph Hollick'

--- people/31155442@N03/user.py
+++ people/31155442@N03/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Joseph Hollick',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/31155442@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/31155442@N03/',
-  'markers': 8065,
-  'photos': 10272
+  'markers': 8073,
+  'photos': 10284
 }
