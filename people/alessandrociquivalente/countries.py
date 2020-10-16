f974102fa11364f565583951c0f1e7eacd7dd2db
The Map Group <admin@the-map-group.pictures>
2020-10-16 02:41:57 -0300

    Updated map for member 'Alessandro Ciqui Valente'

---
+++ people/alessandrociquivalente/countries.py
@@ -0,0 +1,5 @@
+countries_dict = {
+  'ES': ['Spain', 9, 11],
+  'JP': ['Japan', 20, 20],
+  'IT': ['Italy', 110, 113]
+}
---
+++ people/alessandrociquivalente/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '143552306@N02',
+  'alias': 'alessandrociquivalente',
+  'name': 'Alessandro Valente',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/143552306@N02_r.jpg',
+  'url': 'https://www.flickr.com/photos/alessandrociquivalente/',
+  'markers': 139,
+  'photos': 144
+}
