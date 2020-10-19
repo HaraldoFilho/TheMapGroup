1082dd65df7596317047299015f8f6a59f2c3d3d
The Map Group <admin@the-map-group.pictures>
2020-10-19 02:41:11 -0300

    Updated map for member 'ByronDelgado'

--- people/delgadobyron/countries.py
+++ people/delgadobyron/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
+  'HU': ['Hungary', 4, 5],
+  'GB': ['United Kingdom', 77, 222],
   'DE': ['Germany', 1, 1],
   'GR': ['Greece', 5, 6],
-  'IT': ['Italy', 6, 60],
-  'HU': ['Hungary', 4, 5],
-  'GB': ['United Kingdom', 76, 221]
+  'IT': ['Italy', 6, 60]
 }
--- people/delgadobyron/user.py
+++ people/delgadobyron/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'ByronDelgado',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/28214158@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/delgadobyron/',
-  'markers': 92,
-  'photos': 293
+  'markers': 93,
+  'photos': 294
 }
