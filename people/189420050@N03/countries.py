faeb881b996922df18f301a6285de882f28afc25
The Map Group <admin@the-map-group.pictures>
2020-10-21 03:04:21 -0300

    Removed member '189420050@N03'

--- people/189420050@N03/countries.py
+++
@@ -1,5 +0,0 @@
-countries_dict = {
-  'GB': ['United Kingdom', 3, 3],
-  'HU': ['Hungary', 18, 20],
-  'CA': ['Canada', 17, 23]
-}
--- people/189420050@N03/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '189420050@N03',
-  'alias': '189420050@N03',
-  'name': 'Irene Steeves',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/189420050@N03_r.jpg',
-  'url': 'https://www.flickr.com/photos/189420050@N03/',
-  'markers': 38,
-  'photos': 46
-}
