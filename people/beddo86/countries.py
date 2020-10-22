7c6eae0888c7cfe90ad6a2bfdfe4da2af8d7b659
The Map Group <admin@the-map-group.pictures>
2020-10-22 02:33:30 -0300

    Updated map for member 'beddo86'

---
+++ people/beddo86/countries.py
@@ -0,0 +1,13 @@
+countries_dict = {
+  'DK': ['Denmark', 10, 11],
+  'IE': ['Ireland', 5, 5],
+  'CH': ['Switzerland', 3, 3],
+  'DE': ['Germany', 3, 3],
+  'SI': ['Slovenia', 3, 3],
+  'IT': ['Italy', 63, 65],
+  'GB': ['United Kingdom', 4, 4],
+  'IS': ['Iceland', 10, 11],
+  'PT': ['Portugal', 9, 9],
+  'HU': ['Hungary', 3, 3],
+  'ES': ['Spain', 1, 1]
+}
---
+++ people/beddo86/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '130942318@N07',
+  'alias': 'beddo86',
+  'name': 'Andrea Beddini',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/130942318@N07_r.jpg',
+  'url': 'https://www.flickr.com/photos/beddo86/',
+  'markers': 114,
+  'photos': 118
+}
