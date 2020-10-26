82f2a38dbc90a5eea859a2efb3416d76f35821e0
The Map Group <admin@the-map-group.pictures>
2020-10-26 02:37:21 -0300

    Updated map for member 'K@moeiras'

--- people/kamo_kamoeiras/user.py
+++ people/kamo_kamoeiras/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'KAMO KAMOEIRAS',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/163799240@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/kamo_kamoeiras/',
-  'markers': 842,
-  'photos': 1289
+  'markers': 844,
+  'photos': 1294
 }
