8cd024d85d83c3094c96afcc0acb5a48e5311b5e
The Map Group <admin@the-map-group.pictures>
2020-10-28 02:10:55 -0300

    Updated map for member 'GaryC4'

--- people/51207582@N08/countries.py
+++ people/51207582@N08/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'GR': ['Greece', 13, 13],
-  'GB': ['United Kingdom', 2426, 2938],
   'ES': ['Spain', 77, 80],
+  'GR': ['Greece', 13, 13],
+  'GB': ['United Kingdom', 2428, 2940],
   'IT': ['Italy', 94, 102],
   'PT': ['Portugal', 18, 21]
 }
--- people/51207582@N08/user.py
+++ people/51207582@N08/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Gary Cadman',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/51207582@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/51207582@N08/',
-  'markers': 2628,
-  'photos': 3154
+  'markers': 2630,
+  'photos': 3156
 }
