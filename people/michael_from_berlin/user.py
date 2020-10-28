d71fbe7ee05cda1882dcd8e2c46b6af3b6103314
The Map Group <admin@the-map-group.pictures>
2020-10-28 02:09:46 -0300

    Updated map for member 'michael_jeddah'

--- people/michael_from_berlin/user.py
+++ people/michael_from_berlin/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'michael_jeddah',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/15029120@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/michael_from_berlin/',
-  'markers': 3195,
-  'photos': 3292
+  'markers': 3197,
+  'photos': 3294
 }
