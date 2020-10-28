e393da040af9fcd0bcff783063a2937d84621822
The Map Group <admin@the-map-group.pictures>
2020-10-28 03:01:19 -0300

    Removed member 'alessandrociquivalente'

--- people/alessandrociquivalente/countries.py
+++
@@ -1,5 +0,0 @@
-countries_dict = {
-  'IT': ['Italy', 112, 117],
-  'JP': ['Japan', 20, 20],
-  'ES': ['Spain', 9, 11]
-}
--- people/alessandrociquivalente/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '143552306@N02',
-  'alias': 'alessandrociquivalente',
-  'name': 'Alessandro Valente',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/143552306@N02_r.jpg',
-  'url': 'https://www.flickr.com/photos/alessandrociquivalente/',
-  'markers': 141,
-  'photos': 148
-}
