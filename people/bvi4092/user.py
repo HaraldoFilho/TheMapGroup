5789219db6f7070f1c4de28e3eeba0d4f4e31fb6
The Map Group <admin@the-map-group.pictures>
2020-10-28 02:23:12 -0300

    Updated map for member 'bvi4092'

--- people/bvi4092/user.py
+++ people/bvi4092/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'bvi4092',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/13445173@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/bvi4092/',
-  'markers': 534,
-  'photos': 1030
+  'markers': 536,
+  'photos': 1032
 }
