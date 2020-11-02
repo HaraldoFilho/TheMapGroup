ab5b803468137700d027bdbe2b0783b998805215
The Map Group <admin@the-map-group.pictures>
2020-11-02 02:25:23 -0300

    Updated map for member 'letang.gilles'

--- people/132920569@N02/user.py
+++ people/132920569@N02/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/132920569@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/132920569@N02/',
   'markers': 169,
-  'photos': 188
+  'photos': 189
 }
