69edd11d25cd4afbada77bc6221af992c3a6eaf5
The Map Group <admin@the-map-group.pictures>
2020-11-02 02:56:06 -0300

    Updated map for member 'Harald Schulz'

---
+++ people/121195437@N06/countries.py
@@ -0,0 +1,20 @@
+countries_dict = {
+  'IT': ['Italy', 2, 17],
+  'DE': ['Germany', 556, 988],
+  'ES': ['Spain', 182, 216],
+  'US': ['United States of America', 97, 444],
+  'FR': ['France', 178, 246],
+  'IS': ['Iceland', 102, 250],
+  'AT': ['Austria', 1, 1],
+  'CH': ['Switzerland', 100, 132],
+  'SE': ['Sweden', 2, 2],
+  'NL': ['The Netherlands', 29, 40],
+  'IE': ['Ireland', 1, 1],
+  'PT': ['Portugal', 2, 2],
+  'CA': ['Canada', 5, 116],
+  'GR': ['Greece', 3, 3],
+  'SI': ['Slovenia', 3, 3],
+  'NO': ['Norway', 4, 4],
+  'HU': ['Hungary', 1, 1],
+  'HR': ['Croatia', 1, 1]
+}
