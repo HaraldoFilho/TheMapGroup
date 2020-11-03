e63c96ec71e96b99ae8cd88b7eba876f1da10b54
The Map Group <admin@the-map-group.pictures>
2020-11-03 03:18:52 -0300

    Updated map for member 'Suni Lynn Lee'

--- people/slynnlee/countries.py
+++ people/slynnlee/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
   'GB': ['United Kingdom', 154, 1455],
+  'IE': ['Ireland', 154, 724],
   'IT': ['Italy', 1, 1],
-  'US': ['United States of America', 151, 1362],
-  'DE': ['Germany', 28, 34],
   'VA': ['Vatican City', 1, 1],
   'CA': ['Canada', 1, 39],
-  'IE': ['Ireland', 154, 724]
+  'DE': ['Germany', 28, 34],
+  'US': ['United States of America', 152, 1364]
 }
