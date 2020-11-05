579a7f8aa39d39a68d3a8c16f2f0e5e7e50a9752
The Map Group <admin@the-map-group.pictures>
2020-11-05 02:36:35 -0300

    Updated map for member 'Suni Lynn Lee'

--- people/slynnlee/countries.py
+++ people/slynnlee/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'GB': ['United Kingdom', 154, 1455],
-  'IE': ['Ireland', 154, 724],
-  'IT': ['Italy', 1, 1],
   'VA': ['Vatican City', 1, 1],
+  'GB': ['United Kingdom', 154, 1455],
   'CA': ['Canada', 1, 39],
-  'DE': ['Germany', 28, 34],
-  'US': ['United States of America', 152, 1364]
+  'US': ['United States of America', 155, 1367],
+  'IT': ['Italy', 1, 1],
+  'IE': ['Ireland', 154, 724],
+  'DE': ['Germany', 28, 34]
 }
--- people/slynnlee/user.py
+++ people/slynnlee/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Suni Lynn Lee',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/144075308@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/slynnlee/',
-  'markers': 491,
-  'photos': 3618
+  'markers': 494,
+  'photos': 3621
 }
