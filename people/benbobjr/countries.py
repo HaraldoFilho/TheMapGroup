d73e4ec3553a06c59432bba1a69e156f866909e9
The Map Group <admin@the-map-group.pictures>
2020-11-08 05:10:12 -0300

    Updated map for member 'benbobjr'

---
+++ people/benbobjr/countries.py
@@ -0,0 +1,11 @@
+countries_dict = {
+  'AE': ['United Arab Emirates', 167, 175],
+  'BE': ['Belgium', 65, 114],
+  'NL': ['The Netherlands', 29, 33],
+  'AU': ['Australia', 357, 396],
+  'DK': ['Denmark', 9, 12],
+  'GB': ['United Kingdom', 31843, 36786],
+  'SE': ['Sweden', 20, 54],
+  'US': ['United States of America', 9, 9],
+  'IT': ['Italy', 95, 118]
+}
---
+++ people/benbobjr/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '52559257@N02',
+  'alias': 'benbobjr',
+  'name': 'Ben Abel',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/52559257@N02_r.jpg',
+  'url': 'https://www.flickr.com/photos/benbobjr/',
+  'markers': 32594,
+  'photos': 37697
+}
