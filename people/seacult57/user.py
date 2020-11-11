36e421de5158362412444aadd7f522bffd3541d5
The Map Group <admin@the-map-group.pictures>
2020-11-11 03:02:32 -0300

    Updated map for member 'seacult'

---
+++ people/seacult57/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '137934966@N06',
+  'alias': 'seacult57',
+  'name': 'seacult seacult',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/137934966@N06_r.jpg',
+  'url': 'https://www.flickr.com/photos/seacult57/',
+  'markers': 12,
+  'photos': 12
+}
