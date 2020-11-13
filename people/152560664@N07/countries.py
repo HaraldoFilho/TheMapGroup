c01182cef6efa8d5d2b6cabe76a36949e1ce63ae
The Map Group <admin@the-map-group.pictures>
2020-11-13 02:45:32 -0300

    Updated map for member 'Jеdd'

--- people/152560664@N07/countries.py
+++ people/152560664@N07/countries.py
@@ -1,9 +1,10 @@
 countries_dict = {
-  'CZ': ['Czech Republic', 2, 2],
+  'FR': ['France', 3, 4],
   'TH': ['Thailand', 20, 26],
+  'MT': ['Malta', 1, 1],
   'PL': ['Poland', 4, 5],
   'RU': ['Russia', 25, 27],
-  'MT': ['Malta', 1, 1],
-  'ES': ['Spain', 14, 15],
-  'CH': ['Switzerland', 1, 1]
+  'CH': ['Switzerland', 1, 1],
+  'CZ': ['Czech Republic', 2, 2],
+  'ES': ['Spain', 14, 15]
 }
