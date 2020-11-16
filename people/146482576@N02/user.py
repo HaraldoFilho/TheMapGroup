2ff16ee22450985e8c252b4a17d0e6eefacc0495
The Map Group <admin@the-map-group.pictures>
2020-11-16 09:49:55 -0300

    Updated map for member 'a1-4'

---
+++ people/146482576@N02/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '146482576@N02',
+  'alias': '146482576@N02',
+  'name': 'Alexey Kozlov',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/146482576@N02_r.jpg',
+  'url': 'https://www.flickr.com/photos/146482576@N02/',
+  'markers': 0,
+  'photos': 0
+}
