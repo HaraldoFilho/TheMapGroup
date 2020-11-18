5611a0ff37edd02608a8c4e368cde44787315bf4
The Map Group <admin@the-map-group.pictures>
2020-11-18 03:13:58 -0300

    Removed member '181615223@N07'

--- people/181615223@N07/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '181615223@N07',
-  'alias': '181615223@N07',
-  'name': 'Kaspazak II',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/181615223@N07_r.jpg',
-  'url': 'https://www.flickr.com/photos/181615223@N07/',
-  'markers': 113,
-  'photos': 113
-}
