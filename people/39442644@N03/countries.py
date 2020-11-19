7622bfb08e1ab868262e4dea412b888057176f63
The Map Group <admin@the-map-group.pictures>
2020-11-19 02:49:08 -0300

    Updated map for member 'Cranky Oldman'

--- people/39442644@N03/countries.py
+++ people/39442644@N03/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'FI': ['Finland', 51, 54],
   'CN': ['South China Sea Islands (disputed)', 1, 1],
+  'FR': ['France', 34, 34],
+  'CU': ['Cuba', 1, 1],
   'PL': ['Poland', 3, 3],
   'ES': ['Spain', 102, 103],
-  'SE': ['Sweden', 1, 1],
-  'CU': ['Cuba', 1, 1],
-  'BE': ['Belgium', 2, 2],
-  'US': ['United States of America', 161, 163],
   'DE': ['Germany', 34, 35],
-  'NO': ['Norway', 8, 8],
-  'CA': ['Canada', 8, 8],
+  'US': ['United States of America', 163, 165],
+  'BE': ['Belgium', 2, 2],
+  'FI': ['Finland', 51, 54],
+  'SE': ['Sweden', 1, 1],
+  'CA': ['Canada', 9, 9],
   'CZ': ['Czech Republic', 4, 4],
-  'FR': ['France', 34, 34]
+  'NO': ['Norway', 8, 8]
 }
--- people/39442644@N03/user.py
+++ people/39442644@N03/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Cranky Oldman',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/39442644@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/39442644@N03/',
-  'markers': 410,
-  'photos': 417
+  'markers': 413,
+  'photos': 420
 }
