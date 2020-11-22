9f8b4fe7e220841036ec3179bce0aee045931e93
The Map Group <admin@the-map-group.pictures>
2020-11-22 03:53:52 -0300

    Updated map for member 'Ludo's World'

---
+++ people/ludomichel/countries.py
@@ -0,0 +1,11 @@
+countries_dict = {
+  'VN': ['Vietnam', 385, 427],
+  'BE': ['Belgium', 1627, 1848],
+  'FR': ['France', 25, 25],
+  'KH': ['Cambodia', 432, 448],
+  'DE': ['Germany', 278, 315],
+  'LU': ['Luxembourg', 38, 38],
+  'TH': ['Thailand', 246, 285],
+  'NL': ['The Netherlands', 51, 51],
+  'US': ['United States of America', 81, 85]
+}
---
+++ people/ludomichel/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '124072682@N04',
+  'alias': 'ludomichel',
+  'name': 'Lцdо\/іс M',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/124072682@N04_r.jpg',
+  'url': 'https://www.flickr.com/photos/ludomichel/',
+  'markers': 3163,
+  'photos': 3522
+}
