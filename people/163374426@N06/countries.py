7277f423dcbeecfb87459c1840e0ffc1c2ee76b9
The Map Group <admin@the-map-group.pictures>
2020-11-22 02:51:05 -0300

    Updated map for member 'xray_jets'

--- people/163374426@N06/countries.py
+++ people/163374426@N06/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'IS': ['Iceland', 3, 3],
-  'AU': ['Australia', 51, 62],
   'PE': ['Peru', 2, 2],
-  'AR': ['Argentina', 1, 1],
   'NZ': ['New Zealand', 1, 1],
-  'CL': ['Chile', 2, 3]
+  'CL': ['Chile', 2, 3],
+  'AU': ['Australia', 52, 63],
+  'IS': ['Iceland', 3, 3],
+  'AR': ['Argentina', 1, 1]
 }
