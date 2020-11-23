34cb6c95ee0b05fd62ec3226c4f11637287c44cc
The Map Group <admin@the-map-group.pictures>
2020-11-23 02:54:08 -0300

    Updated map for member 'Joao de Mio'

---
+++ people/23402613@N03/countries.py
@@ -0,0 +1,5 @@
+countries_dict = {
+  'BR': ['Brazil', 66, 98],
+  'IT': ['Italy', 30, 35],
+  'CL': ['Chile', 10, 10]
+}
---
+++ people/23402613@N03/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '23402613@N03',
+  'alias': '23402613@N03',
+  'name': 'Joao de Mio',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/23402613@N03_r.jpg',
+  'url': 'https://www.flickr.com/photos/23402613@N03/',
+  'markers': 106,
+  'photos': 143
+}
