0fb2044e11f960a7c1b2d9fec55f651453940a09
The Map Group <admin@the-map-group.pictures>
2020-11-25 02:31:37 -0300

    Updated map for member 'www.jamesgreigphotographer.com'

--- people/jimgreig/countries.py
+++ people/jimgreig/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'BR': ['Brazil', 3, 3],
-  'IT': ['Italy', 1, 1],
-  'NL': ['The Netherlands', 18, 21],
-  'GB': ['United Kingdom', 2248, 2723],
-  'MA': ['Morocco', 2, 2],
   'US': ['United States of America', 1, 1],
+  'NL': ['The Netherlands', 18, 21],
+  'BR': ['Brazil', 3, 3],
+  'MA': ['Morocco', 2, 2],
+  'GB': ['United Kingdom', 2249, 2724],
+  'IT': ['Italy', 1, 1],
   'CA': ['Canada', 2, 2]
 }
