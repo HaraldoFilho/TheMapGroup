a6d168a9722673b7e43953e34ea3609db525487b
The Map Group <admin@the-map-group.pictures>
2020-11-26 19:49:00 -0300

    Updated map for member 'Rosie Chilton'

--- people/31248131@N05/countries.py
+++ people/31248131@N05/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'GB': ['United Kingdom', 267, 517]
+  'GB': ['United Kingdom', 267, 522]
 }
--- people/31248131@N05/user.py
+++ people/31248131@N05/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/31248131@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/31248131@N05/',
   'markers': 267,
-  'photos': 517
+  'photos': 522
 }
