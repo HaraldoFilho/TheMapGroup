4958424b26e699ed4915afb4792d92ee3cb12e17
The Map Group <admin@the-map-group.pictures>
2020-11-28 02:40:18 -0300

    Updated map for member 'diwan'

--- people/diwan/user.py
+++ people/diwan/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/81504125@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/diwan/',
   'markers': 7490,
-  'photos': 7919
+  'photos': 7920
 }
