b089b219977b3b0b1f1b5b28ad8707a79aba1dd8
The Map Group <admin@the-map-group.pictures>
2020-11-29 02:27:56 -0300

    Updated map for member 'kbulut58'

--- people/zoi05/user.py
+++ people/zoi05/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'kbulut58',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/7812564@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/zoi05/',
-  'markers': 193,
-  'photos': 653
+  'markers': 194,
+  'photos': 654
 }
