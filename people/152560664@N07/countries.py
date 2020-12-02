828a80604bb85fa512fa20da2c381c5f4f791a68
The Map Group <admin@the-map-group.pictures>
2020-12-02 02:59:33 -0300

    Updated map for member 'Jеdd'

--- people/152560664@N07/countries.py
+++ people/152560664@N07/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'FR': ['France', 14, 21],
-  'TH': ['Thailand', 26, 32],
-  'IT': ['Italy', 3, 3],
-  'GB': ['United Kingdom', 6, 7],
-  'CZ': ['Czech Republic', 2, 2],
-  'ES': ['Spain', 26, 31],
-  'CH': ['Switzerland', 1, 1],
-  'RU': ['Russia', 45, 50],
   'MT': ['Malta', 1, 1],
+  'TH': ['Thailand', 29, 35],
+  'FR': ['France', 14, 21],
+  'ES': ['Spain', 26, 31],
+  'RU': ['Russia', 45, 50],
+  'PL': ['Poland', 6, 8],
+  'GB': ['United Kingdom', 6, 7],
   'VA': ['Vatican City', 1, 1],
-  'PL': ['Poland', 6, 8]
+  'CZ': ['Czech Republic', 2, 2],
+  'IT': ['Italy', 3, 3],
+  'CH': ['Switzerland', 1, 1]
 }
--- people/152560664@N07/user.py
+++ people/152560664@N07/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Jed Jedd',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/152560664@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/152560664@N07/',
-  'markers': 131,
-  'photos': 157
+  'markers': 134,
+  'photos': 160
 }
