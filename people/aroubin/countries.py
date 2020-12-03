179487cfe688288fe1bef234bba81d48ff24ece7
The Map Group <admin@the-map-group.pictures>
2020-12-03 02:51:56 -0300

    Updated map for member 'aroubin - Yay! 3 MILLION views!'

--- people/aroubin/countries.py
+++ people/aroubin/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
+  'US': ['United States of America', 1314, 3330],
   'DO': ['Dominican Republic', 37, 401],
-  'MC': ['Monaco', 3, 56],
   'FR': ['France', 44, 3125],
+  'MC': ['Monaco', 3, 56],
   'IT': ['Italy', 2, 75],
-  'CA': ['Canada', 15, 17],
-  'US': ['United States of America', 1313, 3329]
+  'CA': ['Canada', 15, 17]
 }
--- people/aroubin/user.py
+++ people/aroubin/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'aroubin - Yay! 3 MILLION views',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/37775553@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/aroubin/',
-  'markers': 1414,
-  'photos': 7003
+  'markers': 1415,
+  'photos': 7004
 }
