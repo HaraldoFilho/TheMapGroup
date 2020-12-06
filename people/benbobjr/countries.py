f9fe9a5a6a801e57902ff6fcfcd604b195b83790
The Map Group <admin@the-map-group.pictures>
2020-12-06 02:50:39 -0300

    Updated map for member 'benbobjr'

--- people/benbobjr/countries.py
+++ people/benbobjr/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
+  'GB': ['United Kingdom', 31961, 36959],
   'BE': ['Belgium', 65, 116],
-  'AU': ['Australia', 357, 396],
-  'US': ['United States of America', 9, 9],
+  'AE': ['United Arab Emirates', 167, 175],
   'SE': ['Sweden', 20, 54],
   'DK': ['Denmark', 9, 12],
-  'GB': ['United Kingdom', 31922, 36911],
-  'IT': ['Italy', 97, 122],
   'NL': ['The Netherlands', 29, 33],
-  'AE': ['United Arab Emirates', 167, 175]
+  'AU': ['Australia', 357, 396],
+  'IT': ['Italy', 97, 123],
+  'US': ['United States of America', 9, 9]
 }
