7496347cdd01e33f89c0ee032fec4009ccf01582
The Map Group <admin@the-map-group.pictures>
2020-12-07 02:51:06 -0300

    Updated map for member 'Susie_D52'

--- people/susannemichelle/countries.py
+++ people/susannemichelle/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'US': ['United States of America', 158, 3203]
+  'US': ['United States of America', 159, 3204]
 }
--- people/susannemichelle/user.py
+++ people/susannemichelle/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Susie',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/21593175@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/susannemichelle/',
-  'markers': 158,
-  'photos': 3203
+  'markers': 159,
+  'photos': 3204
 }
