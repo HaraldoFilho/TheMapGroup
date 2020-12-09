88813bf6aba415df608f209ffc6bd3dbb6703ceb
The Map Group <admin@the-map-group.pictures>
2020-12-09 02:34:27 -0300

    Updated map for member 'K@moeiras'

--- people/kamo_kamoeiras/countries.py
+++ people/kamo_kamoeiras/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'PT': ['Portugal', 10, 17],
+  'MX': ['Mexico', 1, 2],
   'AD': ['Andorra', 1, 1],
-  'ES': ['Spain', 853, 1296],
-  'MX': ['Mexico', 1, 2]
+  'PT': ['Portugal', 10, 17],
+  'ES': ['Spain', 857, 1300]
 }
--- people/kamo_kamoeiras/user.py
+++ people/kamo_kamoeiras/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'KAMO KAMOEIRAS',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/163799240@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/kamo_kamoeiras/',
-  'markers': 865,
-  'photos': 1316
+  'markers': 869,
+  'photos': 1320
 }
