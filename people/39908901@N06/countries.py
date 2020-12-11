3d9e7d2433c65a0fabf5f200ec8cf0cdbad65679
The Map Group <admin@the-map-group.pictures>
2020-12-11 02:33:57 -0300

    Updated map for member 'm01229'

--- people/39908901@N06/countries.py
+++ people/39908901@N06/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'MV': ['Maldives', 12, 24],
-  'ES': ['Spain', 9, 27],
-  'NL': ['The Netherlands', 14, 32],
-  'IN': ['India', 72, 473],
-  'CA': ['Canada', 188, 419],
-  'TH': ['Thailand', 12, 82],
-  'PT': ['Portugal', 73, 298],
-  'US': ['United States of America', 5804, 21691],
-  'LK': ['Sri Lanka', 14, 80],
   'PK': ['Pakistan', 1, 2],
+  'ES': ['Spain', 9, 27],
+  'IN': ['India', 72, 473],
+  'NL': ['The Netherlands', 14, 32],
+  'LK': ['Sri Lanka', 14, 80],
+  'PT': ['Portugal', 73, 298],
+  'TH': ['Thailand', 12, 82],
+  'CA': ['Canada', 188, 419],
+  'US': ['United States of America', 5820, 21737],
+  'MV': ['Maldives', 12, 24],
   'FR': ['France', 2, 7]
 }
--- people/39908901@N06/user.py
+++ people/39908901@N06/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'm01229',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/39908901@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/39908901@N06/',
-  'markers': 6201,
-  'photos': 23135
+  'markers': 6217,
+  'photos': 23181
 }
