d0ef3f33e77f3eafc005210a9849be8f01281cf3
The Map Group <admin@the-map-group.pictures>
2020-12-13 02:19:27 -0300

    Updated map for member 'karen_ds1101'

--- people/karen_ds1101/countries.py
+++ people/karen_ds1101/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'MY': ['Malaysia', 8, 11],
-  'IE': ['Ireland', 3, 3],
-  'BN': ['Brunei Darussalam', 2, 3],
-  'DE': ['Germany', 1, 1],
-  'GB': ['United Kingdom', 4, 4],
-  'NZ': ['New Zealand', 10, 10],
-  'US': ['United States of America', 11, 19],
-  'JP': ['Japan', 10, 10],
   'PE': ['Peru', 11, 16],
-  'CN': ['South China Sea Islands (disputed)', 79, 125],
-  'AU': ['Australia', 749, 873],
+  'IE': ['Ireland', 3, 3],
+  'US': ['United States of America', 11, 19],
+  'NZ': ['New Zealand', 10, 10],
   'CL': ['Chile', 5, 9],
+  'IT': ['Italy', 5, 6],
+  'JP': ['Japan', 10, 10],
+  'MY': ['Malaysia', 8, 11],
+  'BN': ['Brunei Darussalam', 2, 3],
+  'CN': ['South China Sea Islands (disputed)', 79, 125],
+  'GB': ['United Kingdom', 4, 4],
   'SG': ['Singapore', 66, 78],
-  'IT': ['Italy', 5, 6]
+  'AU': ['Australia', 752, 876],
+  'DE': ['Germany', 1, 1]
 }
--- people/karen_ds1101/user.py
+++ people/karen_ds1101/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Karen',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/51073972@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/karen_ds1101/',
-  'markers': 964,
-  'photos': 1168
+  'markers': 967,
+  'photos': 1171
 }
