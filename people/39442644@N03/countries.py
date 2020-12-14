10bc0179ade2b6b5b91b97d5c0c77ba69d758fe9
The Map Group <admin@the-map-group.pictures>
2020-12-14 02:16:12 -0300

    Updated map for member 'Cranky Oldman'

--- people/39442644@N03/countries.py
+++ people/39442644@N03/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'FI': ['Finland', 56, 59],
-  'NO': ['Norway', 8, 8],
-  'CA': ['Canada', 12, 12],
-  'FR': ['France', 34, 34],
   'CU': ['Cuba', 1, 1],
-  'BE': ['Belgium', 3, 3],
-  'CZ': ['Czech Republic', 4, 4],
   'IT': ['Italy', 1, 1],
   'DE': ['Germany', 37, 38],
-  'ES': ['Spain', 111, 113],
-  'US': ['United States of America', 182, 184],
-  'PL': ['Poland', 4, 4],
+  'NO': ['Norway', 8, 8],
+  'BE': ['Belgium', 3, 3],
   'CN': ['South China Sea Islands (disputed)', 1, 1],
-  'SE': ['Sweden', 1, 1]
+  'FI': ['Finland', 57, 60],
+  'ES': ['Spain', 112, 114],
+  'CZ': ['Czech Republic', 4, 4],
+  'PL': ['Poland', 4, 4],
+  'US': ['United States of America', 182, 184],
+  'FR': ['France', 34, 34],
+  'SE': ['Sweden', 1, 1],
+  'CA': ['Canada', 12, 12]
 }
--- people/39442644@N03/user.py
+++ people/39442644@N03/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Cranky Oldman',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/39442644@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/39442644@N03/',
-  'markers': 455,
-  'photos': 463
+  'markers': 457,
+  'photos': 465
 }
