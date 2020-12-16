7cabe23e5cdd953592fef88ec12b8dc2f2703e7e
The Map Group <admin@the-map-group.pictures>
2020-12-16 02:15:38 -0300

    Updated map for member 'javiernoval'

--- people/jnoval/countries.py
+++ people/jnoval/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'GB': ['United Kingdom', 17, 18],
-  'IT': ['Italy', 4, 5],
-  'AT': ['Austria', 1, 1],
-  'HR': ['Croatia', 1, 1],
-  'PT': ['Portugal', 3, 3],
-  'SE': ['Sweden', 16, 18],
   'US': ['United States of America', 2, 2],
-  'ES': ['Spain', 114, 263],
-  'DE': ['Germany', 127, 181],
+  'GB': ['United Kingdom', 17, 18],
+  'SE': ['Sweden', 16, 18],
+  'IT': ['Italy', 4, 5],
+  'PT': ['Portugal', 3, 3],
   'GR': ['Greece', 7, 8],
-  'FR': ['France', 15, 18]
+  'ES': ['Spain', 114, 263],
+  'FR': ['France', 15, 18],
+  'AT': ['Austria', 1, 1],
+  'DE': ['Germany', 128, 182],
+  'HR': ['Croatia', 1, 1]
 }
