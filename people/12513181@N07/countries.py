eeb55181ba8d7b9234f6c89b5f0449cdfb27c8d9
The Map Group <admin@the-map-group.pictures>
2020-12-17 02:32:34 -0300

    Updated map for member 'InnAtElmwood'

---
+++ people/12513181@N07/countries.py
@@ -0,0 +1,6 @@
+countries_dict = {
+  'US': ['United States', 502, 653],
+  'FR': ['France', 1, 1],
+  'TC': ['Turks and Caicos Islands', 9, 18],
+  'GR': ['Greece', 1, 1]
+}
---
+++ people/12513181@N07/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '12513181@N07',
+  'alias': '12513181@N07',
+  'name': 'K & R',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/12513181@N07_r.jpg',
+  'url': 'https://www.flickr.com/photos/12513181@N07/',
+  'markers': 513,
+  'photos': 673
+}
