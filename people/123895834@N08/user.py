9c7a49e6be9122e5135184ef20e3092cc989ad86
The Map Group <admin@the-map-group.pictures>
2020-12-18 02:13:13 -0300

    Updated map for member 'tquist24'

--- people/123895834@N08/user.py
+++ people/123895834@N08/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/123895834@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/123895834@N08/',
   'markers': 2365,
-  'photos': 3236
+  'photos': 3237
 }
