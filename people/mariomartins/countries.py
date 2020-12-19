6c385e9fc844b642dd751e78ee2b7c4656f2aa46
The Map Group <admin@the-map-group.pictures>
2020-12-19 02:19:43 -0300

    Updated map for member 'pepemartin2006'

--- people/mariomartins/countries.py
+++ people/mariomartins/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'PT': ['Portugal', 15, 38],
-  'FR': ['France', 5, 5],
-  'ES': ['Spain', 50, 82]
+  'PT': ['Portugal', 16, 39],
+  'ES': ['Spain', 50, 82],
+  'FR': ['France', 5, 5]
 }
--- people/mariomartins/user.py
+++ people/mariomartins/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Mário  Martins (Pepe Martín)',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/188218905@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/mariomartins/',
-  'markers': 70,
-  'photos': 125
+  'markers': 71,
+  'photos': 126
 }
