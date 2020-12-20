1b6c2bcb3987c663e87f649cfb0d6c2cd895c6e1
The Map Group <admin@the-map-group.pictures>
2020-12-20 02:05:42 -0300

    Updated map for member 'NebraskaSC (16 Million Views TU Everyone!)'

--- people/nebraskasc/countries.py
+++ people/nebraskasc/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'US': ['United States of America', 1932, 17714]
+  'US': ['United States of America', 1958, 17859]
 }
--- people/nebraskasc/user.py
+++ people/nebraskasc/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Dale  Kaminski',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/14320768@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/nebraskasc/',
-  'markers': 1932,
-  'photos': 17714
+  'markers': 1958,
+  'photos': 17859
 }
