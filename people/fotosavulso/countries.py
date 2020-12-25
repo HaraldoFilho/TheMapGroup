6f02008e8b88f07c35a607b19ea240159cf4ba4e
The Map Group <admin@the-map-group.pictures>
2020-12-25 02:41:11 -0300

    Updated map for member 'Fotos Avulso'

---
+++ people/fotosavulso/countries.py
@@ -0,0 +1,10 @@
+countries_dict = {
+  'AT': ['Austria', 1, 1],
+  'TL': ['East Timor', 1, 1],
+  'FR': ['France', 57, 59],
+  'PT': ['Portugal', 274, 293],
+  'ST': ['São Tomé and Príncipe', 71, 75],
+  'AD': ['Andorra', 2, 2],
+  'US': ['United States of America', 1, 2],
+  'ES': ['Spain', 20, 22]
+}
