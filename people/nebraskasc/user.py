1130e227019adf53e489de0146b577815bc3adcd
The Map Group <admin@the-map-group.pictures>
2020-12-27 02:42:14 -0300

    Removed member 'nebraskasc'

--- people/nebraskasc/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '14320768@N07',
-  'alias': 'nebraskasc',
-  'name': 'Dale  Kaminski',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/14320768@N07_r.jpg',
-  'url': 'https://www.flickr.com/photos/nebraskasc/',
-  'markers': 1958,
-  'photos': 17860
-}
