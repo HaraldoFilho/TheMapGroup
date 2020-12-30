e2706a0af28e5c238e7f6a8546d9a99d0d310069
The Map Group <admin@the-map-group.pictures>
2020-12-30 03:26:44 -0300

    Updated map for member '::ErWin'

---
+++ people/lenzmoser/countries.py
@@ -0,0 +1,21 @@
+countries_dict = {
+  'PT': ['Portugal', 81, 81],
+  'RS': ['Serbia', 3, 3],
+  'SK': ['Slovakia', 46, 46],
+  'BG': ['Bulgaria', 44, 44],
+  'NO': ['Norway', 52, 53],
+  'IN': ['India', 15, 15],
+  'NP': ['Nepal', 118, 122],
+  'FR': ['France', 85, 91],
+  'CZ': ['Czechia', 80, 87],
+  'ES': ['Spain', 359, 359],
+  'DE': ['Germany', 36, 36],
+  'HU': ['Hungary', 1, 1],
+  'MA': ['Morocco', 99, 105],
+  'GE': ['Georgia', 97, 102],
+  'SI': ['Slovenia', 10, 10],
+  'VA': ['Vatican City', 7, 8],
+  'IT': ['Italy', 439, 450],
+  'CH': ['Switzerland', 41, 43],
+  'AT': ['Austria', 2455, 2526]
+}
---
+++ people/lenzmoser/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '123096458@N03',
+  'alias': 'lenzmoser',
+  'name': '::ErWin',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/123096458@N03_r.jpg',
+  'url': 'https://www.flickr.com/photos/lenzmoser/',
+  'markers': 4068,
+  'photos': 4182
+}
