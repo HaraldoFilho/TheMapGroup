baead1b72ab04efebba150f3b4b44660290d2673
The Map Group <admin@the-map-group.pictures>
2021-01-02 02:39:33 -0300

    Updated map for member 'Atila Yumusakkaya'

---
+++ people/atila_y/countries.py
@@ -0,0 +1,23 @@
+countries_dict = {
+  'HU': ['Hungary', 16, 17],
+  'PL': ['Poland', 1, 3],
+  'CZ': ['Czechia', 7, 9],
+  'DK': ['Denmark', 3, 3],
+  'BE': ['Belgium', 1, 1],
+  'NO': ['Norway', 86, 87],
+  'US': ['United States of America', 23, 27],
+  'DE': ['Germany', 12, 17],
+  'AD': ['Andorra', 1, 1],
+  'IT': ['Italy', 16, 18],
+  'CH': ['Switzerland', 30, 30],
+  'GR': ['Greece', 59, 72],
+  'SE': ['Sweden', 1, 1],
+  'AE': ['United Arab Emirates', 108, 129],
+  'MC': ['Monaco', 13, 13],
+  'FR': ['France', 100, 106],
+  'CY': ['Cyprus', 3, 3],
+  'SK': ['Slovakia', 19, 22],
+  'TR': ['Turkey', 467, 526],
+  'AT': ['Austria', 136, 155],
+  'ES': ['Spain', 13, 16]
+}
---
+++ people/atila_y/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '8009375@N03',
+  'alias': 'atila_y',
+  'name': 'Atila Yumusakkaya',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/8009375@N03_r.jpg',
+  'url': 'https://www.flickr.com/photos/atila_y/',
+  'markers': 1115,
+  'photos': 1256
+}
