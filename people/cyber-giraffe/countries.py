7427208c2308ed2048b625aa6efb0eb2a3a7494e
The Map Group <admin@the-map-group.pictures>
2021-01-04 03:34:23 -0300

    Updated map for member 'Endless_5piral'

---
+++ people/cyber-giraffe/countries.py
@@ -0,0 +1,4 @@
+countries_dict = {
+  'GB': ['United Kingdom', 27, 176],
+  'EG': ['Egypt', 1, 14]
+}
---
+++ people/cyber-giraffe/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '32142243@N04',
+  'alias': 'cyber-giraffe',
+  'name': 'Endless_5piral',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/32142243@N04_r.jpg',
+  'url': 'https://www.flickr.com/photos/cyber-giraffe/',
+  'markers': 28,
+  'photos': 190
+}
