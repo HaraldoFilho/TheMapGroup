16a801392f0291c086d15dcf845ac9a6d91902c9
The Map Group <admin@the-map-group.pictures>
2021-01-09 02:06:38 -0300

    Updated map for member 'Brit 70013 fan'

--- people/93936890@N02/user.py
+++ people/93936890@N02/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/93936890@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/93936890@N02/',
   'markers': 650,
-  'photos': 949
+  'photos': 950
 }
