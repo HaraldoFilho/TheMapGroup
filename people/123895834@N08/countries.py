09364a88048bd6bbf62a2b70ea573fb8ffdf0a5f
The Map Group <admin@the-map-group.pictures>
2021-01-10 02:21:01 -0300

    Updated map for member 'tquist24'

--- people/123895834@N08/countries.py
+++ people/123895834@N08/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'PR': ['Puerto Rico', 4, 4],
-  'US': ['United States of America', 2319, 3162],
-  'CU': ['Cuba', 3, 3],
-  'CA': ['Canada', 10, 12],
   'NL': ['The Netherlands', 19, 21],
+  'CA': ['Canada', 10, 12],
+  'CU': ['Cuba', 3, 3],
   'BS': ['Bahamas', 12, 12],
   'AI': ['Anguilla', 7, 9],
-  'DO': ['Dominican Republic', 4, 33]
+  'PR': ['Puerto Rico', 4, 4],
+  'DO': ['Dominican Republic', 4, 33],
+  'US': ['United States of America', 2320, 3163]
 }
--- people/123895834@N08/user.py
+++ people/123895834@N08/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'tquist24',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/123895834@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/123895834@N08/',
-  'markers': 2378,
-  'photos': 3256
+  'markers': 2379,
+  'photos': 3257
 }
