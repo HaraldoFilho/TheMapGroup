4d6a84ed3a2166a84b3445acc1d69a286c05484b
The Map Group <admin@the-map-group.pictures>
2021-01-16 02:22:31 -0300

    Updated map for member 'Fernando Forniés Revuelta'

--- people/darth-fer-75/countries.py
+++ people/darth-fer-75/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'IT': ['Italy', 131, 523],
-  'ES': ['Spain', 767, 4994],
-  'FR': ['France', 180, 793],
-  'EG': ['Egypt', 88, 515],
-  'PH': ['Philippines', 19, 418],
-  'GB': ['United Kingdom', 52, 529],
+  'ES': ['Spain', 791, 5048],
   'DE': ['Germany', 133, 343],
-  'MT': ['Malta', 129, 524],
-  'VA': ['Vatican City', 4, 59],
   'AT': ['Austria', 63, 270],
-  'QA': ['Qatar', 1, 10]
+  'QA': ['Qatar', 1, 10],
+  'VA': ['Vatican City', 4, 59],
+  'FR': ['France', 180, 793],
+  'IT': ['Italy', 131, 523],
+  'EG': ['Egypt', 88, 515],
+  'MT': ['Malta', 129, 524],
+  'PH': ['Philippines', 19, 418],
+  'GB': ['United Kingdom', 52, 529]
 }
--- people/darth-fer-75/user.py
+++ people/darth-fer-75/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Fernando Forniés Revuelta',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/30148069@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/darth-fer-75/',
-  'markers': 1567,
-  'photos': 8978
+  'markers': 1591,
+  'photos': 9032
 }
