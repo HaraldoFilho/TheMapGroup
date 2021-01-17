b3957b8406263bb7ab0893982880fe500da31c28
The Map Group <admin@the-map-group.pictures>
2021-01-17 02:59:42 -0300

    Updated map for member 'TIA International Photography'

--- people/tiascapes/countries.py
+++ people/tiascapes/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'AR': ['Argentina', 41, 43],
   'FR': ['France', 23, 23],
   'GB': ['United Kingdom', 21, 21],
+  'GH': ['Ghana', 2, 2],
   'CA': ['Canada', 83, 98],
+  'AU': ['Australia', 11, 12],
   'MY': ['Malaysia', 7, 8],
   'CN': ['South China Sea Islands (disputed)', 51, 51],
-  'GH': ['Ghana', 2, 2],
-  'US': ['United States of America', 615, 944],
-  'AU': ['Australia', 11, 12],
   'SG': ['Singapore', 7, 7],
-  'MX': ['Mexico', 4, 4]
+  'MX': ['Mexico', 4, 4],
+  'US': ['United States of America', 615, 945],
+  'AR': ['Argentina', 41, 43]
 }
