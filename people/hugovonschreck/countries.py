bd7f78f4929cb9df987776c1f03616132bcc2745
The Map Group <admin@the-map-group.pictures>
2021-01-27 02:39:56 -0300

    [auto] Updated map for member 'Hugo von Schreck'

--- people/hugovonschreck/countries.py
+++ people/hugovonschreck/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'NL': ['Netherlands', 40, 67],
+  'FR': ['France', 91, 194],
+  'HR': ['Croatia', 7, 17],
+  'DE': ['Germany', 1251, 4516],
   'IT': ['Italy', 71, 140],
   'LU': ['Luxembourg', 6, 15],
-  'FR': ['France', 90, 193],
-  'DE': ['Germany', 1251, 4516],
   'CH': ['Switzerland', 17, 38],
-  'HR': ['Croatia', 7, 17],
-  'AT': ['Austria', 16, 25]
+  'AT': ['Austria', 16, 25],
+  'NL': ['Netherlands', 40, 67]
 }
--- people/hugovonschreck/user.py
+++ people/hugovonschreck/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Hugo von Schreck',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/74758246@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/hugovonschreck/',
-  'markers': 1498,
-  'photos': 5011
+  'markers': 1499,
+  'photos': 5012
 }
