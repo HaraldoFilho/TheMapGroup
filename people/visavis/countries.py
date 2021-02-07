1978c54e4b6fdc98b854280f44676cf91e69645b
The Map Group <admin@the-map-group.pictures>
2021-02-07 02:46:03 -0300

    [auto] Updated map for member 'Visavis..'

---
+++ people/visavis/countries.py
@@ -0,0 +1,22 @@
+countries_dict = {
+  'UA': ['Ukraine', 121, 135],
+  'IT': ['Italy', 11, 11],
+  'GR': ['Greece', 1, 1],
+  'DE': ['Germany', 2, 2],
+  'LT': ['Lithuania', 2, 2],
+  'TR': ['Turkey', 7, 7],
+  'US': ['United States of America', 81, 82],
+  'GE': ['Georgia', 9, 10],
+  'BY': ['Belarus', 2, 2],
+  'AT': ['Austria', 1, 1],
+  'DK': ['Denmark', 1, 1],
+  'LV': ['Latvia', 1, 1],
+  'CA': ['Canada', 12, 13],
+  'SE': ['Sweden', 1, 1],
+  'FR': ['France', 14, 14],
+  'PT': ['Portugal', 67, 78],
+  'AD': ['Andorra', 1, 1],
+  'HU': ['Hungary', 2, 2],
+  'CH': ['Switzerland', 6, 6],
+  'ES': ['Spain', 34, 35]
+}
