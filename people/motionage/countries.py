8d607e4c0f4c9f73b9a4c6e7fbd7dcf4b6b1d369
The Map Group <admin@the-map-group.pictures>
2021-02-12 02:43:59 -0300

    [auto] Updated map for member 'asarstudios'

---
+++ people/motionage/countries.py
@@ -0,0 +1,7 @@
+countries_dict = {
+  'NZ': ['New Zealand', 1, 2],
+  'DE': ['Germany', 2, 2],
+  'US': ['United States of America', 32, 46],
+  'GB': ['United Kingdom', 3, 36],
+  'TR': ['Turkey', 1, 1]
+}
---
+++ people/motionage/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '59509714@N06',
+  'alias': 'motionage',
+  'name': 'Ahmet Asar',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/59509714@N06_r.jpg',
+  'url': 'https://www.flickr.com/photos/motionage/',
+  'markers': 39,
+  'photos': 87
+}
