0374ce19ec1acf636b805c0649ad5b97c321b3fd
The Map Group <admin@the-map-group.pictures>
2021-02-17 03:15:13 -0300

    [auto] Updated map for member 'Ludo's World'

--- people/ludomichel/countries.py
+++ people/ludomichel/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'BE': ['Belgium', 1636, 1857],
-  'DE': ['Germany', 278, 315],
   'VN': ['Vietnam', 385, 427],
+  'TH': ['Thailand', 246, 285],
+  'DE': ['Germany', 278, 315],
+  'KH': ['Cambodia', 432, 448],
   'LU': ['Luxembourg', 38, 38],
+  'BE': ['Belgium', 1637, 1858],
   'US': ['United States of America', 81, 85],
   'FR': ['France', 25, 25],
-  'TH': ['Thailand', 246, 285],
-  'KH': ['Cambodia', 432, 448],
   'NL': ['The Netherlands', 51, 51]
 }
