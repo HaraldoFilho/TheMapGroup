5e26e7dd6ce06a3776ed275eec76a6b54742a14b
The Map Group <admin@the-map-group.pictures>
2021-02-21 02:52:20 -0300

    [auto] Updated map for member 'Dmitry Djouce'

--- people/nothingpersonal/countries.py
+++ people/nothingpersonal/countries.py
@@ -1,18 +1,18 @@
 countries_dict = {
-  'VA': ['Vatican City', 2, 3],
-  'FR': ['France', 399, 441],
-  'BE': ['Belgium', 53, 56],
+  'HU': ['Hungary', 37, 38],
   'GB': ['United Kingdom', 668, 764],
-  'CH': ['Switzerland', 31, 42],
-  'DE': ['Germany', 75, 81],
   'RU': ['Russia', 388, 516],
-  'IT': ['Italy', 226, 247],
-  'UA': ['Ukraine', 128, 137],
+  'BE': ['Belgium', 53, 56],
   'CZ': ['Czechia', 59, 59],
+  'DE': ['Germany', 75, 81],
+  'IT': ['Italy', 226, 247],
+  'LU': ['Luxembourg', 17, 19],
+  'DK': ['Denmark', 8, 12],
+  'FR': ['France', 389, 431],
   'NL': ['Netherlands', 76, 79],
   'AT': ['Austria', 45, 46],
-  'LU': ['Luxembourg', 17, 19],
-  'HU': ['Hungary', 37, 38],
   'ES': ['Spain', 180, 196],
-  'DK': ['Denmark', 8, 12]
+  'CH': ['Switzerland', 31, 42],
+  'UA': ['Ukraine', 128, 137],
+  'VA': ['Vatican City', 2, 3]
 }
--- people/nothingpersonal/user.py
+++ people/nothingpersonal/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Dmitry Djouce',
   'avatar': '../../icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/nothingpersonal/',
-  'markers': 2392,
-  'photos': 2736
+  'markers': 2382,
+  'photos': 2726
 }
