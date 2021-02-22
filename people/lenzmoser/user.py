86aeb9a4e7d63dec3fb4bb42412cf43eec27cdb0
The Map Group <admin@the-map-group.pictures>
2021-02-22 02:41:31 -0300

    [auto] Updated map for member '::ErWin'

--- people/lenzmoser/user.py
+++ people/lenzmoser/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': '::ErWin',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/123096458@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/lenzmoser/',
-  'markers': 4214,
-  'photos': 4335
+  'markers': 4216,
+  'photos': 4337
 }
