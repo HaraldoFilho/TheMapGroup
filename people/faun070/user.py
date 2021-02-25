b00d85ae8913638095779e8cbdc9441804428311
The Map Group <admin@the-map-group.pictures>
2021-02-25 02:24:26 -0300

    [auto] Updated map for member 'faun070'

--- people/faun070/user.py
+++ people/faun070/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'faun070',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/33277678@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/faun070/',
-  'markers': 16873,
-  'photos': 41272
+  'markers': 16876,
+  'photos': 41275
 }
