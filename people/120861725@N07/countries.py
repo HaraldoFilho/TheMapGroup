4f7f3130e517f90d0f59de32a75703638c92260d
The Map Group <admin@the-map-group.pictures>
2021-02-27 02:41:57 -0300

    [auto] Updated map for member 'GSB Photography'

--- people/120861725@N07/countries.py
+++ people/120861725@N07/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'AT': ['Austria', 3, 3],
-  'CH': ['Switzerland', 5, 6],
-  'DE': ['Germany', 8, 10],
+  'US': ['United States of America', 54, 62],
   'GB': ['United Kingdom', 17, 19],
-  'US': ['United States of America', 53, 61],
+  'CH': ['Switzerland', 5, 6],
+  'AT': ['Austria', 3, 3],
+  'DE': ['Germany', 8, 10],
   'IT': ['Italy', 50, 57]
 }
