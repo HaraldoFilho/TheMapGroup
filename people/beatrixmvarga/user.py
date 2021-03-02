d8d3b0e6a38e37297b8e86d4137801ead1be50bf
The Map Group <admin@the-map-group.pictures>
2021-03-02 20:40:00 -0300

    [auto] Updated map for member 'Beatrix MK'

--- people/beatrixmvarga/user.py
+++ people/beatrixmvarga/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Beatrix',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/33989856@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/beatrixmvarga/',
-  'markers': 279,
-  'photos': 279
+  'markers': 280,
+  'photos': 280
 }
