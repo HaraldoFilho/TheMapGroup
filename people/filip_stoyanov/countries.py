748c4f21d49ac67cae737241f29efb5b3bbae60f
The Map Group <admin@the-map-group.pictures>
2021-03-02 21:13:21 -0300

    [auto] Updated map for member 'Filip Stoyanov'

---
+++ people/filip_stoyanov/countries.py
@@ -0,0 +1,8 @@
+countries_dict = {
+  'AT': ['Austria', 1, 1],
+  'GR': ['Greece', 1, 1],
+  'HR': ['Croatia', 2, 2],
+  'AE': ['United Arab Emirates', 2, 2],
+  'BG': ['Bulgaria', 117, 117],
+  'IT': ['Italy', 20, 20]
+}
---
+++ people/filip_stoyanov/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '58071047@N07',
+  'alias': 'filip_stoyanov',
+  'name': 'Filip Stoyanov',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/58071047@N07_r.jpg',
+  'url': 'https://www.flickr.com/photos/filip_stoyanov/',
+  'markers': 143,
+  'photos': 143
+}
