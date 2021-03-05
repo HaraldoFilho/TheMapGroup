26636a8298f012b42cde842a1b6c916ab88f43e6
The Map Group <admin@the-map-group.pictures>
2021-03-05 12:55:20 -0300

    [auto] Updated map for member 'javiernoval'

--- people/jnoval/countries.py
+++ people/jnoval/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'GR': ['Greece', 7, 8],
-  'HR': ['Croatia', 1, 1],
-  'US': ['United States of America', 2, 2],
-  'GB': ['United Kingdom', 19, 20],
+  'DE': ['Germany', 176, 246],
   'ES': ['Spain', 116, 265],
-  'AT': ['Austria', 1, 1],
   'FR': ['France', 15, 18],
-  'SE': ['Sweden', 16, 18],
-  'IT': ['Italy', 4, 5],
   'PT': ['Portugal', 3, 3],
-  'DE': ['Germany', 174, 244]
+  'GR': ['Greece', 7, 8],
+  'SE': ['Sweden', 16, 18],
+  'GB': ['United Kingdom', 19, 20],
+  'US': ['United States of America', 2, 2],
+  'IT': ['Italy', 4, 5],
+  'AT': ['Austria', 1, 1],
+  'HR': ['Croatia', 1, 1]
 }
--- people/jnoval/user.py
+++ people/jnoval/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Javier Noval',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/66031636@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/jnoval/',
-  'markers': 358,
-  'photos': 585
+  'markers': 360,
+  'photos': 587
 }
