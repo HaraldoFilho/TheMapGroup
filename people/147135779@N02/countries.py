08c564d6ed67bb053d2a68fb4010fd01dceec20d
The Map Group <admin@the-map-group.pictures>
2021-03-06 23:01:02 -0300

    [auto] Updated map for member 'giovannicampus'

--- people/147135779@N02/countries.py
+++ people/147135779@N02/countries.py
@@ -1,18 +1,18 @@
 countries_dict = {
-  'CU': ['Cuba', 154, 281],
-  'TZ': ['Tanzania', 1, 1],
-  'AE': ['United Arab Emirates', 1, 1],
   'MV': ['Maldives', 1, 1],
-  'IN': ['India', 835, 870],
-  'US': ['United States of America', 7, 7],
+  'TZ': ['Tanzania', 1, 1],
+  'FR': ['France', 1, 1],
   'KE': ['Kenya', 9, 9],
   'VA': ['Vatican City', 9, 9],
+  'NL': ['The Netherlands', 53, 104],
   'KZ': ['Kazakhstan', 2, 2],
+  'IT': ['Italy', 831, 928],
+  'IN': ['India', 835, 870],
   'PT': ['Portugal', 1, 1],
-  'UZ': ['Uzbekistan', 240, 243],
-  'IT': ['Italy', 830, 926],
-  'RO': ['Romania', 1, 1],
-  'NL': ['The Netherlands', 53, 103],
   'LK': ['Sri Lanka', 183, 191],
-  'FR': ['France', 1, 1]
+  'UZ': ['Uzbekistan', 240, 243],
+  'CU': ['Cuba', 154, 281],
+  'US': ['United States of America', 7, 7],
+  'RO': ['Romania', 1, 1],
+  'AE': ['United Arab Emirates', 1, 1]
 }
