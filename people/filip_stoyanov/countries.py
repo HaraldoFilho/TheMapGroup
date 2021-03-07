7e9f8780bec69a0e675007cea40dd0b8a198b369
The Map Group <admin@the-map-group.pictures>
2021-03-07 12:28:47 -0300

    [auto] Updated map for member 'Filip Stoyanov'

--- people/filip_stoyanov/countries.py
+++ people/filip_stoyanov/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
   'AT': ['Austria', 1, 1],
-  'GR': ['Greece', 1, 1],
-  'HR': ['Croatia', 2, 2],
+  'BG': ['Bulgaria', 121, 121],
   'AE': ['United Arab Emirates', 2, 2],
-  'BG': ['Bulgaria', 117, 117],
-  'IT': ['Italy', 20, 20]
+  'IT': ['Italy', 20, 20],
+  'GR': ['Greece', 1, 1],
+  'HR': ['Croatia', 2, 2]
 }
