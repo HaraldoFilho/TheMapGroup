ee2b0684fe9cd94be2d12e9e4f45b64a195c6274
The Map Group <admin@the-map-group.pictures>
2021-03-11 02:08:22 -0300

    [auto] Updated map for member 'sebastiánaguilar'

--- people/144107033@N04/countries.py
+++ people/144107033@N04/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'ES': ['Spain', 10750, 13811],
   'FR': ['France', 83, 119],
-  'PT': ['Portugal', 624, 767]
+  'PT': ['Portugal', 624, 767],
+  'ES': ['Spain', 10770, 13831]
 }
