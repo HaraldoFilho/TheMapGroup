26d708d73fcc6ccc029f70ecada9fc09b1554dd8
The Map Group <admin@the-map-group.pictures>
2021-03-16 03:13:57 -0300

    [auto] Updated map for member 'khoitran1957'

---
+++ people/khoitranduc/countries.py
@@ -0,0 +1,4 @@
+countries_dict = {
+  'CN': ['China', 1, 1],
+  'VN': ['Vietnam', 311, 481]
+}
---
+++ people/khoitranduc/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '45919374@N08',
+  'alias': 'khoitranduc',
+  'name': 'khoi tranduc',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/45919374@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/khoitranduc/',
+  'markers': 312,
+  'photos': 482
+}
