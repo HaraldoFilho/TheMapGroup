3cf7dceff434a9f5dc1cfb33e763f7aaff657b69
The Map Group <admin@the-map-group.pictures>
2021-03-21 02:18:12 -0300

    [auto] Updated map for member 'Cranky Oldman'

--- people/39442644@N03/countries.py
+++ people/39442644@N03/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'ES': ['Spain', 131, 133],
-  'US': ['United States of America', 231, 233],
+  'BE': ['Belgium', 4, 4],
   'NO': ['Norway', 8, 8],
+  'CN': ['South China Sea Islands (disputed)', 1, 1],
+  'FI': ['Finland', 92, 95],
+  'ES': ['Spain', 132, 134],
+  'CA': ['Canada', 12, 12],
+  'LU': ['Luxembourg', 1, 1],
+  'US': ['United States of America', 231, 233],
+  'DE': ['Germany', 47, 48],
   'IT': ['Italy', 1, 1],
   'CZ': ['Czechia', 4, 4],
-  'CA': ['Canada', 12, 12],
-  'CU': ['Cuba', 1, 1],
-  'CN': ['South China Sea Islands (disputed)', 1, 1],
-  'FR': ['France', 41, 41],
-  'FI': ['Finland', 91, 94],
-  'BE': ['Belgium', 4, 4],
+  'FR': ['France', 42, 42],
   'SE': ['Sweden', 1, 1],
-  'PL': ['Poland', 4, 4],
-  'LU': ['Luxembourg', 1, 1],
-  'DE': ['Germany', 47, 48]
+  'CU': ['Cuba', 1, 1],
+  'PL': ['Poland', 4, 4]
 }
