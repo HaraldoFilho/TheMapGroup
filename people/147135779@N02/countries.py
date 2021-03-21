744dc7888658dc9221378b08d03ba37b032c5583
The Map Group <admin@the-map-group.pictures>
2021-03-21 02:05:59 -0300

    [auto] Updated map for member 'giovannicampus'

--- people/147135779@N02/countries.py
+++ people/147135779@N02/countries.py
@@ -1,18 +1,18 @@
 countries_dict = {
-  'LK': ['Sri Lanka', 185, 194],
-  'US': ['United States of America', 7, 7],
-  'PT': ['Portugal', 1, 1],
-  'CU': ['Cuba', 163, 292],
-  'KZ': ['Kazakhstan', 2, 2],
+  'IN': ['India', 853, 888],
+  'KE': ['Kenya', 9, 9],
   'NL': ['The Netherlands', 54, 107],
   'FR': ['France', 1, 1],
-  'IN': ['India', 852, 887],
   'UZ': ['Uzbekistan', 240, 243],
+  'RO': ['Romania', 1, 1],
+  'CU': ['Cuba', 163, 293],
+  'PT': ['Portugal', 1, 1],
+  'KZ': ['Kazakhstan', 2, 2],
   'VA': ['Vatican City', 9, 9],
   'TZ': ['Tanzania', 1, 1],
-  'IT': ['Italy', 833, 933],
+  'US': ['United States of America', 7, 7],
+  'LK': ['Sri Lanka', 185, 194],
+  'IT': ['Italy', 833, 934],
   'AE': ['United Arab Emirates', 1, 1],
-  'RO': ['Romania', 1, 1],
-  'MV': ['Maldives', 1, 1],
-  'KE': ['Kenya', 9, 9]
+  'MV': ['Maldives', 1, 1]
 }
