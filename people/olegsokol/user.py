7a718141cee3e0b531bfc026f5ebf3e4cbef03e3
The Map Group <admin@the-map-group.pictures>
2021-03-25 02:17:37 -0300

    [auto] Updated map for member 'Oleg S .'

--- people/olegsokol/user.py
+++ people/olegsokol/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Oleg S',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/36531876@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/olegsokol/',
-  'markers': 500,
-  'photos': 599
+  'markers': 501,
+  'photos': 600
 }
