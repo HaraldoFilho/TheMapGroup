755c203421f64e0198b841690dd3d24fa7bf9823
The Map Group <admin@the-map-group.pictures>
2021-03-25 02:57:42 -0300

    Removed member 'filip_stoyanov'

--- people/filip_stoyanov/countries.py
+++
@@ -1,8 +0,0 @@
-countries_dict = {
-  'BG': ['Bulgaria', 136, 136],
-  'HR': ['Croatia', 2, 2],
-  'AT': ['Austria', 1, 1],
-  'IT': ['Italy', 20, 20],
-  'GR': ['Greece', 1, 1],
-  'AE': ['United Arab Emirates', 2, 2]
-}
--- people/filip_stoyanov/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '58071047@N07',
-  'alias': 'filip_stoyanov',
-  'name': 'Filip Stoyanov',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/58071047@N07_r.jpg',
-  'url': 'https://www.flickr.com/photos/filip_stoyanov/',
-  'markers': 162,
-  'photos': 162
-}
