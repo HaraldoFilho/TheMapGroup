3f2fe2f48d48c5fbd89c79c9aecc734d35e567ce
The Map Group <admin@the-map-group.pictures>
2021-04-02 02:30:30 -0300

    Removed member 'beddo86'

--- people/beddo86/countries.py
+++
@@ -1,6 +0,0 @@
-countries_dict = {
-  'IS': ['Iceland', 7, 7],
-  'ES': ['Spain', 1, 1],
-  'IE': ['Ireland', 5, 5],
-  'IT': ['Italy', 20, 20]
-}
--- people/beddo86/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '130942318@N07',
-  'alias': 'beddo86',
-  'name': 'Andrea Beddini',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/130942318@N07_r.jpg',
-  'url': 'https://www.flickr.com/photos/beddo86/',
-  'markers': 33,
-  'photos': 33
-}
