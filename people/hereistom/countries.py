62b2e0aef80a6308bf4a6d16af3abf1c484b193b
The Map Group <admin@the-map-group.pictures>
2021-04-02 02:10:51 -0300

    [auto] Updated map for member 'HereIsTom'

--- people/hereistom/countries.py
+++ people/hereistom/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'GR': ['Greece', 1, 1],
-  'SI': ['Slovenia', 1, 1],
-  'DE': ['Germany', 2, 2],
-  'TR': ['Turkey', 63, 63],
-  'PT': ['Portugal', 95, 96],
-  'BR': ['Brazil', 2, 2],
   'BE': ['Belgium', 3, 3],
-  'TH': ['Thailand', 48, 56],
-  'HR': ['Croatia', 1, 1],
-  'ES': ['Spain', 48, 48],
+  'PT': ['Portugal', 95, 96],
+  'NL': ['The Netherlands', 1204, 1303],
   'IT': ['Italy', 1, 1],
-  'NL': ['The Netherlands', 1203, 1302]
+  'SI': ['Slovenia', 1, 1],
+  'ES': ['Spain', 48, 48],
+  'HR': ['Croatia', 1, 1],
+  'DE': ['Germany', 2, 2],
+  'BR': ['Brazil', 2, 2],
+  'TH': ['Thailand', 48, 56],
+  'GR': ['Greece', 1, 1],
+  'TR': ['Turkey', 63, 63]
 }
