ddca23c9ea05767138405b0cd2b23e408e9f5e27
The Map Group <admin@the-map-group.pictures>
2021-04-07 02:51:03 -0300

    [auto] Updated map for member 'Florian106'

--- people/28330648@N03/user.py
+++ people/28330648@N03/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Florian106',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/28330648@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/28330648@N03/',
-  'markers': 3189,
-  'photos': 3760
+  'markers': 3191,
+  'photos': 3762
 }
