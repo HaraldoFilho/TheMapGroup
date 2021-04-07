7cc641f21db78b097fc3d4605d124fcf4ed9e3e5
The Map Group <admin@the-map-group.pictures>
2021-04-07 02:59:45 -0300

    [auto] Updated map for member 'chrichrilecture'

---
+++ people/191886689@N08/countries.py
@@ -0,0 +1,5 @@
+countries_dict = {
+  'IT': ['Italy', 1, 2],
+  'FR': ['France', 22, 23],
+  'PT': ['Portugal', 1, 1]
+}
---
+++ people/191886689@N08/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '191886689@N08',
+  'alias': '191886689@N08',
+  'name': 'chrichrilecture',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/191886689@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/191886689@N08/',
+  'markers': 24,
+  'photos': 26
+}
