2ab8ea288c5eacbf367c29d48a96b882914ff966
The Map Group <admin@the-map-group.pictures>
2021-04-25 02:05:14 -0300

    [auto] Updated map for member 'javiernoval'

--- people/jnoval/countries.py
+++ people/jnoval/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'US': ['United States of America', 2, 2],
-  'GR': ['Greece', 7, 8],
-  'HR': ['Croatia', 1, 1],
-  'SE': ['Sweden', 16, 18],
-  'IT': ['Italy', 4, 5],
+  'GB': ['United Kingdom', 19, 20],
   'ES': ['Spain', 116, 265],
   'PT': ['Portugal', 3, 3],
-  'AT': ['Austria', 1, 1],
-  'GB': ['United Kingdom', 19, 20],
-  'DE': ['Germany', 179, 249],
-  'FR': ['France', 15, 18]
+  'US': ['United States of America', 2, 2],
+  'IT': ['Italy', 4, 5],
+  'HR': ['Croatia', 1, 1],
+  'SE': ['Sweden', 16, 18],
+  'GR': ['Greece', 7, 8],
+  'DE': ['Germany', 180, 250],
+  'FR': ['France', 15, 18],
+  'AT': ['Austria', 1, 1]
 }
