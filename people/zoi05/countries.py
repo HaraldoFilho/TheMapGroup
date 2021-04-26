1e6e443dcd70f0424731af1540c4d8399901acad
The Map Group <admin@the-map-group.pictures>
2021-04-26 02:04:22 -0300

    [auto] Updated map for member 'kbulut58'

--- people/zoi05/countries.py
+++ people/zoi05/countries.py
@@ -1,4 +1,4 @@
 countries_dict = {
   'TR': ['Turkey', 7, 13],
-  'GR': ['Greece', 191, 718]
+  'GR': ['Greece', 192, 719]
 }
--- people/zoi05/user.py
+++ people/zoi05/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'kbulut58',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/7812564@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/zoi05/',
-  'markers': 198,
-  'photos': 731
+  'markers': 199,
+  'photos': 732
 }
