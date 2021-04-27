205c937cd150e59d971d46ccd438b49d609af7c5
The Map Group <admin@the-map-group.pictures>
2021-04-27 02:05:54 -0300

    [auto] Updated map for member 'giovannicampus'

--- people/147135779@N02/countries.py
+++ people/147135779@N02/countries.py
@@ -1,18 +1,18 @@
 countries_dict = {
   'PT': ['Portugal', 1, 1],
-  'IN': ['India', 878, 914],
+  'TZ': ['Tanzania', 1, 1],
   'KE': ['Kenya', 9, 9],
+  'UZ': ['Uzbekistan', 252, 256],
+  'LK': ['Sri Lanka', 185, 194],
+  'MV': ['Maldives', 1, 1],
+  'CU': ['Cuba', 167, 308],
+  'US': ['United States of America', 7, 7],
   'VA': ['Vatican City', 9, 9],
-  'FR': ['France', 1, 1],
   'RO': ['Romania', 1, 1],
   'KZ': ['Kazakhstan', 2, 2],
   'AE': ['United Arab Emirates', 1, 1],
-  'CU': ['Cuba', 167, 306],
-  'MV': ['Maldives', 1, 1],
-  'LK': ['Sri Lanka', 185, 194],
   'NL': ['The Netherlands', 54, 108],
-  'US': ['United States of America', 7, 7],
+  'IN': ['India', 878, 914],
   'IT': ['Italy', 839, 948],
-  'TZ': ['Tanzania', 1, 1],
-  'UZ': ['Uzbekistan', 252, 256]
+  'FR': ['France', 1, 1]
 }
