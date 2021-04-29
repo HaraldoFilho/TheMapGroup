a769087a3371fa4b60ff73728c5eeaa54f39b6a7
The Map Group <admin@the-map-group.pictures>
2021-04-29 02:51:36 -0300

    [auto] Updated map for member 'andreybogdanov'

--- people/148320346@N07/user.py
+++ people/148320346@N07/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Andrey Bogdanov',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/148320346@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/148320346@N07/',
-  'markers': 5473,
-  'photos': 5580
+  'markers': 5474,
+  'photos': 5581
 }
