a812138b5b628b7ba33e420eb2206028cb805cfd
The Map Group <admin@the-map-group.pictures>
2021-05-01 02:07:19 -0300

    [auto] Updated map for member 'hó'

--- people/iceland-ho/countries.py
+++ people/iceland-ho/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'GH': ['Ghana', 7, 7],
-  'US': ['United States of America', 6, 6],
-  'IS': ['Iceland', 2311, 2313],
   'CN': ['South China Sea Islands (disputed)', 16, 16],
-  'FO': ['Faroe Islands', 9, 9]
+  'IS': ['Iceland', 2312, 2314],
+  'FO': ['Faroe Islands', 9, 9],
+  'GH': ['Ghana', 7, 7],
+  'US': ['United States of America', 6, 6]
 }
