b6846d1f1dae98031ea3957b6a251db80d0a2ab8
The Map Group <admin@the-map-group.pictures>
2021-05-06 02:00:32 -0300

    [auto] Updated map for member 'Elena Lebedeva (IG: ellebedeva_)'

--- people/ellebedeva/user.py
+++ people/ellebedeva/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Elena Lebedeva (IG: ellebedeva',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/79292646@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/ellebedeva/',
-  'markers': 213,
-  'photos': 224
+  'markers': 214,
+  'photos': 225
 }
