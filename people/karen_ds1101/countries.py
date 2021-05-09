d6a0bae52404b7368dd3a8d53a8f3b1e95939b72
The Map Group <admin@the-map-group.pictures>
2021-05-09 02:23:25 -0300

    [auto] Updated map for member 'karen_ds1101'

--- people/karen_ds1101/countries.py
+++ people/karen_ds1101/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'MY': ['Malaysia', 8, 11],
-  'CN': ['South China Sea Islands (disputed)', 79, 125],
-  'SG': ['Singapore', 66, 78],
-  'PE': ['Peru', 11, 16],
-  'IT': ['Italy', 5, 6],
-  'AU': ['Australia', 759, 883],
-  'BN': ['Brunei Darussalam', 2, 3],
-  'GB': ['United Kingdom', 4, 4],
-  'CL': ['Chile', 5, 9],
-  'DE': ['Germany', 1, 1],
   'JP': ['Japan', 10, 10],
-  'IE': ['Ireland', 3, 3],
+  'CN': ['South China Sea Islands (disputed)', 79, 125],
   'NZ': ['New Zealand', 10, 10],
-  'US': ['United States of America', 11, 19]
+  'AU': ['Australia', 768, 892],
+  'BN': ['Brunei Darussalam', 2, 3],
+  'SG': ['Singapore', 66, 78],
+  'GB': ['United Kingdom', 4, 4],
+  'MY': ['Malaysia', 8, 11],
+  'DE': ['Germany', 1, 1],
+  'CL': ['Chile', 5, 9],
+  'US': ['United States of America', 11, 19],
+  'IT': ['Italy', 5, 6],
+  'PE': ['Peru', 11, 16],
+  'IE': ['Ireland', 3, 3]
 }
