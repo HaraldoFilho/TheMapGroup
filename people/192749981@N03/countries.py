45e68b73a363eee097bb3e6292d197afbbbdf05f
The Map Group <admin@the-map-group.pictures>
2021-05-10 02:19:03 -0300

    [auto] Updated map for member 'KAIAZ75'

--- people/192749981@N03/countries.py
+++ people/192749981@N03/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'NO': ['Norway', 5, 5],
-  'CZ': ['Czechia', 3, 3],
-  'DE': ['Germany', 12, 12],
-  'AT': ['Austria', 1, 1],
-  'IT': ['Italy', 2, 2],
   'GB': ['United Kingdom', 2, 2],
-  'FR': ['France', 8, 8]
+  'FR': ['France', 8, 8],
+  'NO': ['Norway', 5, 5],
+  'AT': ['Austria', 1, 1],
+  'CZ': ['Czechia', 3, 3],
+  'IT': ['Italy', 2, 2],
+  'DE': ['Germany', 14, 14]
 }
