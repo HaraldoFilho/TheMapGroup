8e82ad1b270144e6d5b6e007b41bc745d0f766fc
The Map Group <admin@the-map-group.pictures>
2021-05-11 02:27:43 -0300

    [auto] Updated map for member 'Joe Bob Brim'

---
+++ people/jbrimacombe/countries.py
@@ -0,0 +1,5 @@
+countries_dict = {
+  'NZ': ['New Zealand', 3, 3],
+  'AU': ['Australia', 190, 484],
+  'US': ['United States', 1, 5]
+}
---
+++ people/jbrimacombe/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '192536922@N07',
+  'alias': 'jbrimacombe',
+  'name': 'Joseph Brimacombe',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/192536922@N07_r.jpg',
+  'url': 'https://www.flickr.com/photos/jbrimacombe/',
+  'markers': 194,
+  'photos': 492
+}
