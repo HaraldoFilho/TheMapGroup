60793d1f1beecd050dd29e0f61326b0a78646ab3
The Map Group <admin@the-map-group.pictures>
2021-05-21 03:31:29 -0300

    [auto] Updated map for member 'KAIAZ75'

---
+++ people/kai-a-z/countries.py
@@ -0,0 +1,10 @@
+countries_dict = {
+  'CH': ['Switzerland', 3, 3],
+  'GB': ['United Kingdom', 2, 2],
+  'IT': ['Italy', 7, 7],
+  'DE': ['Germany', 18, 18],
+  'AT': ['Austria', 2, 2],
+  'NO': ['Norway', 5, 5],
+  'FR': ['France', 13, 13],
+  'CZ': ['Czechia', 3, 3]
+}
---
+++ people/kai-a-z/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '192749981@N03',
+  'alias': 'kai-a-z',
+  'name': 'KAIAZ75',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/192749981@N03_r.jpg',
+  'url': 'https://www.flickr.com/photos/kai-a-z/',
+  'markers': 53,
+  'photos': 53
+}
