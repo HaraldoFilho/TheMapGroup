fe9ffbd46ced64a88eeb312ee12aeeefdd285d3f
The Map Group <admin@the-map-group.pictures>
2021-05-26 02:18:39 -0300

    [auto] Updated map for member 'GMLSKIS'

--- people/38730049@N08/countries.py
+++ people/38730049@N08/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'US': ['United States of America', 763, 37577]
+  'US': ['United States of America', 769, 37613]
 }
--- people/38730049@N08/user.py
+++ people/38730049@N08/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'George Landis',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/38730049@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/38730049@N08/',
-  'markers': 763,
-  'photos': 37577
+  'markers': 769,
+  'photos': 37613
 }
