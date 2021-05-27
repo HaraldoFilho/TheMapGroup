ba8df7f00eee922ff5c4500bba23943692c10121
The Map Group <admin@the-map-group.pictures>
2021-05-27 02:29:25 -0300

    [auto] Updated map for member 'Cardwell Photo LLC | Thanks for 7 Million Views!'

---
+++ people/russellcardwell/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '55341805@N02',
+  'alias': 'russellcardwell',
+  'name': 'Russell Cardwell',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/55341805@N02_r.jpg',
+  'url': 'https://www.flickr.com/photos/russellcardwell/',
+  'markers': 833,
+  'photos': 1614
+}
