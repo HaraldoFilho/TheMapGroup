68b71c96ef2c6c5b82da427e7eabd2c381183377
The Map Group <admin@the-map-group.pictures>
2021-06-07 02:42:15 -0300

    [auto] Updated map for member 'Florian106'

--- people/28330648@N03/countries.py
+++ people/28330648@N03/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'FR': ['France', 149, 236],
-  'AT': ['Austria', 30, 37],
+  'CH': ['Switzerland', 4, 4],
   'IT': ['Italy', 196, 253],
-  'DE': ['Germany', 2923, 3347],
-  'CH': ['Switzerland', 4, 4]
+  'DE': ['Germany', 2930, 3354],
+  'FR': ['France', 149, 236],
+  'AT': ['Austria', 30, 37]
 }
