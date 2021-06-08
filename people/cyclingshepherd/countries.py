97ac2179bee08bd08a1263f1f75dbdf960f4b90e
The Map Group <admin@the-map-group.pictures>
2021-06-08 02:26:52 -0300

    [auto] Updated map for member 'cyclingshepherd'

---
+++ people/cyclingshepherd/countries.py
@@ -0,0 +1,15 @@
+countries_dict = {
+  'PT': ['Portugal', 621, 729],
+  'ZA': ['South Africa', 3, 3],
+  'IN': ['India', 1, 1],
+  'GB': ['United Kingdom', 116, 183],
+  'ER': ['Eritrea', 3, 3],
+  'VN': ['Vietnam', 4, 4],
+  'CN': ['South China Sea Islands (disputed)', 1, 1],
+  'CY': ['Cyprus', 4, 4],
+  'GR': ['Greece', 2, 2],
+  'NA': ['Namibia', 8, 8],
+  'IT': ['Italy', 9, 10],
+  'BR': ['Brazil', 4, 4],
+  'ES': ['Spain', 3, 3]
+}
