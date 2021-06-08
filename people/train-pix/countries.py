3398a517670ef054b329dcecc7d23624162f39b5
The Map Group <admin@the-map-group.pictures>
2021-06-08 03:54:02 -0300

    [auto] Updated map for member 'Ron Fisher'

---
+++ people/train-pix/countries.py
@@ -0,0 +1,19 @@
+countries_dict = {
+  'MC': ['Monaco', 3, 3],
+  'IM': ['Isle of Man', 20, 55],
+  'DE': ['Germany', 316, 359],
+  'RS': ['Serbia', 3, 3],
+  'CH': ['Switzerland', 1151, 1854],
+  'GB': ['United Kingdom', 8620, 17973],
+  'FR': ['France', 439, 644],
+  'BA': ['Bosnia and Herzegovina', 12, 20],
+  'BE': ['Belgium', 1, 1],
+  'HR': ['Croatia', 5, 9],
+  'ZA': ['South Africa', 311, 320],
+  'IT': ['Italy', 23, 41],
+  'AT': ['Austria', 27, 27],
+  'CA': ['Canada', 1, 2],
+  'SZ': ['Eswatini', 6, 7],
+  'HU': ['Hungary', 19, 28],
+  'ES': ['Spain', 1, 3]
+}
---
+++ people/train-pix/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '40052043@N03',
+  'alias': 'train-pix',
+  'name': 'Ron Fisher',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/40052043@N03_r.jpg',
+  'url': 'https://www.flickr.com/photos/train-pix/',
+  'markers': 10958,
+  'photos': 21349
+}
