d8cd2ca30c72df478b4ca3a6e3cd1d2f1c8279e3
The Map Group <admin@the-map-group.pictures>
2021-06-09 02:23:44 -0300

    [auto] Updated map for member 'Sacred_Spiral_Photography'

--- people/cyber-giraffe/user.py
+++ people/cyber-giraffe/user.py
@@ -1,9 +1,9 @@
 user_info = {
   'id': '32142243@N04',
   'alias': 'cyber-giraffe',
-  'name': 'Endless_5piral',
+  'name': 'Sacred_Spiral_Photography',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/32142243@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/cyber-giraffe/',
   'markers': 36,
-  'photos': 226
+  'photos': 227
 }
