c7b8cab22aaf492de901564bf19596c458efb317
The Map Group <admin@the-map-group.pictures>
2021-06-11 02:37:57 -0300

    [auto] Updated map for member 'khrawlings'

---
+++ people/khrawlings/countries.py
@@ -0,0 +1,21 @@
+countries_dict = {
+  'UG': ['Uganda', 7, 23],
+  'AU': ['Australia', 16, 27],
+  'RO': ['Romania', 30, 30],
+  'FR': ['France', 9, 9],
+  'VA': ['Vatican City', 19, 21],
+  'GB': ['United Kingdom', 591, 864],
+  'ES': ['Spain', 42, 46],
+  'CM': ['Cameroon', 30, 64],
+  'IN': ['India', 8, 35],
+  'RU': ['Russia', 35, 80],
+  'XK': ['Kosovo', 4, 120],
+  'MK': ['North Macedonia', 1, 4],
+  'CZ': ['Czechia', 24, 30],
+  'US': ['United States of America', 7, 9],
+  'TR': ['Turkey', 21, 50],
+  'DK': ['Denmark', 9, 18],
+  'IT': ['Italy', 805, 859],
+  'PT': ['Portugal', 201, 202],
+  'KH': ['Cambodia', 25, 75]
+}
---
+++ people/khrawlings/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '23808252@N00',
+  'alias': 'khrawlings',
+  'name': 'khrawlings',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/23808252@N00_r.jpg',
+  'url': 'https://www.flickr.com/photos/khrawlings/',
+  'markers': 1884,
+  'photos': 2566
+}
