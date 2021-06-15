0d8c532579df6ad3446c268842e9933d52074cb5
The Map Group <admin@the-map-group.pictures>
2021-06-15 02:17:00 -0300

    [auto] Updated map for member 'marcostetter'

--- people/marcostetter/countries.py
+++ people/marcostetter/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'ES': ['Spain', 2, 15],
-  'CA': ['Canada', 2, 6],
-  'DE': ['Germany', 39, 270],
-  'GG': ['Guernsey', 1, 5],
-  'SI': ['Slovenia', 2, 10],
-  'BE': ['Belgium', 1, 5],
-  'CN': ['South China Sea Islands (disputed)', 1, 5],
-  'HR': ['Croatia', 4, 70],
   'NL': ['Netherlands', 3, 15],
   'US': ['United States of America', 10, 55],
+  'SI': ['Slovenia', 2, 10],
+  'CN': ['South China Sea Islands (disputed)', 1, 5],
+  'ES': ['Spain', 2, 16],
+  'CA': ['Canada', 2, 6],
   'DK': ['Denmark', 2, 15],
-  'FR': ['France', 1, 5],
-  'GB': ['United Kingdom', 7, 40],
+  'GG': ['Guernsey', 1, 5],
+  'HR': ['Croatia', 4, 70],
   'CH': ['Switzerland', 2, 10],
-  'IT': ['Italy', 12, 76]
+  'DE': ['Germany', 39, 270],
+  'BE': ['Belgium', 1, 5],
+  'GB': ['United Kingdom', 7, 40],
+  'IT': ['Italy', 12, 76],
+  'FR': ['France', 1, 5]
 }
