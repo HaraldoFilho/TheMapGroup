04436af92c464704bbdf6a1d2462dcd628ef3882
The Map Group <admin@the-map-group.pictures>
2021-06-28 02:10:30 -0300

    [auto] Updated map for member 'Pit Spielmann'

--- people/145400672@N02/user.py
+++ people/145400672@N02/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Pit Spielmann',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/145400672@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/145400672@N02/',
-  'markers': 975,
-  'photos': 1341
+  'markers': 977,
+  'photos': 1343
 }
