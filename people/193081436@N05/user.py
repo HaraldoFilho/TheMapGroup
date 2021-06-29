8341f62a24baf8bd09dbd5e726119a812068be50
The Map Group <admin@the-map-group.pictures>
2021-06-29 02:20:28 -0300

    [auto] Updated map for member 'craigrdaffin'

--- people/193081436@N05/user.py
+++ people/193081436@N05/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Craig Daffin',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/193081436@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/193081436@N05/',
-  'markers': 270,
-  'photos': 287
+  'markers': 281,
+  'photos': 298
 }
