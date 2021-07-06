738a83d0ec973710b10dab2b9705f1c864c9004d
The Map Group <admin@the-map-group.pictures>
2021-07-06 03:02:57 -0300

    [auto] Updated map for member 'malioli'

--- people/malioli/countries.py
+++ people/malioli/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
+  'AT': ['Austria', 5, 5],
+  'HR': ['Croatia', 1297, 3073],
   'DE': ['Germany', 16, 44],
   'SI': ['Slovenia', 1, 14],
-  'AT': ['Austria', 5, 5],
-  'BA': ['Bosnia and Herzegovina', 1, 1],
-  'HR': ['Croatia', 1295, 3071]
+  'BA': ['Bosnia and Herzegovina', 1, 1]
 }
--- people/malioli/user.py
+++ people/malioli/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Oliver Švob',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/79369350@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/malioli/',
-  'markers': 1318,
-  'photos': 3135
+  'markers': 1320,
+  'photos': 3137
 }
