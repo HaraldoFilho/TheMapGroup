9fa05dc37b5b6f6aff89b8a770b97a36b0a13e9b
The Map Group <admin@the-map-group.pictures>
2021-07-09 02:43:43 -0300

    [auto] Updated map for member 'Jens Koschitzki'

---
+++ people/jenskoschitzki/countries.py
@@ -0,0 +1,14 @@
+countries_dict = {
+  'GE': ['Georgia', 155, 176],
+  'LV': ['Latvia', 149, 149],
+  'AM': ['Armenia', 161, 172],
+  'HR': ['Croatia', 214, 244],
+  'FI': ['Finland', 126, 127],
+  'EE': ['Estonia', 170, 170],
+  'AT': ['Austria', 58, 68],
+  'IT': ['Italy', 371, 438],
+  'LT': ['Lithuania', 125, 136],
+  'RU': ['Russia', 428, 510],
+  'GB': ['United Kingdom', 147, 150],
+  'DE': ['Germany', 509, 24267]
+}
---
+++ people/jenskoschitzki/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '108188718@N06',
+  'alias': 'jenskoschitzki',
+  'name': 'Jens Koschitzki',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/108188718@N06_r.jpg',
+  'url': 'https://www.flickr.com/photos/jenskoschitzki/',
+  'markers': 2613,
+  'photos': 26607
+}
