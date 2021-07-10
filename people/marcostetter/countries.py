a7b1456ed0e83f1c878a6501ecffb0c51258b500
The Map Group <admin@the-map-group.pictures>
2021-07-10 02:16:57 -0300

    [auto] Updated map for member 'marcostetter'

--- people/marcostetter/countries.py
+++ people/marcostetter/countries.py
@@ -1,19 +1,19 @@
 countries_dict = {
-  'IS': ['Iceland', 1, 6],
-  'GB': ['United Kingdom', 7, 48],
   'HR': ['Croatia', 6, 89],
-  'DE': ['Germany', 47, 400],
   'US': ['United States', 15, 86],
+  'CH': ['Switzerland', 3, 18],
+  'FR': ['France', 1, 6],
   'BS': ['The Bahamas', 1, 7],
+  'CN': ['South China Sea Islands (disputed)', 1, 6],
   'IT': ['Italy', 19, 122],
   'CA': ['Canada', 3, 19],
-  'FR': ['France', 1, 6],
-  'ES': ['Spain', 3, 50],
-  'NL': ['Netherlands', 3, 13],
-  'CN': ['South China Sea Islands (disputed)', 1, 6],
   'GG': ['Guernsey', 1, 6],
-  'CH': ['Switzerland', 3, 18],
   'DK': ['Denmark', 2, 18],
+  'DE': ['Germany', 48, 401],
   'BE': ['Belgium', 1, 12],
-  'SI': ['Slovenia', 2, 12]
+  'IS': ['Iceland', 1, 6],
+  'NL': ['Netherlands', 3, 13],
+  'GB': ['United Kingdom', 7, 48],
+  'SI': ['Slovenia', 2, 12],
+  'ES': ['Spain', 3, 50]
 }
--- people/marcostetter/user.py
+++ people/marcostetter/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Marco Brandstetter',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/149556095@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/marcostetter/',
-  'markers': 116,
-  'photos': 918
+  'markers': 117,
+  'photos': 919
 }
