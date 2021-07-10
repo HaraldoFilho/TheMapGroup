597fba25f8302413b6e98b8b501e59709abe3fe0
The Map Group <admin@the-map-group.pictures>
2021-07-10 02:14:28 -0300

    [auto] Updated map for member 'aroubin - Yay! 4 MILLION views!'

--- people/aroubin/countries.py
+++ people/aroubin/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
   'MC': ['Monaco', 3, 56],
-  'FR': ['France', 44, 3125],
-  'DO': ['Dominican Republic', 37, 401],
-  'US': ['United States of America', 1360, 3376],
   'CA': ['Canada', 15, 17],
+  'DO': ['Dominican Republic', 37, 401],
+  'US': ['United States of America', 1364, 3380],
+  'FR': ['France', 44, 3125],
   'IT': ['Italy', 2, 75]
 }
