c87374926e833867ee449e71cce7111f11aef051
The Map Group <admin@the-map-group.pictures>
2021-07-11 02:12:45 -0300

    [auto] Updated map for member 'malioli'

--- people/malioli/user.py
+++ people/malioli/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Oliver Švob',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/79369350@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/malioli/',
-  'markers': 1327,
-  'photos': 3144
+  'markers': 1329,
+  'photos': 3146
 }
