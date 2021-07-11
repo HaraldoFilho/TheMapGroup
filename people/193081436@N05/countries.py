472ddf4eb022b8c67f127b97f1f09c6dc30eb2bc
The Map Group <admin@the-map-group.pictures>
2021-07-11 02:23:38 -0300

    [auto] Updated map for member 'craigrdaffin'

--- people/193081436@N05/countries.py
+++ people/193081436@N05/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
   'IN': ['India', 5, 5],
-  'US': ['United States', 18, 18],
-  'AU': ['Australia', 352, 373],
-  'PH': ['Philippines', 1, 1]
+  'PH': ['Philippines', 1, 1],
+  'AU': ['Australia', 360, 383],
+  'US': ['United States', 18, 18]
 }
