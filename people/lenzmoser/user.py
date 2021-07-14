dfa1593a737f73ce78967944cb1113da754641cb
The Map Group <admin@the-map-group.pictures>
2021-07-14 02:38:50 -0300

    [auto] Updated map for member '::ErWin (on vacation)'

--- people/lenzmoser/user.py
+++ people/lenzmoser/user.py
@@ -1,9 +1,9 @@
 user_info = {
   'id': '123096458@N03',
   'alias': 'lenzmoser',
-  'name': '::ErWin',
+  'name': '::ErWin (on vacation)',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/123096458@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/lenzmoser/',
-  'markers': 4516,
-  'photos': 4643
+  'markers': 4520,
+  'photos': 4647
 }
