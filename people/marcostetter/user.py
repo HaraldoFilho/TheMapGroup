46e539a0f17bdfd79d41ef355b980660ded9b6a4
The Map Group <admin@the-map-group.pictures>
2021-07-16 02:26:02 -0300

    [auto] Updated map for member 'marcostetter'

--- people/marcostetter/user.py
+++ people/marcostetter/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/149556095@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/marcostetter/',
   'markers': 124,
-  'photos': 926
+  'photos': 927
 }
