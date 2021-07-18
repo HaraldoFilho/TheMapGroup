fcfbadc7562a93ce60e297e8253dd1de0d5c0d1d
The Map Group <admin@the-map-group.pictures>
2021-07-18 02:21:31 -0300

    [auto] Updated map for member 'Ron Fisher'

--- people/train-pix/user.py
+++ people/train-pix/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Ron Fisher',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/40052043@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/train-pix/',
-  'markers': 10985,
-  'photos': 21391
+  'markers': 10987,
+  'photos': 21393
 }
