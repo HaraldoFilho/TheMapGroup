7401ecc734d4b076fada2695ad1d86321ece5932
The Map Group <admin@the-map-group.pictures>
2021-07-19 02:44:05 -0300

    [auto] Updated map for member 'craigrdaffin'

--- people/193081436@N05/countries.py
+++ people/193081436@N05/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
+  'AU': ['Australia', 422, 448],
+  'IN': ['India', 5, 5],
   'PH': ['Philippines', 1, 1],
-  'US': ['United States', 21, 21],
-  'AU': ['Australia', 418, 444],
-  'IN': ['India', 5, 5]
+  'US': ['United States', 21, 21]
 }
--- people/193081436@N05/user.py
+++ people/193081436@N05/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Craig Daffin',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/193081436@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/193081436@N05/',
-  'markers': 445,
-  'photos': 471
+  'markers': 449,
+  'photos': 475
 }
