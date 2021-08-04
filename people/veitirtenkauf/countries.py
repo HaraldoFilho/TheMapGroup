461f0b143cf99b5c25f1b5c7923c36565ec893f0
The Map Group <admin@the-map-group.pictures>
2021-08-04 10:17:30 -0300

    [auto] Updated map for member '_Veit_'

--- people/veitirtenkauf/countries.py
+++ people/veitirtenkauf/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'DE': ['Germany', 19, 19],
-  'US': ['United States of America', 286, 1051],
+  'AU': ['Australia', 1, 1],
+  'US': ['United States of America', 286, 1057],
   'NZ': ['New Zealand', 3, 3],
-  'CZ': ['Czechia', 2, 2],
-  'AU': ['Australia', 1, 1]
+  'DE': ['Germany', 19, 19],
+  'CZ': ['Czechia', 2, 2]
 }
