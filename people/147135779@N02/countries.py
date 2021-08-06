e96a2fbe8f40da35fd9c55a52f9aa6d87ac8cab8
The Map Group <admin@the-map-group.pictures>
2021-08-06 02:18:21 -0300

    [auto] Updated map for member 'giovannicampus'

--- people/147135779@N02/countries.py
+++ people/147135779@N02/countries.py
@@ -1,18 +1,18 @@
 countries_dict = {
-  'MV': ['Maldives', 1, 1],
   'LK': ['Sri Lanka', 200, 211],
-  'TZ': ['Tanzania', 1, 1],
-  'IT': ['Italy', 897, 1021],
-  'IN': ['India', 907, 943],
   'CU': ['Cuba', 181, 346],
-  'KZ': ['Kazakhstan', 2, 2],
-  'UZ': ['Uzbekistan', 263, 269],
   'US': ['United States of America', 7, 7],
+  'RO': ['Romania', 1, 1],
   'VA': ['Vatican City', 9, 9],
+  'UZ': ['Uzbekistan', 263, 269],
+  'NL': ['Netherlands', 54, 108],
+  'IN': ['India', 908, 944],
+  'IT': ['Italy', 898, 1023],
+  'MV': ['Maldives', 1, 1],
+  'FR': ['France', 1, 1],
+  'PT': ['Portugal', 1, 1],
+  'KZ': ['Kazakhstan', 2, 2],
   'KE': ['Kenya', 9, 9],
   'AE': ['United Arab Emirates', 2, 2],
-  'NL': ['Netherlands', 54, 108],
-  'FR': ['France', 1, 1],
-  'RO': ['Romania', 1, 1],
-  'PT': ['Portugal', 1, 1]
+  'TZ': ['Tanzania', 1, 1]
 }
--- people/147135779@N02/user.py
+++ people/147135779@N02/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'giovanni Campus',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/147135779@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/147135779@N02/',
-  'markers': 2536,
-  'photos': 2932
+  'markers': 2538,
+  'photos': 2935
 }
