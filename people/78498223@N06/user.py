3f139b3681ddf056fab34f994ba06028104f3527
The Map Group <admin@the-map-group.pictures>
2021-08-08 02:45:16 -0300

    [auto] Updated map for member 'LFDV1960'

---
+++ people/78498223@N06/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '78498223@N06',
+  'alias': '78498223@N06',
+  'name': 'LFDV1960',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/78498223@N06_r.jpg',
+  'url': 'https://www.flickr.com/photos/78498223@N06/',
+  'markers': 215,
+  'photos': 266
+}
