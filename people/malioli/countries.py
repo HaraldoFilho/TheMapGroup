b50ae14e243947cf06b0c53a0a57a92fac1b28de
The Map Group <admin@the-map-group.pictures>
2021-08-09 02:22:48 -0300

    [auto] Updated map for member 'malioli'

--- people/malioli/countries.py
+++ people/malioli/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'SI': ['Slovenia', 1, 14],
-  'BA': ['Bosnia and Herzegovina', 1, 1],
   'AT': ['Austria', 5, 5],
-  'HR': ['Croatia', 1357, 3133],
-  'DE': ['Germany', 16, 44]
+  'BA': ['Bosnia and Herzegovina', 1, 1],
+  'DE': ['Germany', 16, 44],
+  'HR': ['Croatia', 1358, 3134],
+  'SI': ['Slovenia', 1, 14]
 }
