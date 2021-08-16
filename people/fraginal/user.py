1d43e30556d913619759bb1255cf6a5f6ca0d710
The Map Group <admin@the-map-group.pictures>
2021-08-16 03:05:31 -0300

    [auto] Updated map for member 'PINOY PHOTOGRAPHER'

---
+++ people/fraginal/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '9060404@N06',
+  'alias': 'fraginal',
+  'name': 'PINOY PHOTOGRAPHER',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/9060404@N06_r.jpg',
+  'url': 'https://www.flickr.com/photos/fraginal/',
+  'markers': 8136,
+  'photos': 38008
+}
