e238a74667aa6f30f3c8765a5fe3c4dbd273bd2a
The Map Group <admin@the-map-group.pictures>
2021-08-19 02:02:38 -0300

    [auto] Updated map for member 'Paul268869'

--- people/167278372@N02/user.py
+++ people/167278372@N02/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'paul manley',
   'avatar': '../../icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/167278372@N02/',
-  'markers': 64,
-  'photos': 66
+  'markers': 65,
+  'photos': 67
 }
