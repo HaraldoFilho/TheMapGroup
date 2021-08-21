356244cd27d01209eb2b0906e9fa0ca2f4b2c2ff
The Map Group <admin@the-map-group.pictures>
2021-08-21 02:23:53 -0300

    [auto] Updated map for member 'salix.caprea'

--- people/55688814@N08/user.py
+++ people/55688814@N08/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Iva',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/55688814@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/55688814@N08/',
-  'markers': 247,
-  'photos': 249
+  'markers': 249,
+  'photos': 251
 }
