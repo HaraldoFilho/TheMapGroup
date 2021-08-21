43eec886256ef0330ce85228d3c172540398f5f8
The Map Group <admin@the-map-group.pictures>
2021-08-21 02:31:44 -0300

    [auto] Updated map for member 'Fotos Avulso'

--- people/fotosavulso/user.py
+++ people/fotosavulso/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Fotos Avulso',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/69623157@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/fotosavulso/',
-  'markers': 475,
-  'photos': 507
+  'markers': 476,
+  'photos': 508
 }
