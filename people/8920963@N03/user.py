ca0acf820275cd44be7a8d57cdb3cce73798a7ba
The Map Group <admin@the-map-group.pictures>
2021-08-25 02:02:00 -0300

    [auto] Updated map for member 'Steenjep'

--- people/8920963@N03/user.py
+++ people/8920963@N03/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Steen Jeppesen',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/8920963@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/8920963@N03/',
-  'markers': 9558,
-  'photos': 13754
+  'markers': 9560,
+  'photos': 13756
 }
