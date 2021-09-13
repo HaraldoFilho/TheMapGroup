99499becf1a0a43ab6f59c36a1d306f7ff3e22d8
The Map Group <admin@the-map-group.pictures>
2021-09-13 02:35:24 -0300

    [auto] Updated map for member 'Steve W3'

--- people/ultimax_1964/countries.py
+++ people/ultimax_1964/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'SG': ['Singapore', 2, 3],
-  'GB': ['United Kingdom', 30, 35],
-  'FR': ['France', 12, 17],
-  'GL': ['Greenland', 10, 23],
-  'IS': ['Iceland', 12, 46],
-  'US': ['United States of America', 11, 29],
-  'PT': ['Portugal', 4, 6],
   'ES': ['Spain', 3, 3],
-  'NO': ['Norway', 6, 13],
-  'CA': ['Canada', 4, 13],
-  'JP': ['Japan', 6, 20],
   'NZ': ['New Zealand', 8, 23],
-  'AU': ['Australia', 120, 368]
+  'NO': ['Norway', 6, 13],
+  'AU': ['Australia', 120, 369],
+  'GL': ['Greenland', 10, 23],
+  'US': ['United States of America', 11, 29],
+  'GB': ['United Kingdom', 30, 35],
+  'PT': ['Portugal', 4, 6],
+  'JP': ['Japan', 6, 20],
+  'SG': ['Singapore', 2, 3],
+  'CA': ['Canada', 4, 13],
+  'FR': ['France', 12, 17],
+  'IS': ['Iceland', 12, 46]
 }
