df860aa328b3c17c961e3865652a79f96d66245f
The Map Group <admin@the-map-group.pictures>
2021-09-18 02:49:58 -0300

    [auto] Updated map for member 'Atila Yumusakkaya'

--- people/atila_y/user.py
+++ people/atila_y/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Atila Yumusakkaya',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/8009375@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/atila_y/',
-  'markers': 1158,
-  'photos': 1304
+  'markers': 1160,
+  'photos': 1306
 }
