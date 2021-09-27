2ab2305dd513a7605e44ce5a63d66f23ddbda067
The Map Group <admin@the-map-group.pictures>
2021-09-27 02:45:44 -0300

    [auto] Updated map for member 'Jo 0405'

---
+++ people/basses-lumieres/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '108339617@N08',
+  'alias': 'basses-lumieres',
+  'name': 'Jo 0405',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/108339617@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/basses-lumieres/',
+  'markers': 134,
+  'photos': 676
+}
