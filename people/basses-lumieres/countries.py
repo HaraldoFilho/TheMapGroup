bdf2fdd7e9102f43310f8be90269446be8a8dace
The Map Group <admin@the-map-group.pictures>
2021-09-29 03:37:34 -0300

    [auto] Updated map for member 'Jo 0405'

--- people/basses-lumieres/countries.py
+++ people/basses-lumieres/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'FR': ['France', 134, 676]
+  'FR': ['France', 135, 677]
 }
--- people/basses-lumieres/user.py
+++ people/basses-lumieres/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Jo 0405',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/108339617@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/basses-lumieres/',
-  'markers': 134,
-  'photos': 676
+  'markers': 135,
+  'photos': 677
 }
