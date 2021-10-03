bd4aad6bc7f6e82c6d4307bc6dde25b986622156
The Map Group <admin@the-map-group.pictures>
2021-10-03 02:55:29 -0300

    [auto] Updated map for member 'Ralph Apeldoorn'

--- people/runningralph/user.py
+++ people/runningralph/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Ralph Apeldoorn',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/95381107@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/runningralph/',
-  'markers': 5327,
-  'photos': 10788
+  'markers': 5332,
+  'photos': 10793
 }
