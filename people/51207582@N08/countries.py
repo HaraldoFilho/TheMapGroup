1a2356ddf3ae19cdfbfcfc9dd627cc9adfc47d9a
The Map Group <admin@the-map-group.pictures>
2021-10-06 02:01:41 -0300

    [auto] Updated map for member 'GaryC4'

--- people/51207582@N08/countries.py
+++ people/51207582@N08/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
+  'GB': ['United Kingdom', 2574, 3095],
+  'ES': ['Spain', 77, 80],
   'IT': ['Italy', 94, 102],
   'PT': ['Portugal', 18, 21],
-  'GR': ['Greece', 13, 13],
-  'GB': ['United Kingdom', 2571, 3092],
-  'ES': ['Spain', 77, 80]
+  'GR': ['Greece', 13, 13]
 }
