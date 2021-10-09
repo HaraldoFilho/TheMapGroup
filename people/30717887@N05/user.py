6a42ec5b7393537b9c3ef1fece38033d78626a57
The Map Group <admin@the-map-group.pictures>
2021-10-09 02:02:58 -0300

    [auto] Updated map for member 'Tor Einar Andersen'

--- people/30717887@N05/user.py
+++ people/30717887@N05/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Tor Einar Andersen',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/30717887@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/30717887@N05/',
-  'markers': 238,
-  'photos': 333
+  'markers': 239,
+  'photos': 334
 }
