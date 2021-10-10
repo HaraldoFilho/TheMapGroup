7c711dfd802b04d5d96bdabf5f37f651785e1879
The Map Group <admin@the-map-group.pictures>
2021-10-10 02:31:12 -0300

    [auto] Updated map for member 'fraganda'

--- people/fraganda/countries.py
+++ people/fraganda/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'IL': ['Israel', 17, 17],
-  'PS': ['Palestinian Territory', 3, 3],
-  'BE': ['Belgium', 76, 76],
-  'FR': ['France', 3, 3],
-  'IT': ['Italy', 1, 1],
-  'US': ['United States of America', 35, 35],
-  'DE': ['Germany', 3, 3],
   'HR': ['Croatia', 9, 9],
   'GI': ['Gibraltar', 1, 1],
+  'IT': ['Italy', 1, 1],
+  'US': ['United States of America', 35, 35],
+  'GR': ['Greece', 11, 11],
+  'PS': ['Palestinian Territory', 3, 3],
+  'FR': ['France', 3, 3],
   'NL': ['Netherlands', 4, 4],
   'ES': ['Spain', 40, 40],
-  'GR': ['Greece', 11, 11]
+  'DE': ['Germany', 3, 3],
+  'BE': ['Belgium', 77, 77],
+  'IL': ['Israel', 17, 17]
 }
