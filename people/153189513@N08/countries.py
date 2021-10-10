ecf456e426a39f5c0778e54b852cad685c1fd09e
The Map Group <admin@the-map-group.pictures>
2021-10-10 02:26:45 -0300

    [auto] Updated map for member 'katlamsib'

--- people/153189513@N08/countries.py
+++ people/153189513@N08/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'UZ': ['Uzbekistan', 4, 7],
-  'KZ': ['Kazakhstan', 21, 22],
+  'BY': ['Belarus', 2, 3],
+  'RU': ['Russia', 514, 565],
   'AZ': ['Azerbaijan', 10, 10],
-  'RU': ['Russia', 511, 562],
-  'BY': ['Belarus', 2, 3]
+  'KZ': ['Kazakhstan', 21, 22],
+  'UZ': ['Uzbekistan', 4, 7]
 }
