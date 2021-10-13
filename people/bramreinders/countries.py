d41028920d880933413a5470c9b126deb8566989
The Map Group <admin@the-map-group.pictures>
2021-10-13 02:37:29 -0300

    [auto] Updated map for member 'Bram Reinders(I'M BACK)'

--- people/bramreinders/countries.py
+++ people/bramreinders/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'CZ': ['Czechia', 6, 11],
-  'TR': ['Turkey', 11, 11],
-  'DE': ['Germany', 21, 31],
-  'NL': ['Netherlands', 1967, 2462],
-  'GR': ['Greece', 8, 8],
+  'EG': ['Egypt', 3, 8],
   'TH': ['Thailand', 25, 28],
-  'EG': ['Egypt', 3, 8]
+  'CZ': ['Czechia', 6, 11],
+  'NL': ['Netherlands', 1968, 2463],
+  'DE': ['Germany', 21, 31],
+  'GR': ['Greece', 8, 8],
+  'TR': ['Turkey', 11, 11]
 }
--- people/bramreinders/user.py
+++ people/bramreinders/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'bram reinders',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/28628934@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/bramreinders/',
-  'markers': 2041,
-  'photos': 2559
+  'markers': 2042,
+  'photos': 2560
 }
