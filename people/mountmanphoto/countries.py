a1b1d66345eaa2639aa2f7fc8ed7b6d2ec221442
The Map Group <admin@the-map-group.pictures>
2021-10-14 02:18:47 -0300

    [auto] Updated map for member 'MountMan Photo'

--- people/mountmanphoto/countries.py
+++ people/mountmanphoto/countries.py
@@ -1,4 +1,4 @@
 countries_dict = {
-  'HR': ['Croatia', 396, 479],
+  'HR': ['Croatia', 397, 480],
   'SI': ['Slovenia', 5, 7]
 }
--- people/mountmanphoto/user.py
+++ people/mountmanphoto/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'MountMan Photo',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/125923483@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/mountmanphoto/',
-  'markers': 401,
-  'photos': 486
+  'markers': 402,
+  'photos': 487
 }
