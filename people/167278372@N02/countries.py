49f573ca7569730fa488aed7057e80a9464619f3
The Map Group <admin@the-map-group.pictures>
2021-10-18 02:37:23 -0300

    [auto] Updated map for member 'Paul268869'

--- people/167278372@N02/countries.py
+++ people/167278372@N02/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'GB': ['United Kingdom', 68, 70]
+  'GB': ['United Kingdom', 69, 71]
 }
--- people/167278372@N02/user.py
+++ people/167278372@N02/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'paul manley',
   'avatar': '../../icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/167278372@N02/',
-  'markers': 68,
-  'photos': 70
+  'markers': 69,
+  'photos': 71
 }
