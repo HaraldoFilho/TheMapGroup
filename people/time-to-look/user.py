a97c33779df932766cc5feb971a643b685c12aed
The Map Group <admin@the-map-group.pictures>
2021-10-19 02:36:07 -0300

    [auto] Updated map for member 'Ted's photos - For Me & You'

--- people/time-to-look/user.py
+++ people/time-to-look/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Ted McGrath',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/90955804@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/time-to-look/',
-  'markers': 10088,
-  'photos': 11770
+  'markers': 10090,
+  'photos': 11772
 }
