8e5df7e117f37d14d5dc710a9f42fa8ada93b769
The Map Group <admin@the-map-group.pictures>
2021-10-20 02:40:30 -0300

    [auto] Updated map for member 'Bram Reinders(I'M BACK)'

--- people/bramreinders/countries.py
+++ people/bramreinders/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'TR': ['Turkey', 11, 11],
   'GR': ['Greece', 8, 8],
   'CZ': ['Czechia', 6, 11],
-  'DE': ['Germany', 21, 31],
-  'NL': ['Netherlands', 1973, 2468],
+  'TR': ['Turkey', 11, 11],
+  'NL': ['Netherlands', 1974, 2470],
   'EG': ['Egypt', 3, 8],
+  'DE': ['Germany', 21, 31],
   'TH': ['Thailand', 25, 28]
 }
