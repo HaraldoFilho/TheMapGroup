c46790ce13b504f92f32fec5d8956831939437fd
The Map Group <admin@the-map-group.pictures>
2021-10-27 03:07:57 -0300

    [auto] Updated map for member 'jonnyboi37419'

---
+++ people/jebrailwayphotography/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '187366688@N05',
+  'alias': 'jebrailwayphotography',
+  'name': 'Jonathan Brooks',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/187366688@N05_r.jpg',
+  'url': 'https://www.flickr.com/photos/jebrailwayphotography/',
+  'markers': 110,
+  'photos': 213
+}
