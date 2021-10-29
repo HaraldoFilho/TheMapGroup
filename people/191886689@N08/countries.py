9f8896423bb04cabe2c165c9fcf12e6506f9d510
The Map Group <admin@the-map-group.pictures>
2021-10-29 02:40:20 -0300

    [auto] Updated map for member 'MesphotosAmateur.'

--- people/191886689@N08/countries.py
+++ people/191886689@N08/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'IT': ['Italy', 5, 6],
-  'FR': ['France', 72, 77],
-  'PT': ['Portugal', 5, 5]
+  'PT': ['Portugal', 5, 5],
+  'FR': ['France', 73, 78],
+  'IT': ['Italy', 5, 6]
 }
