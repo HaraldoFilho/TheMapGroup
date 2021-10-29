48b20507432189e2dc76f68bde2145583fab941f
The Map Group <admin@the-map-group.pictures>
2021-10-29 03:07:49 -0300

    Removed member '193081436@N05'

--- people/193081436@N05/countries.py
+++
@@ -1,6 +0,0 @@
-countries_dict = {
-  'US': ['United States', 21, 21],
-  'AU': ['Australia', 651, 690],
-  'IN': ['India', 5, 5],
-  'PH': ['Philippines', 1, 1]
-}
--- people/193081436@N05/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '193081436@N05',
-  'alias': '193081436@N05',
-  'name': 'Craig Daffin',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/193081436@N05_r.jpg',
-  'url': 'https://www.flickr.com/photos/193081436@N05/',
-  'markers': 678,
-  'photos': 717
-}
