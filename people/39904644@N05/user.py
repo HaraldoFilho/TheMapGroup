22e0c93174f2ca51d405ada77b5705a047e8ec10
The Map Group <admin@the-map-group.pictures>
2021-10-30 02:45:00 -0300

    [auto] Updated map for member 'Bilderschreiber'

--- people/39904644@N05/user.py
+++ people/39904644@N05/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Bilderschreiber',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/39904644@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/39904644@N05/',
-  'markers': 1308,
-  'photos': 1557
+  'markers': 1311,
+  'photos': 1560
 }
