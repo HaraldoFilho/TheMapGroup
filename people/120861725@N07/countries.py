8a62b5de09dc2b33470520d315da87359636255a
The Map Group <admin@the-map-group.pictures>
2021-10-30 02:11:46 -0300

    [auto] Updated map for member 'GSB Photography'

--- people/120861725@N07/countries.py
+++ people/120861725@N07/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
+  'DE': ['Germany', 8, 10],
+  'NL': ['Netherlands', 4, 4],
+  'US': ['United States of America', 63, 72],
+  'AT': ['Austria', 3, 3],
   'GB': ['United Kingdom', 17, 19],
   'IT': ['Italy', 50, 57],
-  'AT': ['Austria', 3, 3],
-  'US': ['United States of America', 63, 72],
-  'DE': ['Germany', 8, 10],
-  'CH': ['Switzerland', 5, 6],
-  'NL': ['Netherlands', 3, 3]
+  'CH': ['Switzerland', 5, 6]
 }
--- people/120861725@N07/user.py
+++ people/120861725@N07/user.py
@@ -1,9 +1,9 @@
 user_info = {
   'id': '120861725@N07',
   'alias': '120861725@N07',
-  'name': 'GSB',
+  'name': 'GSB Photography',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/120861725@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/120861725@N07/',
-  'markers': 149,
-  'photos': 170
+  'markers': 150,
+  'photos': 171
 }
