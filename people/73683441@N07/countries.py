1a952349ebdb6278bde03a2b49cdcf9f474655dc
The Map Group <admin@the-map-group.pictures>
2021-11-03 02:43:54 -0300

    [auto] Updated map for member 'Gerald (Wayne) Prout'

--- people/73683441@N07/countries.py
+++ people/73683441@N07/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'US': ['United States of America', 1627, 3271],
+  'CA': ['Canada', 1989, 4641],
   'FJ': ['Fiji', 9, 20],
+  'NZ': ['New Zealand', 52, 172],
   'PT': ['Portugal', 108, 172],
-  'AU': ['Australia', 67, 98],
-  'CA': ['Canada', 1989, 4640],
-  'GI': ['Gibraltar', 2, 4],
   'ES': ['Spain', 29, 31],
-  'NZ': ['New Zealand', 52, 172]
+  'US': ['United States of America', 1627, 3271],
+  'AU': ['Australia', 67, 98],
+  'GI': ['Gibraltar', 2, 4]
 }
--- people/73683441@N07/user.py
+++ people/73683441@N07/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/73683441@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/73683441@N07/',
   'markers': 3883,
-  'photos': 8408
+  'photos': 8409
 }
