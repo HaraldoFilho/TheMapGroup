76a2f1b39cd9c9c0cebcc9e5b729097051a2154a
The Map Group <admin@the-map-group.pictures>
2021-11-06 03:45:25 -0300

    [auto] Updated map for member 'Gary S Bond'

--- people/shabbagaz/countries.py
+++ people/shabbagaz/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
   'ES': ['Spain', 21, 103],
   'IE': ['Ireland', 54, 58],
-  'GB': ['United Kingdom', 10731, 15784],
-  'DE': ['Germany', 25, 230]
+  'DE': ['Germany', 25, 230],
+  'GB': ['United Kingdom', 10786, 15839]
 }
--- people/shabbagaz/user.py
+++ people/shabbagaz/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Gary Bond',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/33352310@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/shabbagaz/',
-  'markers': 10831,
-  'photos': 16175
+  'markers': 10886,
+  'photos': 16230
 }
