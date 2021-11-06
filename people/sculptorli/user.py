60803e330fe8fb1e58f86d12f9da1e634d063951
The Map Group <admin@the-map-group.pictures>
2021-11-06 03:17:56 -0300

    [auto] Updated map for member 'sculptorli'

--- people/sculptorli/user.py
+++ people/sculptorli/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'sculptorli',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/23631104@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/sculptorli/',
-  'markers': 197,
-  'photos': 203
+  'markers': 198,
+  'photos': 204
 }
