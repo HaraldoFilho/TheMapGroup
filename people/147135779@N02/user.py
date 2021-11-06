f02e2b5634f324fc64e1ae575bdafe767fd4048d
The Map Group <admin@the-map-group.pictures>
2021-11-06 02:25:42 -0300

    [auto] Updated map for member 'giovannicampus'

--- people/147135779@N02/user.py
+++ people/147135779@N02/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'giovanni Campus',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/147135779@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/147135779@N02/',
-  'markers': 2678,
-  'photos': 3131
+  'markers': 2683,
+  'photos': 3140
 }
