f5dbfbeac3a1250c55c63b36d64e1f1c39371ac3
The Map Group <admin@the-map-group.pictures>
2021-11-06 04:04:58 -0300

    Removed member 'lenzmoser'

--- people/lenzmoser/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '123096458@N03',
-  'alias': 'lenzmoser',
-  'name': '::ErWin',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/123096458@N03_r.jpg',
-  'url': 'https://www.flickr.com/photos/lenzmoser/',
-  'markers': 4756,
-  'photos': 4895
-}
