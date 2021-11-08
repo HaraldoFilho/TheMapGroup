0ddb3a8d06ec5b92985a2148e06229d21f90bbf2
The Map Group <admin@the-map-group.pictures>
2021-11-08 03:21:43 -0300

    [auto] Updated map for member 'Ignacio M. Jiménez'

--- people/ignaciomjimenez/countries.py
+++ people/ignaciomjimenez/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'AT': ['Austria', 1, 1],
-  'ES': ['Spain', 922, 1237],
-  'BE': ['Belgium', 7, 7],
+  'PT': ['Portugal', 4, 4],
   'FR': ['France', 1, 1],
   'GB': ['United Kingdom', 24, 24],
-  'PT': ['Portugal', 4, 4]
+  'BE': ['Belgium', 7, 7],
+  'AT': ['Austria', 1, 1],
+  'ES': ['Spain', 923, 1238]
 }
--- people/ignaciomjimenez/user.py
+++ people/ignaciomjimenez/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Nacho',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/23499399@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/ignaciomjimenez/',
-  'markers': 959,
-  'photos': 1274
+  'markers': 960,
+  'photos': 1275
 }
