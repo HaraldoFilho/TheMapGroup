a2009da94be5bc7a64e4282c861fd2e7883bdc22
The Map Group <admin@the-map-group.pictures>
2021-11-08 22:52:59 -0300

    Added map for member 'Adam Swaine'

---
+++ people/adam_swaine/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '43499552@N03',
+  'alias': 'adam_swaine',
+  'name': 'Adam Swaine',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/43499552@N03_r.jpg',
+  'url': 'https://www.flickr.com/photos/adam_swaine/',
+  'markers': 13490,
+  'photos': 20868
+}
