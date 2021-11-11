49d5577f21adc1ca0708e2f892ebbd4a1c9249d3
The Map Group <admin@the-map-group.pictures>
2021-11-11 03:56:28 -0300

    [auto] Updated map for member 'jonnyboi37419'

--- people/jebrailwayphotography/countries.py
+++ people/jebrailwayphotography/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'GB': ['United Kingdom', 112, 219]
+  'GB': ['United Kingdom', 113, 220]
 }
--- people/jebrailwayphotography/user.py
+++ people/jebrailwayphotography/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Jonathan Brooks',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/187366688@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/jebrailwayphotography/',
-  'markers': 112,
-  'photos': 219
+  'markers': 113,
+  'photos': 220
 }
