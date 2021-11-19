8d4d658928f5cfecbff7b9a5cc59a9d58516a2c5
The Map Group <admin@the-map-group.pictures>
2021-11-19 03:28:11 -0300

    [auto] Updated map for member 'Edouard55'

--- people/19239093@N03/countries.py
+++ people/19239093@N03/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'FR': ['France', 3910, 5106],
   'DE': ['Germany', 996, 1341],
+  'FR': ['France', 3914, 5110],
   'CH': ['Switzerland', 78, 105]
 }
--- people/19239093@N03/user.py
+++ people/19239093@N03/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Edouard',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/19239093@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/19239093@N03/',
-  'markers': 4984,
-  'photos': 6552
+  'markers': 4988,
+  'photos': 6556
 }
