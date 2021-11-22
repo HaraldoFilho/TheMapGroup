8af9226d6276ce569257a9323f508ccc04d4bef1
The Map Group <admin@the-map-group.pictures>
2021-11-22 03:09:36 -0300

    [auto] Updated map for member 'acase1968'

---
+++ people/60035031@N06/countries.py
@@ -0,0 +1,17 @@
+countries_dict = {
+  'MX': ['Mexico', 27, 27],
+  'CZ': ['Czechia', 32, 33],
+  'KR': ['South Korea', 23, 23],
+  'DE': ['Germany', 11, 11],
+  'US': ['United States', 944, 1062],
+  'IS': ['Iceland', 21, 21],
+  'GB': ['United Kingdom', 20, 21],
+  'DK': ['Denmark', 157, 209],
+  'HU': ['Hungary', 6, 6],
+  'SE': ['Sweden', 35, 42],
+  'NO': ['Norway', 2, 2],
+  'IT': ['Italy', 6, 6],
+  'NL': ['Netherlands', 3, 3],
+  'CH': ['Switzerland', 15, 16],
+  'JP': ['Japan', 141, 153]
+}
---
+++ people/60035031@N06/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '60035031@N06',
+  'alias': '60035031@N06',
+  'name': 'Al Case',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/60035031@N06_r.jpg',
+  'url': 'https://www.flickr.com/photos/60035031@N06/',
+  'markers': 1443,
+  'photos': 1635
+}
