09c5e4fa3eac91808f3fc45f446c60d741c6b0cf
The Map Group <admin@the-map-group.pictures>
2021-12-05 02:15:12 -0300

    [auto] Updated map for member 'astolfi.m'

--- people/160471444@N05/countries.py
+++ people/160471444@N05/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
+  'FR': ['France', 10, 96],
+  'IT': ['Italy', 376, 2455],
+  'HU': ['Hungary', 35, 160],
   'AR': ['Argentina', 28, 382],
-  'PT': ['Portugal', 29, 161],
+  'PE': ['Peru', 115, 961],
   'CG': ['Republic of the Congo', 1, 1],
   'CL': ['Chile', 4, 68],
-  'PE': ['Peru', 115, 961],
-  'HU': ['Hungary', 35, 160],
-  'ES': ['Spain', 27, 223],
-  'FR': ['France', 10, 96],
-  'IT': ['Italy', 375, 2449]
+  'PT': ['Portugal', 29, 161],
+  'ES': ['Spain', 27, 223]
 }
--- people/160471444@N05/user.py
+++ people/160471444@N05/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Mauro Astolfi',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/160471444@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/160471444@N05/',
-  'markers': 624,
-  'photos': 4501
+  'markers': 625,
+  'photos': 4507
 }
