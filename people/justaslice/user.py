fe59edd4d2c37ced581a8c1396c97719eeff3c00
The Map Group <admin@the-map-group.pictures>
2021-12-05 02:10:05 -0300

    [auto] Updated map for member '█ Slices of Light ✴ █▀ ▀ ▀'

--- people/justaslice/user.py
+++ people/justaslice/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': '█ Slices of Light ✴ █▀ ▀ ▀',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/95282411@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/justaslice/',
-  'markers': 2299,
-  'photos': 3777
+  'markers': 2301,
+  'photos': 3779
 }
