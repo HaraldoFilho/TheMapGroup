38a57663bad371e0698416794ca024e8ab526583
The Map Group <admin@the-map-group.pictures>
2021-12-06 02:15:40 -0300

    [auto] Updated map for member 'astolfi.m'

--- people/160471444@N05/user.py
+++ people/160471444@N05/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Mauro Astolfi',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/160471444@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/160471444@N05/',
-  'markers': 625,
-  'photos': 4507
+  'markers': 630,
+  'photos': 4514
 }
