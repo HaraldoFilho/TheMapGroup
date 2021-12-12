b80e69a0038b45b4ec3f946c17ed5b17f2f05d20
The Map Group <admin@the-map-group.pictures>
2021-12-12 02:36:20 -0300

    [auto] Updated map for member 'ByronDelgado'

--- people/delgadobyron/countries.py
+++ people/delgadobyron/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
+  'GB': ['United Kingdom', 84, 235],
   'HU': ['Hungary', 4, 5],
-  'IT': ['Italy', 6, 60],
   'DE': ['Germany', 1, 1],
-  'GB': ['United Kingdom', 83, 234],
-  'GR': ['Greece', 5, 6],
-  'PL': ['Poland', 6, 6]
+  'PL': ['Poland', 6, 6],
+  'IT': ['Italy', 6, 60],
+  'GR': ['Greece', 5, 6]
 }
