980c597121afdb051f256a55dc75129a508ba215
The Map Group <admin@the-map-group.pictures>
2021-12-13 03:11:14 -0300

    Removed member 'bramreinders'

--- people/bramreinders/countries.py
+++
@@ -1,9 +0,0 @@
-countries_dict = {
-  'EG': ['Egypt', 3, 8],
-  'CZ': ['Czechia', 6, 11],
-  'TR': ['Turkey', 11, 11],
-  'GR': ['Greece', 8, 8],
-  'NL': ['Netherlands', 2002, 2498],
-  'TH': ['Thailand', 25, 28],
-  'DE': ['Germany', 23, 33]
-}
--- people/bramreinders/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '28628934@N02',
-  'alias': 'bramreinders',
-  'name': 'bram reinders',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/28628934@N02_r.jpg',
-  'url': 'https://www.flickr.com/photos/bramreinders/',
-  'markers': 2078,
-  'photos': 2597
-}
