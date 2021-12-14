79b540a1456af091e1055bdb0e73cbc0def28c3d
The Map Group <admin@the-map-group.pictures>
2021-12-14 03:07:18 -0300

    Removed member 'tguppy37401'

--- people/tguppy37401/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '90238473@N02',
-  'alias': 'tguppy37401',
-  'name': 'Thirty Seven 401',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/90238473@N02_r.jpg',
-  'url': 'https://www.flickr.com/photos/tguppy37401/',
-  'markers': 743,
-  'photos': 2636
-}
