f06630d7ef6348c798b590ab4fa7209aba85cc2f
The Map Group <admin@the-map-group.pictures>
2021-12-22 02:36:48 -0300

    [auto] Updated map for member 'ForceMajeureMontenegro'

--- people/montenegrina/user.py
+++ people/montenegrina/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'ForceMajeureMontenegro',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/92496593@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/montenegrina/',
-  'markers': 958,
-  'photos': 1105
+  'markers': 960,
+  'photos': 1107
 }
