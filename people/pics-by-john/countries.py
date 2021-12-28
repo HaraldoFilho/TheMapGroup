32571566ca679beac0b4e2576ccb140e71f2ff76
The Map Group <admin@the-map-group.pictures>
2021-12-28 02:48:23 -0300

    [auto] Updated map for member 'John Woolley Photos'

---
+++ people/pics-by-john/countries.py
@@ -0,0 +1,20 @@
+countries_dict = {
+  'AT': ['Austria', 1, 2],
+  'NL': ['Netherlands', 7, 10],
+  'US': ['United States of America', 1, 4],
+  'GG': ['Guernsey', 9, 10],
+  'GB': ['United Kingdom', 1918, 8707],
+  'CA': ['Canada', 9, 11],
+  'ES': ['Spain', 3, 4],
+  'IT': ['Italy', 17, 20],
+  'IN': ['India', 1, 1],
+  'FR': ['France', 5, 6],
+  'MC': ['Monaco', 1, 1],
+  'CH': ['Switzerland', 1, 3],
+  'JE': ['Jersey', 15, 18],
+  'IE': ['Ireland', 3, 8],
+  'DE': ['Germany', 1, 1],
+  'PT': ['Portugal', 18, 33],
+  'GR': ['Greece', 14, 22],
+  'ID': ['Indonesia', 1, 2]
+}
---
+++ people/pics-by-john/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '59961745@N06',
+  'alias': 'pics-by-john',
+  'name': 'John Woolley',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/59961745@N06_r.jpg',
+  'url': 'https://www.flickr.com/photos/pics-by-john/',
+  'markers': 2025,
+  'photos': 8863
+}
