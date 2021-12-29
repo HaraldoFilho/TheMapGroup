ab1f2f22e431f6529f99fd533d6c743f3a8f8fb5
The Map Group <admin@the-map-group.pictures>
2021-12-29 02:26:33 -0300

    [auto] Updated map for member 'NiBe60'

--- people/162684392@N05/countries.py
+++ people/162684392@N05/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'AT': ['Austria', 32, 36],
-  'FR': ['France', 1, 1],
-  'IT': ['Italy', 22, 25],
-  'DE': ['Germany', 64, 65],
   'ES': ['Spain', 22, 23],
-  'GR': ['Greece', 7, 7],
-  'CH': ['Switzerland', 14, 16]
+  'IT': ['Italy', 22, 25],
+  'FR': ['France', 1, 1],
+  'AT': ['Austria', 33, 37],
+  'DE': ['Germany', 64, 65],
+  'CH': ['Switzerland', 14, 16],
+  'GR': ['Greece', 7, 7]
 }
