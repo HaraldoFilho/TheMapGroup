9d4892d1b6db6d2fddd414f9f160c3bf0abf8090
The Map Group <admin@the-map-group.pictures>
2021-12-30 02:48:17 -0300

    [auto] Updated map for member 'iLOVEnature's Photography Inspiration'

---
+++ people/159070028@N08/countries.py
@@ -0,0 +1,5 @@
+countries_dict = {
+  'SG': ['Singapore', 18, 18],
+  'CN': ['South China Sea Islands (disputed)', 3, 3],
+  'US': ['United States', 93, 107]
+}
---
+++ people/159070028@N08/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '159070028@N08',
+  'alias': '159070028@N08',
+  'name': 'iLOVEnature Photography Inspir',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/159070028@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/159070028@N08/',
+  'markers': 114,
+  'photos': 128
+}
