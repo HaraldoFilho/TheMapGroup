326c4d3908020a9d8b5c730a69bab241032242cb
The Map Group <admin@the-map-group.pictures>
2021-12-31 02:31:05 -0300

    [auto] Updated map for member 'Edouard55'

--- people/19239093@N03/user.py
+++ people/19239093@N03/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Edouard',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/19239093@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/19239093@N03/',
-  'markers': 5125,
-  'photos': 6707
+  'markers': 5129,
+  'photos': 6711
 }
