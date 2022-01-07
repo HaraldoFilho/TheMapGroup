6701fa9a8c7434b03b2c16f3c2c936bfd2582210
The Map Group <admin@the-map-group.pictures>
2022-01-07 02:36:42 -0300

    [auto] Updated map for member 'amalthus'

--- people/168583569@N03/user.py
+++ people/168583569@N03/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'amalthus',
   'avatar': '../../icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/168583569@N03/',
-  'markers': 5,
-  'photos': 445
+  'markers': 6,
+  'photos': 415
 }
