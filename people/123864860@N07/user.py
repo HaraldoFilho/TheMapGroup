b089cb59a6d74db3fc923f41069909d0e957eb89
The Map Group <admin@the-map-group.pictures>
2022-01-08 02:32:05 -0300

    [auto] Updated map for member 'Luigibalza'

--- people/123864860@N07/user.py
+++ people/123864860@N07/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Luigibalza',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/123864860@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/123864860@N07/',
-  'markers': 4135,
-  'photos': 5387
+  'markers': 4140,
+  'photos': 5393
 }
