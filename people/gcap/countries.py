cd79f130c18b9808b66c7dd03d00b8994ba13531
The Map Group <admin@the-map-group.pictures>
2022-01-08 02:26:26 -0300

    [auto] Updated map for member 'Giovanni C.'

--- people/gcap/countries.py
+++ people/gcap/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'AT': ['Austria', 1, 1],
   'BE': ['Belgium', 2, 3],
-  'RW': ['Rwanda', 3, 3],
-  'MV': ['Maldives', 2, 14],
-  'QA': ['Qatar', 2, 3],
-  'GR': ['Greece', 699, 1698],
-  'IT': ['Italy', 1, 1],
   'AL': ['Albania', 1, 2],
-  'MA': ['Morocco', 20, 23]
+  'QA': ['Qatar', 2, 3],
+  'MV': ['Maldives', 2, 14],
+  'RW': ['Rwanda', 3, 3],
+  'IT': ['Italy', 1, 1],
+  'GR': ['Greece', 699, 1699],
+  'MA': ['Morocco', 20, 23],
+  'AT': ['Austria', 1, 1]
 }
