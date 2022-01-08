e3a1266a24f4f27220a308dd6d28271b7b174895
The Map Group <admin@the-map-group.pictures>
2022-01-08 02:28:38 -0300

    [auto] Updated map for member 'Bilderschreiber'

--- people/39904644@N05/countries.py
+++ people/39904644@N05/countries.py
@@ -1,19 +1,19 @@
 countries_dict = {
-  'GI': ['Gibraltar', 12, 12],
   'NO': ['Norway', 146, 161],
-  'IT': ['Italy', 76, 95],
+  'HR': ['Croatia', 1, 1],
+  'SE': ['Sweden', 9, 9],
+  'FR': ['France', 2, 2],
   'NZ': ['New Zealand', 283, 371],
+  'IS': ['Iceland', 319, 358],
+  'IT': ['Italy', 76, 95],
   'CZ': ['Czechia', 5, 5],
   'GB': ['United Kingdom', 113, 152],
+  'AT': ['Austria', 27, 27],
   'DE': ['Germany', 237, 267],
-  'FR': ['France', 2, 2],
+  'GI': ['Gibraltar', 12, 12],
   'PT': ['Portugal', 48, 62],
-  'IS': ['Iceland', 317, 356],
-  'GR': ['Greece', 47, 51],
-  'HR': ['Croatia', 1, 1],
-  'TH': ['Thailand', 2, 2],
-  'SE': ['Sweden', 9, 9],
   'ES': ['Spain', 52, 53],
+  'TH': ['Thailand', 2, 2],
   'DK': ['Denmark', 3, 4],
-  'AT': ['Austria', 27, 27]
+  'GR': ['Greece', 47, 51]
 }
