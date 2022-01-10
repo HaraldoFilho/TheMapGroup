fbab54c2f656c09bafbe64bad3aef168c92787b6
The Map Group <admin@the-map-group.pictures>
2022-01-10 02:49:28 -0300

    [auto] Updated map for member 'salix.caprea'

--- people/55688814@N08/countries.py
+++ people/55688814@N08/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
+  'HR': ['Croatia', 2, 2],
+  'DE': ['Germany', 9, 9],
   'IT': ['Italy', 2, 2],
   'SI': ['Slovenia', 1, 1],
-  'RS': ['Serbia', 250, 251],
-  'DE': ['Germany', 9, 9],
+  'AT': ['Austria', 1, 2],
   'US': ['United States', 3, 3],
-  'HR': ['Croatia', 2, 2],
-  'AT': ['Austria', 1, 2]
+  'RS': ['Serbia', 251, 252]
 }
