90efbcf332b9a87bb30e249c4496ea56650f9520
The Map Group <admin@the-map-group.pictures>
2022-01-10 02:57:41 -0300

    [auto] Updated map for member 'NiBe60'

--- people/162684392@N05/countries.py
+++ people/162684392@N05/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
+  'ES': ['Spain', 22, 23],
   'GR': ['Greece', 7, 7],
   'IT': ['Italy', 22, 25],
+  'FR': ['France', 1, 1],
+  'DE': ['Germany', 65, 67],
   'CH': ['Switzerland', 14, 16],
-  'AT': ['Austria', 35, 40],
-  'DE': ['Germany', 65, 66],
-  'ES': ['Spain', 22, 23],
-  'FR': ['France', 1, 1]
+  'AT': ['Austria', 35, 40]
 }
--- people/162684392@N05/user.py
+++ people/162684392@N05/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/162684392@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/162684392@N05/',
   'markers': 166,
-  'photos': 178
+  'photos': 179
 }
