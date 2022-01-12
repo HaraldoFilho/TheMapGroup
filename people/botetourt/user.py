ccafe7c5bc77104299f2e393ec1aa03c65abb67d
The Map Group <admin@the-map-group.pictures>
2022-01-12 02:40:47 -0300

    [auto] Updated map for member 'Brunswick Forge'

--- people/botetourt/user.py
+++ people/botetourt/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/82487858@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/botetourt/',
   'markers': 3707,
-  'photos': 6848
+  'photos': 6849
 }
