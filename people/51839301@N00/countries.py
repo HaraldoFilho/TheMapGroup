9c649c549f31c75b9a5b5f1f8550b520cbebff18
The Map Group <admin@the-map-group.pictures>
2022-01-15 02:02:21 -0300

    [auto] Updated map for member 'hans pohl'

--- people/51839301@N00/countries.py
+++ people/51839301@N00/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'PT': ['Portugal', 2096, 3073],
-  'MA': ['Morocco', 93, 126],
   'ES': ['Spain', 445, 643],
-  'TN': ['Tunisia', 220, 409],
+  'MA': ['Morocco', 93, 126],
   'FR': ['France', 159, 323],
+  'TN': ['Tunisia', 220, 409],
+  'PT': ['Portugal', 2096, 3074],
   'IT': ['Italy', 12, 13]
 }
