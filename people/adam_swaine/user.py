aa097137995f32f7017037c29de1a3e09089f0c4
The Map Group <admin@the-map-group.pictures>
2022-01-18 02:41:38 -0300

    [auto] Updated map for member 'Adam Swaine'

--- people/adam_swaine/user.py
+++ people/adam_swaine/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Adam Swaine',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/43499552@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/adam_swaine/',
-  'markers': 13578,
-  'photos': 20982
+  'markers': 13583,
+  'photos': 20987
 }
