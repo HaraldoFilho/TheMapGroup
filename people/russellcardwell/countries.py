d546e9841741ff97b281a5cbe062b973b4082f4a
The Map Group <admin@the-map-group.pictures>
2022-01-27 03:08:40 -0300

    [auto] Updated map for member 'Cardwell Photo LLC | Thanks for 9 Million Views!'

--- people/russellcardwell/countries.py
+++ people/russellcardwell/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'US': ['United States', 866, 1764]
+  'US': ['United States', 866, 1765]
 }
--- people/russellcardwell/user.py
+++ people/russellcardwell/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/55341805@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/russellcardwell/',
   'markers': 866,
-  'photos': 1764
+  'photos': 1765
 }
