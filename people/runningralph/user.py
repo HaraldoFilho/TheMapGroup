7a54c0bc8e8f9b9931e14089529486f210e3120c
The Map Group <admin@the-map-group.pictures>
2022-02-02 03:22:38 -0300

    Removed member 'runningralph'

--- people/runningralph/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '95381107@N03',
-  'alias': 'runningralph',
-  'name': 'Ralph Apeldoorn',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/95381107@N03_r.jpg',
-  'url': 'https://www.flickr.com/photos/runningralph/',
-  'markers': 5483,
-  'photos': 11010
-}
