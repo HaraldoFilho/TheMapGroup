4e12f8cc88c0b40e71b64782f941d8a4e9178e9c
The Map Group <admin@the-map-group.pictures>
2022-02-03 04:46:10 -0300

    [auto] Updated map for member 'The Gidinski'

--- people/ggphoto36/countries.py
+++ people/ggphoto36/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
   'IT': ['Italy', 4, 4],
+  'US': ['United States', 893, 936],
+  'CR': ['Costa Rica', 1, 1],
   'BS': ['The Bahamas', 1, 1],
-  'US': ['United States', 890, 933],
-  'CA': ['Canada', 5, 5],
-  'CR': ['Costa Rica', 1, 1]
+  'CA': ['Canada', 5, 5]
 }
