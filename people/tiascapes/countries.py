79ad6a6bebcf35dca0c5240bd2b04c092498194d
The Map Group <admin@the-map-group.pictures>
2022-02-07 02:18:21 -0300

    [auto] Updated map for member 'TIA International Photography'

--- people/tiascapes/countries.py
+++ people/tiascapes/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'MX': ['Mexico', 4, 4],
-  'GH': ['Ghana', 2, 2],
-  'CN': ['South China Sea Islands (disputed)', 51, 51],
-  'FR': ['France', 23, 23],
   'GB': ['United Kingdom', 21, 21],
-  'AR': ['Argentina', 41, 43],
   'SG': ['Singapore', 7, 7],
+  'AR': ['Argentina', 41, 43],
+  'GH': ['Ghana', 2, 2],
+  'FR': ['France', 23, 23],
+  'CN': ['South China Sea Islands (disputed)', 51, 51],
+  'MX': ['Mexico', 4, 4],
+  'AU': ['Australia', 11, 12],
+  'US': ['United States of America', 620, 958],
   'CA': ['Canada', 84, 100],
-  'US': ['United States of America', 619, 957],
-  'MY': ['Malaysia', 7, 8],
-  'AU': ['Australia', 11, 12]
+  'MY': ['Malaysia', 7, 8]
 }
--- people/tiascapes/user.py
+++ people/tiascapes/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'TIA INTERNATIONAL PHOTOGRAPHY',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/12744878@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/tiascapes/',
-  'markers': 870,
-  'photos': 1228
+  'markers': 871,
+  'photos': 1229
 }
