9b5cc12c8102bae37c1de0933692806596c47f85
The Map Group <admin@the-map-group.pictures>
2022-02-10 02:16:38 -0300

    [auto] Updated map for member 'ManxieB- prefer comments!'

--- people/barbaraelaw/countries.py
+++ people/barbaraelaw/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
+  'IM': ['Isle of Man', 707, 843],
   'IE': ['Ireland', 35, 53],
   'GB': ['United Kingdom', 1638, 2043],
-  'IM': ['Isle of Man', 707, 842],
   'TZ': ['Tanzania', 62, 335]
 }
