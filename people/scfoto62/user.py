1828ce7d610ba333199ecf939fad02b79e6bdc29
The Map Group <admin@the-map-group.pictures>
2022-02-12 02:35:59 -0300

    [auto] Updated map for member 'CrËOS Photographie'

--- people/scfoto62/user.py
+++ people/scfoto62/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Sébastien Croës',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/51101129@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/scfoto62/',
-  'markers': 1467,
-  'photos': 2503
+  'markers': 1468,
+  'photos': 2504
 }
