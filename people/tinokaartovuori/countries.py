7638b4626c45a8a1a7ecf36e05972f584494d231
The Map Group <admin@the-map-group.pictures>
2022-02-18 02:40:13 -0300

    [auto] Updated map for member 'Kart5a'

--- people/tinokaartovuori/countries.py
+++ people/tinokaartovuori/countries.py
@@ -1,4 +1,5 @@
 countries_dict = {
+  'SC': ['Seychelles', 1, 1],
   'FI': ['Finland', 35, 36],
   'US': ['United States', 3, 3]
 }
--- people/tinokaartovuori/user.py
+++ people/tinokaartovuori/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Tino Kaartovuori',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/99502057@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/tinokaartovuori/',
-  'markers': 38,
-  'photos': 39
+  'markers': 39,
+  'photos': 40
 }
