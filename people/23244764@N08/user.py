22f43bce41623e984a562ea5ab3d76a7ed40c1c6
The Map Group <admin@the-map-group.pictures>
2022-02-19 02:39:40 -0300

    [auto] Updated map for member 'suntrana3'

---
+++ people/23244764@N08/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '23244764@N08',
+  'alias': '23244764@N08',
+  'name': 'Jim Smith',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/23244764@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/23244764@N08/',
+  'markers': 159,
+  'photos': 271
+}
