aa412911c885e84e6227b40ddac94de8b3696e89
The Map Group <admin@the-map-group.pictures>
2022-02-23 09:54:50 -0300

    [auto] Updated map for member 'fagniez'

---
+++ people/pascalfagniez/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '127946907@N07',
+  'alias': 'pascalfagniez',
+  'name': 'Pascal FAGNIEZ',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/127946907@N07_r.jpg',
+  'url': 'https://www.flickr.com/photos/pascalfagniez/',
+  'markers': 0,
+  'photos': 0
+}
