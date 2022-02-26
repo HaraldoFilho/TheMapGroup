f8953b5ad28f2ffedcd62f443ae40e885608a501
The Map Group <admin@the-map-group.pictures>
2022-02-26 03:48:09 -0300

    [auto] Updated map for member 'The Gidinski'

--- people/ggphoto36/countries.py
+++ people/ggphoto36/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'IT': ['Italy', 4, 4],
-  'US': ['United States', 893, 936],
   'CR': ['Costa Rica', 1, 1],
+  'CA': ['Canada', 5, 5],
+  'US': ['United States', 895, 938],
   'BS': ['The Bahamas', 1, 1],
-  'CA': ['Canada', 5, 5]
+  'IT': ['Italy', 4, 4]
 }
--- people/ggphoto36/user.py
+++ people/ggphoto36/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Gary Giddens',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/28018306@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/ggphoto36/',
-  'markers': 904,
-  'photos': 947
+  'markers': 906,
+  'photos': 949
 }
