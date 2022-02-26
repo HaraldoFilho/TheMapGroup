ed17cd75aaf1f95be3cb891887f1710c587a66c6
The Map Group <admin@the-map-group.pictures>
2022-02-26 02:09:41 -0300

    [auto] Updated map for member 'giovannicampus'

--- people/147135779@N02/countries.py
+++ people/147135779@N02/countries.py
@@ -1,18 +1,18 @@
 countries_dict = {
-  'VA': ['Vatican City', 9, 10],
-  'UZ': ['Uzbekistan', 328, 338],
+  'FR': ['France', 1, 1],
+  'TZ': ['Tanzania', 1, 1],
+  'US': ['United States of America', 7, 7],
   'AE': ['United Arab Emirates', 2, 2],
-  'RO': ['Romania', 1, 1],
-  'IT': ['Italy', 1071, 1249],
+  'IT': ['Italy', 1072, 1250],
   'CU': ['Cuba', 196, 384],
   'KZ': ['Kazakhstan', 2, 2],
-  'FR': ['France', 1, 1],
   'MV': ['Maldives', 1, 1],
+  'PT': ['Portugal', 1, 1],
+  'VA': ['Vatican City', 9, 10],
+  'UZ': ['Uzbekistan', 329, 339],
   'NL': ['Netherlands', 63, 124],
-  'KE': ['Kenya', 9, 9],
-  'LK': ['Sri Lanka', 229, 241],
-  'US': ['United States of America', 7, 7],
   'IN': ['India', 997, 1037],
-  'TZ': ['Tanzania', 1, 1],
-  'PT': ['Portugal', 1, 1]
+  'LK': ['Sri Lanka', 230, 242],
+  'KE': ['Kenya', 9, 9],
+  'RO': ['Romania', 1, 1]
 }
