3e7ad14a5fa03cdc2245332dd96115074e54742d
The Map Group <admin@the-map-group.pictures>
2022-03-02 02:27:59 -0300

    [auto] Updated map for member '七福神'

--- people/1one/countries.py
+++ people/1one/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'IT': ['Italy', 2, 2],
-  'JP': ['Japan', 1976, 35420],
-  'BS': ['Bahamas', 6, 8],
   'GB': ['United Kingdom', 1, 1],
+  'BS': ['Bahamas', 6, 8],
+  'JP': ['Japan', 1980, 35426],
+  'IT': ['Italy', 2, 2],
   'US': ['United States', 18, 21]
 }
