55c993b2decd9eda0c361b1b5ba62a3399fe9f8a
The Map Group <admin@the-map-group.pictures>
2022-03-07 03:08:30 -0300

    [auto] Updated map for member 'karsaiag'

---
+++ people/120304125@N06/countries.py
@@ -0,0 +1,11 @@
+countries_dict = {
+  'GR': ['Greece', 20, 20],
+  'AT': ['Austria', 6, 6],
+  'HR': ['Croatia', 16, 16],
+  'RO': ['Romania', 3, 3],
+  'ES': ['Spain', 7, 7],
+  'DE': ['Germany', 1, 1],
+  'HU': ['Hungary', 241, 242],
+  'SK': ['Slovakia', 10, 10],
+  'IL': ['Israel', 3, 3]
+}
---
+++ people/120304125@N06/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '120304125@N06',
+  'alias': '120304125@N06',
+  'name': 'András Karsai',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/120304125@N06_r.jpg',
+  'url': 'https://www.flickr.com/photos/120304125@N06/',
+  'markers': 307,
+  'photos': 308
+}
