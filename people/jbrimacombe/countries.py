31f9fdd426afc0ab4806e81651d0a54d24374439
The Map Group <admin@the-map-group.pictures>
2022-03-08 02:39:12 -0300

    [auto] Updated map for member 'Joe Bob Brim'

--- people/jbrimacombe/countries.py
+++ people/jbrimacombe/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'GB': ['United Kingdom', 7, 7],
-  'AU': ['Australia', 625, 1374],
   'US': ['United States', 10, 27],
-  'NZ': ['New Zealand', 3, 3]
+  'NZ': ['New Zealand', 3, 3],
+  'AU': ['Australia', 625, 1377],
+  'GB': ['United Kingdom', 7, 7]
 }
