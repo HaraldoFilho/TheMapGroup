015238e8b3bc4c89eb12f3ac0f1a729913479c69
The Map Group <admin@the-map-group.pictures>
2022-03-14 02:33:06 -0300

    [auto] Updated map for member 'Matjaž Skrinar'

--- people/130486903@N04/countries.py
+++ people/130486903@N04/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
   'IT': ['Italy', 3, 3],
-  'HU': ['Hungary', 1, 1],
+  'SI': ['Slovenia', 236, 244],
   'HR': ['Croatia', 6, 6],
-  'SI': ['Slovenia', 242, 250]
+  'HU': ['Hungary', 1, 1]
 }
