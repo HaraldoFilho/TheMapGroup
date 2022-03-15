f5c40417190ed7df8401c7314fbd7565f940efc8
The Map Group <admin@the-map-group.pictures>
2022-03-15 02:18:24 -0300

    [auto] Updated map for member 'gsegelken'

--- people/oldboyscout/countries.py
+++ people/oldboyscout/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'NZ': ['New Zealand', 112, 121],
-  'US': ['United States', 503, 529],
-  'CZ': ['Czechia', 1, 1],
-  'BE': ['Belgium', 5, 6],
   'JO': ['Jordan', 1, 1],
-  'CH': ['Switzerland', 8, 8],
-  'GL': ['Greenland', 1, 1],
+  'NZ': ['New Zealand', 112, 121],
+  'AT': ['Austria', 13, 15],
+  'CA': ['Canada', 6, 6],
+  'CN': ['South China Sea Islands (disputed)', 97, 107],
   'IS': ['Iceland', 47, 49],
   'IT': ['Italy', 2, 2],
-  'CN': ['South China Sea Islands (disputed)', 97, 107],
-  'DE': ['Germany', 21, 21],
+  'CH': ['Switzerland', 8, 8],
+  'US': ['United States', 511, 537],
   'HU': ['Hungary', 5, 7],
-  'CA': ['Canada', 6, 6],
-  'AT': ['Austria', 13, 15]
+  'DE': ['Germany', 21, 21],
+  'BE': ['Belgium', 5, 6],
+  'CZ': ['Czechia', 1, 1],
+  'GL': ['Greenland', 1, 1]
 }
