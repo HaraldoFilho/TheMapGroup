1887b987832370106b102b155d931877ad1851da
The Map Group <admin@the-map-group.pictures>
2022-03-15 02:26:07 -0300

    [auto] Updated map for member 'Sharon Mollerus'

--- people/clairity/countries.py
+++ people/clairity/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'IT': ['Italy', 16, 35],
   'DE': ['Germany', 15, 15],
-  'US': ['United States', 2833, 12137],
+  'FR': ['France', 42, 211],
+  'IT': ['Italy', 16, 35],
   'GR': ['Greece', 51, 255],
-  'CA': ['Canada', 95, 614],
   'ES': ['Spain', 63, 100],
-  'FR': ['France', 42, 211]
+  'US': ['United States', 2834, 12138],
+  'CA': ['Canada', 95, 614]
 }
