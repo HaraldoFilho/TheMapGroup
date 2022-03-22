25a908e64ffdbd140218b98444c47a223725b79b
The Map Group <admin@the-map-group.pictures>
2022-03-22 02:19:43 -0300

    [auto] Updated map for member 'Giovanni C.'

--- people/gcap/user.py
+++ people/gcap/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Giovanni C.',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/8299662@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/gcap/',
-  'markers': 743,
-  'photos': 1788
+  'markers': 744,
+  'photos': 1789
 }
