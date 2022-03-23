bc1895cb4788c513a1d06a6f881ef264874ef525
The Map Group <admin@the-map-group.pictures>
2022-03-23 02:11:09 -0300

    [auto] Updated map for member 'Ted's photos - Glory to Ukraine'

--- people/time-to-look/user.py
+++ people/time-to-look/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Ted McGrath',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/90955804@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/time-to-look/',
-  'markers': 10242,
-  'photos': 11993
+  'markers': 10243,
+  'photos': 11995
 }
