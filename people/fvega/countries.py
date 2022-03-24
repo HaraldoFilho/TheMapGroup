f38cefe36028fb3eccc1a6ae7b6e2fc851ed3734
The Map Group <admin@the-map-group.pictures>
2022-03-24 02:03:19 -0300

    [auto] Updated map for member 'Fernando Sa Rapita'

--- people/fvega/countries.py
+++ people/fvega/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
+  'NL': ['Netherlands', 20, 21],
+  'IS': ['Iceland', 634, 855],
   'CH': ['Switzerland', 7, 7],
   'DE': ['Germany', 25, 26],
-  'GB': ['United Kingdom', 690, 816],
   'ES': ['Spain', 1405, 2302],
-  'NL': ['Netherlands', 20, 21],
-  'IS': ['Iceland', 634, 854],
+  'GB': ['United Kingdom', 690, 816],
   'FI': ['Finland', 25, 25]
 }
