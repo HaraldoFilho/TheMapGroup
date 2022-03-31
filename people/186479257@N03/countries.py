c74119f428396a453a4f9184e44fd81a3b5e250c
The Map Group <admin@the-map-group.pictures>
2022-03-31 02:51:32 -0300

    [auto] Updated map for member 'SeaLord 50+'

--- people/186479257@N03/countries.py
+++ people/186479257@N03/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'GB': ['United Kingdom', 1, 1],
+  'US': ['United States of America', 1, 1],
+  'DE': ['Germany', 59, 60],
   'DK': ['Denmark', 1, 2],
   'TR': ['Turkey', 1, 1],
-  'DE': ['Germany', 58, 59],
+  'GB': ['United Kingdom', 1, 1],
   'IT': ['Italy', 1, 1],
-  'PT': ['Portugal', 2, 2],
   'HR': ['Croatia', 7, 12],
-  'US': ['United States of America', 1, 1]
+  'PT': ['Portugal', 2, 2]
 }
--- people/186479257@N03/user.py
+++ people/186479257@N03/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'SeaLord 50+',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/186479257@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/186479257@N03/',
-  'markers': 72,
-  'photos': 79
+  'markers': 73,
+  'photos': 80
 }
