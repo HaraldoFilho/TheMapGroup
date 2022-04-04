9cc8e5aec5c6f824b7688962acedff61fdd42572
The Map Group <admin@the-map-group.pictures>
2022-04-04 02:51:00 -0300

    [auto] Updated map for member 'Alexander Sand Halvorsen'

--- people/airscan/user.py
+++ people/airscan/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Alexander Sand Halvorsen',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/88321829@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/airscan/',
-  'markers': 119,
-  'photos': 144
+  'markers': 120,
+  'photos': 145
 }
