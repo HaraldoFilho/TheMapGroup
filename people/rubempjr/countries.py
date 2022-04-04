52826b19322c0dcfc20b522db94b4d5a0846eaa3
The Map Group <admin@the-map-group.pictures>
2022-04-04 02:55:23 -0300

    [auto] Updated map for member 'Rubem Jr'

--- people/rubempjr/countries.py
+++ people/rubempjr/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'PT': ['Portugal', 1844, 4334],
-  'IT': ['Italy', 294, 574],
   'AR': ['Argentina', 61, 123],
-  'RU': ['Russia', 1, 1],
-  'ES': ['Spain', 601, 1328],
-  'CL': ['Chile', 1, 1],
-  'GB': ['United Kingdom', 780, 2197],
-  'FR': ['France', 6, 24],
-  'US': ['United States of America', 5, 87],
-  'UY': ['Uruguay', 25, 183],
+  'VA': ['Vatican City', 12, 17],
   'BR': ['Brazil', 2147, 6044],
-  'VA': ['Vatican City', 12, 17]
+  'PT': ['Portugal', 1845, 4335],
+  'IT': ['Italy', 294, 574],
+  'CL': ['Chile', 1, 1],
+  'FR': ['France', 6, 24],
+  'ES': ['Spain', 601, 1328],
+  'UY': ['Uruguay', 25, 183],
+  'RU': ['Russia', 1, 1],
+  'US': ['United States of America', 5, 87],
+  'GB': ['United Kingdom', 780, 2197]
 }
--- people/rubempjr/user.py
+++ people/rubempjr/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Rubem Porto Jr',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/38795342@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/rubempjr/',
-  'markers': 5777,
-  'photos': 14913
+  'markers': 5778,
+  'photos': 14914
 }
