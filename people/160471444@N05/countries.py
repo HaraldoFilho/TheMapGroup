595f72bca148372220337bcbe6e1c28c1a12ba1f
The Map Group <admin@the-map-group.pictures>
2022-04-11 02:11:10 -0300

    [auto] Updated map for member 'astolfi.m'

--- people/160471444@N05/countries.py
+++ people/160471444@N05/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
+  'PE': ['Peru', 115, 961],
+  'IT': ['Italy', 763, 2980],
+  'CL': ['Chile', 4, 68],
   'CG': ['Republic of the Congo', 1, 1],
   'FR': ['France', 10, 96],
-  'PE': ['Peru', 115, 961],
-  'IT': ['Italy', 758, 2974],
   'ES': ['Spain', 27, 223],
-  'CL': ['Chile', 4, 68],
-  'PT': ['Portugal', 29, 161],
+  'HU': ['Hungary', 35, 160],
   'AR': ['Argentina', 28, 382],
-  'HU': ['Hungary', 35, 160]
+  'PT': ['Portugal', 29, 161]
 }
