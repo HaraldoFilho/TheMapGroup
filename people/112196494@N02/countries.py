d08f153a67b605260c096ead7ced28347d26893c
The Map Group <admin@the-map-group.pictures>
2022-04-11 02:22:07 -0300

    [auto] Updated map for member 'thunderbird-72'

--- people/112196494@N02/countries.py
+++ people/112196494@N02/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'HR': ['Croatia', 3, 3],
-  'PT': ['Portugal', 19, 28],
-  'SI': ['Slovenia', 71, 112],
   'LU': ['Luxembourg', 37, 62],
-  'IT': ['Italy', 83, 143],
-  'CH': ['Switzerland', 4, 4],
-  'DE': ['Germany', 760, 1509],
-  'ES': ['Spain', 4, 14],
-  'FR': ['France', 207, 479],
+  'HR': ['Croatia', 3, 3],
   'AT': ['Austria', 118, 168],
-  'CZ': ['Czechia', 9, 11]
+  'CZ': ['Czechia', 9, 11],
+  'DE': ['Germany', 761, 1511],
+  'IT': ['Italy', 83, 143],
+  'FR': ['France', 207, 479],
+  'ES': ['Spain', 4, 14],
+  'SI': ['Slovenia', 71, 112],
+  'PT': ['Portugal', 19, 28],
+  'CH': ['Switzerland', 4, 4]
 }
