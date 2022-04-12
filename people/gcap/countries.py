0638f65eca95a0117b6474c7f13467ad41eb9af9
The Map Group <admin@the-map-group.pictures>
2022-04-12 02:24:17 -0300

    [auto] Updated map for member 'Giovanni C.'

--- people/gcap/countries.py
+++ people/gcap/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'GR': ['Greece', 715, 1750],
-  'RW': ['Rwanda', 3, 3],
-  'MA': ['Morocco', 20, 23],
-  'AL': ['Albania', 1, 2],
-  'BE': ['Belgium', 2, 3],
-  'IT': ['Italy', 1, 1],
   'MV': ['Maldives', 2, 14],
+  'MA': ['Morocco', 20, 23],
+  'RW': ['Rwanda', 3, 3],
   'QA': ['Qatar', 2, 3],
-  'AT': ['Austria', 1, 1]
+  'AT': ['Austria', 1, 1],
+  'GR': ['Greece', 715, 1751],
+  'IT': ['Italy', 1, 1],
+  'BE': ['Belgium', 2, 3],
+  'AL': ['Albania', 1, 2]
 }
--- people/gcap/user.py
+++ people/gcap/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/8299662@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/gcap/',
   'markers': 747,
-  'photos': 1800
+  'photos': 1801
 }
