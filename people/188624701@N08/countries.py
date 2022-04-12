95fd10b9e40beea65d475eace9061bf8705a4255
The Map Group <admin@the-map-group.pictures>
2022-04-12 02:17:13 -0300

    [auto] Updated map for member 'Pepper peppers'

--- people/188624701@N08/countries.py
+++ people/188624701@N08/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'HR': ['Croatia', 40, 59]
+  'HR': ['Croatia', 41, 60]
 }
--- people/188624701@N08/user.py
+++ people/188624701@N08/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Vedran Peruničić',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/188624701@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/188624701@N08/',
-  'markers': 40,
-  'photos': 59
+  'markers': 41,
+  'photos': 60
 }
