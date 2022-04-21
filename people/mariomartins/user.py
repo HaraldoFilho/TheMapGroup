0cef7b66f253d5cfc0e9b71799d6dfcd29573be2
The Map Group <admin@the-map-group.pictures>
2022-04-21 02:06:19 -0300

    [auto] Updated map for member 'pepemartin2006'

--- people/mariomartins/user.py
+++ people/mariomartins/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Mário  Martins (Pepe Martín)',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/188218905@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/mariomartins/',
-  'markers': 89,
-  'photos': 145
+  'markers': 90,
+  'photos': 146
 }
