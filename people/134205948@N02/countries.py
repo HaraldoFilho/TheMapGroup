dafdd079933d20a93b999d0b4178acbf16e28175
The Map Group <admin@the-map-group.pictures>
2022-04-22 02:15:20 -0300

    [auto] Updated map for member 'giorgiorodano46'

--- people/134205948@N02/countries.py
+++ people/134205948@N02/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
+  'VA': ['Vatican City', 10, 10],
   'CH': ['Switzerland', 597, 648],
   'HR': ['Croatia', 12, 13],
-  'FR': ['France', 93, 103],
-  'AT': ['Austria', 1, 1],
-  'VA': ['Vatican City', 10, 10],
   'GB': ['United Kingdom', 8, 8],
-  'IT': ['Italy', 4356, 4658]
+  'IT': ['Italy', 4358, 4660],
+  'FR': ['France', 93, 103],
+  'AT': ['Austria', 1, 1]
 }
--- people/134205948@N02/user.py
+++ people/134205948@N02/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Giorgio Rodano',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/134205948@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/134205948@N02/',
-  'markers': 5077,
-  'photos': 5441
+  'markers': 5079,
+  'photos': 5443
 }
