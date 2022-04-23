89c73988d1e6f15cd06cb8b332131ddd746a3460
The Map Group <admin@the-map-group.pictures>
2022-04-23 02:31:19 -0300

    [auto] Updated map for member 'thunderbird-72'

--- people/112196494@N02/countries.py
+++ people/112196494@N02/countries.py
@@ -1,13 +1,14 @@
 countries_dict = {
-  'LU': ['Luxembourg', 37, 62],
-  'HR': ['Croatia', 3, 3],
-  'AT': ['Austria', 118, 168],
-  'CZ': ['Czechia', 9, 11],
-  'DE': ['Germany', 761, 1511],
   'IT': ['Italy', 83, 143],
+  'CZ': ['Czechia', 9, 11],
+  'CH': ['Switzerland', 4, 4],
   'FR': ['France', 207, 479],
+  'NL': ['Netherlands', 1, 1],
   'ES': ['Spain', 4, 14],
   'SI': ['Slovenia', 71, 112],
+  'DE': ['Germany', 761, 1511],
+  'LU': ['Luxembourg', 37, 62],
   'PT': ['Portugal', 19, 28],
-  'CH': ['Switzerland', 4, 4]
+  'HR': ['Croatia', 3, 3],
+  'AT': ['Austria', 118, 168]
 }
--- people/112196494@N02/user.py
+++ people/112196494@N02/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'thunderbird-72',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/112196494@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/112196494@N02/',
-  'markers': 1316,
-  'photos': 2535
+  'markers': 1317,
+  'photos': 2536
 }
