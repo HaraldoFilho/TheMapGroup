cebbeed75179a0e84e272568acac30dee951be36
The Map Group <admin@the-map-group.pictures>
2022-04-26 02:04:29 -0300

    [auto] Updated map for member '5.25 mil views - Thank you all.'

--- people/staneastwood/countries.py
+++ people/staneastwood/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
+  'ES': ['Spain', 67, 665],
+  'FR': ['France', 9, 295],
+  'GL': ['Greenland', 8, 335],
+  'IM': ['Isle of Man', 994, 1808],
+  'PT': ['Portugal', 43, 358],
+  'NO': ['Norway', 42, 553],
+  'CA': ['Canada', 357, 1134],
+  'DK': ['Denmark', 1, 59],
   'IE': ['Ireland', 20, 187],
   'GI': ['Gibraltar', 1, 94],
-  'FR': ['France', 9, 295],
-  'GB': ['United Kingdom', 3797, 25019],
-  'NO': ['Norway', 42, 553],
-  'DK': ['Denmark', 1, 59],
-  'ES': ['Spain', 67, 665],
-  'PT': ['Portugal', 43, 358],
   'US': ['United States', 169, 580],
   'IS': ['Iceland', 45, 295],
-  'CA': ['Canada', 357, 1134],
-  'IM': ['Isle of Man', 994, 1808],
-  'GL': ['Greenland', 8, 335]
+  'GB': ['United Kingdom', 3798, 25025]
 }
--- people/staneastwood/user.py
+++ people/staneastwood/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Stanley Eastwood',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/63713558@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/staneastwood/',
-  'markers': 5557,
-  'photos': 31507
+  'markers': 5558,
+  'photos': 31513
 }
