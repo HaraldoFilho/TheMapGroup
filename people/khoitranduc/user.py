e6af757baf1e475e2a9b921dc3b63cba0b211b6f
The Map Group <admin@the-map-group.pictures>
2022-04-27 02:17:43 -0300

    [auto] Updated map for member 'khoitran1957'

--- people/khoitranduc/user.py
+++ people/khoitranduc/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'khoi tranduc',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/45919374@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/khoitranduc/',
-  'markers': 404,
-  'photos': 631
+  'markers': 405,
+  'photos': 632
 }
