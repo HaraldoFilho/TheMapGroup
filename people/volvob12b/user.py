f95fdb02997edb62088a014f13afd32a2861a94f
The Map Group <admin@the-map-group.pictures>
2022-04-27 02:08:28 -0300

    [auto] Updated map for member 'Bernard Spragg'

--- people/volvob12b/user.py
+++ people/volvob12b/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/88123769@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/volvob12b/',
   'markers': 4136,
-  'photos': 5679
+  'photos': 5680
 }
