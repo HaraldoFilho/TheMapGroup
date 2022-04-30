912a9fcd75ab85349cc3d8ec9535a0a1e1ead1d4
The Map Group <admin@the-map-group.pictures>
2022-04-30 02:18:56 -0300

    [auto] Updated map for member 'gsegelken'

--- people/oldboyscout/countries.py
+++ people/oldboyscout/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'NZ': ['New Zealand', 112, 121],
-  'AT': ['Austria', 13, 15],
-  'US': ['United States', 466, 489],
-  'CZ': ['Czechia', 1, 1],
-  'IS': ['Iceland', 48, 50],
-  'IT': ['Italy', 2, 2],
-  'BE': ['Belgium', 5, 6],
-  'CA': ['Canada', 6, 6],
-  'JO': ['Jordan', 1, 1],
   'HU': ['Hungary', 5, 7],
-  'CH': ['Switzerland', 8, 8],
+  'IS': ['Iceland', 48, 50],
+  'BE': ['Belgium', 5, 6],
+  'IT': ['Italy', 2, 2],
+  'NZ': ['New Zealand', 112, 121],
+  'CZ': ['Czechia', 1, 1],
+  'GL': ['Greenland', 1, 1],
+  'CA': ['Canada', 6, 6],
+  'AT': ['Austria', 13, 15],
   'DE': ['Germany', 21, 21],
+  'US': ['United States', 467, 490],
+  'JO': ['Jordan', 1, 1],
   'CN': ['South China Sea Islands (disputed)', 97, 107],
-  'GL': ['Greenland', 1, 1]
+  'CH': ['Switzerland', 8, 8]
 }
--- people/oldboyscout/user.py
+++ people/oldboyscout/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'gsegelken',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/100209004@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/oldboyscout/',
-  'markers': 786,
-  'photos': 835
+  'markers': 787,
+  'photos': 836
 }
