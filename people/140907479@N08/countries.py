670c42bbdddc7f024f21bdfd9bd5b2b11c4540cc
The Map Group <admin@the-map-group.pictures>
2022-04-30 02:26:20 -0300

    [auto] Updated map for member 'Crystal.Rain'

--- people/140907479@N08/countries.py
+++ people/140907479@N08/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'RU': ['Russia', 445, 449],
   'NL': ['Netherlands', 3, 3],
   'US': ['United States of America', 3, 3],
-  'AQ': ['Antarctica', 1, 3]
+  'AQ': ['Antarctica', 1, 3],
+  'RU': ['Russia', 436, 440]
 }
--- people/140907479@N08/user.py
+++ people/140907479@N08/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Dmitry Ivanov',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/140907479@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/140907479@N08/',
-  'markers': 452,
-  'photos': 458
+  'markers': 443,
+  'photos': 449
 }
