a1a7b18a6f8cf754149c47214e865b49fd113a7e
The Map Group <admin@the-map-group.pictures>
2022-05-01 02:10:20 -0300

    [auto] Updated map for member 'Ted's photos - Returns mid June'

--- people/time-to-look/user.py
+++ people/time-to-look/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Ted McGrath',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/90955804@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/time-to-look/',
-  'markers': 10341,
-  'photos': 12132
+  'markers': 10344,
+  'photos': 12135
 }
