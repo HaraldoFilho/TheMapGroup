8b900b2360ab8415f568dfef75b7b9973e0c9401
The Map Group <admin@the-map-group.pictures>
2022-05-02 02:27:05 -0300

    [auto] Updated map for member 'Ivan Ujevic'

---
+++ people/194472729@N05/countries.py
@@ -0,0 +1,4 @@
+countries_dict = {
+  'SI': ['Slovenia', 2, 2],
+  'HR': ['Croatia', 15, 15]
+}
---
+++ people/194472729@N05/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '194472729@N05',
+  'alias': '194472729@N05',
+  'name': 'IVAN UJEVIC',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/194472729@N05_r.jpg',
+  'url': 'https://www.flickr.com/photos/194472729@N05/',
+  'markers': 17,
+  'photos': 17
+}
