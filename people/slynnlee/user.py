553c2ca63685fecda50844733908e3ac70e403b6
The Map Group <admin@the-map-group.pictures>
2022-05-04 02:11:38 -0300

    [auto] Updated map for member 'S LYNN LEE'

--- people/slynnlee/user.py
+++ people/slynnlee/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'S Lynn Lee',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/144075308@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/slynnlee/',
-  'markers': 685,
-  'photos': 4167
+  'markers': 687,
+  'photos': 4169
 }
