2813dad25088792a53dfcd1b6f002aee37963432
The Map Group <admin@the-map-group.pictures>
2022-05-06 02:00:36 -0300

    [auto] Updated map for member 'hans pohl'

--- people/51839301@N00/countries.py
+++ people/51839301@N00/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'TN': ['Tunisia', 220, 409],
-  'ES': ['Spain', 478, 683],
+  'PT': ['Portugal', 2157, 3141],
   'MA': ['Morocco', 93, 126],
-  'IT': ['Italy', 12, 13],
   'FR': ['France', 161, 326],
-  'PT': ['Portugal', 2157, 3141]
+  'IT': ['Italy', 12, 13],
+  'TN': ['Tunisia', 220, 409],
+  'ES': ['Spain', 479, 684]
 }
--- people/51839301@N00/user.py
+++ people/51839301@N00/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Hans Pohl',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/51839301@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/51839301@N00/',
-  'markers': 3121,
-  'photos': 4698
+  'markers': 3122,
+  'photos': 4699
 }
