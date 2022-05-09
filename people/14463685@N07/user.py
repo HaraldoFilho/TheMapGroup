46d43275f0279baee26945b7567a5de948f10b15
The Map Group <admin@the-map-group.pictures>
2022-05-09 02:16:42 -0300

    [auto] Updated map for member 'Howard Somerville'

--- people/14463685@N07/user.py
+++ people/14463685@N07/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Howard Somerville',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/14463685@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/14463685@N07/',
-  'markers': 541,
-  'photos': 626
+  'markers': 542,
+  'photos': 627
 }
