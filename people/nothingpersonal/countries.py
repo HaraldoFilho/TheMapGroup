44a98bff65588e11a8594e46f7c636ddaee1375c
The Map Group <admin@the-map-group.pictures>
2022-05-10 02:10:23 -0300

    [auto] Updated map for member 'Dmitry Djouce'

--- people/nothingpersonal/countries.py
+++ people/nothingpersonal/countries.py
@@ -1,18 +1,18 @@
 countries_dict = {
-  'FR': ['France', 384, 426],
-  'CH': ['Switzerland', 30, 39],
+  'GB': ['United Kingdom', 720, 819],
   'AT': ['Austria', 45, 46],
+  'LU': ['Luxembourg', 17, 19],
+  'ES': ['Spain', 180, 196],
+  'UA': ['Ukraine', 142, 151],
+  'DK': ['Denmark', 8, 12],
   'HU': ['Hungary', 37, 38],
   'DE': ['Germany', 75, 81],
-  'DK': ['Denmark', 8, 12],
-  'IT': ['Italy', 224, 245],
-  'UA': ['Ukraine', 142, 151],
-  'ES': ['Spain', 180, 196],
   'RU': ['Russia', 378, 504],
-  'GB': ['United Kingdom', 718, 817],
+  'FR': ['France', 384, 426],
+  'CH': ['Switzerland', 30, 39],
   'CZ': ['Czechia', 58, 58],
-  'BE': ['Belgium', 53, 56],
+  'IT': ['Italy', 224, 245],
   'VA': ['Vatican City', 2, 3],
-  'LU': ['Luxembourg', 17, 19],
-  'NL': ['Netherlands', 76, 79]
+  'NL': ['Netherlands', 76, 79],
+  'BE': ['Belgium', 53, 56]
 }
