4173778b1b7957a2d09289729cd69335d2c45d05
The Map Group <admin@the-map-group.pictures>
2022-05-10 02:46:16 -0300

    [auto] Updated map for member 'katlamsib'

--- people/153189513@N08/countries.py
+++ people/153189513@N08/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
   'AZ': ['Azerbaijan', 10, 10],
-  'KZ': ['Kazakhstan', 21, 22],
-  'BY': ['Belarus', 12, 15],
   'UZ': ['Uzbekistan', 4, 7],
-  'RU': ['Russia', 561, 625]
+  'RU': ['Russia', 564, 629],
+  'BY': ['Belarus', 12, 15],
+  'KZ': ['Kazakhstan', 21, 22]
 }
--- people/153189513@N08/user.py
+++ people/153189513@N08/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Pavel',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/153189513@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/153189513@N08/',
-  'markers': 608,
-  'photos': 679
+  'markers': 611,
+  'photos': 683
 }
