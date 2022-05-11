c102ba7cd78dab9d0f1b22e2e863e628ad5c11aa
The Map Group <admin@the-map-group.pictures>
2022-05-11 03:04:16 -0300

    Removed member '160471444@N05'

--- people/160471444@N05/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '160471444@N05',
-  'alias': '160471444@N05',
-  'name': 'Mauro Astolfi',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/160471444@N05_r.jpg',
-  'url': 'https://www.flickr.com/photos/160471444@N05/',
-  'markers': 1068,
-  'photos': 5112
-}
