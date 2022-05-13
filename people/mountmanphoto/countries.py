c94a171aed77ca8a5468b1a1458159f21b42c795
The Map Group <admin@the-map-group.pictures>
2022-05-13 02:16:05 -0300

    [auto] Updated map for member 'MountMan Photo temp. OFF/ON'

--- people/mountmanphoto/countries.py
+++ people/mountmanphoto/countries.py
@@ -1,4 +1,4 @@
 countries_dict = {
-  'SI': ['Slovenia', 5, 7],
-  'HR': ['Croatia', 455, 550]
+  'HR': ['Croatia', 456, 551],
+  'SI': ['Slovenia', 5, 7]
 }
--- people/mountmanphoto/user.py
+++ people/mountmanphoto/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'MountMan Photo',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/125923483@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/mountmanphoto/',
-  'markers': 460,
-  'photos': 557
+  'markers': 461,
+  'photos': 558
 }
