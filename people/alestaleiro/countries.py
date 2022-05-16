40bdb6cc5001b0ab9a339dec9de6804f2b9f8d3a
The Map Group <admin@the-map-group.pictures>
2022-05-16 02:28:13 -0300

    [auto] Updated map for member 'alestaleiro'

--- people/alestaleiro/countries.py
+++ people/alestaleiro/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'PE': ['Peru', 14, 24],
-  'AR': ['Argentina', 85, 89],
-  'CL': ['Chile', 26, 31],
-  'NL': ['Netherlands', 2, 2],
-  'BE': ['Belgium', 2, 2],
-  'IT': ['Italy', 10, 11],
-  'DE': ['Germany', 8, 8],
-  'US': ['United States of America', 82, 106],
   'FR': ['France', 1, 1],
   'ZA': ['South Africa', 129, 165],
-  'BR': ['Brazil', 1678, 2270]
+  'NL': ['Netherlands', 2, 2],
+  'BR': ['Brazil', 1679, 2273],
+  'AR': ['Argentina', 85, 89],
+  'BE': ['Belgium', 2, 2],
+  'US': ['United States of America', 82, 106],
+  'IT': ['Italy', 10, 11],
+  'CL': ['Chile', 26, 31],
+  'DE': ['Germany', 8, 8],
+  'PE': ['Peru', 14, 24]
 }
--- people/alestaleiro/user.py
+++ people/alestaleiro/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Alejandro David Olivera',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/99949037@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/alestaleiro/',
-  'markers': 2037,
-  'photos': 2709
+  'markers': 2038,
+  'photos': 2712
 }
