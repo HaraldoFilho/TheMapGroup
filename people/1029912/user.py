7a917b46f1aa50d2830990c3a88d22b932cc054c
The Map Group <admin@the-map-group.pictures>
2022-05-24 02:37:02 -0300

    Removed member '1029912'

--- people/1029912/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '94511792@N03',
-  'alias': '1029912',
-  'name': 'jack .b.',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/94511792@N03_r.jpg',
-  'url': 'https://www.flickr.com/photos/1029912/',
-  'markers': 27,
-  'photos': 28
-}
