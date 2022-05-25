ead19df3ac46f7ee4515489690762ad912464f1c
The Map Group <admin@the-map-group.pictures>
2022-05-25 02:11:03 -0300

    [auto] Updated map for member 'GMLSKIS'

--- people/38730049@N08/countries.py
+++ people/38730049@N08/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'US': ['United States of America', 841, 40186]
+  'US': ['United States of America', 841, 40221]
 }
--- people/38730049@N08/user.py
+++ people/38730049@N08/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/38730049@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/38730049@N08/',
   'markers': 841,
-  'photos': 40186
+  'photos': 40221
 }
