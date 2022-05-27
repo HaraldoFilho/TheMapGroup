6e2aefebc56c843290aec6a03f0d1cab97db6678
The Map Group <admin@the-map-group.pictures>
2022-05-27 02:39:11 -0300

    Removed member '194472729@N05'

--- people/194472729@N05/countries.py
+++
@@ -1,4 +0,0 @@
-countries_dict = {
-  'HR': ['Croatia', 21, 22],
-  'SI': ['Slovenia', 2, 2]
-}
--- people/194472729@N05/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '194472729@N05',
-  'alias': '194472729@N05',
-  'name': 'IVAN UJEVIC',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/194472729@N05_r.jpg',
-  'url': 'https://www.flickr.com/photos/194472729@N05/',
-  'markers': 23,
-  'photos': 24
-}
