581356ff510a4005bf331ed961bbc575559999c1
The Map Group <admin@the-map-group.pictures>
2022-05-29 02:24:16 -0300

    [auto] Updated map for member 'Domikawa4'

--- people/153072396@N07/countries.py
+++ people/153072396@N07/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'GB': ['United Kingdom', 54, 91],
-  'FR': ['France', 81, 308],
-  'SK': ['Slovakia', 2, 2],
+  'IE': ['Ireland', 20, 26],
   'AT': ['Austria', 9, 12],
-  'NL': ['Netherlands', 15, 26],
+  'MT': ['Malta', 20, 41],
   'BE': ['Belgium', 1, 1],
   'DK': ['Denmark', 58, 93],
+  'NL': ['Netherlands', 15, 26],
   'DE': ['Germany', 7, 8],
+  'SK': ['Slovakia', 2, 2],
   'CZ': ['Czechia', 3, 3],
-  'MT': ['Malta', 20, 41],
-  'IE': ['Ireland', 20, 25]
+  'GB': ['United Kingdom', 54, 91],
+  'FR': ['France', 81, 308]
 }
