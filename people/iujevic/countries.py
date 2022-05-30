ce2e190059cc23b4a62be79ec0ed4cbb5e432931
The Map Group <admin@the-map-group.pictures>
2022-05-30 02:38:37 -0300

    Removed member 'iujevic'

--- people/iujevic/countries.py
+++
@@ -1,4 +0,0 @@
-countries_dict = {
-  'HR': ['Croatia', 29, 30],
-  'SI': ['Slovenia', 2, 2]
-}
--- people/iujevic/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '194472729@N05',
-  'alias': 'iujevic',
-  'name': 'IVAN UJEVIC',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/194472729@N05_r.jpg',
-  'url': 'https://www.flickr.com/photos/iujevic/',
-  'markers': 31,
-  'photos': 32
-}
