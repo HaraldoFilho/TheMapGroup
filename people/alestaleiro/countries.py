7dc8d898e14041f0eb60de7c02914f91615edf19
The Map Group <admin@the-map-group.pictures>
2022-06-02 02:22:43 -0300

    [auto] Updated map for member 'alestaleiro'

--- people/alestaleiro/countries.py
+++ people/alestaleiro/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'NL': ['Netherlands', 2, 2],
-  'FR': ['France', 1, 1],
-  'AR': ['Argentina', 85, 89],
-  'BR': ['Brazil', 1697, 2301],
   'BE': ['Belgium', 2, 2],
-  'ZA': ['South Africa', 129, 165],
-  'PE': ['Peru', 14, 24],
-  'US': ['United States of America', 82, 106],
   'DE': ['Germany', 8, 8],
   'IT': ['Italy', 10, 11],
-  'CL': ['Chile', 26, 31]
+  'AR': ['Argentina', 85, 89],
+  'FR': ['France', 1, 1],
+  'PE': ['Peru', 14, 24],
+  'CL': ['Chile', 26, 31],
+  'ZA': ['South Africa', 129, 165],
+  'NL': ['Netherlands', 2, 2],
+  'US': ['United States of America', 82, 106],
+  'BR': ['Brazil', 1698, 2302]
 }
