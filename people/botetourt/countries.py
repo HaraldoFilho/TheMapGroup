691478675578d25d8a26539e9407ccec2af8d4e7
The Map Group <admin@the-map-group.pictures>
2022-06-05 02:20:19 -0300

    [auto] Updated map for member 'Brunswick Forge'

--- people/botetourt/countries.py
+++ people/botetourt/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'MX': ['Mexico', 8, 10],
-  'US': ['United States of America', 3033, 5465],
-  'GB': ['United Kingdom', 51, 94],
-  'NL': ['Netherlands', 1, 1],
-  'IT': ['Italy', 349, 476],
-  'TR': ['Turkey', 1, 1],
+  'TZ': ['Tanzania', 288, 848],
+  'CA': ['Canada', 10, 13],
   'VA': ['Vatican City', 16, 19],
-  'TZ': ['Tanzania', 288, 847],
-  'CA': ['Canada', 10, 13]
+  'GB': ['United Kingdom', 51, 94],
+  'IT': ['Italy', 349, 476],
+  'US': ['United States of America', 3033, 5465],
+  'NL': ['Netherlands', 1, 1],
+  'TR': ['Turkey', 1, 1],
+  'MX': ['Mexico', 8, 10]
 }
--- people/botetourt/user.py
+++ people/botetourt/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/82487858@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/botetourt/',
   'markers': 3757,
-  'photos': 6926
+  'photos': 6927
 }
