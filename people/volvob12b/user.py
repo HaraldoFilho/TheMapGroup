d136091f661157d132d2df432a726d5b049f3799
The Map Group <admin@the-map-group.pictures>
2022-06-09 02:08:45 -0300

    [auto] Updated map for member 'Bernard Spragg'

--- people/volvob12b/user.py
+++ people/volvob12b/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Bernard Spragg. NZ',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/88123769@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/volvob12b/',
-  'markers': 4170,
-  'photos': 5724
+  'markers': 4171,
+  'photos': 5725
 }
