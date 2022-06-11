0e460b9969673d74357c025044d4d0415e6040f9
The Map Group <admin@the-map-group.pictures>
2022-06-11 02:20:03 -0300

    [auto] Updated map for member 'Ignacio M. Jiménez'

--- people/ignaciomjimenez/user.py
+++ people/ignaciomjimenez/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Nacho',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/23499399@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/ignaciomjimenez/',
-  'markers': 1004,
-  'photos': 1328
+  'markers': 1005,
+  'photos': 1329
 }
