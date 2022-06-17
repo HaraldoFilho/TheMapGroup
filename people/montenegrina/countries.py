169b1180d7978746da9fc73b9227d87baf24e865
The Map Group <admin@the-map-group.pictures>
2022-06-17 02:24:31 -0300

    [auto] Updated map for member 'ForceMajeureMontenegro'

--- people/montenegrina/countries.py
+++ people/montenegrina/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'HR': ['Croatia', 51, 58],
-  'IT': ['Italy', 106, 122],
-  'SI': ['Slovenia', 5, 6],
   'VA': ['Vatican City', 5, 7],
-  'AL': ['Albania', 4, 4],
-  'CZ': ['Czechia', 40, 48],
-  'FR': ['France', 63, 75],
-  'ME': ['Montenegro', 523, 588],
-  'BA': ['Bosnia and Herzegovina', 17, 19],
   'GR': ['Greece', 72, 83],
+  'CZ': ['Czechia', 40, 48],
+  'ME': ['Montenegro', 525, 590],
+  'BA': ['Bosnia and Herzegovina', 17, 19],
   'PT': ['Portugal', 15, 23],
+  'IT': ['Italy', 106, 122],
+  'HR': ['Croatia', 51, 58],
   'ES': ['Spain', 104, 122],
+  'AL': ['Albania', 4, 4],
+  'SI': ['Slovenia', 5, 6],
+  'FR': ['France', 63, 75],
   'MA': ['Morocco', 14, 15]
 }
--- people/montenegrina/user.py
+++ people/montenegrina/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'ForceMajeureMontenegro',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/92496593@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/montenegrina/',
-  'markers': 1023,
-  'photos': 1174
+  'markers': 1025,
+  'photos': 1176
 }
