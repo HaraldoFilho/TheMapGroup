a2c96cb52e688bd15ac90c1285e9a352db1bfeed
The Map Group <admin@the-map-group.pictures>
2022-06-22 02:43:47 -0300

    [auto] Updated map for member 'Guildfordian'

--- people/guildfordian/user.py
+++ people/guildfordian/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Guildfordian',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/35523433@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/guildfordian/',
-  'markers': 918,
-  'photos': 1508
+  'markers': 921,
+  'photos': 1511
 }
