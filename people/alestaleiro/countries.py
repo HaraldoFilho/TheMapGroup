948bd62efe6149c9bf629996ab98d26fb365cd89
The Map Group <admin@the-map-group.pictures>
2022-06-22 02:49:50 -0300

    [auto] Updated map for member 'alestaleiro'

--- people/alestaleiro/countries.py
+++ people/alestaleiro/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'IT': ['Italy', 10, 11],
-  'FR': ['France', 1, 1],
-  'CL': ['Chile', 26, 31],
-  'BR': ['Brazil', 1711, 2320],
-  'AR': ['Argentina', 85, 89],
-  'US': ['United States of America', 82, 106],
-  'DE': ['Germany', 8, 8],
+  'BE': ['Belgium', 2, 2],
   'PE': ['Peru', 14, 24],
-  'NL': ['Netherlands', 2, 2],
+  'CL': ['Chile', 26, 31],
   'ZA': ['South Africa', 129, 165],
-  'BE': ['Belgium', 2, 2]
+  'AR': ['Argentina', 85, 89],
+  'DE': ['Germany', 8, 8],
+  'IT': ['Italy', 10, 11],
+  'US': ['United States of America', 82, 106],
+  'BR': ['Brazil', 1711, 2321],
+  'NL': ['Netherlands', 2, 2],
+  'FR': ['France', 1, 1]
 }
--- people/alestaleiro/user.py
+++ people/alestaleiro/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/99949037@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/alestaleiro/',
   'markers': 2070,
-  'photos': 2759
+  'photos': 2760
 }
