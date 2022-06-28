00f700337f304b8eb2bf59cfe966d4e43ed165bd
The Map Group <admin@the-map-group.pictures>
2022-06-28 02:25:13 -0300

    [auto] Updated map for member 'CrËOS Photographie'

--- people/scfoto62/user.py
+++ people/scfoto62/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/51101129@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/scfoto62/',
   'markers': 1488,
-  'photos': 2530
+  'photos': 2532
 }
