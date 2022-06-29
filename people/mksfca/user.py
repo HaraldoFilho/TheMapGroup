2e3ea10d63faf33c6c27cc16a7b19a59cc4f9dc9
The Map Group <admin@the-map-group.pictures>
2022-06-29 03:19:40 -0300

    [auto] Updated map for member 'mksfca'

---
+++ people/mksfca/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '43982795@N03',
+  'alias': 'mksfca',
+  'name': 'mksfca',
+  'avatar': '../../icons/photographer.svg',
+  'url': 'https://www.flickr.com/photos/mksfca/',
+  'markers': 6048,
+  'photos': 12795
+}
