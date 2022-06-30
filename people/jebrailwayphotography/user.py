e00ccc54c1295c8261df91f5fc5b0a6d34e60c50
The Map Group <admin@the-map-group.pictures>
2022-06-30 02:55:25 -0300

    Removed member 'jebrailwayphotography'

--- people/jebrailwayphotography/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '187366688@N05',
-  'alias': 'jebrailwayphotography',
-  'name': 'Jonathan Brooks',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/187366688@N05_r.jpg',
-  'url': 'https://www.flickr.com/photos/jebrailwayphotography/',
-  'markers': 120,
-  'photos': 234
-}
