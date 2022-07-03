5de9e7961fc789b8af6f766cbd06d61cf16f2430
The Map Group <admin@the-map-group.pictures>
2022-07-03 02:39:36 -0300

    [auto] Updated map for member 'Explore 0ur World'

---
+++ people/195967005@N07/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '195967005@N07',
+  'alias': '195967005@N07',
+  'name': 'La'Nise',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/195967005@N07_r.jpg',
+  'url': 'https://www.flickr.com/photos/195967005@N07/',
+  'markers': 0,
+  'photos': 0
+}
