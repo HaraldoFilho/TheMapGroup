e4f6c76a319e8a0763095dd9a174fca93250822c
The Map Group <admin@the-map-group.pictures>
2022-07-09 02:22:29 -0300

    [auto] Updated map for member 'benbobjr'

--- people/benbobjr/countries.py
+++ people/benbobjr/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'GB': ['United Kingdom', 34515, 42163],
-  'CN': ['South China Sea Islands (disputed)', 8, 69],
-  'US': ['United States of America', 69, 152],
-  'DK': ['Denmark', 29, 64],
-  'NL': ['The Netherlands', 29, 33],
-  'SE': ['Sweden', 23, 97],
   'AT': ['Austria', 21, 75],
+  'DK': ['Denmark', 29, 64],
+  'SE': ['Sweden', 23, 97],
+  'BE': ['Belgium', 82, 174],
   'IT': ['Italy', 232, 340],
-  'AU': ['Australia', 401, 542],
+  'AU': ['Australia', 401, 543],
   'AE': ['United Arab Emirates', 167, 175],
-  'BE': ['Belgium', 82, 174]
+  'CN': ['South China Sea Islands (disputed)', 8, 69],
+  'NL': ['The Netherlands', 29, 33],
+  'GB': ['United Kingdom', 34544, 42233],
+  'US': ['United States of America', 70, 156]
 }
--- people/benbobjr/user.py
+++ people/benbobjr/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Ben Abel',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/52559257@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/benbobjr/',
-  'markers': 35576,
-  'photos': 43884
+  'markers': 35606,
+  'photos': 43959
 }
