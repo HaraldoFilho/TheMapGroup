232fe0fa4a44efbffc35e25e853807b187082c1e
The Map Group <admin@the-map-group.pictures>
2022-07-12 02:50:03 -0300

    [auto] Updated map for member 'strategicus'

---
+++ people/max7777/countries.py
@@ -0,0 +1,13 @@
+countries_dict = {
+  'AF': ['Afghanistan', 20, 31],
+  'TJ': ['Tajikistan', 1, 1],
+  'IQ': ['Iraq', 1, 1],
+  'CN': ['China', 1, 5],
+  'JO': ['Jordan', 1, 1],
+  'GB': ['United Kingdom', 1, 1],
+  'US': ['United States of America', 3, 4],
+  'PK': ['Pakistan', 31, 42],
+  'IR': ['Iran', 4, 4],
+  'IN': ['India', 3, 3],
+  'EG': ['Egypt', 3, 4]
+}
---
+++ people/max7777/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '30866147@N05',
+  'alias': 'max7777',
+  'name': 'strategicus',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/30866147@N05_r.jpg',
+  'url': 'https://www.flickr.com/photos/max7777/',
+  'markers': 71,
+  'photos': 99
+}
