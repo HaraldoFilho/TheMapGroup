31be922acae1d940783893d247de415fd1686337
The Map Group <admin@the-map-group.pictures>
2022-07-14 02:23:21 -0300

    [auto] Updated map for member 'Domikawa4'

--- people/153072396@N07/countries.py
+++ people/153072396@N07/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'MT': ['Malta', 22, 44],
-  'CZ': ['Czechia', 3, 3],
   'DE': ['Germany', 7, 8],
-  'IE': ['Ireland', 19, 25],
+  'IE': ['Ireland', 20, 26],
+  'FR': ['France', 81, 310],
+  'AT': ['Austria', 9, 12],
+  'MT': ['Malta', 22, 44],
+  'SK': ['Slovakia', 2, 2],
+  'CZ': ['Czechia', 3, 3],
   'DK': ['Denmark', 59, 94],
   'BE': ['Belgium', 1, 1],
-  'GB': ['United Kingdom', 54, 90],
-  'AT': ['Austria', 9, 12],
-  'FR': ['France', 81, 310],
-  'SK': ['Slovakia', 2, 2],
-  'NL': ['Netherlands', 15, 27]
+  'NL': ['Netherlands', 15, 27],
+  'GB': ['United Kingdom', 54, 90]
 }
--- people/153072396@N07/user.py
+++ people/153072396@N07/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'dominique FILLION',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/153072396@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/153072396@N07/',
-  'markers': 272,
-  'photos': 616
+  'markers': 273,
+  'photos': 617
 }
