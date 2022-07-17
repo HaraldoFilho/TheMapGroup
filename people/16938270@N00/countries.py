e327c1f02064a717e4e9a483e99026ea86e90f6a
The Map Group <admin@the-map-group.pictures>
2022-07-17 02:38:55 -0300

    [auto] Updated map for member 'Ian & Marg'

---
+++ people/16938270@N00/countries.py
@@ -0,0 +1,17 @@
+countries_dict = {
+  'NL': ['Netherlands', 4, 4],
+  'FI': ['Finland', 7, 7],
+  'DE': ['Germany', 41, 45],
+  'CN': ['South China Sea Islands (disputed)', 1, 1],
+  'SB': ['Solomon Islands', 12, 12],
+  'PL': ['Poland', 17, 19],
+  'GB': ['United Kingdom', 17, 17],
+  'NZ': ['New Zealand', 5, 5],
+  'US': ['United States of America', 1, 1],
+  'ID': ['Indonesia', 6, 6],
+  'SG': ['Singapore', 2, 2],
+  'IE': ['Ireland', 6, 6],
+  'TR': ['Türkiye', 38, 45],
+  'BE': ['Belgium', 16, 16],
+  'AU': ['Australia', 141, 152]
+}
