918d3946a23fd1354ea285e976a131bb2cc7dd95
The Map Group <admin@the-map-group.pictures>
2022-07-21 02:12:51 -0300

    [auto] Updated map for member 'S LYNN LEE'

--- people/slynnlee/countries.py
+++ people/slynnlee/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
-  'RU': ['Russia', 1, 1],
-  'IE': ['Ireland', 184, 858],
-  'CA': ['Canada', 1, 39],
   'US': ['United States', 212, 1524],
-  'GB': ['United Kingdom', 259, 1642],
-  'AU': ['Australia', 6, 6],
-  'DE': ['Germany', 35, 158],
-  'VA': ['Vatican City', 1, 1],
   'NL': ['Netherlands', 1, 1],
-  'IT': ['Italy', 2, 2]
+  'CA': ['Canada', 1, 39],
+  'DE': ['Germany', 35, 158],
+  'IE': ['Ireland', 184, 858],
+  'GB': ['United Kingdom', 260, 1643],
+  'VA': ['Vatican City', 1, 1],
+  'RU': ['Russia', 1, 1],
+  'IT': ['Italy', 2, 2],
+  'AU': ['Australia', 6, 6]
 }
--- people/slynnlee/user.py
+++ people/slynnlee/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'S Lynn Lee',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/144075308@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/slynnlee/',
-  'markers': 702,
-  'photos': 4232
+  'markers': 703,
+  'photos': 4233
 }
