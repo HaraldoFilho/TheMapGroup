07538695cc031472546c639c117ea0325f1f87f8
The Map Group <admin@the-map-group.pictures>
2022-07-21 02:05:12 -0300

    [auto] Updated map for member '5.5 mil views - Thank you all.'

--- people/staneastwood/countries.py
+++ people/staneastwood/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'PT': ['Portugal', 43, 358],
-  'CA': ['Canada', 357, 1134],
-  'DK': ['Denmark', 1, 141],
-  'IM': ['Isle of Man', 994, 1808],
-  'GB': ['United Kingdom', 3829, 25304],
-  'GI': ['Gibraltar', 1, 94],
-  'IS': ['Iceland', 45, 295],
-  'ES': ['Spain', 67, 665],
-  'US': ['United States', 169, 580],
   'SE': ['Sweden', 2, 118],
   'IE': ['Ireland', 20, 187],
   'NO': ['Norway', 42, 553],
-  'GL': ['Greenland', 8, 335],
+  'IS': ['Iceland', 45, 295],
+  'ES': ['Spain', 67, 665],
+  'CA': ['Canada', 357, 1134],
   'FR': ['France', 9, 295],
-  'DE': ['Germany', 5, 34]
+  'GI': ['Gibraltar', 1, 94],
+  'GL': ['Greenland', 8, 335],
+  'IM': ['Isle of Man', 994, 1808],
+  'PT': ['Portugal', 43, 358],
+  'US': ['United States', 169, 580],
+  'DE': ['Germany', 5, 34],
+  'GB': ['United Kingdom', 3850, 25412],
+  'DK': ['Denmark', 1, 141]
 }
