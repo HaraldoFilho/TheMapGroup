e3090e7627364ce9c7a585659f0a26a91325967b
The Map Group <admin@the-map-group.pictures>
2022-08-06 02:19:48 -0300

    [auto] Updated map for member '_Veit_'

--- people/veitirtenkauf/countries.py
+++ people/veitirtenkauf/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'US': ['United States of America', 294, 1505],
-  'DE': ['Germany', 19, 19],
   'CZ': ['Czechia', 2, 2],
+  'DE': ['Germany', 19, 19],
   'AU': ['Australia', 1, 1],
+  'US': ['United States of America', 294, 1507],
   'NZ': ['New Zealand', 3, 3]
 }
--- people/veitirtenkauf/user.py
+++ people/veitirtenkauf/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/27539916@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/veitirtenkauf/',
   'markers': 319,
-  'photos': 1530
+  'photos': 1532
 }
