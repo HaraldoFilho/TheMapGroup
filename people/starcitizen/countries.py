143e364fdd13a80d7c07ffe7497aff9d8457f611
The Map Group <admin@the-map-group.pictures>
2022-08-07 02:40:54 -0300

    [auto] Updated map for member 'Star Citizen (mountain photography)'

--- people/starcitizen/countries.py
+++ people/starcitizen/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'AR': ['Argentina', 180, 181],
-  'AD': ['Andorra', 565, 573],
-  'NO': ['Norway', 650, 676],
-  'IT': ['Italy', 68, 68],
-  'FR': ['France', 172, 173],
   'NZ': ['New Zealand', 194, 208],
-  'CH': ['Switzerland', 6, 6],
   'ES': ['Spain', 1114, 1140],
-  'CL': ['Chile', 69, 73]
+  'CL': ['Chile', 69, 73],
+  'CH': ['Switzerland', 6, 6],
+  'FR': ['France', 172, 173],
+  'IT': ['Italy', 68, 68],
+  'NO': ['Norway', 651, 677],
+  'AR': ['Argentina', 180, 181],
+  'AD': ['Andorra', 565, 573]
 }
