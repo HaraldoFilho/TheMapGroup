d4a4d043622b2a951adb5a41876119cc376a54df
The Map Group <admin@the-map-group.pictures>
2022-08-09 02:27:56 -0300

    [auto] Updated map for member 'Bernard Spragg'

--- people/volvob12b/countries.py
+++ people/volvob12b/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'TH': ['Thailand', 28, 30],
-  'MY': ['Malaysia', 39, 58],
-  'CN': ['South China Sea Islands (disputed)', 179, 236],
-  'NZ': ['New Zealand', 2919, 3962],
-  'AU': ['Australia', 271, 389],
-  'US': ['United States', 372, 543],
-  'JP': ['Japan', 17, 32],
   'SG': ['Singapore', 25, 29],
-  'CA': ['Canada', 257, 344],
   'PH': ['Philippines', 45, 94],
-  'BN': ['Brunei', 49, 62]
+  'US': ['United States', 371, 542],
+  'BN': ['Brunei', 48, 61],
+  'AU': ['Australia', 271, 389],
+  'NZ': ['New Zealand', 2919, 3962],
+  'CA': ['Canada', 257, 344],
+  'TH': ['Thailand', 28, 30],
+  'JP': ['Japan', 17, 32],
+  'CN': ['South China Sea Islands (disputed)', 179, 236],
+  'MY': ['Malaysia', 39, 58]
 }
--- people/volvob12b/user.py
+++ people/volvob12b/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Bernard Spragg. NZ',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/88123769@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/volvob12b/',
-  'markers': 4201,
-  'photos': 5779
+  'markers': 4199,
+  'photos': 5777
 }
