37fdbbd3e668ffcd8bc1a4b374e564b66d775f52
The Map Group <admin@the-map-group.pictures>
2022-08-09 02:50:21 -0300

    [auto] Updated map for member 'Ian & Marg'

--- people/16938270@N00/user.py
+++ people/16938270@N00/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Ian & Marg',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/16938270@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/16938270@N00/',
-  'markers': 505,
-  'photos': 550
+  'markers': 507,
+  'photos': 553
 }
