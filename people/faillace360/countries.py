8c87d294d01fd53047a9137bee28568ed4f80559
The Map Group <admin@the-map-group.pictures>
2022-08-16 02:28:27 -0300

    [auto] Updated map for member 'Faillace360'

--- people/faillace360/countries.py
+++ people/faillace360/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'AT': ['Austria', 3, 3],
-  'AR': ['Argentina', 7, 7],
-  'BR': ['Brazil', 90, 94],
-  'IT': ['Italy', 3, 3],
-  'DE': ['Germany', 1, 1],
   'CH': ['Switzerland', 2, 2],
-  'CZ': ['Czechia', 2, 2]
+  'AR': ['Argentina', 7, 7],
+  'CZ': ['Czechia', 2, 2],
+  'IT': ['Italy', 3, 3],
+  'AT': ['Austria', 3, 3],
+  'DE': ['Germany', 1, 1],
+  'BR': ['Brazil', 93, 97]
 }
--- people/faillace360/user.py
+++ people/faillace360/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Faillace',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/194868714@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/faillace360/',
-  'markers': 108,
-  'photos': 112
+  'markers': 111,
+  'photos': 115
 }
