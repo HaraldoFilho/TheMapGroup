d71c797bf97384b8beef10aba142b5f23a991507
The Map Group <admin@the-map-group.pictures>
2022-08-16 02:18:05 -0300

    [auto] Updated map for member '_Veit_'

--- people/veitirtenkauf/user.py
+++ people/veitirtenkauf/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/27539916@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/veitirtenkauf/',
   'markers': 185,
-  'photos': 1133
+  'photos': 1134
 }
