ef8362609b1a325a266513b672aa29c05d875a3a
The Map Group <admin@the-map-group.pictures>
2022-08-21 02:25:18 -0300

    [auto] Updated map for member 'benbobjr'

--- people/benbobjr/countries.py
+++ people/benbobjr/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'DK': ['Denmark', 29, 65],
-  'IT': ['Italy', 243, 352],
   'SE': ['Sweden', 23, 97],
   'CN': ['South China Sea Islands (disputed)', 8, 72],
+  'AU': ['Australia', 402, 567],
+  'AT': ['Austria', 21, 79],
+  'DK': ['Denmark', 30, 67],
   'BE': ['Belgium', 82, 174],
   'AE': ['United Arab Emirates', 167, 175],
-  'GB': ['United Kingdom', 34678, 42482],
-  'AU': ['Australia', 402, 561],
-  'AT': ['Austria', 21, 76],
+  'IT': ['Italy', 246, 357],
   'NL': ['The Netherlands', 29, 33],
+  'GB': ['United Kingdom', 34706, 42547],
   'US': ['United States of America', 72, 165]
 }
