77d023c99676a426bfda4d2f57e753b5cb4f6bf8
The Map Group <admin@the-map-group.pictures>
2022-08-24 02:25:25 -0300

    [auto] Updated map for member 'giosuèbolis'

--- people/146445851@N04/countries.py
+++ people/146445851@N04/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'DZ': ['Algeria', 2, 2],
-  'ML': ['Mali', 5, 6],
-  'IT': ['Italy', 145, 186],
+  'IT': ['Italy', 146, 188],
   'FR': ['France', 8, 9],
-  'HU': ['Hungary', 2, 2],
-  'NE': ['Niger', 2, 3],
+  'ML': ['Mali', 5, 6],
   'CH': ['Switzerland', 12, 37],
+  'NE': ['Niger', 2, 3],
+  'DZ': ['Algeria', 2, 2],
+  'HU': ['Hungary', 2, 2],
   'UA': ['Ukraine', 1, 1]
 }
