3b17a9c750fec1185a205a25f0448bdc9867953c
The Map Group <admin@the-map-group.pictures>
2022-08-24 02:13:26 -0300

    [auto] Updated map for member 'Cranky Oldman'

--- people/39442644@N03/user.py
+++ people/39442644@N03/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Cranky Oldman',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/39442644@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/39442644@N03/',
-  'markers': 1037,
-  'photos': 1070
+  'markers': 1039,
+  'photos': 1072
 }
