98df40e16dedeb7602f125c9488d60d5478d54cb
The Map Group <admin@the-map-group.pictures>
2022-08-28 02:10:38 -0300

    [auto] Updated map for member 'HereIsTom'

--- people/hereistom/countries.py
+++ people/hereistom/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'NL': ['The Netherlands', 1209, 1309],
-  'HR': ['Croatia', 1, 1],
-  'TH': ['Thailand', 48, 56],
-  'SI': ['Slovenia', 1, 1],
-  'BE': ['Belgium', 3, 3],
-  'ES': ['Spain', 49, 49],
-  'IT': ['Italy', 1, 1],
-  'PT': ['Portugal', 95, 96],
   'GR': ['Greece', 1, 1],
-  'DE': ['Germany', 2, 2],
   'BR': ['Brazil', 2, 2],
-  'TR': ['Turkey', 63, 63]
+  'IT': ['Italy', 1, 1],
+  'DE': ['Germany', 2, 2],
+  'TH': ['Thailand', 48, 56],
+  'NL': ['The Netherlands', 1210, 1310],
+  'BE': ['Belgium', 3, 3],
+  'HR': ['Croatia', 1, 1],
+  'TR': ['Turkey', 63, 63],
+  'SI': ['Slovenia', 1, 1],
+  'PT': ['Portugal', 95, 96],
+  'ES': ['Spain', 49, 49]
 }
--- people/hereistom/user.py
+++ people/hereistom/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Tom Jutte',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/47035764@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/hereistom/',
-  'markers': 1475,
-  'photos': 1584
+  'markers': 1476,
+  'photos': 1585
 }
