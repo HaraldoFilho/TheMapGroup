ec2eebb9310b674860178211ce3ac17e7cac3ba0
The Map Group <admin@the-map-group.pictures>
2022-09-05 02:15:36 -0300

    [auto] Updated map for member 'MiroFedurco'

--- people/134373885@N03/countries.py
+++ people/134373885@N03/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'DE': ['Germany', 23, 24],
-  'SK': ['Slovakia', 114, 158],
+  'RU': ['Russia', 51, 55],
   'CZ': ['Czechia', 23, 25],
-  'FR': ['France', 12, 15],
-  'UA': ['Ukraine', 5, 6],
   'PL': ['Poland', 1, 1],
-  'RU': ['Russia', 51, 55]
+  'FR': ['France', 12, 15],
+  'DE': ['Germany', 23, 24],
+  'UA': ['Ukraine', 5, 6],
+  'SK': ['Slovakia', 115, 159]
 }
