19a8f6911bd7881d2013ad32d5bca04e75ddb6e0
The Map Group <admin@the-map-group.pictures>
2022-09-11 02:00:54 -0300

    [auto] Updated map for member 'Jorge Franganillo'

--- people/franganillo/user.py
+++ people/franganillo/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Jorge Franganillo',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/46191841@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/franganillo/',
-  'markers': 3181,
-  'photos': 5757
+  'markers': 3190,
+  'photos': 5766
 }
