ea6e15cf8e13a07a8fa6d3edb5863a02bfb6cfc6
The Map Group <admin@the-map-group.pictures>
2022-09-16 02:19:39 -0300

    [auto] Updated map for member 'hó'

--- people/iceland-ho/countries.py
+++ people/iceland-ho/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'MD': ['Moldova', 2, 2],
-  'GB': ['United Kingdom', 7, 7],
   'US': ['United States of America', 6, 6],
-  'GH': ['Ghana', 7, 7],
   'CN': ['South China Sea Islands (disputed)', 16, 16],
-  'IS': ['Iceland', 2498, 2500],
+  'FO': ['Faroe Islands', 9, 9],
+  'GB': ['United Kingdom', 7, 7],
+  'GH': ['Ghana', 7, 7],
   'ES': ['Spain', 5, 5],
-  'FO': ['Faroe Islands', 9, 9]
+  'IS': ['Iceland', 2499, 2501],
+  'MD': ['Moldova', 2, 2]
 }
--- people/iceland-ho/user.py
+++ people/iceland-ho/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Hugi Ólafsson',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/59509464@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/iceland-ho/',
-  'markers': 2550,
-  'photos': 2552
+  'markers': 2551,
+  'photos': 2553
 }
