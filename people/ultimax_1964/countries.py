fe4d51d67a1390fe4fb4bde989da8ca98d720fd2
The Map Group <admin@the-map-group.pictures>
2022-09-20 02:34:59 -0300

    [auto] Updated map for member 'Steve W3'

--- people/ultimax_1964/countries.py
+++ people/ultimax_1964/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
   'IS': ['Iceland', 12, 46],
-  'NZ': ['New Zealand', 8, 23],
-  'CA': ['Canada', 4, 13],
-  'US': ['United States of America', 11, 29],
-  'PT': ['Portugal', 4, 6],
-  'ES': ['Spain', 3, 3],
   'JP': ['Japan', 6, 20],
-  'GL': ['Greenland', 10, 23],
-  'AU': ['Australia', 130, 382],
-  'GB': ['United Kingdom', 30, 35],
-  'NO': ['Norway', 6, 13],
+  'US': ['United States of America', 11, 29],
   'FR': ['France', 12, 17],
-  'SG': ['Singapore', 2, 3]
+  'NZ': ['New Zealand', 8, 23],
+  'AU': ['Australia', 130, 384],
+  'SG': ['Singapore', 2, 3],
+  'GL': ['Greenland', 10, 23],
+  'CA': ['Canada', 4, 13],
+  'GB': ['United Kingdom', 30, 35],
+  'ES': ['Spain', 3, 3],
+  'PT': ['Portugal', 4, 6],
+  'NO': ['Norway', 6, 13]
 }
--- people/ultimax_1964/user.py
+++ people/ultimax_1964/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/58435673@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/ultimax_1964/',
   'markers': 238,
-  'photos': 613
+  'photos': 615
 }
