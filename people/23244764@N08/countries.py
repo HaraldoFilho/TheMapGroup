de5df6125bd6109b7ba2e4c82d64d3323276f218
The Map Group <admin@the-map-group.pictures>
2022-09-26 02:28:06 -0300

    [auto] Updated map for member 'suntrana3'

--- people/23244764@N08/countries.py
+++ people/23244764@N08/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
-  'AQ': ['Antarctica', 6, 6],
-  'ES': ['Spain', 38, 56],
-  'GI': ['Gibraltar', 14, 14],
-  'CA': ['Canada', 1, 1],
-  'BZ': ['Belize', 4, 7],
-  'BS': ['The Bahamas', 2, 2],
-  'NL': ['Netherlands', 9, 9],
   'MA': ['Morocco', 13, 13],
+  'BS': ['The Bahamas', 2, 2],
+  'CA': ['Canada', 1, 1],
   'HT': ['Haiti', 1, 1],
-  'US': ['United States of America', 101, 199]
+  'ES': ['Spain', 38, 56],
+  'NL': ['Netherlands', 9, 9],
+  'US': ['United States of America', 101, 200],
+  'AQ': ['Antarctica', 6, 6],
+  'GI': ['Gibraltar', 14, 14],
+  'BZ': ['Belize', 4, 7]
 }
--- people/23244764@N08/user.py
+++ people/23244764@N08/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/23244764@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/23244764@N08/',
   'markers': 190,
-  'photos': 309
+  'photos': 310
 }
