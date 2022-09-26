a9208dcd80054d1e38a5f4abeb8cb3577f95af6a
The Map Group <admin@the-map-group.pictures>
2022-09-26 02:23:55 -0300

    [auto] Updated map for member 'Bilderschreiber'

--- people/39904644@N05/user.py
+++ people/39904644@N05/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Martin Thum',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/39904644@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/39904644@N05/',
-  'markers': 1491,
-  'photos': 1752
+  'markers': 1492,
+  'photos': 1753
 }
