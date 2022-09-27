027660b287f4412323e4ef7af27863000a438e84
The Map Group <admin@the-map-group.pictures>
2022-09-27 02:20:19 -0300

    [auto] Updated map for member 'Jason Frels'

--- people/jasonfrels/countries.py
+++ people/jasonfrels/countries.py
@@ -1,4 +1,4 @@
 countries_dict = {
-  'US': ['United States', 85, 92],
+  'US': ['United States', 86, 93],
   'SG': ['Singapore', 1, 2]
 }
--- people/jasonfrels/user.py
+++ people/jasonfrels/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Jason',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/76199006@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/jasonfrels/',
-  'markers': 86,
-  'photos': 94
+  'markers': 87,
+  'photos': 95
 }
