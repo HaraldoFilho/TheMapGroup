24321f7e4ce2ddb3c4d2d6b41ad520dde0ec277d
The Map Group <admin@the-map-group.pictures>
2022-09-30 02:50:45 -0300

    [auto] Updated map for member 'grand Yann'

---
+++ people/grandyann/countries.py
@@ -0,0 +1,18 @@
+countries_dict = {
+  'FR': ['France', 350, 482],
+  'ID': ['Indonesia', 28, 30],
+  'HR': ['Croatia', 13, 18],
+  'PT': ['Portugal', 63, 72],
+  'RO': ['Romania', 1, 1],
+  'TH': ['Thailand', 61, 94],
+  'BE': ['Belgium', 2, 2],
+  'SG': ['Singapore', 8, 16],
+  'IN': ['India', 69, 70],
+  'MY': ['Malaysia', 23, 30],
+  'AT': ['Austria', 13, 22],
+  'US': ['United States of America', 145, 204],
+  'CV': ['Cape Verde', 2, 2],
+  'GR': ['Greece', 75, 82],
+  'CA': ['Canada', 9, 19],
+  'EG': ['Egypt', 5, 5]
+}
