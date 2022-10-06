444e6fcb524619bde041fb5066c9975064e4e570
The Map Group <admin@the-map-group.pictures>
2022-10-06 02:04:56 -0300

    [auto] Updated map for member 'pascalvu1'

--- people/quanito/user.py
+++ people/quanito/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Pascal VU',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/135298455@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/quanito/',
-  'markers': 3173,
-  'photos': 22451
+  'markers': 3183,
+  'photos': 22469
 }
