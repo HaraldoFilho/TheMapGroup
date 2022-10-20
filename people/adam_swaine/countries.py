61e2cd04876a6d0c1f422d964081657099478948
The Map Group <admin@the-map-group.pictures>
2022-10-20 02:49:43 -0300

    [auto] Updated map for member 'Adam Swaine'

--- people/adam_swaine/countries.py
+++ people/adam_swaine/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'GB': ['United Kingdom', 13920, 21229],
+  'FR': ['France', 154, 370],
+  'GB': ['United Kingdom', 13923, 21232],
   'MC': ['Monaco', 7, 16],
   'ES': ['Spain', 73, 104],
-  'NO': ['Norway', 63, 69],
-  'FR': ['France', 154, 370]
+  'NO': ['Norway', 63, 69]
 }
--- people/adam_swaine/user.py
+++ people/adam_swaine/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Adam Swaine',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/43499552@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/adam_swaine/',
-  'markers': 14217,
-  'photos': 21788
+  'markers': 14220,
+  'photos': 21791
 }
