cb7634eeef372aa477e37b653ea042a40d8c32b4
The Map Group <admin@the-map-group.pictures>
2022-10-21 02:40:22 -0300

    [auto] Updated map for member 'CoronaViking (mountain photography)'

--- people/starcitizen/user.py
+++ people/starcitizen/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'CoronaViking (mountain photogr',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/169246257@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/starcitizen/',
-  'markers': 3068,
-  'photos': 3151
+  'markers': 3069,
+  'photos': 3152
 }
