da7a5abaa47439586d24c7340549c495fd2cd24c
The Map Group <admin@the-map-group.pictures>
2022-10-23 02:27:25 -0300

    [auto] Updated map for member 'jonnyboi800'

---
+++ people/jonnyboi800/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '195937996@N05',
+  'alias': 'jonnyboi800',
+  'name': 'Jonathan Brooks',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/195937996@N05_r.jpg',
+  'url': 'https://www.flickr.com/photos/jonnyboi800/',
+  'markers': 0,
+  'photos': 0
+}
