bcc6cedb8ae2f3d37d46d3c0247410424a014514
The Map Group <admin@the-map-group.pictures>
2022-10-24 02:28:24 -0300

    [auto] Updated map for member 'zimwizdotcom'

--- people/zimwiz/countries.py
+++ people/zimwiz/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'CA': ['Canada', 1, 1],
   'BM': ['Bermuda', 3, 3],
   'AQ': ['Antarctica', 1, 1],
-  'US': ['United States of America', 431, 531]
+  'CA': ['Canada', 1, 1],
+  'US': ['United States of America', 431, 532]
 }
