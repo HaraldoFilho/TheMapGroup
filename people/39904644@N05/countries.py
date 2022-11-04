b70812a761ff98fc7e86f2cdb9d100413aacacff
The Map Group <admin@the-map-group.pictures>
2022-11-04 03:04:59 -0300

    [auto] Updated map for member 'Bilderschreiber'

--- people/39904644@N05/countries.py
+++ people/39904644@N05/countries.py
@@ -1,19 +1,19 @@
 countries_dict = {
+  'DK': ['Denmark', 5, 6],
   'CZ': ['Czechia', 5, 5],
   'AT': ['Austria', 27, 27],
-  'DK': ['Denmark', 5, 6],
-  'PT': ['Portugal', 48, 62],
   'DE': ['Germany', 265, 296],
   'IT': ['Italy', 76, 95],
   'HR': ['Croatia', 1, 1],
-  'SE': ['Sweden', 9, 9],
-  'GB': ['United Kingdom', 113, 152],
-  'NZ': ['New Zealand', 283, 371],
   'TH': ['Thailand', 2, 2],
+  'NZ': ['New Zealand', 283, 371],
+  'NO': ['Norway', 168, 183],
+  'GR': ['Greece', 47, 51],
+  'ES': ['Spain', 52, 53],
+  'GB': ['United Kingdom', 113, 152],
   'GI': ['Gibraltar', 12, 12],
   'FR': ['France', 2, 2],
+  'SE': ['Sweden', 9, 9],
   'IS': ['Iceland', 399, 448],
-  'ES': ['Spain', 52, 53],
-  'NO': ['Norway', 167, 182],
-  'GR': ['Greece', 47, 51]
+  'PT': ['Portugal', 48, 62]
 }
