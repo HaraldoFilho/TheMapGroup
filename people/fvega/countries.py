0f6f3d5fae84b8e535461b09d04f51a5be822a8f
The Map Group <admin@the-map-group.pictures>
2022-11-06 02:11:36 -0300

    [auto] Updated map for member 'Fernando Sa Rapita ( on holiday )'

--- people/fvega/countries.py
+++ people/fvega/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'DE': ['Germany', 25, 26],
-  'ES': ['Spain', 1444, 2462],
-  'NL': ['Netherlands', 20, 21],
-  'GB': ['United Kingdom', 697, 825],
-  'IS': ['Iceland', 637, 858],
   'CH': ['Switzerland', 7, 7],
   'IE': ['Ireland', 10, 11],
-  'FI': ['Finland', 25, 25]
+  'FI': ['Finland', 25, 25],
+  'ES': ['Spain', 1444, 2462],
+  'NL': ['Netherlands', 20, 21],
+  'DE': ['Germany', 25, 26],
+  'IS': ['Iceland', 637, 858],
+  'GB': ['United Kingdom', 700, 829]
 }
--- people/fvega/user.py
+++ people/fvega/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Fernando Vega',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/158292715@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/fvega/',
-  'markers': 2865,
-  'photos': 4235
+  'markers': 2868,
+  'photos': 4239
 }
