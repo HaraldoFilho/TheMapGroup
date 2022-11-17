faa71a47f993a352d626d749f9a6967b9a219e45
The Map Group <admin@the-map-group.pictures>
2022-11-17 03:36:32 -0300

    Removed member 'abdullah_al-amoudi'

--- people/abdullah_al-amoudi/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '188561327@N07',
-  'alias': 'abdullah_al-amoudi',
-  'name': 'Abdullah Al-Amoudi',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/188561327@N07_r.jpg',
-  'url': 'https://www.flickr.com/photos/abdullah_al-amoudi/',
-  'markers': 0,
-  'photos': 0
-}
