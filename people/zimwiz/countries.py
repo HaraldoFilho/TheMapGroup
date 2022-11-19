74735a918d5d7869d96a4c2e23474969bb3f86ec
The Map Group <admin@the-map-group.pictures>
2022-11-19 03:27:10 -0300

    [auto] Updated map for member 'zimwizdotcom'

--- people/zimwiz/countries.py
+++ people/zimwiz/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'US': ['United States of America', 431, 533],
-  'CA': ['Canada', 1, 1],
+  'US': ['United States of America', 432, 535],
   'AQ': ['Antarctica', 1, 1],
-  'BM': ['Bermuda', 3, 3]
+  'BM': ['Bermuda', 3, 3],
+  'CA': ['Canada', 1, 1]
 }
--- people/zimwiz/user.py
+++ people/zimwiz/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Charlie Zimmerman',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/14461370@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/zimwiz/',
-  'markers': 436,
-  'photos': 538
+  'markers': 437,
+  'photos': 540
 }
