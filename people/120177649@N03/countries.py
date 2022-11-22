363e2f7d137610a760fd60905600659fad38451a
The Map Group <admin@the-map-group.pictures>
2022-11-22 02:53:49 -0300

    [auto] Updated map for member 'mpfau64'

--- people/120177649@N03/countries.py
+++ people/120177649@N03/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'FR': ['France', 123, 797],
-  'AT': ['Austria', 4, 16],
-  'DE': ['Germany', 621, 2974],
   'BE': ['Belgium', 3, 29],
-  'IT': ['Italy', 18, 71],
   'CH': ['Switzerland', 35, 131],
+  'FR': ['France', 123, 797],
   'JE': ['Jersey', 14, 81],
-  'GB': ['United Kingdom', 341, 2207]
+  'AT': ['Austria', 4, 16],
+  'IT': ['Italy', 18, 71],
+  'GB': ['United Kingdom', 341, 2207],
+  'DE': ['Germany', 622, 2976]
 }
--- people/120177649@N03/user.py
+++ people/120177649@N03/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Michael Pfau',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/120177649@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/120177649@N03/',
-  'markers': 1159,
-  'photos': 6306
+  'markers': 1160,
+  'photos': 6308
 }
