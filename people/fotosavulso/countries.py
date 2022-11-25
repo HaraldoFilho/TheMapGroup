49bca2d41b7865e27355db4f6bbd7cf12f2d7610
The Map Group <admin@the-map-group.pictures>
2022-11-25 02:24:08 -0300

    [auto] Updated map for member 'Fotos Avulso'

--- people/fotosavulso/countries.py
+++ people/fotosavulso/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'FR': ['France', 61, 63],
-  'US': ['United States of America', 3, 3],
+  'PT': ['Portugal', 335, 363],
+  'IT': ['Italy', 1, 1],
+  'TL': ['East Timor', 1, 1],
   'ES': ['Spain', 20, 22],
   'GW': ['Guinea-Bissau', 1, 1],
-  'PT': ['Portugal', 335, 363],
   'AT': ['Austria', 1, 1],
-  'AD': ['Andorra', 2, 2],
-  'ST': ['São Tomé and Príncipe', 101, 109],
+  'ST': ['São Tomé and Príncipe', 103, 111],
+  'FR': ['France', 61, 63],
   'CV': ['Cape Verde', 1, 1],
   'AO': ['Angola', 1, 1],
   'CN': ['South China Sea Islands (disputed)', 1, 2],
-  'IT': ['Italy', 1, 1],
-  'TL': ['East Timor', 1, 1]
+  'AD': ['Andorra', 2, 2],
+  'US': ['United States of America', 3, 3]
 }
