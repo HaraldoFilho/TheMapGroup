ec0d8a212d2fef2f9b873e87ca5035a1eae5608b
The Map Group <admin@the-map-group.pictures>
2022-11-26 02:28:05 -0300

    [auto] Updated map for member 'khrawlings'

--- people/khrawlings/user.py
+++ people/khrawlings/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'khrawlings',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/23808252@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/khrawlings/',
-  'markers': 2355,
-  'photos': 3080
+  'markers': 2356,
+  'photos': 3081
 }
