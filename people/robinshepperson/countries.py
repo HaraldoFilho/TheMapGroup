241e0107faf9858f9ecedba4572026b6a241cfbb
The Map Group <admin@the-map-group.pictures>
2022-12-07 02:29:50 -0300

    [auto] Updated map for member 'Robin Shepperson Photography'

--- people/robinshepperson/countries.py
+++ people/robinshepperson/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'DE': ['Germany', 4, 6]
+  'DE': ['Germany', 5, 7]
 }
--- people/robinshepperson/user.py
+++ people/robinshepperson/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Robin Shepperson',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/156210536@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/robinshepperson/',
-  'markers': 4,
-  'photos': 6
+  'markers': 5,
+  'photos': 7
 }
