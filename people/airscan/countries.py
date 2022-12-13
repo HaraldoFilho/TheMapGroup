87d2f6d2152ffdf027fd5f506362672f4ae03e31
The Map Group <admin@the-map-group.pictures>
2022-12-13 02:28:43 -0300

    [auto] Updated map for member 'Alexander Sand Halvorsen'

--- people/airscan/countries.py
+++ people/airscan/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'DE': ['Germany', 1, 1],
-  'CH': ['Switzerland', 1, 1],
-  'IT': ['Italy', 2, 2],
   'US': ['United States of America', 2, 2],
-  'TH': ['Thailand', 2, 2],
+  'CH': ['Switzerland', 1, 1],
+  'GB': ['United Kingdom', 15, 15],
+  'IT': ['Italy', 2, 2],
   'SE': ['Sweden', 3, 3],
-  'NO': ['Norway', 137, 178],
-  'GB': ['United Kingdom', 15, 15]
+  'NO': ['Norway', 138, 179],
+  'DE': ['Germany', 1, 1],
+  'TH': ['Thailand', 2, 2]
 }
