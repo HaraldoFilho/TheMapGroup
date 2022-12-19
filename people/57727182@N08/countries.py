4b90dea4d8facff37a5b6c1c10987dc053660270
The Map Group <admin@the-map-group.pictures>
2022-12-19 02:04:04 -0300

    [auto] Updated map for member 'bobbyblack51'

--- people/57727182@N08/countries.py
+++ people/57727182@N08/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
+  'AT': ['Austria', 10, 65],
+  'CZ': ['Czechia', 45, 360],
+  'FR': ['France', 30, 98],
+  'GB': ['United Kingdom', 178, 8962],
   'PL': ['Poland', 1, 5],
+  'LU': ['Luxembourg', 1, 8],
+  'IT': ['Italy', 1, 1],
   'DE': ['Germany', 76, 499],
   'IE': ['Ireland', 21, 432],
-  'NL': ['Netherlands', 11, 52],
-  'CZ': ['Czechia', 45, 360],
-  'GB': ['United Kingdom', 178, 8948],
-  'IT': ['Italy', 1, 1],
-  'AT': ['Austria', 10, 65],
-  'LU': ['Luxembourg', 1, 8],
   'CH': ['Switzerland', 13, 76],
   'BE': ['Belgium', 11, 76],
-  'FR': ['France', 30, 98]
+  'NL': ['Netherlands', 11, 52]
 }
