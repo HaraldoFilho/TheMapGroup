5aec3a8b44bf9c3a81477808f0d72e7b55bc328c
The Map Group <admin@the-map-group.pictures>
2022-12-21 02:30:00 -0300

    [auto] Updated map for member 'giosuèbolis'

--- people/146445851@N04/user.py
+++ people/146445851@N04/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Giosuè Bolis',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/146445851@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/146445851@N04/',
-  'markers': 194,
-  'photos': 266
+  'markers': 195,
+  'photos': 267
 }
