dbd23576972097387b503ea13393272dc3c2b677
The Map Group <admin@the-map-group.pictures>
2022-12-27 02:28:01 -0300

    [auto] Updated map for member 'dwightcarr_77'

---
+++ people/197276323@N06/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '197276323@N06',
+  'alias': '197276323@N06',
+  'name': 'Dwight Carr',
+  'avatar': '../../icons/photographer.svg',
+  'url': 'https://www.flickr.com/photos/197276323@N06/',
+  'markers': 0,
+  'photos': 0
+}
