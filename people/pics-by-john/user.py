df4e4347a21cb323d6a0577a3134d8e120cece0c
The Map Group <admin@the-map-group.pictures>
2022-12-31 02:32:27 -0300

    [auto] Updated map for member 'John Woolley Photos'

--- people/pics-by-john/user.py
+++ people/pics-by-john/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'John Woolley',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/59961745@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/pics-by-john/',
-  'markers': 2153,
-  'photos': 9275
+  'markers': 2154,
+  'photos': 9277
 }
