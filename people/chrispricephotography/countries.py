18b928f826a04b40abf0b91193360c7156b288b8
The Map Group <admin@the-map-group.pictures>
2023-01-05 02:00:26 -0300

    [auto] Updated map for member 'Christopher Wallace'

--- people/chrispricephotography/countries.py
+++ people/chrispricephotography/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
+  'AU': ['Australia', 11, 11],
+  'VA': ['Vatican City', 11, 14],
+  'IT': ['Italy', 66, 82],
+  'CA': ['Canada', 90, 158],
   'CH': ['Switzerland', 6, 9],
   'NZ': ['New Zealand', 9, 9],
-  'AU': ['Australia', 11, 11],
-  'GB': ['United Kingdom', 26, 28],
-  'VA': ['Vatican City', 11, 13],
-  'IT': ['Italy', 66, 82],
-  'US': ['United States of America', 532, 750],
-  'RU': ['Russia', 1, 1],
   'PA': ['Panama', 1, 2],
-  'CA': ['Canada', 90, 158]
+  'GB': ['United Kingdom', 26, 28],
+  'US': ['United States of America', 532, 750],
+  'RU': ['Russia', 1, 1]
 }
--- people/chrispricephotography/user.py
+++ people/chrispricephotography/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/chrispricephotography/',
   'markers': 753,
-  'photos': 1063
+  'photos': 1064
 }
