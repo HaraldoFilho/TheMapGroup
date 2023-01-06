054b2eabb14888f3e303873daa6827c429237c1b
The Map Group <admin@the-map-group.pictures>
2023-01-06 02:21:09 -0300

    [auto] Updated map for member 'K@moeiras'

--- people/kamo_kamoeiras/countries.py
+++ people/kamo_kamoeiras/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'PT': ['Portugal', 11, 18],
   'MX': ['Mexico', 1, 2],
+  'ES': ['Spain', 1247, 1756],
   'AD': ['Andorra', 2, 2],
-  'FR': ['France', 2, 2],
-  'ES': ['Spain', 1246, 1755]
+  'PT': ['Portugal', 11, 18],
+  'FR': ['France', 2, 2]
 }
