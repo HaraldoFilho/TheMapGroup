9587b84f2248e0766a318611982d3dcb4dc4f51b
The Map Group <admin@the-map-group.pictures>
2023-01-08 02:00:28 -0300

    [auto] Updated map for member 'Christopher Wallace'

--- people/chrispricephotography/countries.py
+++ people/chrispricephotography/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
-  'AU': ['Australia', 11, 11],
   'NZ': ['New Zealand', 9, 9],
   'RU': ['Russia', 1, 1],
-  'GB': ['United Kingdom', 26, 28],
-  'CH': ['Switzerland', 6, 9],
-  'CA': ['Canada', 90, 158],
-  'IT': ['Italy', 66, 82],
-  'VA': ['Vatican City', 12, 16],
   'US': ['United States of America', 532, 750],
-  'PA': ['Panama', 1, 2]
+  'VA': ['Vatican City', 13, 17],
+  'GB': ['United Kingdom', 26, 28],
+  'CA': ['Canada', 90, 158],
+  'PA': ['Panama', 1, 2],
+  'CH': ['Switzerland', 6, 9],
+  'AU': ['Australia', 11, 11],
+  'IT': ['Italy', 66, 82]
 }
