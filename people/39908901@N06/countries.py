b15bc319ae351ddbb18b6dc14c534b9f5bf15fcc
The Map Group <admin@the-map-group.pictures>
2023-01-15 02:06:20 -0300

    [auto] Updated map for member 'm01229'

--- people/39908901@N06/countries.py
+++ people/39908901@N06/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'IN': ['India', 136, 712],
-  'PT': ['Portugal', 104, 429],
-  'FR': ['France', 2, 7],
   'MV': ['Maldives', 16, 58],
+  'IN': ['India', 136, 712],
+  'CA': ['Canada', 350, 672],
   'LK': ['Sri Lanka', 17, 104],
-  'NL': ['The Netherlands', 15, 37],
   'ES': ['Spain', 12, 36],
-  'US': ['United States of America', 7480, 25427],
-  'CA': ['Canada', 333, 652],
+  'PT': ['Portugal', 104, 429],
+  'NL': ['The Netherlands', 15, 37],
+  'US': ['United States of America', 7506, 25472],
+  'FR': ['France', 2, 7],
   'PK': ['Pakistan', 2, 7],
   'TH': ['Thailand', 12, 82]
 }
