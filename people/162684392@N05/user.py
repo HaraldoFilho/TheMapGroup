ef06765caa0e00677ba001cf62a04f0beba9ab0c
The Map Group <admin@the-map-group.pictures>
2023-01-18 02:47:29 -0300

    [auto] Updated map for member 'NiBe60'

--- people/162684392@N05/user.py
+++ people/162684392@N05/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Bernhard Niedermeier',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/162684392@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/162684392@N05/',
-  'markers': 279,
-  'photos': 312
+  'markers': 280,
+  'photos': 313
 }
