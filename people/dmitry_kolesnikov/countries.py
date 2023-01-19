2836b58bb5d26ed3d2b4b1b5d1afae54eed51eb8
The Map Group <admin@the-map-group.pictures>
2023-01-19 02:31:51 -0300

    [auto] Updated map for member 'Dmitry Kolesnikov'

--- people/dmitry_kolesnikov/countries.py
+++ people/dmitry_kolesnikov/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'CL': ['Chile', 4, 4],
-  'BY': ['Belarus', 3, 3],
   'RU': ['Russia', 79, 166],
-  'AM': ['Armenia', 1, 2]
+  'AM': ['Armenia', 2, 3],
+  'CL': ['Chile', 4, 4],
+  'BY': ['Belarus', 3, 3]
 }
