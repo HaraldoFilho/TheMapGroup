81f55b8083461c069f525f942065dfbad6c2c348
The Map Group <admin@the-map-group.pictures>
2023-01-20 02:34:38 -0300

    [auto] Updated map for member 'Zvonimir S.'

--- people/zvonimir_skramonin/countries.py
+++ people/zvonimir_skramonin/countries.py
@@ -1,4 +1,4 @@
 countries_dict = {
-  'HR': ['Croatia', 288, 290],
+  'HR': ['Croatia', 289, 291],
   'BA': ['Bosnia and Herzegovina', 1, 1]
 }
--- people/zvonimir_skramonin/user.py
+++ people/zvonimir_skramonin/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Zvonimir Skramončin',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/185660941@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/zvonimir_skramonin/',
-  'markers': 289,
-  'photos': 291
+  'markers': 290,
+  'photos': 292
 }
