ca870c866e898ffed6be1fe0fbcfd07aa0b5a904
The Map Group <admin@the-map-group.pictures>
2023-01-20 02:39:18 -0300

    [auto] Updated map for member 'Ivor Jones'

--- people/photos-by-ivor/countries.py
+++ people/photos-by-ivor/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'GB': ['United Kingdom', 1, 1],
-  'NZ': ['New Zealand', 35, 1888],
-  'VU': ['Vanuatu', 4, 142],
-  'CA': ['Canada', 17, 803],
   'FR': ['France', 3, 48],
-  'AU': ['Australia', 1051, 23579],
+  'NZ': ['New Zealand', 35, 1888],
+  'AU': ['Australia', 1052, 23602],
+  'CA': ['Canada', 17, 803],
+  'GB': ['United Kingdom', 1, 1],
+  'US': ['United States', 1, 156],
   'FJ': ['Fiji', 4, 147],
-  'US': ['United States', 1, 156]
+  'VU': ['Vanuatu', 4, 142]
 }
