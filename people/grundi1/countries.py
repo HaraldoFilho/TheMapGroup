bfeb68b1c04895e4a2bcb38f75873267d4a349f7
The Map Group <admin@the-map-group.pictures>
2023-02-09 02:17:33 -0300

    [auto] Updated map for member 'grundi1'

--- people/grundi1/countries.py
+++ people/grundi1/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'HR': ['Croatia', 13, 36],
   'GR': ['Greece', 6, 8],
-  'IT': ['Italy', 34, 57],
-  'AT': ['Austria', 133, 507],
+  'HR': ['Croatia', 13, 36],
+  'SI': ['Slovenia', 1, 7],
   'IE': ['Ireland', 38, 84],
-  'SI': ['Slovenia', 1, 7]
+  'IT': ['Italy', 34, 57],
+  'AT': ['Austria', 136, 514]
 }
--- people/grundi1/user.py
+++ people/grundi1/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Christian Grundner',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/7182681@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/grundi1/',
-  'markers': 225,
-  'photos': 699
+  'markers': 228,
+  'photos': 706
 }
