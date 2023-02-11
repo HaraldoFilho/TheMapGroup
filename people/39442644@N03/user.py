98e325b234e2dc4b29df44e142f5784a7eace9f1
The Map Group <admin@the-map-group.pictures>
2023-02-11 03:05:04 -0300

    Removed member '39442644@N03'

--- people/39442644@N03/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '39442644@N03',
-  'alias': '39442644@N03',
-  'name': 'Cranky Oldman',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/39442644@N03_r.jpg',
-  'url': 'https://www.flickr.com/photos/39442644@N03/',
-  'markers': 1160,
-  'photos': 1205
-}
