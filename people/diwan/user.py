51c091cfded8c20027b57ef05323215fd3fcdda7
The Map Group <admin@the-map-group.pictures>
2023-02-15 02:17:12 -0300

    [auto] Updated map for member 'diwan'

--- people/diwan/user.py
+++ people/diwan/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Dirk Wandel',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/81504125@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/diwan/',
-  'markers': 7772,
-  'photos': 8234
+  'markers': 7774,
+  'photos': 8236
 }
