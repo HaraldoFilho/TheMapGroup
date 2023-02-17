b0254d48608d89909772d4884bce5141e679953e
The Map Group <admin@the-map-group.pictures>
2023-02-17 02:17:08 -0300

    [auto] Updated map for member 'Harald Schulz'

--- people/121195437@N06/user.py
+++ people/121195437@N06/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Harri Schulz',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/121195437@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/121195437@N06/',
-  'markers': 2953,
-  'photos': 4627
+  'markers': 2955,
+  'photos': 4629
 }
