d7f76ded9dff65acea7d10bb5a419695e44615b2
The Map Group <admin@the-map-group.pictures>
2023-02-19 02:27:16 -0300

    [auto] Updated map for member 'donnebryant'

---
+++ people/donnebryant/countries.py
@@ -0,0 +1,14 @@
+countries_dict = {
+  'MX': ['Mexico', 13, 347],
+  'GT': ['Guatemala', 4, 134],
+  'IT': ['Italy', 4, 553],
+  'HR': ['Croatia', 10, 455],
+  'CU': ['Cuba', 2, 39],
+  'PA': ['Panama', 1, 116],
+  'AR': ['Argentina', 2, 36],
+  'HN': ['Honduras', 1, 46],
+  'GD': ['Grenada', 2, 57],
+  'CL': ['Chile', 1, 11],
+  'VE': ['Venezuela', 3, 81],
+  'ES': ['Spain', 5, 99]
+}
---
+++ people/donnebryant/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '197365363@N08',
+  'alias': 'donnebryant',
+  'name': 'donnebryant',
+  'avatar': '../../icons/photographer.svg',
+  'url': 'https://www.flickr.com/photos/donnebryant/',
+  'markers': 48,
+  'photos': 1974
+}
