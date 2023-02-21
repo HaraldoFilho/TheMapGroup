3a0a77549dafe1affd37d87cdc43387f593ce0d7
The Map Group <admin@the-map-group.pictures>
2023-02-21 02:47:27 -0300

    [auto] Updated map for member 'S LYNN LEE'

--- people/slynnlee/countries.py
+++ people/slynnlee/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
   'AU': ['Australia', 6, 6],
   'IT': ['Italy', 3, 3],
-  'NL': ['Netherlands', 1, 1],
-  'GB': ['United Kingdom', 334, 1769],
   'CA': ['Canada', 1, 39],
-  'US': ['United States', 246, 1590],
-  'VA': ['Vatican City', 1, 1],
-  'RU': ['Russia', 1, 1],
   'DE': ['Germany', 46, 198],
+  'US': ['United States', 251, 1631],
+  'VA': ['Vatican City', 1, 1],
+  'NL': ['Netherlands', 1, 1],
+  'RU': ['Russia', 1, 1],
+  'GB': ['United Kingdom', 335, 1766],
   'IE': ['Ireland', 210, 953]
 }
