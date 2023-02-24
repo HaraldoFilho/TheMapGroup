29230b06d6e56b78fe427cf114efc4f7257ba1c4
The Map Group <admin@the-map-group.pictures>
2023-02-24 02:00:20 -0300

    [auto] Updated map for member 'H. P. Filho'

--- people/hpfilho/user.py
+++ people/hpfilho/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Haraldo Albergaria',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/hpfilho/',
-  'markers': 534,
-  'photos': 965
+  'markers': 535,
+  'photos': 966
 }
