339759f09afad25fef9a779052905e962e055ed0
The Map Group <admin@the-map-group.pictures>
2023-02-24 02:12:24 -0300

    [auto] Updated map for member 'Hugo von Schreck'

--- people/hugovonschreck/countries.py
+++ people/hugovonschreck/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
-  'AT': ['Austria', 16, 27],
-  'HR': ['Croatia', 7, 17],
-  'IT': ['Italy', 83, 190],
   'NL': ['Netherlands', 40, 69],
+  'LU': ['Luxembourg', 6, 15],
   'FR': ['France', 91, 197],
   'PL': ['Poland', 1, 3],
   'CH': ['Switzerland', 18, 39],
-  'LU': ['Luxembourg', 6, 15],
+  'IT': ['Italy', 84, 191],
   'SM': ['San Marino', 1, 1],
-  'DE': ['Germany', 1259, 5034]
+  'HR': ['Croatia', 7, 17],
+  'DE': ['Germany', 1259, 5034],
+  'AT': ['Austria', 16, 27]
 }
--- people/hugovonschreck/user.py
+++ people/hugovonschreck/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Heinz Dieter  Heil',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/74758246@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/hugovonschreck/',
-  'markers': 1522,
-  'photos': 5592
+  'markers': 1523,
+  'photos': 5593
 }
