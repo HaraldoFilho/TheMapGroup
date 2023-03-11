80747c06084466f85a5e1f2ffc7e8b5e25e4d7d8
The Map Group <admin@the-map-group.pictures>
2023-03-11 02:12:07 -0300

    [auto] Updated map for member 'fam_nordstrom'

--- people/fam_nordstrom/user.py
+++ people/fam_nordstrom/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/10456518@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/fam_nordstrom/',
   'markers': 4762,
-  'photos': 13863
+  'photos': 13867
 }
