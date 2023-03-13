3adcb0b9e88d46ae2afcd537c5e6037e517d4fa8
The Map Group <admin@the-map-group.pictures>
2023-03-13 02:24:56 -0300

    [auto] Updated map for member 'marionkaminski'

--- people/147422800@N02/countries.py
+++ people/147422800@N02/countries.py
@@ -1,16 +1,17 @@
 countries_dict = {
-  'NA': ['Namibia', 6, 8],
-  'BO': ['Bolivia', 1, 1],
-  'CL': ['Chile', 31, 70],
-  'IS': ['Iceland', 14, 22],
-  'PT': ['Portugal', 2, 3],
+  'JO': ['Jordan', 1, 1],
+  'VA': ['Vatican City', 1, 2],
+  'ES': ['Spain', 7, 10],
   'IT': ['Italy', 15, 20],
-  'CH': ['Switzerland', 8, 12],
-  'DE': ['Germany', 45, 72],
-  'FR': ['France', 2, 4],
+  'BO': ['Bolivia', 1, 1],
+  'PT': ['Portugal', 2, 3],
+  'CL': ['Chile', 31, 70],
+  'PE': ['Peru', 10, 16],
   'EC': ['Ecuador', 1, 1],
   'MA': ['Morocco', 11, 24],
-  'ES': ['Spain', 7, 10],
-  'VA': ['Vatican City', 1, 2],
-  'PE': ['Peru', 10, 16]
+  'FR': ['France', 2, 4],
+  'IS': ['Iceland', 14, 22],
+  'CH': ['Switzerland', 8, 12],
+  'DE': ['Germany', 45, 72],
+  'NA': ['Namibia', 6, 8]
 }
