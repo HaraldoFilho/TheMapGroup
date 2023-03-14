58ec5f2fa8e4b45e7427b24dd4fc01b34f411120
The Map Group <admin@the-map-group.pictures>
2023-03-14 02:32:20 -0300

    [auto] Updated map for member 'Doctor Fergie'

---
+++ people/186241381@N05/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '186241381@N05',
+  'alias': '186241381@N05',
+  'name': 'Dr Fergie Christabol',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/186241381@N05_r.jpg',
+  'url': 'https://www.flickr.com/photos/186241381@N05/',
+  'markers': 0,
+  'photos': 0
+}
