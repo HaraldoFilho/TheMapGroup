164cabad8373a78ada881dfccb31634966675cea
The Map Group <admin@the-map-group.pictures>
2023-03-19 02:00:42 -0300

    [auto] Updated map for member 'hans pohl'

--- people/51839301@N00/user.py
+++ people/51839301@N00/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Hans Pohl',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/51839301@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/51839301@N00/',
-  'markers': 3330,
-  'photos': 5035
+  'markers': 3331,
+  'photos': 5036
 }
