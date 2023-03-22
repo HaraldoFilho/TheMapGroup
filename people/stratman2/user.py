fb18a9a51f633e63b776dcb533bec3f6f5599703
The Map Group <admin@the-map-group.pictures>
2023-03-22 02:28:07 -0300

    [auto] Updated map for member 'Stratman2'

--- people/stratman2/user.py
+++ people/stratman2/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Stratman2',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/8278193@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/stratman2/',
-  'markers': 524,
-  'photos': 4645
+  'markers': 523,
+  'photos': 4646
 }
