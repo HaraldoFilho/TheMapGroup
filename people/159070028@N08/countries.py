18427dd145522afc77c3b783cec271511a13ce6e
The Map Group <admin@the-map-group.pictures>
2023-03-24 02:25:32 -0300

    [auto] Updated map for member 'iLOVEnature's Photography Inspiration'

--- people/159070028@N08/countries.py
+++ people/159070028@N08/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
+  'CN': ['South China Sea Islands (disputed)', 3, 3],
   'SG': ['Singapore', 18, 18],
-  'US': ['United States', 95, 109],
-  'CN': ['South China Sea Islands (disputed)', 3, 3]
+  'US': ['United States', 96, 110]
 }
