94ee805fa381c40a1d215be592fd9dbd6eadd0c1
The Map Group <admin@the-map-group.pictures>
2023-03-27 02:31:23 -0300

    [auto] Updated map for member 'Ron Fisher'

--- people/train-pix/countries.py
+++ people/train-pix/countries.py
@@ -1,19 +1,19 @@
 countries_dict = {
-  'CA': ['Canada', 1, 2],
   'RS': ['Serbia', 3, 3],
   'ZA': ['South Africa', 311, 320],
-  'GB': ['United Kingdom', 8880, 18752],
   'BE': ['Belgium', 1, 1],
-  'CH': ['Switzerland', 1158, 1866],
-  'FR': ['France', 440, 648],
-  'ES': ['Spain', 1, 3],
-  'HR': ['Croatia', 5, 9],
-  'HU': ['Hungary', 19, 28],
   'IM': ['Isle of Man', 20, 55],
+  'HR': ['Croatia', 5, 9],
+  'ES': ['Spain', 1, 3],
   'DE': ['Germany', 316, 359],
-  'SZ': ['Eswatini', 6, 7],
   'MC': ['Monaco', 3, 3],
+  'SZ': ['Eswatini', 6, 7],
+  'HU': ['Hungary', 19, 28],
+  'CA': ['Canada', 1, 2],
+  'CH': ['Switzerland', 1158, 1866],
   'BA': ['Bosnia and Herzegovina', 12, 20],
+  'IT': ['Italy', 23, 41],
   'AT': ['Austria', 28, 28],
-  'IT': ['Italy', 23, 41]
+  'GB': ['United Kingdom', 8882, 18758],
+  'FR': ['France', 440, 648]
 }
