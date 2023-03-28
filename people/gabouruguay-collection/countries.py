166b5a80ae766c654043884205f749d2641d1c19
The Map Group <admin@the-map-group.pictures>
2023-03-28 02:31:05 -0300

    [auto] Updated map for member 'Gabriel Paladino Ibáñez'

--- people/gabouruguay-collection/countries.py
+++ people/gabouruguay-collection/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'CL': ['Chile', 5, 9],
   'MX': ['Mexico', 26, 295],
-  'PE': ['Peru', 41, 80],
-  'AR': ['Argentina', 40, 98],
-  'UY': ['Uruguay', 565, 1981],
+  'AR': ['Argentina', 40, 99],
+  'DO': ['Dominican Republic', 11, 16],
   'ZA': ['South Africa', 57, 127],
-  'DO': ['Dominican Republic', 11, 16]
+  'CL': ['Chile', 5, 9],
+  'PE': ['Peru', 41, 80],
+  'UY': ['Uruguay', 565, 1982]
 }
