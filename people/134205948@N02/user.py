7a7cfbe2c33797c16aef7caa8b894555953ca548
The Map Group <admin@the-map-group.pictures>
2023-03-30 02:18:56 -0300

    [auto] Updated map for member 'giorgiorodano46'

--- people/134205948@N02/user.py
+++ people/134205948@N02/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Giorgio Rodano',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/134205948@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/134205948@N02/',
-  'markers': 5507,
-  'photos': 5951
+  'markers': 5509,
+  'photos': 5953
 }
