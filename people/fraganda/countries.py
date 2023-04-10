611ec25764f561e9ab6138773fafd03f1a84a134
The Map Group <admin@the-map-group.pictures>
2023-04-10 02:35:25 -0300

    [auto] Updated map for member 'fraganda.photo'

--- people/fraganda/countries.py
+++ people/fraganda/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'PS': ['Palestinian Territory', 3, 3],
-  'GI': ['Gibraltar', 1, 1],
+  'BE': ['Belgium', 67, 67],
   'DE': ['Germany', 3, 3],
   'HR': ['Croatia', 13, 13],
-  'ES': ['Spain', 41, 41],
-  'US': ['United States of America', 33, 33],
-  'BE': ['Belgium', 67, 67],
   'IL': ['Israel', 17, 17],
+  'GR': ['Greece', 22, 22],
+  'ES': ['Spain', 41, 41],
+  'PS': ['Palestinian Territory', 3, 3],
+  'GI': ['Gibraltar', 1, 1],
+  'FR': ['France', 3, 3],
   'NL': ['Netherlands', 8, 8],
-  'GR': ['Greece', 19, 19],
-  'IT': ['Italy', 3, 3],
-  'FR': ['France', 3, 3]
+  'US': ['United States of America', 33, 33],
+  'IT': ['Italy', 3, 3]
 }
--- people/fraganda/user.py
+++ people/fraganda/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'fraganda.photo',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/39777440@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/fraganda/',
-  'markers': 211,
-  'photos': 211
+  'markers': 214,
+  'photos': 214
 }
