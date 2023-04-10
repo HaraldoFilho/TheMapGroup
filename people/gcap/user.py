21ab503eb64ad407237889cf73440579c406c64d
The Map Group <admin@the-map-group.pictures>
2023-04-10 02:55:41 -0300

    Removed member 'gcap'

--- people/gcap/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '8299662@N02',
-  'alias': 'gcap',
-  'name': 'Giovanni C.',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/8299662@N02_r.jpg',
-  'url': 'https://www.flickr.com/photos/gcap/',
-  'markers': 757,
-  'photos': 1832
-}
