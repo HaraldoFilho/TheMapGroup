447c495d74b2bea381574de3aa6dac9a2a0da859
The Map Group <admin@the-map-group.pictures>
2023-04-10 02:17:51 -0300

    [auto] Updated map for member 'Gary S Brooks - GSB Photography'

--- people/120861725@N07/countries.py
+++ people/120861725@N07/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
   'CH': ['Switzerland', 5, 5],
   'NL': ['Netherlands', 8, 8],
-  'US': ['United States of America', 87, 100],
   'DE': ['Germany', 8, 10],
-  'AT': ['Austria', 2, 2],
   'IT': ['Italy', 49, 55],
-  'GB': ['United Kingdom', 15, 17]
+  'US': ['United States of America', 87, 101],
+  'GB': ['United Kingdom', 15, 17],
+  'AT': ['Austria', 2, 2]
 }
