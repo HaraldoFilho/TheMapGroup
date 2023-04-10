69aa0f6529cc6bd8a5ed953dd71fb6cca5526cf1
The Map Group <admin@the-map-group.pictures>
2023-04-10 02:29:21 -0300

    [auto] Updated map for member 'giorgiorodano46'

--- people/134205948@N02/countries.py
+++ people/134205948@N02/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'CH': ['Switzerland', 655, 728],
-  'IT': ['Italy', 4735, 5094],
+  'HR': ['Croatia', 14, 17],
   'VA': ['Vatican City', 10, 10],
-  'HR': ['Croatia', 12, 14],
+  'IT': ['Italy', 4742, 5101],
   'FR': ['France', 94, 105],
   'AT': ['Austria', 1, 1],
+  'CH': ['Switzerland', 655, 728],
   'GB': ['United Kingdom', 8, 8]
 }
