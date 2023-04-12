055f703cf1c7335bcfe9c411e27d2242d5535355
The Map Group <admin@the-map-group.pictures>
2023-04-12 02:13:49 -0300

    [auto] Updated map for member 'NiBe60'

--- people/162684392@N05/countries.py
+++ people/162684392@N05/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
   'IT': ['Italy', 64, 73],
-  'CH': ['Switzerland', 18, 21],
   'GR': ['Greece', 8, 8],
   'FR': ['France', 1, 1],
-  'ES': ['Spain', 29, 31],
-  'DE': ['Germany', 121, 134],
-  'AT': ['Austria', 65, 75]
+  'CH': ['Switzerland', 18, 21],
+  'DE': ['Germany', 122, 135],
+  'AT': ['Austria', 65, 75],
+  'ES': ['Spain', 29, 31]
 }
--- people/162684392@N05/user.py
+++ people/162684392@N05/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Bernhard Niedermeier',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/162684392@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/162684392@N05/',
-  'markers': 306,
-  'photos': 343
+  'markers': 307,
+  'photos': 344
 }
