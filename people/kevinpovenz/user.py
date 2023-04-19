29a12e6984aa242a18758817046ae175c9eca0ae
The Map Group <admin@the-map-group.pictures>
2023-04-19 02:21:12 -0300

    [auto] Updated map for member 'Kevin Povenz'

--- people/kevinpovenz/user.py
+++ people/kevinpovenz/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/19613665@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/kevinpovenz/',
   'markers': 713,
-  'photos': 2145
+  'photos': 2146
 }
