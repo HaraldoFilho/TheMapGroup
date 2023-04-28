38d92cf818b9fb784aabcdd09134922c8f938c75
The Map Group <admin@the-map-group.pictures>
2023-04-28 02:21:32 -0300

    [auto] Updated map for member 'Bernard Spragg'

--- people/volvob12b/countries.py
+++ people/volvob12b/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'BN': ['Brunei', 36, 49],
-  'JP': ['Japan', 16, 28],
-  'CA': ['Canada', 226, 301],
-  'CN': ['South China Sea Islands (disputed)', 150, 190],
-  'TH': ['Thailand', 25, 26],
-  'US': ['United States of America', 280, 434],
+  'CN': ['South China Sea Islands (disputed)', 148, 188],
+  'NZ': ['New Zealand', 2574, 3430],
   'SG': ['Singapore', 10, 11],
-  'MY': ['Malaysia', 39, 58],
-  'PH': ['Philippines', 51, 92],
+  'BN': ['Brunei', 36, 49],
+  'US': ['United States of America', 280, 434],
+  'MY': ['Malaysia', 38, 56],
+  'JP': ['Japan', 16, 28],
+  'TH': ['Thailand', 25, 26],
+  'CA': ['Canada', 226, 301],
   'AU': ['Australia', 237, 341],
-  'NZ': ['New Zealand', 2577, 3439]
+  'PH': ['Philippines', 50, 91]
 }
