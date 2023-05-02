c33dd131429f17a38a2d1f2079f7a0e0dae96ece
The Map Group <admin@the-map-group.pictures>
2023-05-02 02:37:53 -0300

    [auto] Updated map for member 'Gaz 1984'

--- people/shabbagaz/countries.py
+++ people/shabbagaz/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'GB': ['United Kingdom', 11652, 16791],
   'DE': ['Germany', 25, 230],
   'IE': ['Ireland', 54, 58],
+  'GB': ['United Kingdom', 11713, 16857],
   'ES': ['Spain', 21, 103]
 }
