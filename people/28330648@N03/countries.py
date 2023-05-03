124da3343f17d0dcdd318ef61397e22a602a675e
The Map Group <admin@the-map-group.pictures>
2023-05-03 02:12:49 -0300

    [auto] Updated map for member 'Florian106'

--- people/28330648@N03/countries.py
+++ people/28330648@N03/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
+  'DE': ['Germany', 4011, 4489],
   'CH': ['Switzerland', 4, 4],
   'IT': ['Italy', 231, 289],
   'AT': ['Austria', 100, 109],
-  'FR': ['France', 149, 236],
-  'DE': ['Germany', 4006, 4484]
+  'FR': ['France', 149, 236]
 }
--- people/28330648@N03/user.py
+++ people/28330648@N03/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Florian106',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/28330648@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/28330648@N03/',
-  'markers': 4490,
-  'photos': 5122
+  'markers': 4495,
+  'photos': 5127
 }
