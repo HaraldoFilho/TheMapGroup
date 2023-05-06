21337a0fdbc701e335890c8ac44cfc157bcf4e52
The Map Group <admin@the-map-group.pictures>
2023-05-06 02:01:11 -0300

    [auto] Updated map for member 'haroldmoses'

--- people/mosesharold/user.py
+++ people/mosesharold/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Harold  Moses',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/57729348@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/mosesharold/',
-  'markers': 2976,
-  'photos': 4210
+  'markers': 2980,
+  'photos': 4214
 }
