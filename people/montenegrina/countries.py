5804b18620f05df4654eace05d26ff09d3d8d27c
The Map Group <admin@the-map-group.pictures>
2023-05-14 02:18:32 -0300

    [auto] Updated map for member 'ForceMajeureMontenegro'

--- people/montenegrina/countries.py
+++ people/montenegrina/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'SI': ['Slovenia', 5, 6],
-  'PT': ['Portugal', 15, 23],
   'HR': ['Croatia', 51, 58],
-  'MA': ['Morocco', 14, 15],
-  'AL': ['Albania', 4, 4],
-  'CZ': ['Czechia', 46, 54],
-  'VA': ['Vatican City', 5, 7],
-  'GR': ['Greece', 73, 84],
-  'BA': ['Bosnia and Herzegovina', 17, 19],
-  'IT': ['Italy', 113, 129],
-  'FR': ['France', 65, 77],
+  'SI': ['Slovenia', 5, 6],
   'ES': ['Spain', 107, 126],
-  'ME': ['Montenegro', 652, 736]
+  'MA': ['Morocco', 14, 15],
+  'FR': ['France', 65, 77],
+  'ME': ['Montenegro', 652, 736],
+  'CZ': ['Czechia', 46, 54],
+  'IT': ['Italy', 114, 130],
+  'VA': ['Vatican City', 5, 7],
+  'PT': ['Portugal', 15, 23],
+  'AL': ['Albania', 4, 4],
+  'GR': ['Greece', 73, 84],
+  'BA': ['Bosnia and Herzegovina', 17, 19]
 }
--- people/montenegrina/user.py
+++ people/montenegrina/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'ForceMajeureMontenegro',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/92496593@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/montenegrina/',
-  'markers': 1171,
-  'photos': 1342
+  'markers': 1172,
+  'photos': 1343
 }
