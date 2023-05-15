cb91f722050a05e23956819515814e59932a21a3
The Map Group <admin@the-map-group.pictures>
2023-05-15 02:19:15 -0300

    [auto] Updated map for member 'amalthus'

--- people/168583569@N03/countries.py
+++ people/168583569@N03/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'DE': ['Germany', 1, 8],
   'FR': ['France', 1, 15],
-  'US': ['United States of America', 1, 1]
+  'US': ['United States of America', 1, 1],
+  'DE': ['Germany', 1, 17]
 }
