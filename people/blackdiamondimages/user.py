a4842eda8d7b7df5ed4d9d6ebc56b5cafa8d153a
The Map Group <admin@the-map-group.pictures>
2023-05-19 02:04:49 -0300

    [auto] Updated map for member 'Black Diamond Images'

--- people/blackdiamondimages/user.py
+++ people/blackdiamondimages/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Black Diamond Images',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/97974874@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/blackdiamondimages/',
-  'markers': 12785,
-  'photos': 17020
+  'markers': 12790,
+  'photos': 17027
 }
