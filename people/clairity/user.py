a633aabe3ebec814ea7bea8f51966641dd1dbc20
The Map Group <admin@the-map-group.pictures>
2023-05-24 02:22:24 -0300

    [auto] Updated map for member 'Sharon Mollerus'

--- people/clairity/user.py
+++ people/clairity/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Sharon Mollerus',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/38315261@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/clairity/',
-  'markers': 3504,
-  'photos': 14101
+  'markers': 3505,
+  'photos': 14102
 }
