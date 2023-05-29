816a3a28f78d4907f36e1bf3a77725385a3794a3
The Map Group <admin@the-map-group.pictures>
2023-05-29 02:15:55 -0300

    [auto] Updated map for member 'Beatrix MK'

--- people/beatrixmvarga/countries.py
+++ people/beatrixmvarga/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
   'HU': ['Hungary', 69, 70],
-  'US': ['United States of America', 376, 378],
-  'CA': ['Canada', 2, 2],
   'IT': ['Italy', 7, 7],
-  'AT': ['Austria', 101, 101]
+  'US': ['United States of America', 377, 379],
+  'AT': ['Austria', 101, 101],
+  'CA': ['Canada', 2, 2]
 }
--- people/beatrixmvarga/user.py
+++ people/beatrixmvarga/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Beatrix',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/33989856@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/beatrixmvarga/',
-  'markers': 555,
-  'photos': 558
+  'markers': 556,
+  'photos': 559
 }
