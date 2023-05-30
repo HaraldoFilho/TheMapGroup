d3e42dd978b4260706daf1709b1e886f7aedb374
The Map Group <admin@the-map-group.pictures>
2023-05-30 02:17:26 -0300

    [auto] Updated map for member 'Ignacio M. Jiménez'

--- people/ignaciomjimenez/user.py
+++ people/ignaciomjimenez/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/23499399@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/ignaciomjimenez/',
   'markers': 1066,
-  'photos': 1404
+  'photos': 1405
 }
