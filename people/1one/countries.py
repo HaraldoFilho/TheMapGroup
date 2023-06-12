eaa49ab79a9900cba79f4ee758259a72c47ea657
The Map Group <admin@the-map-group.pictures>
2023-06-12 02:16:24 -0300

    [auto] Updated map for member '七福神'

--- people/1one/countries.py
+++ people/1one/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'JP': ['Japan', 2389, 36342],
-  'GB': ['United Kingdom', 1, 1],
-  'IT': ['Italy', 2, 2],
   'US': ['United States', 18, 21],
-  'BS': ['The Bahamas', 6, 8]
+  'BS': ['The Bahamas', 6, 8],
+  'GB': ['United Kingdom', 1, 1],
+  'JP': ['Japan', 2389, 36343],
+  'IT': ['Italy', 2, 2]
 }
--- people/1one/user.py
+++ people/1one/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/52009851@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/1one/',
   'markers': 2416,
-  'photos': 36374
+  'photos': 36375
 }
