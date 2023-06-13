356ae22b7cae3b2b7897addf325603fbf80af241
The Map Group <admin@the-map-group.pictures>
2023-06-13 02:11:30 -0300

    [auto] Updated map for member 'Fernando Forniés Revuelta'

--- people/darth-fer-75/countries.py
+++ people/darth-fer-75/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'AT': ['Austria', 63, 270],
-  'IT': ['Italy', 131, 523],
-  'FR': ['France', 180, 793],
-  'QA': ['Qatar', 1, 10],
-  'MT': ['Malta', 129, 524],
-  'EG': ['Egypt', 88, 515],
-  'PH': ['Philippines', 19, 418],
   'GB': ['United Kingdom', 52, 529],
-  'ES': ['Spain', 962, 6827],
+  'MT': ['Malta', 129, 524],
+  'AT': ['Austria', 63, 270],
   'DE': ['Germany', 135, 345],
-  'VA': ['Vatican City', 4, 59]
+  'PH': ['Philippines', 19, 418],
+  'VA': ['Vatican City', 4, 59],
+  'IT': ['Italy', 131, 523],
+  'ES': ['Spain', 963, 6844],
+  'FR': ['France', 180, 793],
+  'EG': ['Egypt', 88, 515],
+  'QA': ['Qatar', 1, 10]
 }
