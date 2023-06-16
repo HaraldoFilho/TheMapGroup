3deb6d39c97c91a6be9a6b52e9380f641dd0bd77
The Map Group <admin@the-map-group.pictures>
2023-06-16 02:23:44 -0300

    [auto] Updated map for member '- Ozymandias -'

--- people/archstanton/user.py
+++ people/archstanton/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': '- Ozymandias -',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/138177073@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/archstanton/',
-  'markers': 17281,
-  'photos': 21674
+  'markers': 17295,
+  'photos': 21688
 }
