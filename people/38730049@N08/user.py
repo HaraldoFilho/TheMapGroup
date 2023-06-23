e1cda2c3482e8ff4942a3f706d84dbb069888ac6
The Map Group <admin@the-map-group.pictures>
2023-06-23 02:08:21 -0300

    [auto] Updated map for member 'Gmlskis'

--- people/38730049@N08/user.py
+++ people/38730049@N08/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'George Landis',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/38730049@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/38730049@N08/',
-  'markers': 856,
-  'photos': 48581
+  'markers': 858,
+  'photos': 48818
 }
