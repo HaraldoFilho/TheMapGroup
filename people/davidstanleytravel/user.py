b8f3d468d38b78ebe2fd3c02d441596e40e25189
The Map Group <admin@the-map-group.pictures>
2023-06-26 02:41:29 -0300

    [auto] Updated map for member 'D-Stanley'

--- people/davidstanleytravel/user.py
+++ people/davidstanleytravel/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'David Stanley',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/79721788@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/davidstanleytravel/',
-  'markers': 6233,
-  'photos': 7204
+  'markers': 6236,
+  'photos': 7207
 }
