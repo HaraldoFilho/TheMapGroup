d0adac270c07530dc2e3b70619776d4a79864d9f
The Map Group <admin@the-map-group.pictures>
2023-06-27 02:48:31 -0300

    [auto] Updated map for member 'donnebryant'

--- people/donnebryant/countries.py
+++ people/donnebryant/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'MX': ['Mexico', 15, 349],
-  'CO': ['Colombia', 3, 145],
-  'IT': ['Italy', 4, 553],
-  'VE': ['Venezuela', 3, 81],
-  'ES': ['Spain', 6, 99],
-  'GT': ['Guatemala', 4, 133],
   'CL': ['Chile', 1, 11],
-  'HR': ['Croatia', 10, 455],
-  'GD': ['Grenada', 2, 57],
-  'CU': ['Cuba', 2, 39],
-  'HN': ['Honduras', 1, 46],
   'PA': ['Panama', 1, 116],
+  'MX': ['Mexico', 15, 349],
+  'GD': ['Grenada', 2, 57],
+  'VE': ['Venezuela', 3, 81],
+  'IT': ['Italy', 4, 553],
+  'GT': ['Guatemala', 4, 133],
+  'CO': ['Colombia', 3, 145],
+  'HN': ['Honduras', 1, 46],
+  'CU': ['Cuba', 2, 39],
   'AR': ['Argentina', 2, 36],
-  'US': ['United States', 35, 371]
+  'HR': ['Croatia', 10, 455],
+  'US': ['United States', 36, 372],
+  'ES': ['Spain', 6, 99]
 }
