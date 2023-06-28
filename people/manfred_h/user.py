9ae74a00b4ef185cdbfe4bb6877006f82773bb0b
The Map Group <admin@the-map-group.pictures>
2023-06-28 02:07:03 -0300

    [auto] Updated map for member 'Mani_H.'

--- people/manfred_h/user.py
+++ people/manfred_h/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Manfred Hermann',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/165713115@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/manfred_h/',
-  'markers': 630,
-  'photos': 1096
+  'markers': 631,
+  'photos': 1097
 }
