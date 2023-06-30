95da8156930d893aa8b0fd89d1350b77d44ecd2b
The Map Group <admin@the-map-group.pictures>
2023-06-30 02:42:52 -0300

    [auto] Updated map for member 'kundun59'

---
+++ people/188891681@N08/countries.py
@@ -0,0 +1,7 @@
+countries_dict = {
+  'US': ['United States of America', 2, 2],
+  'JP': ['Japan', 1, 1],
+  'GR': ['Greece', 8, 10],
+  'FR': ['France', 1, 1],
+  'IT': ['Italy', 1, 1]
+}
---
+++ people/188891681@N08/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '188891681@N08',
+  'alias': '188891681@N08',
+  'name': 'kundun59',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/188891681@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/188891681@N08/',
+  'markers': 31,
+  'photos': 259
+}
