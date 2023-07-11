e8f7d28fc3cefbc38a4fe86a868e4d9c56f8b2f5
The Map Group <admin@the-map-group.pictures>
2023-07-11 02:17:16 -0300

    [auto] Updated map for member 'Brunswick Forge'

--- people/botetourt/countries.py
+++ people/botetourt/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'KE': ['Kenya', 108, 149],
-  'NL': ['Netherlands', 1, 1],
-  'IT': ['Italy', 349, 476],
-  'TZ': ['Tanzania', 291, 856],
-  'US': ['United States of America', 3175, 5680],
+  'QA': ['Qatar', 1, 1],
   'TR': ['Turkey', 1, 1],
-  'CA': ['Canada', 10, 13],
-  'MX': ['Mexico', 8, 10],
+  'US': ['United States of America', 3176, 5681],
+  'TZ': ['Tanzania', 291, 856],
+  'IT': ['Italy', 349, 476],
   'GB': ['United Kingdom', 51, 94],
-  'VA': ['Vatican City', 16, 19],
-  'QA': ['Qatar', 1, 1]
+  'MX': ['Mexico', 8, 10],
+  'CA': ['Canada', 10, 13],
+  'NL': ['Netherlands', 1, 1],
+  'KE': ['Kenya', 108, 149],
+  'VA': ['Vatican City', 16, 19]
 }
--- people/botetourt/user.py
+++ people/botetourt/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'John McNeely Hudson',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/82487858@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/botetourt/',
-  'markers': 4011,
-  'photos': 7300
+  'markers': 4012,
+  'photos': 7301
 }
