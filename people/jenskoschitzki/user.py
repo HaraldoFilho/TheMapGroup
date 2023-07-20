98d487d04932a1bda80312968a68cc4ba24710df
The Map Group <admin@the-map-group.pictures>
2023-07-20 02:28:40 -0300

    [auto] Updated map for member 'Jens Koschitzki'

--- people/jenskoschitzki/user.py
+++ people/jenskoschitzki/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Jens Koschitzki',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/108188718@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/jenskoschitzki/',
-  'markers': 3998,
-  'photos': 30723
+  'markers': 4020,
+  'photos': 30746
 }
