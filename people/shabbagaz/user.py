7dc082abe7c3d95a3aeafe3e31acb392dd8daed9
The Map Group <admin@the-map-group.pictures>
2023-07-24 02:37:01 -0300

    [auto] Updated map for member 'Gaz 1984'

--- people/shabbagaz/user.py
+++ people/shabbagaz/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Gary Bond',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/33352310@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/shabbagaz/',
-  'markers': 12062,
-  'photos': 17529
+  'markers': 12097,
+  'photos': 17569
 }
