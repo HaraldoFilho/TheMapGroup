2abee3b597b950e28c04b990e2f7962db37cfd7b
The Map Group <admin@the-map-group.pictures>
2023-07-25 02:19:19 -0300

    [auto] Updated map for member 'malioli'

--- people/malioli/user.py
+++ people/malioli/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/79369350@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/malioli/',
   'markers': 1969,
-  'photos': 4219
+  'photos': 4220
 }
