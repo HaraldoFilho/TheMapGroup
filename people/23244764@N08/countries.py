e3c9e8a88145a14a0f0ee13bc0654179f53435d8
The Map Group <admin@the-map-group.pictures>
2023-07-26 02:34:23 -0300

    [auto] Updated map for member 'suntrana3'

--- people/23244764@N08/countries.py
+++ people/23244764@N08/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
-  'AQ': ['Antarctica', 11, 11],
-  'CA': ['Canada', 1, 1],
   'NL': ['Netherlands', 13, 13],
-  'BZ': ['Belize', 4, 7],
-  'BS': ['The Bahamas', 2, 2],
-  'MA': ['Morocco', 13, 13],
-  'US': ['United States of America', 136, 258],
-  'GI': ['Gibraltar', 14, 14],
   'ES': ['Spain', 40, 58],
-  'HT': ['Haiti', 1, 1]
+  'HT': ['Haiti', 1, 1],
+  'BZ': ['Belize', 4, 7],
+  'US': ['United States of America', 137, 259],
+  'BS': ['The Bahamas', 2, 2],
+  'AQ': ['Antarctica', 11, 11],
+  'GI': ['Gibraltar', 14, 14],
+  'MA': ['Morocco', 13, 13],
+  'CA': ['Canada', 1, 1]
 }
