95dc8bbf434a557099724af97f4043c9948b5e33
The Map Group <admin@the-map-group.pictures>
2023-07-27 02:21:51 -0300

    [auto] Updated map for member 'Hugo von Schreck'

--- people/hugovonschreck/countries.py
+++ people/hugovonschreck/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'CH': ['Switzerland', 18, 39],
-  'NL': ['Netherlands', 40, 69],
-  'DK': ['Denmark', 3, 3],
-  'AT': ['Austria', 16, 27],
-  'FR': ['France', 91, 198],
-  'DE': ['Germany', 1259, 5065],
-  'IT': ['Italy', 88, 225],
-  'HR': ['Croatia', 7, 17],
   'LU': ['Luxembourg', 6, 15],
   'SM': ['San Marino', 1, 1],
-  'PL': ['Poland', 1, 3]
+  'IT': ['Italy', 88, 225],
+  'DE': ['Germany', 1259, 5065],
+  'PL': ['Poland', 1, 3],
+  'HR': ['Croatia', 7, 17],
+  'FR': ['France', 91, 198],
+  'DK': ['Denmark', 3, 4],
+  'NL': ['Netherlands', 40, 69],
+  'AT': ['Austria', 16, 27],
+  'CH': ['Switzerland', 18, 39]
 }
--- people/hugovonschreck/user.py
+++ people/hugovonschreck/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/74758246@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/hugovonschreck/',
   'markers': 1530,
-  'photos': 5662
+  'photos': 5663
 }
