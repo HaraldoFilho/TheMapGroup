d7a04f9c5de70c1010b669e859ef6ed2fbcbac2b
The Map Group <admin@the-map-group.pictures>
2023-08-01 02:26:40 -0300

    [auto] Updated map for member 'xd_travel'

--- people/111314495@N05/user.py
+++ people/111314495@N05/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Mike X-d',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/111314495@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/111314495@N05/',
-  'markers': 4712,
-  'photos': 8371
+  'markers': 4714,
+  'photos': 8375
 }
