5d715f85811c96ca4164f7a9afd54f1273beacd0
The Map Group <admin@the-map-group.pictures>
2023-08-02 02:21:50 -0300

    [auto] Updated map for member 'jamesgreig@dr.com'

--- people/jimgreig/countries.py
+++ people/jimgreig/countries.py
@@ -1,9 +1,4 @@
 countries_dict = {
-  'US': ['United States', 1, 1],
-  'IT': ['Italy', 1, 1],
-  'NL': ['Netherlands', 18, 21],
-  'BR': ['Brazil', 3, 3],
-  'GB': ['United Kingdom', 2243, 2718],
-  'MA': ['Morocco', 2, 2],
-  'CA': ['Canada', 2, 2]
+  'NL': ['Netherlands', 7, 7],
+  'GB': ['United Kingdom', 760, 926]
 }
--- people/jimgreig/user.py
+++ people/jimgreig/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'James Greig',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/38286807@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/jimgreig/',
-  'markers': 2271,
-  'photos': 2749
+  'markers': 768,
+  'photos': 934
 }
