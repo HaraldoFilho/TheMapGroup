4de487f70b6c9494e15c4796baaeeabedfa7a279
The Map Group <admin@the-map-group.pictures>
2023-08-14 02:44:40 -0300

    [auto] Updated map for member 'Thanks for 7.5 Million Views'

--- people/joeinpenticton/user.py
+++ people/joeinpenticton/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Joe (José)  Garcia',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/23349126@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/joeinpenticton/',
-  'markers': 594,
-  'photos': 603
+  'markers': 595,
+  'photos': 604
 }
