bac75445f395909a76937e722b7335fcbfa6e6e9
The Map Group <admin@the-map-group.pictures>
2023-08-15 02:02:26 -0300

    [auto] Updated map for member 'alfsan'

--- people/santelia/user.py
+++ people/santelia/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Alfonso Santelia',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/32557441@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/santelia/',
-  'markers': 615,
-  'photos': 859
+  'markers': 616,
+  'photos': 860
 }
