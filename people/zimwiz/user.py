4182b3f2595aec5419a9a5b7e43dbf3f807502a3
The Map Group <admin@the-map-group.pictures>
2023-08-18 02:28:16 -0300

    [auto] Updated map for member 'zimwizdotcom'

--- people/zimwiz/user.py
+++ people/zimwiz/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'zimwizdotcom',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/14461370@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/zimwiz/',
-  'markers': 514,
-  'photos': 685
+  'markers': 515,
+  'photos': 686
 }
