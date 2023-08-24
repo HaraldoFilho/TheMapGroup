3c837396e0001df664461403f6fb4e3508b4dfb4
The Map Group <admin@the-map-group.pictures>
2023-08-24 02:18:10 -0300

    [auto] Updated map for member 'CrËOS Photographie'

--- people/scfoto62/countries.py
+++ people/scfoto62/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'DE': ['Germany', 2, 2],
-  'FR': ['France', 1654, 2655],
-  'BE': ['Belgium', 69, 140]
+  'FR': ['France', 1655, 2656],
+  'BE': ['Belgium', 69, 140],
+  'DE': ['Germany', 2, 2]
 }
