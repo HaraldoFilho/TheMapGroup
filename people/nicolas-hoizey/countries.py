e2ef5360ad0c746650d6c00d241c8721442fe1da
The Map Group <admin@the-map-group.pictures>
2023-08-29 02:40:43 -0300

    [auto] Updated map for member 'Nicolas Hoizey'

---
+++ people/nicolas-hoizey/countries.py
@@ -0,0 +1,20 @@
+countries_dict = {
+  'GB': ['United Kingdom', 17, 17],
+  'BE': ['Belgium', 2, 2],
+  'FR': ['France', 99, 129],
+  'US': ['United States of America', 14, 15],
+  'PT': ['Portugal', 5, 5],
+  'EG': ['Egypt', 6, 7],
+  'ES': ['Spain', 8, 9],
+  'CA': ['Canada', 1, 1],
+  'GE': ['Georgia', 15, 15],
+  'MA': ['Morocco', 6, 6],
+  'GR': ['Greece', 1, 1],
+  'SE': ['Sweden', 6, 8],
+  'DO': ['Dominican Republic', 1, 1],
+  'IT': ['Italy', 13, 15],
+  'KE': ['Kenya', 73, 83],
+  'CN': ['South China Sea Islands (disputed)', 19, 20],
+  'VA': ['Vatican City', 2, 2],
+  'NL': ['Netherlands', 14, 17]
+}
