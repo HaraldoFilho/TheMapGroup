d7bdc8e9590c5bbd564cc4ac30feed54fa412d12
The Map Group <admin@the-map-group.pictures>
2023-09-03 02:49:06 -0300

    [auto] Updated map for member 'christopherferguson'

---
+++ people/107755237@N08/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '107755237@N08',
+  'alias': '107755237@N08',
+  'name': 'Christopher Ferguson',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/107755237@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/107755237@N08/',
+  'markers': 0,
+  'photos': 0
+}
