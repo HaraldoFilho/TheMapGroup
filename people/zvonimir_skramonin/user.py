6ac46ec25071ce0cd1a996edec023424489c2eb4
The Map Group <admin@the-map-group.pictures>
2023-09-04 02:18:22 -0300

    [auto] Updated map for member 'Zvonimir S.'

--- people/zvonimir_skramonin/user.py
+++ people/zvonimir_skramonin/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Zvonimir Skramončin',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/185660941@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/zvonimir_skramonin/',
-  'markers': 300,
-  'photos': 302
+  'markers': 301,
+  'photos': 303
 }
