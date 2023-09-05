9f57cc367c7508c7f37466d357c9cba166cc1c22
The Map Group <admin@the-map-group.pictures>
2023-09-05 02:58:10 -0300

    [auto] Updated map for member 'diffendale'

---
+++ people/dandiffendale/countries.py
@@ -0,0 +1,19 @@
+countries_dict = {
+  'PT': ['Portugal', 50, 184],
+  'RO': ['Romania', 4, 48],
+  'GR': ['Greece', 621, 3809],
+  'DE': ['Germany', 15, 127],
+  'TR': ['Turkey', 20, 194],
+  'VA': ['Vatican City', 5, 51],
+  'JO': ['Jordan', 4, 24],
+  'GB': ['United Kingdom', 24, 71],
+  'IE': ['Ireland', 4, 19],
+  'US': ['United States of America', 213, 964],
+  'AT': ['Austria', 5, 97],
+  'BG': ['Bulgaria', 12, 106],
+  'FR': ['France', 2, 2],
+  'IT': ['Italy', 583, 4370],
+  'CA': ['Canada', 1, 18],
+  'MA': ['Morocco', 119, 592],
+  'EG': ['Egypt', 34, 397]
+}
---
+++ people/dandiffendale/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '7945858@N08',
+  'alias': 'dandiffendale',
+  'name': 'Dan Diffendale',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/7945858@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/dandiffendale/',
+  'markers': 1716,
+  'photos': 11073
+}
