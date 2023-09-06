e67ef45ecab44eba7be76124beea5a9f14687f87
The Map Group <admin@the-map-group.pictures>
2023-09-06 02:01:22 -0300

    [auto] Updated map for member 'Tor Einar Andersen'

--- people/30717887@N05/countries.py
+++ people/30717887@N05/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
+  'RU': ['Russia', 9, 18],
+  'FR': ['France', 9, 9],
   'FI': ['Finland', 4, 11],
   'DE': ['Germany', 9, 9],
-  'DK': ['Denmark', 22, 28],
-  'FR': ['France', 9, 9],
-  'SE': ['Sweden', 15, 38],
-  'ES': ['Spain', 30, 39],
-  'NL': ['Netherlands', 1, 1],
-  'GB': ['United Kingdom', 19, 37],
-  'IT': ['Italy', 33, 38],
-  'NO': ['Norway', 194, 236],
   'EE': ['Estonia', 2, 2],
-  'RU': ['Russia', 9, 18]
+  'GB': ['United Kingdom', 19, 37],
+  'NO': ['Norway', 194, 237],
+  'IT': ['Italy', 33, 38],
+  'DK': ['Denmark', 22, 28],
+  'SE': ['Sweden', 15, 38],
+  'NL': ['Netherlands', 1, 1],
+  'ES': ['Spain', 30, 39]
 }
