32d2723c1314dd497cf51864738fa04d7f1eb787
The Map Group <admin@the-map-group.pictures>
2023-09-07 03:54:04 -0300

    [auto] Updated map for member 'enrico377'

--- people/enricoveneruso/user.py
+++ people/enricoveneruso/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Enrico Veneruso',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/193758259@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/enricoveneruso/',
-  'markers': 80,
-  'photos': 83
+  'markers': 81,
+  'photos': 84
 }
