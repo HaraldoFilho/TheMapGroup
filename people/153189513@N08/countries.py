b2ed2bd232af4659bc35a82b34b02d4dcff5a04d
The Map Group <admin@the-map-group.pictures>
2023-09-08 03:14:47 -0300

    Removed member '153189513@N08'

--- people/153189513@N08/countries.py
+++
@@ -1,7 +0,0 @@
-countries_dict = {
-  'AZ': ['Azerbaijan', 10, 10],
-  'RU': ['Russia', 657, 745],
-  'BY': ['Belarus', 12, 14],
-  'UZ': ['Uzbekistan', 4, 7],
-  'KZ': ['Kazakhstan', 21, 22]
-}
--- people/153189513@N08/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '153189513@N08',
-  'alias': '153189513@N08',
-  'name': 'Pavel',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/153189513@N08_r.jpg',
-  'url': 'https://www.flickr.com/photos/153189513@N08/',
-  'markers': 704,
-  'photos': 798
-}
