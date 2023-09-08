4462e1695bde51bb7d52cf5943a99ddcaf97d79c
The Map Group <admin@the-map-group.pictures>
2023-09-08 02:22:27 -0300

    [auto] Updated map for member 'Ted's photos - Stand With Ukraine'

--- people/time-to-look/user.py
+++ people/time-to-look/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Ted McGrath',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/90955804@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/time-to-look/',
-  'markers': 12236,
-  'photos': 14283
+  'markers': 12242,
+  'photos': 14289
 }
