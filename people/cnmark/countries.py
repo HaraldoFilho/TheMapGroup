06656a0b130ef023903fb6321677a02a0e583efa
The Map Group <admin@the-map-group.pictures>
2023-09-08 03:01:36 -0300

    [auto] Updated map for member 'cnmark'

--- people/cnmark/countries.py
+++ people/cnmark/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'HU': ['Hungary', 23, 24],
-  'DE': ['Germany', 1556, 2366],
-  'SE': ['Sweden', 1, 1],
-  'CH': ['Switzerland', 1, 1],
-  'GB': ['United Kingdom', 3, 3],
-  'IT': ['Italy', 35, 51],
-  'CA': ['Canada', 20, 21],
-  'AE': ['United Arab Emirates', 7, 11],
-  'CN': ['South China Sea Islands (disputed)', 1826, 2223],
   'TH': ['Thailand', 2, 2],
-  'JP': ['Japan', 11, 12],
   'FR': ['France', 31, 34],
-  'US': ['United States', 6, 6],
-  'AT': ['Austria', 16, 18]
+  'DE': ['Germany', 1558, 2368],
+  'HU': ['Hungary', 23, 24],
+  'GB': ['United Kingdom', 3, 3],
+  'CA': ['Canada', 20, 21],
+  'IT': ['Italy', 35, 51],
+  'SE': ['Sweden', 1, 1],
+  'CN': ['South China Sea Islands (disputed)', 1825, 2222],
+  'AT': ['Austria', 16, 18],
+  'AE': ['United Arab Emirates', 7, 11],
+  'CH': ['Switzerland', 1, 1],
+  'JP': ['Japan', 11, 12],
+  'US': ['United States', 6, 6]
 }
--- people/cnmark/user.py
+++ people/cnmark/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Markus Bahlmann',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/48646584@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/cnmark/',
-  'markers': 3538,
-  'photos': 4773
+  'markers': 3539,
+  'photos': 4774
 }
