75fc1252cbd40e0171742c64e76b8114779d172e
The Map Group <admin@the-map-group.pictures>
2023-09-10 02:31:29 -0300

    [auto] Updated map for member 'Faillace360'

--- people/faillace360/countries.py
+++ people/faillace360/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'AT': ['Austria', 3, 3],
-  'GB': ['United Kingdom', 3, 3],
-  'AR': ['Argentina', 7, 7],
   'CZ': ['Czechia', 1, 1],
-  'BR': ['Brazil', 114, 118],
-  'PE': ['Peru', 4, 4],
   'IT': ['Italy', 3, 3],
+  'PE': ['Peru', 4, 4],
   'CH': ['Switzerland', 2, 2],
-  'DE': ['Germany', 1, 1]
+  'DE': ['Germany', 1, 1],
+  'BR': ['Brazil', 115, 119],
+  'AR': ['Argentina', 7, 7],
+  'AT': ['Austria', 3, 3],
+  'GB': ['United Kingdom', 3, 3]
 }
