bfa71f2684dc5d84d47fb4d3f722b239e90874b7
The Map Group <admin@the-map-group.pictures>
2023-09-13 02:30:37 -0300

    [auto] Updated map for member 'Steve O'Day'

--- people/22875671@N04/countries.py
+++ people/22875671@N04/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
   'IT': ['Italy', 3, 3],
-  'FR': ['France', 14, 15],
-  'US': ['United States of America', 244, 268]
+  'US': ['United States of America', 244, 269],
+  'FR': ['France', 14, 15]
 }
