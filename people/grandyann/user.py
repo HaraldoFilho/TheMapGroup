cceb427fafcdac44dbda285f891ae15b75c84c85
The Map Group <admin@the-map-group.pictures>
2023-09-20 03:19:12 -0300

    [auto] Updated map for member 'grand Yann'

--- people/grandyann/user.py
+++ people/grandyann/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Yann Pinczon du Sel',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/24482362@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/grandyann/',
-  'markers': 872,
-  'photos': 1154
+  'markers': 873,
+  'photos': 1155
 }
