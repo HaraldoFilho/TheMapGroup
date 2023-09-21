98b41196ffb835739d88c870388429378240ceea
The Map Group <admin@the-map-group.pictures>
2023-09-21 02:48:43 -0300

    Removed member 'zvonimir_skramonin'

--- people/zvonimir_skramonin/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '185660941@N08',
-  'alias': 'zvonimir_skramonin',
-  'name': 'Zvonimir Skramončin',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/185660941@N08_r.jpg',
-  'url': 'https://www.flickr.com/photos/zvonimir_skramonin/',
-  'markers': 301,
-  'photos': 303
-}
