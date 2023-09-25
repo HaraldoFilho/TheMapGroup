19f303248940f6c0518ca179f281b675459c0e5d
The Map Group <admin@the-map-group.pictures>
2023-09-25 02:23:54 -0300

    [auto] Updated map for member '( Daves.Pixel ) Photography'

---
+++ people/dropiso/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '170935808@N08',
+  'alias': 'dropiso',
+  'name': '( Daves.Pixel ) Photography',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/170935808@N08_r.jpg',
+  'url': 'https://www.flickr.com/photos/dropiso/',
+  'markers': 0,
+  'photos': 0
+}
