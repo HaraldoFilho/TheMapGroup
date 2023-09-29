e4ad0831081ab9d29145a54568d469a99bc9f9fa
The Map Group <admin@the-map-group.pictures>
2023-09-29 02:24:49 -0300

    [auto] Updated map for member 'enrico377'

--- people/enricoveneruso/countries.py
+++ people/enricoveneruso/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'NL': ['Netherlands', 1, 1],
-  'US': ['United States of America', 14, 14],
+  'IT': ['Italy', 43, 46],
   'GR': ['Greece', 10, 10],
-  'IE': ['Ireland', 9, 9],
-  'IT': ['Italy', 42, 45],
+  'NL': ['Netherlands', 1, 1],
   'FR': ['France', 2, 2],
+  'US': ['United States of America', 15, 15],
+  'IE': ['Ireland', 9, 9],
   'ES': ['Spain', 7, 7],
   'GB': ['United Kingdom', 4, 4]
 }
--- people/enricoveneruso/user.py
+++ people/enricoveneruso/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Enrico Veneruso',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/193758259@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/enricoveneruso/',
-  'markers': 89,
-  'photos': 92
+  'markers': 91,
+  'photos': 94
 }
