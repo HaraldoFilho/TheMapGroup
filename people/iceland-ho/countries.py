61ee709d1415a5badb5b7e0789ef0104bc79a1f3
The Map Group <admin@the-map-group.pictures>
2023-10-02 02:23:05 -0300

    [auto] Updated map for member 'hó'

--- people/iceland-ho/countries.py
+++ people/iceland-ho/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'GB': ['United Kingdom', 13, 13],
   'UY': ['Uruguay', 6, 6],
-  'SE': ['Sweden', 1, 1],
-  'IS': ['Iceland', 2704, 2706],
-  'MA': ['Morocco', 11, 11],
-  'MD': ['Moldova', 2, 2],
-  'ES': ['Spain', 5, 5],
-  'US': ['United States of America', 8, 8],
-  'CN': ['South China Sea Islands (disputed)', 16, 16],
   'JP': ['Japan', 6, 6],
+  'SE': ['Sweden', 1, 1],
+  'CN': ['South China Sea Islands (disputed)', 16, 16],
+  'IS': ['Iceland', 2705, 2707],
+  'MD': ['Moldova', 2, 2],
   'FO': ['Faroe Islands', 9, 9],
-  'GH': ['Ghana', 7, 7]
+  'MA': ['Morocco', 11, 11],
+  'GH': ['Ghana', 7, 7],
+  'ES': ['Spain', 5, 5],
+  'GB': ['United Kingdom', 13, 13],
+  'US': ['United States of America', 8, 8]
 }
--- people/iceland-ho/user.py
+++ people/iceland-ho/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Hugi Ólafsson',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/59509464@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/iceland-ho/',
-  'markers': 2788,
-  'photos': 2790
+  'markers': 2789,
+  'photos': 2791
 }
