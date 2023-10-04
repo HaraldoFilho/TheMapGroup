d316968f60cf47380aff54f7b935825cb076a747
The Map Group <admin@the-map-group.pictures>
2023-10-04 02:23:51 -0300

    [auto] Updated map for member 'PeterThoeny'

--- people/peterthoeny/countries.py
+++ people/peterthoeny/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'DE': ['Germany', 2, 3],
-  'IS': ['Iceland', 66, 97],
-  'AT': ['Austria', 5, 8],
-  'IT': ['Italy', 30, 43],
-  'JP': ['Japan', 73, 89],
-  'GL': ['Greenland', 4, 4],
-  'US': ['United States of America', 1571, 3055],
   'CA': ['Canada', 2, 2],
-  'EE': ['Estonia', 1, 1],
-  'CN': ['South China Sea Islands (disputed)', 71, 95],
+  'GL': ['Greenland', 4, 4],
+  'US': ['United States of America', 1571, 3057],
   'UA': ['Ukraine', 1, 1],
   'NO': ['Norway', 1, 1],
-  'CH': ['Switzerland', 213, 372],
   'LI': ['Liechtenstein', 4, 5],
-  'GB': ['United Kingdom', 17, 34]
+  'EE': ['Estonia', 1, 1],
+  'DE': ['Germany', 2, 3],
+  'IT': ['Italy', 30, 43],
+  'JP': ['Japan', 73, 89],
+  'CN': ['South China Sea Islands (disputed)', 71, 95],
+  'GB': ['United Kingdom', 17, 34],
+  'CH': ['Switzerland', 213, 372],
+  'IS': ['Iceland', 66, 97],
+  'AT': ['Austria', 5, 8]
 }
