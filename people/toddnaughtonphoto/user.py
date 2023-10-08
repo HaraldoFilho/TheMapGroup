98194c84d26bfd0f3e5915dfb4224810e5a1669a
The Map Group <admin@the-map-group.pictures>
2023-10-08 02:49:40 -0300

    Removed member 'toddnaughtonphoto'

--- people/toddnaughtonphoto/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '197647139@N07',
-  'alias': 'toddnaughtonphoto',
-  'name': 'Photos.By.Todd',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/197647139@N07_r.jpg',
-  'url': 'https://www.flickr.com/photos/toddnaughtonphoto/',
-  'markers': 1065,
-  'photos': 1554
-}
