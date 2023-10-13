7aabace88125a1c2535058b140ae77924d0af285
The Map Group <admin@the-map-group.pictures>
2023-10-13 02:16:25 -0300

    [auto] Updated map for member 'hó'

--- people/iceland-ho/countries.py
+++ people/iceland-ho/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'JP': ['Japan', 6, 6],
-  'IS': ['Iceland', 2706, 2708],
   'CN': ['South China Sea Islands (disputed)', 16, 16],
   'GB': ['United Kingdom', 13, 13],
+  'US': ['United States of America', 13, 13],
   'FO': ['Faroe Islands', 9, 9],
-  'UY': ['Uruguay', 6, 6],
-  'GH': ['Ghana', 7, 7],
-  'MD': ['Moldova', 2, 2],
   'MA': ['Morocco', 11, 11],
+  'JP': ['Japan', 6, 6],
+  'IS': ['Iceland', 2706, 2708],
+  'GH': ['Ghana', 7, 7],
   'ES': ['Spain', 5, 5],
+  'MD': ['Moldova', 2, 2],
   'SE': ['Sweden', 1, 1],
-  'US': ['United States of America', 12, 12]
+  'UY': ['Uruguay', 6, 6]
 }
--- people/iceland-ho/user.py
+++ people/iceland-ho/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Hugi Ólafsson',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/59509464@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/iceland-ho/',
-  'markers': 2794,
-  'photos': 2796
+  'markers': 2795,
+  'photos': 2797
 }
