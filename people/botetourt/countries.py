e68aeeb6c4b9114f51027dc9e6b37dd495985120
The Map Group <admin@the-map-group.pictures>
2023-10-16 02:26:12 -0300

    [auto] Updated map for member 'Brunswick Forge'

--- people/botetourt/countries.py
+++ people/botetourt/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'MX': ['Mexico', 8, 10],
-  'IT': ['Italy', 349, 477],
-  'US': ['United States of America', 3235, 5775],
-  'KE': ['Kenya', 137, 200],
-  'TR': ['Turkey', 1, 1],
-  'QA': ['Qatar', 3, 3],
-  'VA': ['Vatican City', 16, 19],
-  'GB': ['United Kingdom', 51, 94],
-  'TZ': ['Tanzania', 298, 882],
   'CA': ['Canada', 10, 13],
-  'NL': ['Netherlands', 1, 1]
+  'VA': ['Vatican City', 16, 19],
+  'US': ['United States of America', 3235, 5775],
+  'NL': ['Netherlands', 1, 1],
+  'TZ': ['Tanzania', 298, 882],
+  'IT': ['Italy', 349, 477],
+  'QA': ['Qatar', 3, 3],
+  'KE': ['Kenya', 137, 201],
+  'TR': ['Turkey', 1, 1],
+  'MX': ['Mexico', 8, 10],
+  'GB': ['United Kingdom', 51, 94]
 }
