fa5d5fa2cf41b24be05ddbb6d5e91cf1f85b28b3
The Map Group <admin@the-map-group.pictures>
2023-10-17 02:13:52 -0300

    [auto] Updated map for member 'Dmitry Kolesnikov'

--- people/dmitry_kolesnikov/countries.py
+++ people/dmitry_kolesnikov/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
   'BY': ['Belarus', 3, 3],
   'RU': ['Russia', 79, 166],
-  'AM': ['Armenia', 9, 10],
-  'CL': ['Chile', 4, 4]
+  'CL': ['Chile', 4, 4],
+  'AM': ['Armenia', 16, 17]
 }
--- people/dmitry_kolesnikov/user.py
+++ people/dmitry_kolesnikov/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Dmitry Kolesnikov',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/68420538@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/dmitry_kolesnikov/',
-  'markers': 95,
-  'photos': 183
+  'markers': 102,
+  'photos': 190
 }
