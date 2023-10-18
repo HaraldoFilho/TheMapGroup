a09023d6c6598b006a46cf8c45966d7ae2ec23d5
The Map Group <admin@the-map-group.pictures>
2023-10-18 02:11:59 -0300

    [auto] Updated map for member 'Fernando Forniés Revuelta'

--- people/darth-fer-75/user.py
+++ people/darth-fer-75/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Fernando Forniés Revuelta',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/30148069@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/darth-fer-75/',
-  'markers': 1773,
-  'photos': 11042
+  'markers': 1780,
+  'photos': 11069
 }
