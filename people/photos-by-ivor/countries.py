e464e53a3392eaae635333779d343046f3dedba0
The Map Group <admin@the-map-group.pictures>
2023-10-20 02:37:29 -0300

    [auto] Updated map for member 'Ivor Jones'

--- people/photos-by-ivor/countries.py
+++ people/photos-by-ivor/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'AU': ['Australia', 1248, 24784],
-  'NZ': ['New Zealand', 36, 1902],
-  'GB': ['United Kingdom', 1, 1],
   'FR': ['France', 4, 70],
-  'US': ['United States', 1, 156],
-  'CA': ['Canada', 22, 808],
   'FJ': ['Fiji', 4, 147],
-  'VU': ['Vanuatu', 5, 174]
+  'CA': ['Canada', 22, 808],
+  'GB': ['United Kingdom', 1, 1],
+  'VU': ['Vanuatu', 5, 174],
+  'NZ': ['New Zealand', 36, 1902],
+  'US': ['United States', 1, 156],
+  'AU': ['Australia', 1249, 24856]
 }
--- people/photos-by-ivor/user.py
+++ people/photos-by-ivor/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Ivor Jones',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/74974888@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/photos-by-ivor/',
-  'markers': 1321,
-  'photos': 28042
+  'markers': 1322,
+  'photos': 28114
 }
