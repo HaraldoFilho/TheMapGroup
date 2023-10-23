9bff5ab12339e8ac5f9f53802174ba39cc1f926d
The Map Group <admin@the-map-group.pictures>
2023-10-23 02:45:11 -0300

    Removed member 'dmitry_kolesnikov'

--- people/dmitry_kolesnikov/countries.py
+++
@@ -1,6 +0,0 @@
-countries_dict = {
-  'AM': ['Armenia', 17, 18],
-  'BY': ['Belarus', 3, 3],
-  'RU': ['Russia', 79, 166],
-  'CL': ['Chile', 4, 4]
-}
--- people/dmitry_kolesnikov/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '68420538@N03',
-  'alias': 'dmitry_kolesnikov',
-  'name': 'Dmitry Kolesnikov',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/68420538@N03_r.jpg',
-  'url': 'https://www.flickr.com/photos/dmitry_kolesnikov/',
-  'markers': 103,
-  'photos': 191
-}
