3df23187e453d79aa1d1d17e78e5c3681d4f11f6
The Map Group <admin@the-map-group.pictures>
2023-10-23 02:16:04 -0300

    [auto] Updated map for member 'InnAtElmwood'

--- people/12513181@N07/user.py
+++ people/12513181@N07/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'K & R',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/12513181@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/12513181@N07/',
-  'markers': 528,
-  'photos': 691
+  'markers': 529,
+  'photos': 692
 }
