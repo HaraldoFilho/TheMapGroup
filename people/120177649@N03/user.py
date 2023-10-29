e9ad97e31f5881bc3ec288fdaa53e8d7e18cf718
The Map Group <admin@the-map-group.pictures>
2023-10-29 02:10:11 -0300

    [auto] Updated map for member 'mpfau64'

--- people/120177649@N03/user.py
+++ people/120177649@N03/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/120177649@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/120177649@N03/',
   'markers': 1221,
-  'photos': 7018
+  'photos': 7026
 }
