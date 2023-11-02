cf81bc9fb221bc090536157ea3fc48e8fa6865fb
The Map Group <admin@the-map-group.pictures>
2023-11-02 03:20:48 -0300

    [auto] Updated map for member 'cnmark'

--- people/cnmark/countries.py
+++ people/cnmark/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'JP': ['Japan', 11, 12],
-  'IT': ['Italy', 35, 51],
-  'CA': ['Canada', 20, 21],
-  'FR': ['France', 31, 34],
   'CH': ['Switzerland', 1, 1],
-  'HU': ['Hungary', 23, 24],
+  'CA': ['Canada', 20, 21],
   'TH': ['Thailand', 2, 2],
+  'IT': ['Italy', 35, 51],
   'SE': ['Sweden', 1, 1],
-  'GB': ['United Kingdom', 3, 3],
-  'CN': ['South China Sea Islands (disputed)', 1813, 2206],
   'AE': ['United Arab Emirates', 7, 11],
-  'DE': ['Germany', 1593, 2417],
+  'GB': ['United Kingdom', 3, 3],
+  'FR': ['France', 31, 34],
+  'JP': ['Japan', 11, 12],
+  'AT': ['Austria', 16, 18],
   'US': ['United States', 6, 6],
-  'AT': ['Austria', 16, 18]
+  'HU': ['Hungary', 23, 24],
+  'DE': ['Germany', 1594, 2419],
+  'CN': ['South China Sea Islands (disputed)', 1807, 2200]
 }
