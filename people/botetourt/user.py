81f0ddb6bd3f7114058906bdd567db31538e0a57
The Map Group <admin@the-map-group.pictures>
2023-11-05 02:25:22 -0300

    [auto] Updated map for member 'Brunswick Forge'

--- people/botetourt/user.py
+++ people/botetourt/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'John McNeely Hudson',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/82487858@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/botetourt/',
-  'markers': 4172,
-  'photos': 7579
+  'markers': 4174,
+  'photos': 7582
 }
