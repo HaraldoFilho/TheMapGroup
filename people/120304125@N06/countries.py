0968c759bf5353f5ba2355850e36f6d3f994cf9e
The Map Group <admin@the-map-group.pictures>
2023-11-08 02:22:55 -0300

    [auto] Updated map for member 'karsaiag'

--- people/120304125@N06/countries.py
+++ people/120304125@N06/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'AT': ['Austria', 6, 6],
-  'IL': ['Israel', 3, 3],
-  'HR': ['Croatia', 16, 16],
   'RO': ['Romania', 3, 3],
-  'GR': ['Greece', 20, 20],
-  'SK': ['Slovakia', 10, 10],
-  'ES': ['Spain', 7, 7],
+  'HR': ['Croatia', 16, 16],
+  'AT': ['Austria', 6, 6],
+  'HU': ['Hungary', 278, 280],
   'DE': ['Germany', 1, 1],
-  'HU': ['Hungary', 277, 279]
+  'SK': ['Slovakia', 10, 10],
+  'GR': ['Greece', 20, 20],
+  'IL': ['Israel', 3, 3],
+  'ES': ['Spain', 7, 7]
 }
