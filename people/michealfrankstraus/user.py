fa20e0da752fcdbac2358fefeea21b32502bcb1d
The Map Group <admin@the-map-group.pictures>
2023-11-08 02:23:36 -0300

    [auto] Updated map for member 'mstraus52'

--- people/michealfrankstraus/user.py
+++ people/michealfrankstraus/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Micheal Straus',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/188257341@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/michealfrankstraus/',
-  'markers': 1264,
-  'photos': 1358
+  'markers': 1265,
+  'photos': 1359
 }
