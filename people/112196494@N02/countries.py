5fd46c8c625d5356d67aa139a04c460c7c48518a
The Map Group <admin@the-map-group.pictures>
2023-11-12 02:28:27 -0300

    [auto] Updated map for member 'thunderbird-72'

--- people/112196494@N02/countries.py
+++ people/112196494@N02/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'DE': ['Germany', 801, 1581],
-  'FR': ['France', 223, 519],
-  'ES': ['Spain', 4, 14],
-  'IT': ['Italy', 93, 148],
-  'BE': ['Belgium', 6, 10],
   'SI': ['Slovenia', 71, 114],
-  'NL': ['Netherlands', 6, 9],
-  'AT': ['Austria', 120, 170],
+  'ES': ['Spain', 4, 14],
   'CZ': ['Czechia', 9, 11],
+  'IT': ['Italy', 93, 148],
+  'AT': ['Austria', 120, 170],
+  'DE': ['Germany', 802, 1582],
   'HR': ['Croatia', 14, 16],
-  'LU': ['Luxembourg', 39, 70],
+  'FR': ['France', 223, 519],
+  'PT': ['Portugal', 19, 28],
+  'NL': ['Netherlands', 6, 9],
+  'BE': ['Belgium', 6, 10],
   'CH': ['Switzerland', 4, 4],
-  'PT': ['Portugal', 19, 28]
+  'LU': ['Luxembourg', 39, 70]
 }
--- people/112196494@N02/user.py
+++ people/112196494@N02/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'thunderbird-72',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/112196494@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/112196494@N02/',
-  'markers': 1409,
-  'photos': 2694
+  'markers': 1410,
+  'photos': 2695
 }
