aef1732fc8e38221c1ad2e42917c79e17eac70f2
The Map Group <admin@the-map-group.pictures>
2023-11-12 02:15:05 -0300

    [auto] Updated map for member 'tquist24'

--- people/123895834@N08/countries.py
+++ people/123895834@N08/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'AI': ['Anguilla', 7, 9],
-  'CU': ['Cuba', 8, 9],
-  'IT': ['Italy', 36, 40],
-  'CA': ['Canada', 10, 12],
-  'GB': ['United Kingdom', 96, 113],
-  'BS': ['Bahamas', 12, 12],
   'PR': ['Puerto Rico', 4, 4],
-  'US': ['United States of America', 2874, 3828],
-  'KY': ['Cayman Islands', 5, 5],
   'NL': ['The Netherlands', 19, 21],
-  'BZ': ['Belize', 6, 6],
   'DO': ['Dominican Republic', 4, 33],
-  'MX': ['Mexico', 12, 13]
+  'AI': ['Anguilla', 7, 9],
+  'MX': ['Mexico', 12, 13],
+  'CA': ['Canada', 10, 12],
+  'BZ': ['Belize', 6, 6],
+  'IT': ['Italy', 37, 41],
+  'GB': ['United Kingdom', 96, 113],
+  'CU': ['Cuba', 8, 9],
+  'BS': ['Bahamas', 12, 12],
+  'KY': ['Cayman Islands', 5, 5],
+  'US': ['United States of America', 2874, 3828]
 }
--- people/123895834@N08/user.py
+++ people/123895834@N08/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'tquist24',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/123895834@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/123895834@N08/',
-  'markers': 3094,
-  'photos': 4107
+  'markers': 3095,
+  'photos': 4108
 }
