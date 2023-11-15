eaa5baff9f5b9c10dd362fee4506a5031101b063
The Map Group <admin@the-map-group.pictures>
2023-11-15 03:30:36 -0300

    [auto] Updated map for member 'Tatters ✾'

---
+++ people/tgerus/countries.py
@@ -0,0 +1,16 @@
+countries_dict = {
+  'RU': ['Russia', 560, 728],
+  'IR': ['Iran', 1, 1],
+  'KR': ['South Korea', 4, 6],
+  'TR': ['Turkey', 1, 1],
+  'SG': ['Singapore', 1, 2],
+  'US': ['United States of America', 8, 10],
+  'NZ': ['New Zealand', 126, 186],
+  'GB': ['United Kingdom', 237, 338],
+  'TH': ['Thailand', 18, 23],
+  'AU': ['Australia', 2489, 3491],
+  'NR': ['Nauru', 2, 2],
+  'CN': ['South China Sea Islands (disputed)', 15, 16],
+  'AE': ['United Arab Emirates', 2, 2],
+  'JP': ['Japan', 175, 205]
+}
---
+++ people/tgerus/user.py
@@ -0,0 +1,9 @@
+user_info = {
+  'id': '62938898@N00',
+  'alias': 'tgerus',
+  'name': 'Tatters ✾',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/62938898@N00_r.jpg',
+  'url': 'https://www.flickr.com/photos/tgerus/',
+  'markers': 3639,
+  'photos': 5011
+}
