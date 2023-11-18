647697149da2fe28da241afe50107f9e10f0952d
The Map Group <admin@the-map-group.pictures>
2023-11-18 02:44:37 -0300

    [auto] Updated map for member 'donnebryant'

--- people/donnebryant/user.py
+++ people/donnebryant/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'donnebryant',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/197365363@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/donnebryant/',
-  'markers': 161,
-  'photos': 3040
+  'markers': 173,
+  'photos': 3207
 }
