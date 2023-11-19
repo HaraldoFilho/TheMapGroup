e19f01bf93405eed0af7c0acef5aa52f979499e6
The Map Group <admin@the-map-group.pictures>
2023-11-19 02:25:46 -0300

    [auto] Updated map for member 'Ron Fisher'

--- people/train-pix/countries.py
+++ people/train-pix/countries.py
@@ -1,19 +1,19 @@
 countries_dict = {
+  'GB': ['United Kingdom', 8953, 19120],
   'HR': ['Croatia', 5, 9],
-  'RS': ['Serbia', 3, 3],
-  'CA': ['Canada', 1, 2],
-  'GB': ['United Kingdom', 8953, 19118],
-  'ES': ['Spain', 1, 3],
-  'AT': ['Austria', 28, 28],
+  'IM': ['Isle of Man', 20, 55],
+  'ZA': ['South Africa', 311, 320],
+  'IT': ['Italy', 23, 41],
   'FR': ['France', 440, 648],
+  'BA': ['Bosnia and Herzegovina', 12, 20],
   'MC': ['Monaco', 3, 3],
   'DE': ['Germany', 316, 359],
   'BE': ['Belgium', 1, 1],
+  'CA': ['Canada', 1, 2],
+  'AT': ['Austria', 28, 28],
   'CH': ['Switzerland', 1163, 1871],
+  'ES': ['Spain', 1, 3],
   'SZ': ['Eswatini', 6, 7],
-  'BA': ['Bosnia and Herzegovina', 12, 20],
-  'ZA': ['South Africa', 311, 320],
   'HU': ['Hungary', 19, 28],
-  'IT': ['Italy', 23, 41],
-  'IM': ['Isle of Man', 20, 55]
+  'RS': ['Serbia', 3, 3]
 }
