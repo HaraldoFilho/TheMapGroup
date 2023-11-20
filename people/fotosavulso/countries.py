f64a0fa2764b6130fd3d83519c8e6409986f125c
The Map Group <admin@the-map-group.pictures>
2023-11-20 02:23:38 -0300

    [auto] Updated map for member 'Fotos Avulso'

--- people/fotosavulso/countries.py
+++ people/fotosavulso/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'TL': ['East Timor', 1, 1],
-  'CV': ['Cape Verde', 15, 15],
-  'AO': ['Angola', 1, 1],
-  'FR': ['France', 61, 63],
-  'AT': ['Austria', 1, 1],
-  'CN': ['South China Sea Islands (disputed)', 1, 2],
-  'ST': ['São Tomé and Príncipe', 103, 111],
-  'ES': ['Spain', 20, 22],
+  'GW': ['Guinea-Bissau', 1, 1],
+  'CV': ['Cape Verde', 16, 16],
   'AD': ['Andorra', 2, 2],
+  'AO': ['Angola', 1, 1],
+  'ES': ['Spain', 20, 22],
+  'AT': ['Austria', 1, 1],
+  'FR': ['France', 61, 63],
   'US': ['United States of America', 3, 3],
   'IT': ['Italy', 1, 1],
-  'GW': ['Guinea-Bissau', 1, 1],
-  'PT': ['Portugal', 344, 372]
+  'PT': ['Portugal', 344, 372],
+  'ST': ['São Tomé and Príncipe', 103, 111],
+  'CN': ['South China Sea Islands (disputed)', 1, 2],
+  'TL': ['East Timor', 1, 1]
 }
--- people/fotosavulso/user.py
+++ people/fotosavulso/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Fotos Avulso',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/69623157@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/fotosavulso/',
-  'markers': 554,
-  'photos': 595
+  'markers': 555,
+  'photos': 596
 }
