7d120b1c85ec772efd9af71bf1acbdea79454ee8
The Map Group <admin@the-map-group.pictures>
2023-11-23 02:38:50 -0300

    [auto] Updated map for member 'giosuèbolis'

--- people/146445851@N04/countries.py
+++ people/146445851@N04/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'IT': ['Italy', 168, 212],
-  'NE': ['Niger', 2, 3],
-  'HU': ['Hungary', 2, 2],
-  'DZ': ['Algeria', 2, 2],
-  'UA': ['Ukraine', 3, 3],
+  'IT': ['Italy', 169, 213],
   'ML': ['Mali', 5, 6],
+  'FR': ['France', 8, 9],
+  'HU': ['Hungary', 2, 2],
+  'UA': ['Ukraine', 3, 3],
   'CH': ['Switzerland', 13, 37],
-  'FR': ['France', 8, 9]
+  'NE': ['Niger', 2, 3],
+  'DZ': ['Algeria', 2, 2]
 }
--- people/146445851@N04/user.py
+++ people/146445851@N04/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Giosuè Bolis',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/146445851@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/146445851@N04/',
-  'markers': 203,
-  'photos': 274
+  'markers': 204,
+  'photos': 275
 }
