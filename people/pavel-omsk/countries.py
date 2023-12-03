ddb53eca5b382927ec1964e73fb2edbbb3911396
The Map Group <admin@the-map-group.pictures>
2023-12-03 02:49:29 -0300

    [auto] Updated map for member 'katlamsib'

--- people/pavel-omsk/countries.py
+++ people/pavel-omsk/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'BY': ['Belarus', 12, 14],
-  'KZ': ['Kazakhstan', 21, 22],
-  'UZ': ['Uzbekistan', 4, 7],
+  'BY': ['Belarus', 11, 13],
   'AZ': ['Azerbaijan', 10, 10],
-  'RU': ['Russia', 665, 753]
+  'RU': ['Russia', 666, 754],
+  'KZ': ['Kazakhstan', 20, 21],
+  'UZ': ['Uzbekistan', 4, 7]
 }
