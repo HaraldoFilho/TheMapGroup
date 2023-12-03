9717f264448fe57c477ffb010040f0c3efbab73d
The Map Group <admin@the-map-group.pictures>
2023-12-03 02:52:56 -0300

    [auto] Updated map for member 'Adam Swaine'

--- people/adam_swaine/countries.py
+++ people/adam_swaine/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'ES': ['Spain', 73, 104],
   'FR': ['France', 154, 370],
   'NO': ['Norway', 63, 69],
-  'GB': ['United Kingdom', 15026, 22593],
+  'ES': ['Spain', 73, 104],
+  'GB': ['United Kingdom', 15030, 22598],
   'MC': ['Monaco', 7, 16]
 }
