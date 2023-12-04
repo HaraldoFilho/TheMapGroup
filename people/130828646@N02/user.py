33b54aa34cb0f8fb437073e05e03274b062e448f
The Map Group <admin@the-map-group.pictures>
2023-12-04 02:13:52 -0300

    [auto] Updated map for member 'philippeguillot21'

--- people/130828646@N02/user.py
+++ people/130828646@N02/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Philippe Guillot',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/130828646@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/130828646@N02/',
-  'markers': 14185,
-  'photos': 35100
+  'markers': 14192,
+  'photos': 35130
 }
