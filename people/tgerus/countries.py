7a5db57da598d59e20e4e1c2ad04426274405775
The Map Group <admin@the-map-group.pictures>
2023-12-13 02:43:26 -0300

    [auto] Updated map for member 'Tatters ✾'

--- people/tgerus/countries.py
+++ people/tgerus/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'AU': ['Australia', 2498, 3508],
   'CN': ['South China Sea Islands (disputed)', 15, 16],
-  'NR': ['Nauru', 2, 2],
-  'AE': ['United Arab Emirates', 2, 2],
-  'TR': ['Turkey', 1, 1],
-  'IR': ['Iran', 1, 1],
-  'RU': ['Russia', 560, 728],
-  'JP': ['Japan', 175, 205],
-  'GB': ['United Kingdom', 237, 338],
-  'KR': ['South Korea', 4, 6],
   'SG': ['Singapore', 1, 2],
   'NZ': ['New Zealand', 126, 186],
+  'GB': ['United Kingdom', 237, 338],
   'US': ['United States of America', 8, 10],
-  'TH': ['Thailand', 18, 23]
+  'NR': ['Nauru', 2, 2],
+  'IR': ['Iran', 1, 1],
+  'AU': ['Australia', 2502, 3513],
+  'RU': ['Russia', 560, 728],
+  'TR': ['Turkey', 1, 1],
+  'AE': ['United Arab Emirates', 2, 2],
+  'TH': ['Thailand', 18, 23],
+  'KR': ['South Korea', 4, 6],
+  'JP': ['Japan', 175, 205]
 }
--- people/tgerus/user.py
+++ people/tgerus/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Tatters ✾',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/62938898@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/tgerus/',
-  'markers': 3648,
-  'photos': 5028
+  'markers': 3652,
+  'photos': 5033
 }
