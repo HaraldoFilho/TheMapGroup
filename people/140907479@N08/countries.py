b7ab0f199677f29eae8f4c28b6dac282c466e9c3
The Map Group <admin@the-map-group.pictures>
2023-12-13 02:20:52 -0300

    [auto] Updated map for member 'Crystal.Rain'

--- people/140907479@N08/countries.py
+++ people/140907479@N08/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
   'AQ': ['Antarctica', 1, 3],
   'US': ['United States of America', 2, 2],
-  'UZ': ['Uzbekistan', 1, 10],
-  'TR': ['Turkey', 35, 60],
+  'RU': ['Russia', 698, 801],
+  'TR': ['Turkey', 35, 66],
   'NL': ['Netherlands', 3, 3],
-  'RU': ['Russia', 698, 801]
+  'UZ': ['Uzbekistan', 1, 10]
 }
