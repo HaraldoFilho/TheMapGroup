c757aa3af0511a4e31ed897386adfa06a5f456ef
The Map Group <admin@the-map-group.pictures>
2023-12-13 02:21:19 -0300

    [auto] Updated map for member 'ValKamch'

--- people/valkamch/countries.py
+++ people/valkamch/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'KH': ['Cambodia', 316, 378],
-  'MC': ['Monaco', 5, 12],
-  'IR': ['Iran', 1, 1],
-  'UA': ['Ukraine', 1261, 1409],
-  'FR': ['France', 1, 1],
-  'MT': ['Malta', 1, 1],
-  'IT': ['Italy', 1370, 1578],
   'RU': ['Russia', 1326, 1551],
+  'IT': ['Italy', 1370, 1578],
+  'KH': ['Cambodia', 316, 378],
   'SG': ['Singapore', 2, 16],
-  'LK': ['Sri Lanka', 119, 175],
+  'MT': ['Malta', 1, 1],
   'NO': ['Norway', 1, 1],
+  'LK': ['Sri Lanka', 119, 175],
+  'IR': ['Iran', 1, 1],
+  'MC': ['Monaco', 5, 12],
+  'UA': ['Ukraine', 1272, 1422],
+  'FR': ['France', 1, 1],
   'UZ': ['Uzbekistan', 169, 187]
 }
--- people/valkamch/user.py
+++ people/valkamch/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'ValKamch',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/143593890@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/valkamch/',
-  'markers': 4572,
-  'photos': 5310
+  'markers': 4583,
+  'photos': 5323
 }
