9ce49c67535a63ddcd2e66fcc303470f99677adc
The Map Group <admin@the-map-group.pictures>
2023-12-13 02:05:42 -0300

    [auto] Updated map for member 'm01229'

--- people/39908901@N06/countries.py
+++ people/39908901@N06/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'ES': ['Spain', 12, 36],
   'TH': ['Thailand', 12, 82],
+  'ES': ['Spain', 12, 36],
+  'IS': ['Iceland', 229, 446],
+  'NL': ['The Netherlands', 15, 37],
   'FR': ['France', 2, 7],
-  'UG': ['Uganda', 99, 197],
-  'MV': ['Maldives', 16, 58],
   'IN': ['India', 136, 712],
-  'TZ': ['Tanzania', 115, 289],
-  'PK': ['Pakistan', 2, 7],
+  'UG': ['Uganda', 99, 197],
+  'PT': ['Portugal', 104, 429],
+  'MV': ['Maldives', 16, 58],
   'LK': ['Sri Lanka', 17, 104],
   'CA': ['Canada', 536, 1086],
-  'US': ['United States of America', 7848, 26300],
-  'PT': ['Portugal', 104, 429],
-  'KE': ['Kenya', 270, 555],
-  'IS': ['Iceland', 225, 430],
-  'NL': ['The Netherlands', 15, 37]
+  'PK': ['Pakistan', 2, 7],
+  'TZ': ['Tanzania', 116, 293],
+  'KE': ['Kenya', 280, 567],
+  'US': ['United States of America', 7854, 26309]
 }
--- people/39908901@N06/user.py
+++ people/39908901@N06/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'm01229',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/39908901@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/39908901@N06/',
-  'markers': 9409,
-  'photos': 30329
+  'markers': 9430,
+  'photos': 30370
 }
