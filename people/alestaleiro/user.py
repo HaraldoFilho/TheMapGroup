6b922162e1cb64999d37b4e9ab140f98e4bab200
The Map Group <admin@the-map-group.pictures>
2023-12-16 02:20:51 -0300

    [auto] Updated map for member 'alestaleiro'

--- people/alestaleiro/user.py
+++ people/alestaleiro/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Alejandro David Olivera',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/99949037@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/alestaleiro/',
-  'markers': 3222,
-  'photos': 4348
+  'markers': 3224,
+  'photos': 4350
 }
