5d4909385504b85ced28d54c75a7f4c1526a1a8e
The Map Group <admin@the-map-group.pictures>
2023-12-29 02:52:46 -0300

    [auto] Updated map for member 'marionkaminski'

--- people/147422800@N02/user.py
+++ people/147422800@N02/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/147422800@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/147422800@N02/',
   'markers': 161,
-  'photos': 278
+  'photos': 279
 }
