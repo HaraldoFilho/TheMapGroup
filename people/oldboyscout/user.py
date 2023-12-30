f7685fc3fd032ab969a7a9f406dc8d5769e9e6ee
The Map Group <admin@the-map-group.pictures>
2023-12-30 02:13:30 -0300

    [auto] Updated map for member 'gsegelken'

--- people/oldboyscout/user.py
+++ people/oldboyscout/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'gsegelken',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/100209004@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/oldboyscout/',
-  'markers': 1471,
-  'photos': 1653
+  'markers': 1473,
+  'photos': 1655
 }
