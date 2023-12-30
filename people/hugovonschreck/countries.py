3f8fb01674b611e7bd4e11b6966f2a2f0823b848
The Map Group <admin@the-map-group.pictures>
2023-12-30 02:11:08 -0300

    [auto] Updated map for member 'Hugo von Schreck'

--- people/hugovonschreck/countries.py
+++ people/hugovonschreck/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'FR': ['France', 91, 198],
   'AT': ['Austria', 16, 27],
+  'HR': ['Croatia', 7, 17],
+  'PL': ['Poland', 1, 3],
+  'CH': ['Switzerland', 18, 39],
   'DK': ['Denmark', 6, 7],
   'IT': ['Italy', 88, 228],
-  'HR': ['Croatia', 7, 17],
-  'CH': ['Switzerland', 18, 39],
-  'NL': ['Netherlands', 40, 69],
-  'PL': ['Poland', 1, 3],
-  'DE': ['Germany', 1262, 5193],
+  'LU': ['Luxembourg', 6, 15],
+  'FR': ['France', 91, 198],
   'SM': ['San Marino', 1, 1],
-  'LU': ['Luxembourg', 6, 15]
+  'DE': ['Germany', 1262, 5194],
+  'NL': ['Netherlands', 40, 69]
 }
