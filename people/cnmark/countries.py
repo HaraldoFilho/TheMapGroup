d395d6a2678f24fa303851ec034ea7c3c9b929a8
The Map Group <admin@the-map-group.pictures>
2024-01-01 02:31:05 -0300

    [auto] Updated map for member 'cnmark'

--- people/cnmark/countries.py
+++ people/cnmark/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'IT': ['Italy', 35, 51],
-  'CA': ['Canada', 22, 23],
-  'SE': ['Sweden', 1, 1],
-  'FR': ['France', 31, 34],
+  'GB': ['United Kingdom', 3, 3],
   'JP': ['Japan', 11, 12],
-  'AE': ['United Arab Emirates', 7, 11],
+  'CN': ['South China Sea Islands (disputed)', 1785, 2178],
+  'IT': ['Italy', 35, 51],
+  'AT': ['Austria', 16, 18],
   'US': ['United States', 6, 6],
-  'CH': ['Switzerland', 1, 1],
   'HU': ['Hungary', 23, 24],
   'TH': ['Thailand', 2, 2],
-  'DE': ['Germany', 1618, 2470],
-  'AT': ['Austria', 16, 18],
-  'CN': ['South China Sea Islands (disputed)', 1785, 2177],
-  'GB': ['United Kingdom', 3, 3]
+  'CA': ['Canada', 22, 23],
+  'AE': ['United Arab Emirates', 7, 11],
+  'CH': ['Switzerland', 1, 1],
+  'FR': ['France', 31, 34],
+  'SE': ['Sweden', 1, 1],
+  'DE': ['Germany', 1618, 2470]
 }
--- people/cnmark/user.py
+++ people/cnmark/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/48646584@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/cnmark/',
   'markers': 3561,
-  'photos': 4833
+  'photos': 4834
 }
