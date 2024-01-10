36fd5ea0271b14f987d24888ab36854b6140ba8f
The Map Group <admin@the-map-group.pictures>
2024-01-10 19:37:46 -0300

    Removed member 'tgerus'

--- people/tgerus/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '62938898@N00',
-  'alias': 'tgerus',
-  'name': 'Tatters ✾',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/62938898@N00_r.jpg',
-  'url': 'https://www.flickr.com/photos/tgerus/',
-  'markers': 3652,
-  'photos': 5033
-}
