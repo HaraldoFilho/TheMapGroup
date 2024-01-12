4578ab802e69879840e6791a996d559a61a50101
The Map Group <admin@the-map-group.pictures>
2024-01-12 08:57:52 -0300

    [auto] Updated map for member 'tquist24'

--- people/123895834@N08/countries.py
+++ people/123895834@N08/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'CU': ['Cuba', 8, 9],
   'AI': ['Anguilla', 7, 9],
-  'CA': ['Canada', 10, 12],
+  'US': ['United States of America', 2875, 3831],
+  'MX': ['Mexico', 12, 13],
   'GB': ['United Kingdom', 96, 113],
   'BS': ['Bahamas', 12, 12],
-  'BZ': ['Belize', 6, 6],
-  'PR': ['Puerto Rico', 4, 4],
-  'US': ['United States of America', 2875, 3831],
-  'IT': ['Italy', 80, 85],
-  'NL': ['The Netherlands', 19, 21],
-  'MX': ['Mexico', 12, 13],
   'DO': ['Dominican Republic', 4, 33],
-  'KY': ['Cayman Islands', 5, 5]
+  'KY': ['Cayman Islands', 5, 5],
+  'IT': ['Italy', 81, 86],
+  'CU': ['Cuba', 8, 9],
+  'BZ': ['Belize', 6, 6],
+  'NL': ['The Netherlands', 19, 21],
+  'CA': ['Canada', 10, 12],
+  'PR': ['Puerto Rico', 4, 4]
 }
