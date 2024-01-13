a47fef38315034a0be7b70a107170c4ceabdf491
The Map Group <admin@the-map-group.pictures>
2024-01-13 03:47:25 -0300

    [auto] Updated map for member 'Gérard Cachon'

--- people/bennett-cachon/countries.py
+++ people/bennett-cachon/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'CL': ['Chile', 1, 1],
   'NZ': ['New Zealand', 18, 19],
-  'US': ['United States of America', 110, 130],
+  'CA': ['Canada', 3, 5],
+  'CL': ['Chile', 1, 1],
+  'NL': ['Netherlands', 124, 186],
   'IS': ['Iceland', 21, 28],
-  'CA': ['Canada', 2, 4],
-  'NL': ['Netherlands', 124, 186]
+  'US': ['United States of America', 110, 130]
 }
--- people/bennett-cachon/user.py
+++ people/bennett-cachon/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Gérard Cachon',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/96671573@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/bennett-cachon/',
-  'markers': 276,
-  'photos': 368
+  'markers': 277,
+  'photos': 369
 }
