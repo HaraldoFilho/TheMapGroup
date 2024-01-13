296c8c5e874a596946a98aab213901d9dc6d73ef
The Map Group <admin@the-map-group.pictures>
2024-01-13 03:46:48 -0300

    [auto] Updated map for member 'diffendale'

--- people/dandiffendale/user.py
+++ people/dandiffendale/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/7945858@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/dandiffendale/',
   'markers': 1762,
-  'photos': 11653
+  'photos': 11668
 }
