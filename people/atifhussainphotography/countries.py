06ec45ba672c6cef052ed3bd65dd235501605ba9
The Map Group <admin@the-map-group.pictures>
2024-01-15 03:36:00 -0300

    [auto] Updated map for member 'Atif Hussain Photography'

---
+++ people/atifhussainphotography/countries.py
@@ -0,0 +1,19 @@
+countries_dict = {
+  'BH': ['Bahrain', 1, 1],
+  'CH': ['Switzerland', 68, 68],
+  'BE': ['Belgium', 7, 7],
+  'AT': ['Austria', 49, 49],
+  'PK': ['Pakistan', 2, 2],
+  'CZ': ['Czechia', 25, 25],
+  'NL': ['Netherlands', 10, 10],
+  'QA': ['Qatar', 113, 116],
+  'SA': ['Saudi Arabia', 9, 9],
+  'TR': ['Turkey', 91, 91],
+  'DE': ['Germany', 24, 24],
+  'HU': ['Hungary', 26, 28],
+  'SK': ['Slovakia', 8, 8],
+  'IT': ['Italy', 26, 26],
+  'VA': ['Vatican City', 1, 1],
+  'AE': ['United Arab Emirates', 8, 9],
+  'FR': ['France', 16, 16]
+}
