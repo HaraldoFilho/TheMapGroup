b0b98a7d3aa5ec42e77600f900aab63305b2ba2e
The Map Group <admin@the-map-group.pictures>
2024-01-16 18:30:45 -0300

    Reset map for hpfilho

--- people/hpfilho/user.py
+++ people/hpfilho/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Haraldo Albergaria',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/hpfilho/',
-  'markers': 599,
-  'photos': 1077
+  'markers': 601,
+  'photos': 1079
 }
