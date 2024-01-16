9819f1e60b43aea51339b644ea7b64a84d12207d
The Map Group <admin@the-map-group.pictures>
2024-01-16 15:25:14 -0300

    Reset map for hpfilho

--- people/hpfilho/countries.py
+++ people/hpfilho/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
   'IT': ['Italy', 77, 126],
-  'AR': ['Argentina', 27, 38],
-  'PY': ['Paraguay', 1, 1],
-  'HR': ['Croatia', 184, 243],
-  'CL': ['Chile', 62, 112],
   'BR': ['Brazil', 175, 393],
   'VA': ['Vatican City', 22, 31],
-  'US': ['United States of America', 50, 132]
+  'HR': ['Croatia', 184, 243],
+  'PY': ['Paraguay', 1, 1],
+  'CL': ['Chile', 62, 112],
+  'US': ['United States of America', 50, 132],
+  'AR': ['Argentina', 27, 38]
 }
