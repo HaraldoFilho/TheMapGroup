ed54c56001417283e0efb559cae78369ec9858fa
The Map Group <admin@the-map-group.pictures>
2024-01-18 03:11:16 -0300

    [auto] Updated map for member 'Professor Otto Grotto (9 Million Views)'

--- people/14463685@N07/user.py
+++ people/14463685@N07/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Howard Somerville',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/14463685@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/14463685@N07/',
-  'markers': 687,
-  'photos': 816
+  'markers': 688,
+  'photos': 817
 }
