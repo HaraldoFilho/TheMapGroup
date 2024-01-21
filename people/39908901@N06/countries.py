c14c0bae09d5d8312a7da9fdc772404496f05d25
The Map Group <admin@the-map-group.pictures>
2024-01-21 03:16:37 -0300

    [auto] Updated map for member 'm01229'

--- people/39908901@N06/countries.py
+++ people/39908901@N06/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'CA': ['Canada', 543, 1097],
-  'PT': ['Portugal', 104, 429],
-  'TZ': ['Tanzania', 148, 430],
-  'LK': ['Sri Lanka', 17, 104],
-  'ES': ['Spain', 12, 36],
-  'MV': ['Maldives', 16, 58],
   'KE': ['Kenya', 320, 647],
+  'CA': ['Canada', 543, 1097],
   'PK': ['Pakistan', 2, 7],
-  'UG': ['Uganda', 105, 222],
-  'TH': ['Thailand', 12, 82],
-  'IS': ['Iceland', 288, 700],
   'FR': ['France', 2, 7],
-  'US': ['United States of America', 8103, 26787],
   'IN': ['India', 136, 712],
-  'NL': ['The Netherlands', 15, 37]
+  'MV': ['Maldives', 16, 58],
+  'TH': ['Thailand', 12, 82],
+  'TZ': ['Tanzania', 148, 430],
+  'NL': ['The Netherlands', 15, 37],
+  'LK': ['Sri Lanka', 17, 104],
+  'IS': ['Iceland', 293, 729],
+  'ES': ['Spain', 12, 36],
+  'US': ['United States of America', 8110, 26801],
+  'UG': ['Uganda', 105, 222],
+  'PT': ['Portugal', 104, 429]
 }
