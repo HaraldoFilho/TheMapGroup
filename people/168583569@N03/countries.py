a8a1e4b43e1fe192371d2b5f8fd9d9e74590ead6
The Map Group <admin@the-map-group.pictures>
2024-01-24 03:29:28 -0300

    [auto] Updated map for member 'amalthus'

--- people/168583569@N03/countries.py
+++ people/168583569@N03/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'BG': ['Bulgaria', 1, 1],
-  'PT': ['Portugal', 1, 1],
-  'CA': ['Canada', 1, 1],
-  'AT': ['Austria', 1, 1],
-  'DE': ['Germany', 3, 62],
-  'IT': ['Italy', 2, 2],
-  'US': ['United States of America', 1, 1],
-  'CY': ['Cyprus', 1, 1],
-  'GR': ['Greece', 3, 4],
   'ES': ['Spain', 1, 1],
-  'FR': ['France', 1, 15]
+  'PT': ['Portugal', 1, 1],
+  'FR': ['France', 1, 15],
+  'AT': ['Austria', 1, 1],
+  'BG': ['Bulgaria', 1, 1],
+  'IT': ['Italy', 2, 2],
+  'CY': ['Cyprus', 1, 1],
+  'US': ['United States of America', 1, 1],
+  'DE': ['Germany', 3, 63],
+  'GR': ['Greece', 3, 4],
+  'CA': ['Canada', 1, 1]
 }
--- people/168583569@N03/user.py
+++ people/168583569@N03/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': '../../icons/photographer.svg',
   'url': 'https://www.flickr.com/photos/168583569@N03/',
   'markers': 32,
-  'photos': 603
+  'photos': 604
 }
