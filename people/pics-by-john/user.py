c998946b03db78c67364fbeac4b28c25320f281e
The Map Group <admin@the-map-group.pictures>
2024-01-24 04:06:42 -0300

    Removed member 'pics-by-john'

--- people/pics-by-john/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '59961745@N06',
-  'alias': 'pics-by-john',
-  'name': 'John Woolley',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/59961745@N06_r.jpg',
-  'url': 'https://www.flickr.com/photos/pics-by-john/',
-  'markers': 2309,
-  'photos': 9769
-}
