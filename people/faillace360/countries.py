d40c697d0fa0a338ca666edf904896940aeb0ab0
The Map Group <admin@the-map-group.pictures>
2024-02-04 17:38:43 -0300

    [auto] Updated map for member 'Faillace360'

--- people/faillace360/countries.py
+++ people/faillace360/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
   'AR': ['Argentina', 7, 7],
-  'CZ': ['Czechia', 1, 1],
-  'CH': ['Switzerland', 2, 2],
   'IT': ['Italy', 3, 3],
+  'BR': ['Brazil', 118, 122],
+  'GB': ['United Kingdom', 3, 3],
   'AT': ['Austria', 3, 3],
   'PE': ['Peru', 4, 4],
+  'CH': ['Switzerland', 2, 2],
   'DE': ['Germany', 1, 1],
-  'GB': ['United Kingdom', 3, 3],
-  'BR': ['Brazil', 117, 121]
+  'CZ': ['Czechia', 1, 1]
 }
--- people/faillace360/user.py
+++ people/faillace360/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Faillace',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/194868714@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/faillace360/',
-  'markers': 141,
-  'photos': 145
+  'markers': 142,
+  'photos': 146
 }
