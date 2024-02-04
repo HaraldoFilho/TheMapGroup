e090a28217eacdb1f8b7cbbc3bb0fa8ada490625
The Map Group <admin@the-map-group.pictures>
2024-02-04 18:06:27 -0300

    [auto] Updated map for member 'Julian Munilla Rio'

--- people/35690321@N07/countries.py
+++ people/35690321@N07/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
   'IE': ['Ireland', 11, 11],
-  'ES': ['Spain', 815, 820],
-  'PT': ['Portugal', 123, 123],
+  'ES': ['Spain', 818, 823],
   'BG': ['Bulgaria', 2, 2],
+  'PT': ['Portugal', 125, 125],
   'NO': ['Norway', 11, 11]
 }
--- people/35690321@N07/user.py
+++ people/35690321@N07/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Julian Munilla Rio',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/35690321@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/35690321@N07/',
-  'markers': 962,
-  'photos': 967
+  'markers': 967,
+  'photos': 972
 }
