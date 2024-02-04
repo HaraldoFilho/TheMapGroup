d3b613f321a1b6da92fcd98cb1ec1d2b57013c3f
The Map Group <admin@the-map-group.pictures>
2024-02-04 16:51:16 -0300

    [auto] Updated map for member 'Professor Otto Grotto'

--- people/byhowardsomerville/user.py
+++ people/byhowardsomerville/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Howard Somerville',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/14463685@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/byhowardsomerville/',
-  'markers': 696,
-  'photos': 829
+  'markers': 697,
+  'photos': 830
 }
