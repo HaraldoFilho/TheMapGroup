f295437b153be4c0c24edd5f74397d487c2c8c0e
The Map Group <admin@the-map-group.pictures>
2024-02-11 03:04:16 -0300

    [auto] Updated map for member 'Jason Frels'

--- people/jasonfrels/countries.py
+++ people/jasonfrels/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'US': ['United States', 107, 124],
   'SG': ['Singapore', 1, 2],
-  'CA': ['Canada', 4, 4],
-  'CL': ['Chile', 9, 10]
+  'US': ['United States', 107, 124],
+  'CL': ['Chile', 9, 10],
+  'CA': ['Canada', 4, 5]
 }
