0e1eb230d991b7e373d30958a8b87f7526d4d5eb
The Map Group <admin@the-map-group.pictures>
2024-02-12 03:38:14 -0300

    [auto] Updated map for member 'fraganda.photo'

--- people/fraganda/user.py
+++ people/fraganda/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'fraganda.photo',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/39777440@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/fraganda/',
-  'markers': 260,
-  'photos': 260
+  'markers': 261,
+  'photos': 261
 }
