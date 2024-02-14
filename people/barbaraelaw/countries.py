76bb555cf68f3951d88eb9e57c58a492ffb2c4e5
The Map Group <admin@the-map-group.pictures>
2024-02-14 03:02:47 -0300

    [auto] Updated map for member 'ManxieB- prefer comments!'

--- people/barbaraelaw/countries.py
+++ people/barbaraelaw/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'IM': ['Isle of Man', 2034, 2462],
-  'TZ': ['Tanzania', 59, 318],
   'GB': ['United Kingdom', 334, 453],
-  'IE': ['Ireland', 35, 53]
+  'IE': ['Ireland', 35, 53],
+  'IM': ['Isle of Man', 2035, 2463],
+  'TZ': ['Tanzania', 59, 318]
 }
--- people/barbaraelaw/user.py
+++ people/barbaraelaw/user.py
@@ -1,9 +1,9 @@
 user_info = {
   'id': '25404639@N08',
   'alias': 'barbaraelaw',
-  'name': 'ManxieB- prefer comments!',
+  'name': 'Barbara Lawrence',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/25404639@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/barbaraelaw/',
-  'markers': 2462,
-  'photos': 3286
+  'markers': 2463,
+  'photos': 3287
 }
