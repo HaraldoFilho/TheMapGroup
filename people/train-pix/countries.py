2abc90c46d76e75b1f71b7e8ba6a92aab2181fc9
The Map Group <admin@the-map-group.pictures>
2024-02-18 03:23:11 -0300

    [auto] Updated map for member 'Ron Fisher'

--- people/train-pix/countries.py
+++ people/train-pix/countries.py
@@ -1,19 +1,19 @@
 countries_dict = {
-  'HU': ['Hungary', 19, 28],
-  'AT': ['Austria', 28, 28],
   'RS': ['Serbia', 3, 3],
   'BE': ['Belgium', 1, 1],
+  'MC': ['Monaco', 3, 3],
+  'IM': ['Isle of Man', 20, 55],
+  'HR': ['Croatia', 5, 9],
   'DE': ['Germany', 316, 359],
-  'GB': ['United Kingdom', 8988, 19299],
+  'GB': ['United Kingdom', 8988, 19301],
   'ZA': ['South Africa', 311, 320],
-  'SZ': ['Eswatini', 6, 7],
-  'ES': ['Spain', 1, 3],
+  'BA': ['Bosnia and Herzegovina', 12, 20],
   'IT': ['Italy', 23, 41],
   'FR': ['France', 440, 648],
-  'HR': ['Croatia', 5, 9],
   'CA': ['Canada', 1, 2],
-  'IM': ['Isle of Man', 20, 55],
   'CH': ['Switzerland', 1166, 1875],
-  'MC': ['Monaco', 3, 3],
-  'BA': ['Bosnia and Herzegovina', 12, 20]
+  'AT': ['Austria', 28, 28],
+  'HU': ['Hungary', 19, 28],
+  'ES': ['Spain', 1, 3],
+  'SZ': ['Eswatini', 6, 7]
 }
