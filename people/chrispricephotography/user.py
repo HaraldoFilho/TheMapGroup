2b1e63998ef466372190d03410002a34c1485b3c
The Map Group <admin@the-map-group.pictures>
2024-02-25 03:00:24 -0300

    [auto] Updated map for member 'Christopher Wallace'

--- people/chrispricephotography/user.py
+++ people/chrispricephotography/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Chris Price',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/8121206@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/chrispricephotography/',
-  'markers': 970,
-  'photos': 1354
+  'markers': 971,
+  'photos': 1355
 }
