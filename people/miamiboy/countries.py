4fc35fe553acd34edebaa25ac8c4ab634edd2ce8
The Map Group <admin@the-map-group.pictures>
2024-02-28 04:29:12 -0300

    [auto] Updated map for member 'Urban Florida Photographer'

---
+++ people/miamiboy/countries.py
@@ -0,0 +1,23 @@
+countries_dict = {
+  'TR': ['Turkey', 10, 19],
+  'NL': ['Aruba', 18, 61],
+  'PE': ['Peru', 7, 12],
+  'MA': ['Morocco', 1, 1],
+  'NI': ['Nicaragua', 1, 1],
+  'KY': ['Cayman Islands', 1, 4],
+  'IT': ['Italy', 1, 3],
+  'BM': ['Bermuda', 7, 21],
+  'US': ['United States of America', 7075, 11834],
+  'GR': ['Greece', 7, 15],
+  'CA': ['Canada', 11, 20],
+  'CO': ['Colombia', 2, 8],
+  'HN': ['Honduras', 31, 62],
+  'BS': ['Bahamas', 9, 10],
+  'ES': ['Spain', 10, 20],
+  'CU': ['Cuba', 4, 5],
+  'MX': ['Mexico', 9, 29],
+  'TC': ['Turks and Caicos Islands', 2, 37],
+  'GT': ['Guatemala', 12, 18],
+  'HR': ['Croatia', 8, 10],
+  'DO': ['Dominican Republic', 5, 11]
+}
