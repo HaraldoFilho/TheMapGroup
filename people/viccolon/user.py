1057c60f84b22828d28e9ae50eadd1496ca36dd3
The Map Group <admin@the-map-group.pictures>
2024-02-28 03:17:43 -0300

    [auto] Updated map for member 'VicColon'

--- people/viccolon/user.py
+++ people/viccolon/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'VicColon',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/90347682@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/viccolon/',
-  'markers': 150,
-  'photos': 336
+  'markers': 151,
+  'photos': 337
 }
