ac9ac8c58cef817267c05f7ef0330e3b428a3dc1
The Map Group <admin@the-map-group.pictures>
2024-03-01 03:37:03 -0300

    [auto] Updated map for member 'ReNato '59'

--- people/renato59/countries.py
+++ people/renato59/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
   'FR': ['France', 4, 6],
   'IT': ['Italy', 13, 13],
-  'AD': ['Andorra', 12, 21],
   'RU': ['Russia', 1, 1],
-  'ES': ['Spain', 405, 1443]
+  'ES': ['Spain', 406, 1444],
+  'AD': ['Andorra', 12, 21]
 }
--- people/renato59/user.py
+++ people/renato59/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'ReNato \'59',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/87940223@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/renato59/',
-  'markers': 439,
-  'photos': 1489
+  'markers': 440,
+  'photos': 1490
 }
