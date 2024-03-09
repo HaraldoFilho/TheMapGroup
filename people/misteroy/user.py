789f1ca7f2d5dc2f810d1eed7586c2003f1f2983
The Map Group <admin@the-map-group.pictures>
2024-03-09 03:27:18 -0300

    [auto] Updated map for member 'Mister Oy'

--- people/misteroy/user.py
+++ people/misteroy/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/7126815@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/misteroy/',
   'markers': 4184,
-  'photos': 5986
+  'photos': 5987
 }
