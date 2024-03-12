1214ac11d2f15a76f23668404d0830d40390f097
The Map Group <admin@the-map-group.pictures>
2024-03-12 03:25:53 -0300

    [auto] Updated map for member 'Thanks for 8.7 Million Views'

--- people/joeinpenticton/countries.py
+++ people/joeinpenticton/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
+  'CA': ['Canada', 481, 488],
   'DE': ['Germany', 2, 2],
-  'CA': ['Canada', 480, 487],
-  'FR': ['France', 1, 1],
-  'ES': ['Spain', 7, 7],
   'US': ['United States', 122, 123],
+  'FR': ['France', 1, 1],
   'PT': ['Portugal', 34, 35],
-  'CU': ['Cuba', 1, 1]
+  'CU': ['Cuba', 1, 1],
+  'ES': ['Spain', 7, 7]
 }
--- people/joeinpenticton/user.py
+++ people/joeinpenticton/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Joe (José)  Garcia',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/23349126@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/joeinpenticton/',
-  'markers': 647,
-  'photos': 656
+  'markers': 648,
+  'photos': 657
 }
