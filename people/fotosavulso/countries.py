02a138ecf54e7cfa203745a1ee360404fcf01f30
The Map Group <admin@the-map-group.pictures>
2024-03-12 03:15:20 -0300

    [auto] Updated map for member 'Fotos Avulso'

--- people/fotosavulso/countries.py
+++ people/fotosavulso/countries.py
@@ -1,16 +1,16 @@
 countries_dict = {
-  'PT': ['Portugal', 344, 372],
-  'FR': ['France', 61, 63],
-  'CV': ['Cape Verde', 17, 19],
-  'ST': ['São Tomé and Príncipe', 103, 111],
-  'CN': ['South China Sea Islands (disputed)', 1, 2],
-  'GW': ['Guinea-Bissau', 1, 1],
-  'AO': ['Angola', 1, 1],
   'IT': ['Italy', 1, 1],
-  'GR': ['Greece', 1, 1],
+  'FR': ['France', 61, 63],
   'AD': ['Andorra', 2, 2],
-  'ES': ['Spain', 20, 22],
-  'AT': ['Austria', 1, 1],
   'TL': ['East Timor', 1, 1],
-  'US': ['United States of America', 3, 3]
+  'US': ['United States of America', 3, 3],
+  'CN': ['South China Sea Islands (disputed)', 1, 2],
+  'ST': ['São Tomé and Príncipe', 103, 111],
+  'GW': ['Guinea-Bissau', 1, 1],
+  'PT': ['Portugal', 344, 372],
+  'AO': ['Angola', 1, 1],
+  'GR': ['Greece', 1, 1],
+  'ES': ['Spain', 20, 22],
+  'CV': ['Cape Verde', 19, 21],
+  'AT': ['Austria', 1, 1]
 }
