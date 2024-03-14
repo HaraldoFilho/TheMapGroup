621ebee7029b9ce47715fa66736486748a3e435e
The Map Group <admin@the-map-group.pictures>
2024-03-14 03:24:46 -0300

    [auto] Updated map for member 'alestaleiro'

--- people/alestaleiro/user.py
+++ people/alestaleiro/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/99949037@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/alestaleiro/',
   'markers': 3273,
-  'photos': 4430
+  'photos': 4431
 }
