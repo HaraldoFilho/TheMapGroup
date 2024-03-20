0ceccc4739a7ed780b62691c93641daa0929457d
The Map Group <admin@the-map-group.pictures>
2024-03-20 03:39:17 -0300

    [auto] Updated map for member 'PeterThoeny'

--- people/peterthoeny/countries.py
+++ people/peterthoeny/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'CN': ['South China Sea Islands (disputed)', 71, 95],
-  'AT': ['Austria', 6, 9],
-  'GL': ['Greenland', 5, 5],
-  'US': ['United States of America', 1620, 3128],
-  'NO': ['Norway', 1, 1],
   'LI': ['Liechtenstein', 4, 5],
-  'IS': ['Iceland', 66, 97],
+  'CH': ['Switzerland', 225, 385],
+  'AT': ['Austria', 6, 9],
+  'EE': ['Estonia', 1, 1],
   'UA': ['Ukraine', 1, 1],
-  'JP': ['Japan', 87, 103],
-  'DE': ['Germany', 2, 3],
+  'IS': ['Iceland', 66, 97],
   'GB': ['United Kingdom', 17, 34],
   'IT': ['Italy', 34, 48],
-  'EE': ['Estonia', 1, 1],
-  'CH': ['Switzerland', 225, 385],
-  'CA': ['Canada', 2, 2]
+  'US': ['United States of America', 1620, 3128],
+  'DE': ['Germany', 2, 3],
+  'NO': ['Norway', 1, 1],
+  'CA': ['Canada', 2, 2],
+  'JP': ['Japan', 88, 104],
+  'GL': ['Greenland', 5, 5],
+  'CN': ['South China Sea Islands (disputed)', 71, 95]
 }
--- people/peterthoeny/user.py
+++ people/peterthoeny/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Peter Thoeny - Quality HDR Pho',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/98786299@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/peterthoeny/',
-  'markers': 2143,
-  'photos': 3918
+  'markers': 2144,
+  'photos': 3919
 }
