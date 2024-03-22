5750e85ad01e507e767cff39cb3e87d4d85a6d18
The Map Group <admin@the-map-group.pictures>
2024-03-22 03:47:06 -0300

    [auto] Updated map for member 'Gérard Cachon'

--- people/bennett-cachon/user.py
+++ people/bennett-cachon/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Gérard Cachon',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/96671573@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/bennett-cachon/',
-  'markers': 281,
-  'photos': 374
+  'markers': 282,
+  'photos': 375
 }
