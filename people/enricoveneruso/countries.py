da4a0253d1191ba678238483de0227a21d03f4a2
The Map Group <admin@the-map-group.pictures>
2024-03-24 03:31:58 -0300

    [auto] Updated map for member 'enrico377'

--- people/enricoveneruso/countries.py
+++ people/enricoveneruso/countries.py
@@ -1,10 +1,10 @@
 countries_dict = {
-  'NL': ['Netherlands', 1, 1],
-  'GB': ['United Kingdom', 9, 9],
-  'IE': ['Ireland', 11, 11],
-  'GR': ['Greece', 11, 11],
   'ES': ['Spain', 7, 7],
-  'IT': ['Italy', 71, 74],
+  'NL': ['Netherlands', 1, 1],
   'FR': ['France', 2, 2],
-  'US': ['United States of America', 23, 23]
+  'US': ['United States of America', 23, 23],
+  'GR': ['Greece', 12, 12],
+  'IT': ['Italy', 71, 74],
+  'IE': ['Ireland', 11, 11],
+  'GB': ['United Kingdom', 9, 9]
 }
