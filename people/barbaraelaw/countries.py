b969e0d000d952ae56622193919b81a23180bc09
The Map Group <admin@the-map-group.pictures>
2024-03-26 03:02:54 -0300

    [auto] Updated map for member 'ManxieB- just over 4 mil views thank you'

--- people/barbaraelaw/countries.py
+++ people/barbaraelaw/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
   'IE': ['Ireland', 35, 53],
-  'TZ': ['Tanzania', 59, 318],
-  'IM': ['Isle of Man', 2052, 2487],
-  'GB': ['United Kingdom', 334, 453]
+  'IM': ['Isle of Man', 2053, 2488],
+  'GB': ['United Kingdom', 334, 453],
+  'TZ': ['Tanzania', 59, 318]
 }
