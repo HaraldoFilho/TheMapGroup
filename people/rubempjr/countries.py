49e23d3816aec6108083bba371130ec9ee4a7513
The Map Group <admin@the-map-group.pictures>
2024-03-27 03:45:19 -0300

    [auto] Updated map for member 'Rubem Jr'

--- people/rubempjr/countries.py
+++ people/rubempjr/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'RU': ['Russia', 1, 1],
-  'FR': ['France', 6, 24],
-  'PT': ['Portugal', 2405, 5524],
-  'ES': ['Spain', 707, 1816],
-  'AR': ['Argentina', 61, 124],
-  'IT': ['Italy', 319, 615],
-  'BR': ['Brazil', 2460, 6814],
-  'CL': ['Chile', 1, 1],
   'US': ['United States of America', 8, 90],
-  'VA': ['Vatican City', 12, 16],
+  'ES': ['Spain', 707, 1816],
+  'RU': ['Russia', 1, 1],
+  'AR': ['Argentina', 61, 124],
   'UY': ['Uruguay', 25, 184],
-  'GB': ['United Kingdom', 886, 2467]
+  'BR': ['Brazil', 2460, 6814],
+  'VA': ['Vatican City', 12, 16],
+  'IT': ['Italy', 319, 615],
+  'GB': ['United Kingdom', 886, 2467],
+  'FR': ['France', 6, 24],
+  'PT': ['Portugal', 2406, 5525],
+  'CL': ['Chile', 1, 1]
 }
