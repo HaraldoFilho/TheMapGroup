2b7dc948b9b738e531edb4ce76bae0d9bbfea4cc
The Map Group <admin@the-map-group.pictures>
2024-03-31 03:14:23 -0300

    [auto] Updated map for member 'fam_nordstrom'

--- people/fam_nordstrom/user.py
+++ people/fam_nordstrom/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Pierre Nordström',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/10456518@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/fam_nordstrom/',
-  'markers': 5803,
-  'photos': 17244
+  'markers': 5806,
+  'photos': 17249
 }
