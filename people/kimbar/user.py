986043c683d5858631a295275b3da9180b788043
The Map Group <admin@the-map-group.pictures>
2024-04-04 04:13:07 -0300

    [auto] Updated map for member 'kimbar/Thanks for 6.7 million views!'

--- people/kimbar/user.py
+++ people/kimbar/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Kim',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/28287831@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/kimbar/',
-  'markers': 5592,
-  'photos': 7816
+  'markers': 5596,
+  'photos': 7820
 }
