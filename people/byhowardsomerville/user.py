3bbe0973bf823190874e09013b034b1ab412718c
The Map Group <admin@the-map-group.pictures>
2024-04-07 03:07:39 -0300

    [auto] Updated map for member 'Howard Somerville'

--- people/byhowardsomerville/user.py
+++ people/byhowardsomerville/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Howard Somerville',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/14463685@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/byhowardsomerville/',
-  'markers': 708,
-  'photos': 842
+  'markers': 709,
+  'photos': 843
 }
