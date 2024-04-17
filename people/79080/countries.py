5256becbe0286737e698982686d6b7f0584d333b
The Map Group <admin@the-map-group.pictures>
2024-04-17 11:31:30 -0300

    [auto] Updated map for member 'Robert C. Abraham'

--- people/79080/countries.py
+++ people/79080/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'GB': ['United Kingdom', 2, 2],
-  'US': ['United States', 9, 9],
-  'SG': ['Singapore', 5, 5],
-  'CN': ['South China Sea Islands (disputed)', 2, 2],
-  'CA': ['Canada', 138, 153],
-  'MY': ['Malaysia', 1, 1],
-  'TH': ['Thailand', 3, 3],
-  'AG': ['Antigua and Barbuda', 1, 1],
-  'MX': ['Mexico', 3, 3],
-  'JP': ['Japan', 1, 1],
   'JM': ['Jamaica', 4, 4],
-  'LC': ['Saint Lucia', 1, 1],
-  'VG': ['British Virgin Islands', 1, 1]
+  'MX': ['Mexico', 3, 3],
+  'US': ['United States', 9, 9],
+  'CA': ['Canada', 138, 153],
+  'VG': ['British Virgin Islands', 1, 1],
+  'JP': ['Japan', 1, 1],
+  'GB': ['United Kingdom', 2, 2],
+  'TH': ['Thailand', 3, 3],
+  'MY': ['Malaysia', 2, 2],
+  'SG': ['Singapore', 5, 5],
+  'AG': ['Antigua and Barbuda', 1, 1],
+  'CN': ['South China Sea Islands (disputed)', 2, 2],
+  'LC': ['Saint Lucia', 1, 1]
 }
