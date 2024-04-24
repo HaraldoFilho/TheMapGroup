b81e9538de864aff74efc5cb6322c372ac492f51
The Map Group <admin@the-map-group.pictures>
2024-04-24 11:04:08 -0300

    [auto] Updated map for member 'Matjaž Skrinar'

--- people/130486903@N04/countries.py
+++ people/130486903@N04/countries.py
@@ -1,6 +1,6 @@
 countries_dict = {
-  'IT': ['Italy', 3, 3],
-  'SI': ['Slovenia', 240, 248],
+  'HR': ['Croatia', 6, 6],
   'HU': ['Hungary', 1, 1],
-  'HR': ['Croatia', 6, 6]
+  'IT': ['Italy', 3, 3],
+  'SI': ['Slovenia', 241, 249]
 }
--- people/130486903@N04/user.py
+++ people/130486903@N04/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Matjaž Skrinar',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/130486903@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/130486903@N04/',
-  'markers': 250,
-  'photos': 258
+  'markers': 251,
+  'photos': 259
 }
