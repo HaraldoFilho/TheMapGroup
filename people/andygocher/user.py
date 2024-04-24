6f65af49d6e34a95a31ddd96e26c7c9f84f562f9
The Map Group <admin@the-map-group.pictures>
2024-04-24 10:00:55 -0300

    [auto] Updated map for member 'Andy.Gocher'

--- people/andygocher/user.py
+++ people/andygocher/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Andy Gocher',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/110839836@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/andygocher/',
-  'markers': 610,
-  'photos': 1515
+  'markers': 612,
+  'photos': 1517
 }
