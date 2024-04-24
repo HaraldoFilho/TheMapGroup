db1d488100f02a30b76a6d05ea2186ce74d8be2a
The Map Group <admin@the-map-group.pictures>
2024-04-24 12:05:07 -0300

    [auto] Updated map for member 'Atif Hussain Photography'

--- people/atifhussainphotography/user.py
+++ people/atifhussainphotography/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Atif Hussain Photography',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/42102968@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/atifhussainphotography/',
-  'markers': 507,
-  'photos': 513
+  'markers': 509,
+  'photos': 515
 }
