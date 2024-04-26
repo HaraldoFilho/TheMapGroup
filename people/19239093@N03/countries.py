3597f9021228becd06b25a986190680d12b9ad7b
The Map Group <admin@the-map-group.pictures>
2024-04-26 09:50:49 -0300

    [auto] Updated map for member 'Edouard55'

--- people/19239093@N03/countries.py
+++ people/19239093@N03/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'FR': ['France', 6335, 8073],
-  'DE': ['Germany', 1022, 1379],
-  'CH': ['Switzerland', 79, 106]
+  'DE': ['Germany', 1028, 1387],
+  'CH': ['Switzerland', 79, 106],
+  'FR': ['France', 6335, 8073]
 }
