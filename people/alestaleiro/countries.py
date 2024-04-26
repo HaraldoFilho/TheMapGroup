6629ca9641d5a16813ff16b4dcd5030ee2492a22
The Map Group <admin@the-map-group.pictures>
2024-04-26 10:00:56 -0300

    [auto] Updated map for member 'alestaleiro'

--- people/alestaleiro/countries.py
+++ people/alestaleiro/countries.py
@@ -1,18 +1,18 @@
 countries_dict = {
-  'DE': ['Germany', 8, 8],
-  'EG': ['Egypt', 118, 156],
   'BE': ['Belgium', 2, 2],
+  'CL': ['Chile', 26, 31],
   'FR': ['France', 1, 1],
-  'AE': ['United Arab Emirates', 27, 40],
   'ES': ['Spain', 126, 176],
   'ZA': ['South Africa', 130, 166],
-  'NL': ['Netherlands', 2, 2],
   'PT': ['Portugal', 50, 59],
-  'MA': ['Morocco', 96, 134],
+  'AE': ['United Arab Emirates', 27, 40],
+  'BR': ['Brazil', 2378, 3299],
   'IT': ['Italy', 13, 14],
   'AR': ['Argentina', 224, 251],
-  'CL': ['Chile', 26, 31],
+  'PE': ['Peru', 14, 24],
+  'EG': ['Egypt', 118, 156],
+  'MA': ['Morocco', 96, 134],
   'US': ['United States of America', 84, 108],
-  'BR': ['Brazil', 2378, 3298],
-  'PE': ['Peru', 14, 24]
+  'DE': ['Germany', 8, 8],
+  'NL': ['Netherlands', 2, 2]
 }
--- people/alestaleiro/user.py
+++ people/alestaleiro/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/99949037@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/alestaleiro/',
   'markers': 3300,
-  'photos': 4471
+  'photos': 4472
 }
