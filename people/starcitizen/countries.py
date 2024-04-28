3138b3b6cee1a539dfcb7756097b9ac97c823076
The Map Group <admin@the-map-group.pictures>
2024-04-28 01:38:57 -0300

    [auto] Updated map for member 'CoronaViking (mountain photography)'

--- people/starcitizen/countries.py
+++ people/starcitizen/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'CL': ['Chile', 84, 94],
-  'AR': ['Argentina', 223, 227],
-  'CH': ['Switzerland', 6, 6],
-  'ES': ['Spain', 1148, 1174],
   'FR': ['France', 188, 190],
+  'AR': ['Argentina', 223, 227],
+  'CL': ['Chile', 84, 94],
+  'CH': ['Switzerland', 6, 6],
   'NO': ['Norway', 772, 803],
+  'NZ': ['New Zealand', 241, 256],
   'IT': ['Italy', 70, 70],
-  'AD': ['Andorra', 613, 621],
-  'NZ': ['New Zealand', 241, 256]
+  'ES': ['Spain', 1149, 1175],
+  'AD': ['Andorra', 613, 621]
 }
