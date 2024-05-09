7c9e62eceda37f014b048d2a0fd4af0f4ebb1cd6
The Map Group <admin@the-map-group.pictures>
2024-05-09 02:09:54 -0300

    [auto] Updated map for member 'giosuèbolis'

--- people/146445851@N04/countries.py
+++ people/146445851@N04/countries.py
@@ -1,13 +1,13 @@
 countries_dict = {
-  'DZ': ['Algeria', 5, 6],
-  'IT': ['Italy', 177, 223],
-  'ML': ['Mali', 9, 10],
-  'UA': ['Ukraine', 3, 3],
-  'TR': ['Turkey', 1, 1],
-  'VA': ['Vatican City', 1, 1],
-  'NE': ['Niger', 5, 6],
+  'DZ': ['Algeria', 6, 7],
   'HU': ['Hungary', 2, 2],
-  'CH': ['Switzerland', 13, 28],
+  'IT': ['Italy', 177, 223],
+  'NE': ['Niger', 5, 6],
+  'FR': ['France', 8, 9],
+  'VA': ['Vatican City', 1, 1],
   'LY': ['Libya', 3, 3],
-  'FR': ['France', 8, 9]
+  'TR': ['Turkey', 1, 1],
+  'UA': ['Ukraine', 3, 3],
+  'CH': ['Switzerland', 13, 28],
+  'ML': ['Mali', 9, 10]
 }
--- people/146445851@N04/user.py
+++ people/146445851@N04/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Giosuè Bolis',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/146445851@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/146445851@N04/',
-  'markers': 227,
-  'photos': 292
+  'markers': 228,
+  'photos': 293
 }
