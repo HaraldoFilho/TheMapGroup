77148addb61c2589e3db248dc1bb398c046942a7
The Map Group <admin@the-map-group.pictures>
2024-05-14 02:12:39 -0300

    [auto] Updated map for member 'Fabrizio Lucchese'

--- people/lucchesefabrizio/user.py
+++ people/lucchesefabrizio/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Fabrizio LUCCHESE',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/86411192@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/lucchesefabrizio/',
-  'markers': 1259,
-  'photos': 1263
+  'markers': 1263,
+  'photos': 1267
 }
