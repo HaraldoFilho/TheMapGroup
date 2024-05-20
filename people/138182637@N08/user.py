e34b178aa3135e44c34905360cb79ae3e08c2305
The Map Group <admin@the-map-group.pictures>
2024-05-20 11:44:54 -0300

    [auto] Updated map for member 'dimitri.verledens'

--- people/138182637@N08/user.py
+++ people/138182637@N08/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/138182637@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/138182637@N08/',
   'markers': 776,
-  'photos': 922
+  'photos': 923
 }
