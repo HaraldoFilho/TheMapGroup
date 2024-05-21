baae60b3e770b49b8f974304d91305bbe4b719b8
The Map Group <admin@the-map-group.pictures>
2024-05-21 11:53:27 -0300

    [auto] Updated map for member 'Lemmo2009'

--- people/lemmo2009/user.py
+++ people/lemmo2009/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Paul',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/40749798@N07_r.jpg',
   'url': 'https://www.flickr.com/photos/lemmo2009/',
-  'markers': 5683,
-  'photos': 6282
+  'markers': 5685,
+  'photos': 6284
 }
