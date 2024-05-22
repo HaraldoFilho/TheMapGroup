341a901e6ac31d16bece8791d41011da8d37e68d
The Map Group <admin@the-map-group.pictures>
2024-05-22 14:18:35 -0300

    [auto] Updated map for member 'benbobjr'

--- people/benbobjr/user.py
+++ people/benbobjr/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Ben Abel',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/52559257@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/benbobjr/',
-  'markers': 37990,
-  'photos': 49938
+  'markers': 38020,
+  'photos': 50031
 }
