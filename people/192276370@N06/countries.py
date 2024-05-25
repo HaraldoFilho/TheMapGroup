b4f5765df75980e3b9a04c20e6ca33faefbffb96
The Map Group <admin@the-map-group.top>
2024-05-25 04:14:01 -0300

    [auto] Updated map for member 'Patrice Dardoize'

--- people/192276370@N06/countries.py
+++ people/192276370@N06/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'FR': ['France', 595, 748]
+  'FR': ['France', 596, 749]
 }
--- people/192276370@N06/user.py
+++ people/192276370@N06/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Patrice Dardoize',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/192276370@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/192276370@N06/',
-  'markers': 595,
-  'photos': 748
+  'markers': 596,
+  'photos': 749
 }
