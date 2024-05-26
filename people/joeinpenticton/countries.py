f5610960da8ecff1c3f5a4833799b06ec818c49e
The Map Group <admin@the-map-group.top>
2024-05-26 14:49:15 -0300

    [auto] Updated map for member 'Thanks for 9.2 Million Views'

--- people/joeinpenticton/countries.py
+++ people/joeinpenticton/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'ES': ['Spain', 7, 7],
+  'US': ['United States', 125, 126],
   'FR': ['France', 1, 1],
-  'PT': ['Portugal', 35, 36],
-  'CU': ['Cuba', 1, 1],
-  'DE': ['Germany', 2, 2],
   'CA': ['Canada', 494, 501],
-  'US': ['United States', 124, 125]
+  'DE': ['Germany', 2, 2],
+  'ES': ['Spain', 7, 7],
+  'CU': ['Cuba', 1, 1],
+  'PT': ['Portugal', 35, 36]
 }
