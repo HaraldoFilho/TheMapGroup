e45c19c5094379a73cf27ddb797717d86bfe5a76
The Map Group <admin@the-map-group.top>
2024-05-29 03:41:58 -0300

    [auto] Updated map for member 'I saw_that'

---
+++ people/saw_that/countries.py
@@ -0,0 +1,20 @@
+countries_dict = {
+  'NZ': ['New Zealand', 1, 1],
+  'IT': ['Italy', 35, 43],
+  'GB': ['United Kingdom', 18, 22],
+  'CN': ['South China Sea Islands (disputed)', 1, 1],
+  'FR': ['France', 9, 12],
+  'ID': ['Indonesia', 1, 1],
+  'NA': ['Namibia', 1, 1],
+  'MN': ['Mongolia', 1, 2],
+  'CA': ['Canada', 870, 1707],
+  'DE': ['Germany', 2, 3],
+  'LY': ['Libya', 1, 1],
+  'TW': ['Taiwan', 1, 1],
+  'CD': ['Democratic Republic of the Congo', 1, 1],
+  'US': ['United States', 85, 150],
+  'QA': ['Qatar', 1, 1],
+  'CU': ['Cuba', 86, 166],
+  'ET': ['Ethiopia', 1, 1],
+  'AU': ['Australia', 1, 1]
+}
