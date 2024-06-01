5605375255595f684bc02ffb2d736402fac83c04
The Map Group <admin@the-map-group.top>
2024-06-01 03:16:52 -0300

    [auto] Updated map for member 'Robert C. Abraham'

--- people/79080/countries.py
+++ people/79080/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
-  'LC': ['Saint Lucia', 1, 1],
-  'MY': ['Malaysia', 2, 2],
-  'MX': ['Mexico', 3, 3],
+  'JP': ['Japan', 1, 1],
   'VG': ['British Virgin Islands', 1, 1],
+  'CA': ['Canada', 138, 153],
   'TH': ['Thailand', 9, 9],
+  'AG': ['Antigua and Barbuda', 1, 1],
+  'MX': ['Mexico', 3, 3],
+  'GB': ['United Kingdom', 2, 2],
+  'US': ['United States', 9, 9],
+  'LC': ['Saint Lucia', 1, 1],
+  'SG': ['Singapore', 11, 11],
   'CN': ['South China Sea Islands (disputed)', 2, 2],
   'JM': ['Jamaica', 4, 4],
-  'AG': ['Antigua and Barbuda', 1, 1],
-  'GB': ['United Kingdom', 2, 2],
-  'SG': ['Singapore', 10, 10],
-  'US': ['United States', 9, 9],
-  'CA': ['Canada', 138, 153],
-  'JP': ['Japan', 1, 1]
+  'MY': ['Malaysia', 2, 2]
 }
--- people/79080/user.py
+++ people/79080/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Robert C. Abraham',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/31693521@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/79080/',
-  'markers': 183,
-  'photos': 198
+  'markers': 184,
+  'photos': 199
 }
