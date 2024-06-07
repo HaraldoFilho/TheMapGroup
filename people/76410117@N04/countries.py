8a628d756f919739739b56885e3ca1d7aa60bbeb
The Map Group <admin@the-map-group.top>
2024-06-07 09:01:14 -0300

    [auto] Updated map for member 'yves62160'

--- people/76410117@N04/countries.py
+++ people/76410117@N04/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'FR': ['France', 1482, 2681],
-  'PT': ['Portugal', 9, 29],
   'GR': ['Greece', 36, 40],
+  'PT': ['Portugal', 9, 29],
+  'BE': ['Belgium', 84, 171],
   'AT': ['Austria', 17, 24],
   'LU': ['Luxembourg', 4, 53],
-  'BE': ['Belgium', 78, 164]
+  'FR': ['France', 1482, 2681]
 }
--- people/76410117@N04/user.py
+++ people/76410117@N04/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'YVES NEVEJANS',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/76410117@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/76410117@N04/',
-  'markers': 1626,
-  'photos': 2991
+  'markers': 1632,
+  'photos': 2998
 }
