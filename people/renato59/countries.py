bb9af60270774f09ba5beea97a3d5061193c4d01
The Map Group <admin@the-map-group.top>
2024-06-20 03:30:10 -0300

    [auto] Updated map for member 'ReNato '59'

--- people/renato59/countries.py
+++ people/renato59/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'ES': ['Spain', 416, 1483],
-  'AD': ['Andorra', 12, 21],
   'FR': ['France', 4, 6],
   'RU': ['Russia', 1, 1],
-  'IT': ['Italy', 13, 13]
+  'AD': ['Andorra', 12, 21],
+  'IT': ['Italy', 13, 13],
+  'ES': ['Spain', 416, 1484]
 }
