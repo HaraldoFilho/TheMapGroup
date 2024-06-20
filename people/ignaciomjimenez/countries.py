0a91d09e944a9cd2d05984ab46f21deb8d2666a6
The Map Group <admin@the-map-group.top>
2024-06-20 03:16:36 -0300

    [auto] Updated map for member 'Ignacio M. Jiménez'

--- people/ignaciomjimenez/countries.py
+++ people/ignaciomjimenez/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
   'FR': ['France', 10, 12],
-  'AT': ['Austria', 1, 1],
   'GB': ['United Kingdom', 25, 25],
   'BE': ['Belgium', 7, 7],
-  'ES': ['Spain', 1044, 1392],
+  'AT': ['Austria', 1, 1],
+  'ES': ['Spain', 1045, 1393],
   'PT': ['Portugal', 16, 16]
 }
