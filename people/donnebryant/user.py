fdcd1967be6d612c81672e19a2d08a44db537c85
The Map Group <admin@the-map-group.top>
2024-06-28 04:00:06 -0300

    Removed member 'donnebryant'

--- people/donnebryant/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '197365363@N08',
-  'alias': 'donnebryant',
-  'name': 'donnebryant',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/197365363@N08_r.jpg',
-  'url': 'https://www.flickr.com/photos/donnebryant/',
-  'markers': 173,
-  'photos': 3206
-}
