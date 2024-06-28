da13a2bf1062f5e42281ba0d44ba526d7e9a3510
The Map Group <admin@the-map-group.top>
2024-06-28 04:00:07 -0300

    Removed member 'time-to-look'

--- people/time-to-look/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '90955804@N04',
-  'alias': 'time-to-look',
-  'name': 'Ted McGrath',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/90955804@N04_r.jpg',
-  'url': 'https://www.flickr.com/photos/time-to-look/',
-  'markers': 13181,
-  'photos': 15377
-}
