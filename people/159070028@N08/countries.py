32719f853ef3cef5e5736120632347bd147dcf21
The Map Group <admin@the-map-group.top>
2024-06-28 04:00:04 -0300

    Removed member '159070028@N08'

--- people/159070028@N08/countries.py
+++
@@ -1,5 +0,0 @@
-countries_dict = {
-  'CN': ['South China Sea Islands (disputed)', 3, 3],
-  'US': ['United States', 99, 113],
-  'SG': ['Singapore', 19, 19]
-}
--- people/159070028@N08/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '159070028@N08',
-  'alias': '159070028@N08',
-  'name': 'iLOVEnature Photography Inspir',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/159070028@N08_r.jpg',
-  'url': 'https://www.flickr.com/photos/159070028@N08/',
-  'markers': 121,
-  'photos': 135
-}
