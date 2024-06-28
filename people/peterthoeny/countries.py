29eed65c924cb962fae4af3dc6e305bf976699a8
The Map Group <admin@the-map-group.top>
2024-06-28 03:45:56 -0300

    [auto] Updated map for member 'PeterThoeny'

--- people/peterthoeny/countries.py
+++ people/peterthoeny/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'CH': ['Switzerland', 225, 385],
-  'UA': ['Ukraine', 1, 1],
   'CA': ['Canada', 2, 2],
-  'AT': ['Austria', 6, 9],
+  'EE': ['Estonia', 1, 1],
   'DE': ['Germany', 2, 3],
   'IT': ['Italy', 34, 48],
-  'GB': ['United Kingdom', 17, 34],
+  'US': ['United States of America', 1634, 3151],
+  'IS': ['Iceland', 66, 97],
   'CN': ['South China Sea Islands (disputed)', 71, 95],
-  'JP': ['Japan', 102, 120],
-  'NO': ['Norway', 1, 1],
-  'LI': ['Liechtenstein', 4, 5],
-  'US': ['United States of America', 1633, 3150],
+  'AT': ['Austria', 6, 9],
+  'GB': ['United Kingdom', 17, 34],
   'GL': ['Greenland', 5, 5],
-  'EE': ['Estonia', 1, 1],
-  'IS': ['Iceland', 66, 97]
+  'JP': ['Japan', 102, 120],
+  'UA': ['Ukraine', 1, 1],
+  'CH': ['Switzerland', 225, 385],
+  'NO': ['Norway', 1, 1],
+  'LI': ['Liechtenstein', 4, 5]
 }
